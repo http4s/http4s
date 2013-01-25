f9414e55b46f2af09d0a2e8469a0a4aefa32b94d
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-25 11:18:37 -0500

    make Request a case class.

--- core/src/main/scala/org/http4s/Request.scala
+++ core/src/main/scala/org/http4s/Request.scala
@@ -1,49 +1,25 @@
 package org.http4s
 
 import java.io.File
-import java.net.{URL, InetAddress}
+import java.net.{URI, URL, InetAddress}
 
-trait Request {
-  def authType: Option[AuthType]
-
+case class Request(
+  val requestMethod: Method = Method.Get,
+  val scriptName: String = "",
+  val pathInfo: String = "",
+  val queryString: String = "",
+  val pathTranslated: Option[File] = None,
+  val protocol: ServerProtocol = HttpVersion.Http_1_1,
+  val headers: RequestHeaders,
+  val urlScheme: UrlScheme = UrlScheme.Http,
+  val serverName: String = InetAddress.getLocalHost.getHostName,
+  val serverPort: Int = 80,
+  val serverSoftware: Option[ServerSoftware] = None,
+  val remote: InetAddress = InetAddress.getLocalHost,
+  val authType: Option[AuthType] = None,
+  val http4sVersion: Http4sVersion = Http4sVersion
+) {
   def contentLength: Option[Long] = headers.contentLength
-
   def contentType: Option[ContentType] = headers.contentType
-
-  def http4sVersion: Http4sVersion = Http4sVersion
-
-  def pathInfo: String = url.getPath
-
-  def pathTranslated: Option[File] = None
-
-  def queryString: String = Option(url.getQuery).getOrElse("")
-
-  def remoteAddr: InetAddress
-
-  def remoteHost: String
-
-  def remoteIdent: Option[String]
-
-  def remoteUser: Option[String]
-
-  def requestMethod: Method
-
-  def scriptName: String = ""
-
-  def serverName: String = url.getHost
-
-  def serverPort: Int = url.getPort
-
-  def serverProtocol: ServerProtocol
-
-  def serverSoftware: ServerSoftware
-
-  def headers: RequestHeaders
-
-  def urlScheme: UrlScheme = url.getProtocol match {
-    case "http" => UrlScheme.Http
-    case "https" => UrlScheme.Https
-  }
-
-  def url: URL
+  lazy val uri: URI = new URI(urlScheme.toString, null, serverName, serverPort, scriptName+pathInfo, queryString, null)
 }
