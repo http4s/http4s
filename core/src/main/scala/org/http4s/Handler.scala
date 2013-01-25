8c2ddd7bba92989a505d681c6f48b677b0b5cc9a
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-25 09:45:37 -0500

    Not transforming the result, so no need to rewrap it in futures.

--- core/src/main/scala/org/http4s/Handler.scala
+++ core/src/main/scala/org/http4s/Handler.scala
@@ -1,12 +1,11 @@
 package org.http4s
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 import play.api.libs.iteratee._
 
 object Handler {
   def apply(result: Result): Handler = Done(result)
 
-  def apply(result: Future[Result])(implicit executor: ExecutionContext): Handler =
-    Iteratee.fold1[Array[Byte], Result](result) { (state, input) => Future { state } }
+  def apply(result: Future[Result]): Handler = Iteratee.fold1[Array[Byte], Result](result) { (_, _) => result }
 }
