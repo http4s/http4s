430f0093261ec24557846d2baa74938f72aa25c8
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-25 01:48:15 -0500

    s/Action/Handler/g for less ambiguity with Play.

--- core/src/main/scala/org/http4s/package.scala
+++ core/src/main/scala/org/http4s/package.scala
@@ -3,6 +3,6 @@ package org
 import play.api.libs.iteratee.Iteratee
 
 package object http4s {
-  type Action = Iteratee[Array[Byte], Response]
-  type Route = PartialFunction[Request, Action]
+  type Handler = Iteratee[Array[Byte], Response]
+  type Route = PartialFunction[Request, Handler]
 }
