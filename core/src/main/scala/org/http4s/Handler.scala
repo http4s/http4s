813a51acdea08b46054500ed1eda29e347dd9d76
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-26 00:31:00 -0500

    Gut feeling: WebSocket is more of a Route than a Response.

--- core/src/main/scala/org/http4s/Handler.scala
+++ core/src/main/scala/org/http4s/Handler.scala
@@ -5,7 +5,7 @@ import scala.concurrent.Future
 import play.api.libs.iteratee._
 
 object Handler {
-  def apply(result: Result): Handler = Done(result)
+  def apply(result: Response): Handler = Done(result)
 
-  def apply(result: Future[Result]): Handler = Iteratee.fold1[Array[Byte], Result](result) { (_, _) => result }
+  def apply(result: Future[Response]): Handler = Iteratee.fold1[Array[Byte], Response](result) { (_, _) => result }
 }
--- core/src/main/scala/org/http4s/Response.scala
+++ core/src/main/scala/org/http4s/Response.scala
@@ -4,14 +4,12 @@ import scala.concurrent.Future
 
 import play.api.libs.iteratee.Enumerator
 
-trait Result
-
-case class HttpResponse(
+case class Response(
   statusLine: StatusLine = StatusLine.Ok,
   headers: ResponseHeaders = ResponseHeaders.Empty,
   entityBody: Enumerator[Array[Byte]] = Enumerator.eof,
   trailer: Future[Option[Headers]] = Future.successful(None)
-) extends Result
+)
 
 case class StatusLine(code: Int, reason: String)
 object StatusLine {
@@ -23,5 +21,3 @@ trait ResponseHeaders extends Headers
 object ResponseHeaders {
   val Empty = ???
 }
-
-trait WebSocket extends Result
