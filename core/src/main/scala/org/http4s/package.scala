8cdb5c8436c8228402f3d2dc26a41446a43bc934
Ross A. Baker <baker@alumni.indiana.edu>
2013-01-26 03:18:48 -0500

    Response rewriter and response transformer.

--- core/src/main/scala/org/http4s/package.scala
+++ core/src/main/scala/org/http4s/package.scala
@@ -11,5 +11,13 @@ package object http4s {
 
   type RequestRewriter = PartialFunction[Request, Request]
 
+  def rewriteRequest(f: RequestRewriter): Middleware = {
+    route: Route => f.orElse({ case req: Request => req }: RequestRewriter).andThen(route)
+  }
+
   type ResponseTransformer = PartialFunction[Response, Response]
+
+  def transformResponse(f: ResponseTransformer): Middleware = {
+    route: Route => route andThen { handler => handler.map(f) }
+  }
 }
