0a7634175bd068488b62d970fc6a73fa0f7cd30f
Ivan Porto Carrero <ivan@flanders.co.nz>
2013-02-11 16:33:53 +0100

    Update grizzly/src/main/scala/org/http4s/grizzly/OutputIteratee.scala

--- grizzly/src/main/scala/org/http4s/grizzly/OutputIteratee.scala
+++ grizzly/src/main/scala/org/http4s/grizzly/OutputIteratee.scala
@@ -6,14 +6,8 @@ import org.glassfish.grizzly.WriteHandler
 import concurrent.{ExecutionContext, Future, Promise}
 import org.glassfish.grizzly.http.server.io.NIOOutputStream
 
-/**
- * @author Bryce Anderson
- * Created on 2/11/13 at 8:44 AM
- */
-class OutputIteratee(os: NIOOutputStream, chunkSize: Int)(implicit executionContext: ExecutionContext) extends Iteratee[Chunk,Unit] {
-
-  // Keep a persistent listener. No need to make more objects than we have too
-   private[this] val writeWatcher = new WriteHandler {
+object OutputIteratee {
+  private class PromisedWriteHandler(os: NIOOutputStream, chunkSize: Int) extends WriteHandler {
     var promise: Promise[Unit] = _
 
     // Makes a new promise and registers the callback to complete it
@@ -30,6 +24,16 @@ class OutputIteratee(os: NIOOutputStream, chunkSize: Int)(implicit executionCont
 
     def onWritePossible() = promise.success(Unit)
   }
+}
+/**
+ * @author Bryce Anderson
+ * Created on 2/11/13 at 8:44 AM
+ */
+class OutputIteratee(os: NIOOutputStream, chunkSize: Int)(implicit executionContext: ExecutionContext) extends Iteratee[Chunk,Unit] {
+
+  import OutputIteratee._
+  // Keep a persistent listener. No need to make more objects than we have too
+   private[this] val writeWatcher = new PromisedWriteHandler(os, chunkSize)
 
   // Complains about reflective if I try to use an anonymous class
 
