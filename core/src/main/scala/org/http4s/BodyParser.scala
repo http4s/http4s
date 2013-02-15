e8702ca039606f33b5ddbb304008f73aed54ddb9
Ross A. Baker <baker@alumni.indiana.edu>
2013-02-15 00:52:47 -0500

    Parameterize consumer so the whole raw body doesn't necessarily get buffered.

--- core/src/main/scala/org/http4s/BodyParser.scala
+++ core/src/main/scala/org/http4s/BodyParser.scala
@@ -8,13 +8,13 @@ object BodyParser {
   val DefaultMaxSize = 2 * 1024 * 1024
 
   def text(request: RequestPrelude, limit: Int = DefaultMaxSize)(f: String => Responder): Iteratee[HttpChunk, Responder] =
-    tooLargeOrHandleRaw(limit) { raw => f(new String(raw, request.charset)) }
+    consumeUpTo(RawConsumer, limit) { raw => f(new String(raw, request.charset)) }
 
   private val RawConsumer: Iteratee[Raw, Raw] = Iteratee.consume[Raw]()
 
-  def tooLargeOrHandleRaw(limit: Int)(f: Raw => Responder): Iteratee[HttpChunk, Responder] =
+  def consumeUpTo[A](consumer: Iteratee[Raw, A], limit: Int)(f: A => Responder): Iteratee[HttpChunk, Responder] =
     Enumeratee.map[HttpChunk](_.bytes) &>> (for {
-      raw <- Traversable.takeUpTo[Raw](limit) &>> RawConsumer
+      raw <- Traversable.takeUpTo[Raw](limit) &>> consumer
       tooLargeOrRaw <- Iteratee.eofOrElse(StatusLine.RequestEntityTooLarge())(raw)
     } yield (tooLargeOrRaw.right.map(f).merge))
 }
