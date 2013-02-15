a6417775b5842a0c33b2ebfd19cb9d4a62b0e854
Bryce Anderson <bryce.anderson22@gmail.com>
2013-02-15 09:14:09 -0500

    Trying to merge.

--- core/src/main/scala/org/http4s/BodyParser.scala
+++ core/src/main/scala/org/http4s/BodyParser.scala
@@ -1,19 +1,29 @@
 package org.http4s
 
 import play.api.libs.iteratee._
+import java.io.{File, PrintStream, FileInputStream, FileOutputStream}
 
 object BodyParser {
   // TODO make configurable
   val DefaultMaxSize = 2 * 1024 * 1024
+  private val RawConsumer: Iteratee[Raw, Raw] = Iteratee.consume[Raw]()
 
   def text(request: RequestPrelude, limit: Int = DefaultMaxSize)(f: String => Responder): Iteratee[HttpChunk, Responder] =
     tooLargeOrHandleRaw(limit) { raw => f(new String(raw, request.charset)) }
-
-  private val RawConsumer: Iteratee[Raw, Raw] = Iteratee.consume[Raw]()
 
   def tooLargeOrHandleRaw(limit: Int)(f: Raw => Responder): Iteratee[HttpChunk, Responder] =
     for {
       raw <- Enumeratee.map[HttpChunk](_.bytes) ><> Traversable.takeUpTo(limit) &>> RawConsumer
       tooLargeOrRaw <- Iteratee.eofOrElse(StatusLine.RequestEntityTooLarge())(raw)
     } yield (tooLargeOrRaw.right.map(f).merge)
+
+  def binFile(in: java.io.File)(f: => Responder): Iteratee[HttpChunk,Responder] = {
+    val is = new java.io.FileOutputStream(in)
+    Iteratee.foreach[HttpChunk]{d=>is.write(d.bytes)}.map{_ => is.close(); f }
+  }
+
+  def textFile(req: RequestPrelude, in: java.io.File)(f: => Responder): Iteratee[HttpChunk,Responder] = {
+    val is = new java.io.PrintStream(new FileOutputStream(in))
+    Iteratee.foreach[HttpChunk]{ d => is.print(new String(d.bytes, req.charset))}.map{ _ => is.close(); f }
+  }
 }
--- core/src/test/scala/org/http4s/BodyParserSpec.scala
+++ core/src/test/scala/org/http4s/BodyParserSpec.scala
@@ -1,9 +1,70 @@
 package org.http4s
 
+import org.specs2.mutable.Specification
+import org.specs2.time.NoTimeConversions
+import play.api.libs.iteratee.Enumerator
+import StatusLine._
+
+import scala.concurrent.duration._
+
+import java.io.{FileOutputStream,FileInputStream,File,InputStreamReader}
+import concurrent.Await
+
 /**
  * @author Bryce Anderson
  * Created on 2/14/13 at 8:44 PM
  */
-class BodyParserSpec {
+
+class BodyParserSpec extends Specification with NoTimeConversions {
+  "A File BodyParser" should {
+    val binData: Array[Byte] = "Bytes 10111".getBytes
+
+    def readFile(in: File): Array[Byte] = {
+      val os = new FileInputStream(in)
+      val data = new Array[Byte](in.length.asInstanceOf[Int])
+      os.read(data)
+      data
+    }
+
+    def readTextFile(in: File): String = {
+      val os = new InputStreamReader(new FileInputStream(in))
+      val data = new Array[Char](in.length.asInstanceOf[Int])
+      os.read(data,0,in.length.asInstanceOf[Int])
+      data.foldLeft("")(_ + _)
+    }
+
+    def mocServe(req: RequestPrelude, route: Route) = {
+      val server = new MockServer(route)
+      Await.result(server(req,Enumerator(binData)), 2 seconds)
+    }
+
+    "Write a text file from raw" in {
+      val tmpFile = File.createTempFile("foo","bar")
+      val response = mocServe(RequestPrelude(), {
+        case req =>
+          BodyParser.textFile(req, tmpFile){
+            Ok("Hello")
+          }
+      })
+
+      readTextFile(tmpFile) must_== new String(binData)
+      response.statusLine must_== StatusLine.Ok
+      response.body must_== "Hello".getBytes
+    }
+
+    "Write a binary file from raw" in {
+      val tmpFile = File.createTempFile("foo","bar")
+      val response = mocServe(RequestPrelude(), {
+        case req =>
+          BodyParser.binFile(tmpFile)(Ok("Hello"))
+      })
+
+      response.statusLine must_== StatusLine.Ok
+      response.body must_== "Hello".getBytes
+      readFile(tmpFile) must_== binData
+    }
+
+
+  }
 
 }
