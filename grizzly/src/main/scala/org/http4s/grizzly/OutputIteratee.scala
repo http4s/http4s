81bee690152642d73da623a65014c8b04aa83273
Bryce Anderson <bryce.anderson22@gmail.com>
2013-08-31 09:29:04 -0400

    Fixed streaming for Grizzly Also added a touch of exception management.

--- grizzly/src/main/scala/org/http4s/grizzly/OutputIteratee.scala
+++ grizzly/src/main/scala/org/http4s/grizzly/OutputIteratee.scala
@@ -1,11 +1,12 @@
 package org.http4s.grizzly
 
-import play.api.libs.iteratee.{Step, Done, Input, Iteratee}
+import play.api.libs.iteratee._
 import org.http4s._
-import org.glassfish.grizzly.WriteHandler
-import concurrent.{ExecutionContext, Future, Promise}
+import concurrent.{ExecutionContext, Future}
 import org.glassfish.grizzly.http.server.io.NIOOutputStream
 import com.typesafe.scalalogging.slf4j.Logging
+import java.io.IOException
+import org.http4s.TrailerChunk
 
 /**
  * @author Bryce Anderson
@@ -15,35 +16,24 @@ class OutputIteratee(os: NIOOutputStream, isChunked: Boolean)(implicit execution
   with Logging
 {
 
-  private[this] var osFuture: Future[Unit] = Future.successful()
-
-//  private[this] def writeBytes(bytes: Array[Byte]): Unit = {
-//    val promise: Promise[Unit] = Promise()
-//
-//    val asyncWriter = new  WriteHandler {
-//      override def onError(t: Throwable) {
-//        promise.failure(t)
-//        sys.error(s"Error on write listener: ${t.getStackTraceString}")
-//      }
-//      override def onWritePossible() = promise.success{os.write(bytes); if(isChunked) os.flush() }
-//    }
-//
-//    osFuture = osFuture.flatMap{ _ => os.notifyCanWrite(asyncWriter, bytes.length); promise.future }
-//  }
-
-  // synchronized so that enumerators that work in different threads cant totally mess it up.
   private[this] def push(in: Input[HttpChunk]): Iteratee[HttpChunk,Unit] =  {
     in match {
       case Input.El(chunk: BodyChunk) =>
-        //writeBytes(chunk.toArray)
-        os.write(chunk.toArray)
-        this
+        try {
+          os.write(chunk.toArray)
+          if(isChunked) os.flush()
+          this
+        } catch {
+          case e: IOException =>
+            logger.error("OutputIteratee hit a snag on write attempt.", e)
+            Error(e.getMessage, in)
+        }
 
       case Input.El(chunk: TrailerChunk) =>
         logger.warn("Grizzly backend does not support trailers. Silently dropped.")
-        Done(Input.Empty) // TODO return it as unconsumed?  This applies to all backends.
+        Done(()) // TODO return it as unconsumed?  This applies to all backends.
 
-      case Input.EOF => Iteratee.flatten(osFuture.map(Done(_)))
+      case Input.EOF => Done(())
       case Input.Empty => this
     }
   }
