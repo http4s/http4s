712e5b0f4c733b5cf3978b7b85d9bec30f0103e1
Ross A. Baker <baker@alumni.indiana.edu>
2013-10-30 11:50:28 -0400

    Refactor Method.

--- core/src/main/scala/org/http4s/Method.scala
+++ core/src/main/scala/org/http4s/Method.scala
@@ -3,58 +3,51 @@ package org.http4s
 import scala.collection.concurrent
 import scala.collection.concurrent.TrieMap
 
-import Method._
-
 /**
  * An HTTP method.
  *
  * @param name the name of the method.
- * @param methodType the safety guarantee of the method.  See RFC 2616, Section 9.1.1.
- * @param register true if the method should be registered
  */
-class Method(val name: String, methodType: MethodType = MethodType.NotIdempotent, register: Boolean = false) {
+abstract case class Method private (name: String) {
   override def toString = name
-
-  if (register)
-    Method.registry(name) = this
 
   def unapply(request: RequestPrelude): Option[Path] =
     if (request.requestMethod.name == name) Some(Path(request.pathInfo) ) else None
 
-  final def isSafe: Boolean = methodType == MethodType.Safe
-
-  final def isIdempotent: Boolean = methodType != MethodType.NotIdempotent
+  def isSafe: Boolean
+  def isIdempotent: Boolean
 
 //  def :/(path: String): Path = new :/(this, Path(path))
 //  def /(path: String): Path = new /(this, Path(path))
 }
 
 object Method {
-  sealed trait MethodType
+  def notIdempotent(name: String): Method = new MethodImpl(name, false, false)
+  def idempotent(name: String): Method = new MethodImpl(name, false, true)
+  def safe(name: String): Method = new MethodImpl(name, true, true)
 
-  object MethodType {
-    case object NotIdempotent extends MethodType
-    case object Idempotent extends MethodType
-    case object Safe extends MethodType
-  }
+  private class MethodImpl(name: String, val isSafe: Boolean, val isIdempotent: Boolean) extends Method(name)
 
-  import MethodType._
-  object Options extends Method("OPTIONS", Idempotent   , true)
-  object Get     extends Method("GET",     Safe         , true)
-  object Head    extends Method("HEAD",    Safe         , true)
-  object Post    extends Method("POST",    NotIdempotent, true)
-  object Put     extends Method("PUT",     Idempotent   , true)
-  object Delete  extends Method("DELETE",  Idempotent   , true)
-  object Trace   extends Method("TRACE",   Idempotent   , true)
-  object Connect extends Method("CONNECT", NotIdempotent, true)
-  // http://tools.ietf.org/html/rfc5789
-  object Patch   extends Method("PATCH",   NotIdempotent, true)
+  val Options = register(idempotent("OPTIONS"))
+  val Get = register(safe("GET"))
+  val Head = register(safe("HEAD"))
+  val Post = register(notIdempotent("POST"))
+  val Put = register(idempotent("PUT"))
+  val Delete = register(idempotent("DELETE"))
+  val Trace = register(idempotent("TRACE"))
+  val Connect = register(notIdempotent("CONNECT"))
+  val Patch = register(notIdempotent("PATCH"))
 
   object Any {
     def unapply(request: RequestPrelude): Option[Path] = Some(Path(request.pathInfo))
   }
 
   private val registry: concurrent.Map[String, Method] = TrieMap.empty
+
+  private def register(method: Method): method.type = {
+    registry.update(method.name, method)
+    method
+  }
 
   /**
    * Returns a set of all registered methods.
@@ -77,5 +70,5 @@ object Method {
    * @param name the name, case insensitive
    * @return the method, if registered; otherwise, an extension method
    */
-  def apply(name: String): Method = get(name).getOrElse(new Method(name, NotIdempotent))
+  def apply(name: String): Method = get(name).getOrElse(notIdempotent(name))
 }
