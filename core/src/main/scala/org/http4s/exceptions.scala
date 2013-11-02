8c4b0018b2761e812fb07c3ea747e5639d59988d
Ross A. Baker <baker@alumni.indiana.edu>
2013-11-02 01:34:08 -0400

    Remove unused exceptions.

--- core/src/main/scala/org/http4s/exceptions.scala
+++
@@ -1,4 +0,0 @@
-package org.http4s
-
-trait Http4sThrowable extends Throwable
-abstract class Http4sException(message: String, cause: Exception = null) extends RuntimeException(message, cause) with Http4sThrowable
