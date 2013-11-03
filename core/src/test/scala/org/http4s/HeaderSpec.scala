e49b735ce8c3cc67207d29ed907aca861c971ecd
Bryce Anderson <bryce.anderson22@gmail.com>
2013-11-03 17:17:28 -0500

    More Header tests.

--- core/src/test/scala/org/http4s/HeaderSpec.scala
+++ core/src/test/scala/org/http4s/HeaderSpec.scala
@@ -21,6 +21,38 @@ class HeaderSpec extends WordSpec with Matchers {
 
       h1 shouldNot equal (h2)
     }
+
+    "equal same raw headers" in {
+      val h1 = Headers.`Content-Length`(4)
+      val h2 = Headers.RawHeader("Content-Length", "4")
+
+      h1 should equal (h2)
+      h2 should equal (h1)
+    }
+
+    "not equal same raw headers" in {
+      val h1 = Headers.`Content-Length`(4)
+      val h2 = Headers.RawHeader("Content-Length", "5")
+
+      h1 shouldNot equal (h2)
+      h2 shouldNot equal (h1)
+    }
+
+    "equate raw to same raw headers" in {
+      val h1 = Headers.RawHeader("Content-Length", "4")
+      val h2 = Headers.RawHeader("Content-Length", "4")
+
+      h1 should equal (h2)
+      h2 should equal (h1)
+    }
+
+    "not equate raw to same raw headers" in {
+      val h1 = Headers.RawHeader("Content-Length", "4")
+      val h2 = Headers.RawHeader("Content-Length", "5")
+
+      h1 shouldNot equal (h2)
+      h2 shouldNot equal (h1)
+    }
   }
 
 }
