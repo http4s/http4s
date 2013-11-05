3600ba33daa56f349fd300d0be6d893fad84fb61
Bryce Anderson <bryce.anderson22@gmail.com>
2013-11-05 17:32:34 -0500

    Changed push support to a pimp and middleware pair

--- core/src/main/scala/org/http4s/PushSupport.scala
+++ core/src/main/scala/org/http4s/PushSupport.scala
@@ -2,49 +2,67 @@ package org.http4s
 
 import scala.concurrent.Future
 import play.api.libs.iteratee.Iteratee
-import org.http4s
 
 /**
  * @author Bryce Anderson
  *         Created on 11/5/13
  */
-trait PushSupport { self: Responder =>
-
-  import scala.concurrent.ExecutionContext.Implicits.global
-
-  type PushResponder = Responder with PushSupport
-
-  private def pushCascade(url: String, content: Future[Responder]): Future[Vector[(String, Responder)]] = {
-    content.flatMap { r =>
-      r.attributes.get(PushSupport.pushKey)
-        .map(fl => fl.map(f => f:+(url, r)))
-        .getOrElse(Future.successful(Vector((url, r))))
-    }
-  }
-  
-  def push(url: String, content: Future[Responder], cascade: Boolean): Responder = {
-    val newPushContent: Future[Vector[(String, Responder)]] = {
-      if (cascade) pushCascade(url, content)
-      else content.map( resp => Vector((url, resp)))
-    }
-
-    val newFullList: Future[Vector[(String,Responder)]] = self.attributes.get(PushSupport.pushKey)
-      .map(fl => fl.flatMap(l => newPushContent.map(l ++ _)))
-      .getOrElse(newPushContent)
-
-    new http4s.Responder(self.prelude, self.body,
-      self.attributes.put(PushSupport.pushKey, newFullList)) with PushSupport
-  }
-
-  def push(url: String, content: Iteratee[Chunk, Responder], cascade: Boolean): Responder =
-    push(url, content.run, cascade)
-
-  def push(url: String, content: Responder, cascade: Boolean): Responder =
-    push(url, Future.successful(content), cascade)
-
-}
-
 
 object PushSupport {
-  private[http4s] val pushKey = AttributeKey[Future[Vector[(String, Responder)]]]("http4sPush")
+
+  import concurrent.ExecutionContext.Implicits.global
+
+  // A pimping class
+  implicit class PushSupportResponder(responder: Responder) extends AnyRef {
+    def push(url: String, cascade: Boolean = true): Responder = {
+      val newPushResouces = responder.attributes.get(pushLocationKey)
+          .map(_.map(_ :+ PushLocation(url, cascade)))
+          .getOrElse(Future.successful(Vector(PushLocation(url,cascade))))
+
+      Responder(responder.prelude, responder.body,
+        responder.attributes.put(PushSupport.pushLocationKey, newPushResouces))
+    }
+  }
+
+  private def locToRequest(push: PushLocation): RequestPrelude = ???
+
+  private def collectResponder(r: Future[Vector[PushLocation]], route: Route): Future[Vector[PushResponder]] = r.flatMap(
+    _.foldLeft(Future.successful(Vector.empty[PushResponder])){ (f, v) =>
+      if (v.cascasde) f.flatMap { vresp => // Need to gather the sub resources
+        route(locToRequest(v)).run
+          .flatMap { responder =>             // Inside the future result of this pushed resource
+            responder.attributes.get(pushLocationKey)
+            .map { fresp =>                   // More resources. Need to collect them and add all this up
+               collectResponder(fresp, route).map(vresp ++ _ :+ PushResponder(v.location, responder))
+            }.getOrElse(Future.successful(vresp:+PushResponder(v.location, responder)))
+          }
+      } else {
+        route(locToRequest(v))
+          .run
+          .flatMap{ resp => f.map(_ :+ PushResponder(v.location, resp))}
+      }
+    }
+  )
+
+  /** Transform the route such that requests will gather pushed resources
+   *
+   * @param route Route to transform
+   * @return      Transformed route
+   */
+  def apply(route: Route): Route = {
+    def gather(i: Iteratee[Chunk, Responder]): Iteratee[Chunk, Responder] = i map { resp =>
+      resp.attributes.get(pushLocationKey).map { fresource =>
+        val collected: Future[Vector[PushResponder]] = collectResponder(fresource, route)
+        Responder(resp.prelude, resp.body, resp.attributes.put(pushRespondersKey, collected))
+      }.getOrElse(resp)
+    }
+
+    route andThen gather   // The backend will need to get the values in pushResponder and utilize them
+  }
+
+  private [PushSupport] case class PushLocation(location: String, cascasde: Boolean)
+  private [PushSupport] case class PushResponder(location: String, resp: Responder)
+
+  private[PushSupport] val pushLocationKey = AttributeKey[Future[Vector[PushLocation]]]("http4sPush")
+  private[http4s] val pushRespondersKey = AttributeKey[Future[Vector[PushResponder]]]("http4sPushResponders")
 }
