a3d0b662434db28fa47fc39a5db89a3b8b03a317
Ross A. Baker <baker@alumni.indiana.edu>
2013-11-17 21:37:13 -0500

    More ServerProtocol tweaks.

--- core/src/main/scala/org/http4s/ServerProtocol.scala
+++ core/src/main/scala/org/http4s/ServerProtocol.scala
@@ -2,13 +2,9 @@ package org.http4s
 
 import org.http4s.util.{CaseInsensitiveString, Registry}
 
-import ServerProtocol.Version
-
 sealed abstract class ServerProtocol(protocol: CaseInsensitiveString, version: Option[ServerProtocol.Version] = None) {
-  lazy val value: CaseInsensitiveString = version.fold(protocol) { case Version(major, minor) =>
-    s"$protocol/${major}.${minor}".ci
-  }
-  override val toString = value.toString
+  def value: CaseInsensitiveString
+  override def toString = value.toString
 }
 
 /**
@@ -32,15 +28,24 @@ object ServerProtocol extends Registry[CaseInsensitiveString, ServerProtocol] {
   def register[A <: ServerProtocol](serverProtocol: A): serverProtocol.type =
     register(serverProtocol.value, serverProtocol)
 
-  sealed case class HttpVersion(major: Int, minor: Int) extends ServerProtocol("HTTP".ci, Some(Version(major, minor)))
+  sealed case class HttpVersion(major: Int, minor: Int) extends ServerProtocol("HTTP".ci, Some(Version(major, minor))) {
+    val value: CaseInsensitiveString = s"HTTP/${major}.${minor}".ci
+  }
   val `HTTP/1.1`: HttpVersion = register(new HttpVersion(1, 1))
   val `HTTP/1.0`: HttpVersion = register(new HttpVersion(1, 0))
 
-  case object INCLUDED extends ServerProtocol("INCLUDED".ci)
+  case object INCLUDED extends ServerProtocol("INCLUDED".ci) {
+    val value = "INCLUDED".ci
+  }
   register(INCLUDED)
 
   sealed case class ExtensionVersion(protocol: CaseInsensitiveString, version: Option[Version] = None)
     extends ServerProtocol(protocol, version)
+  {
+    def value: CaseInsensitiveString = version.fold(protocol) {
+      case Version(major, minor) => s"${protocol}/${major}.${minor}".ci
+    }
+  }
 
   // TODO create a proper parser
   private val ProtocolRegex = """(.*?)(?:/(\d+).(\d+))""".r
