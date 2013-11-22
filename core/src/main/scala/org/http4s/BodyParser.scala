2ea67405d9afbcefb22c0b016a9c8ec027ba3efe
Bryce Anderson <bryce.anderson22@gmail.com>
2013-11-22 10:03:25 -0500

    Make things better, imo.

--- core/src/main/scala/org/http4s/BodyParser.scala
+++ core/src/main/scala/org/http4s/BodyParser.scala
@@ -9,22 +9,44 @@ import scalaz.{\/-, -\/, \/}
 import scalaz.stream.process1
 import scalaz.syntax.id._
 import scalaz.concurrent.Task
-import scalaz.stream.Process
 import scalaz.stream.Process._
-import java.nio.BufferOverflowException
+import scala.collection.mutable.ArrayBuffer
+
+
+/** ParserFailHandler generates a response from a failed parser.
+  * Type parametrized for implicit convenience.
+ *
+ * @tparam A The result type of the parser
+ */
+trait ParserFailHandler[-A] { parent =>
+  def handle(t: Throwable): Task[Response]
+
+  // Just gives the response for this parser, but available for overriding if the case dictates
+  private[http4s] def mutate[B](other: ParserFailHandler[B]): ParserFailHandler[B] = new ParserFailHandler[B] {
+    def handle(t: Throwable): Task[Response] = parent.handle(t)
+  }
+}
+
+object ParserFailHandler {
+  implicit object generalFailure extends ParserFailHandler[Any] {
+    def handle(t: Throwable): Task[Response] = Status.InternalServerError()
+  }
+}
 
 class BodyParser[A] private (p: Process1[Chunk, A], req: Request) { parent =>
-  def apply(f: A => Task[Response], fail: (Throwable) => Task[Response] = _ => Status.InternalServerError()): Task[Response] = {
+  def apply(f: A => Task[Response])(implicit fail: ParserFailHandler[A]): Task[Response] = {
     req.body.pipe(p).runLast.attempt.flatMap {
       case \/-(Some(a)) => f(a)
-      case -\/(t)       => fail(t)
+      case -\/(t)       => fail.handle(t)
       case _            => Status.InternalServerError()
     }
   }
 
-  def map[B](m: A => B): BodyParser[B] = new BodyParser(p.map(m), req) {
-    override def apply(f: (B) => Task[Response], fail: (Throwable) => Task[Response] = _ => Status.InternalServerError()) = {
-      parent(m.andThen(f), fail)
+  def andThen[B](p2: Process1[A, B]) = new BodyParser[B](p.pipe(p2), req)
+
+  def map[B](m: A => B)(implicit first: ParserFailHandler[A]): BodyParser[B] = new BodyParser(p.map(m), req) {
+    override def apply(f: (B) => Task[Response])(implicit fail: ParserFailHandler[B]) = {
+      parent.apply(m.andThen(f))(fail.mutate(first))
     }
   }
 }
@@ -54,31 +76,41 @@ object BodyParser {
   }
 
 
-/*
+
   /**
    * Handles a request body as XML.
    *
    * TODO Not an ideal implementation.  Would be much better with an asynchronous XML parser, such as Aalto.
-   * TODO how to pass the EC correctly?
    *
    * @param charset the charset of the input
    * @param limit the maximum size before an EntityTooLarge error is returned
    * @param parser the SAX parser to use to parse the XML
    * @return a request handler
    */
-  def xml(charset: CharacterSet,
+  def xml(req: Request,
+          charset: CharacterSet = CharacterSet.`UTF-8`,
           limit: Int = DefaultMaxEntitySize,
-          parser: SAXParser = XML.parser,
-          onSaxException: SAXException => Response = { saxEx => /*saxEx.printStackTrace();*/ Status.BadRequest() })
-  : BodyParser[Elem] =
-    consumeUpTo(BodyChunkConsumer, limit).map { bytes =>
-      val in = bytes.iterator.asInputStream
-      val source = new InputSource(in)
+          parser: SAXParser = XML.parser) //,
+          //onSaxException: SAXException => Response = { saxEx => /*saxEx.printStackTrace();*/ Status.BadRequest() })
+  : BodyParser[Elem] = {
+    val p = comsumeUpTo(limit).flatMap{ chunk =>
+      val source = new InputSource(chunk.asInputStream)
       source.setEncoding(charset.value)
-      Try(XML.loadXML(source, parser)).map(Right(_)).recover {
-        case e: SAXException => Left(onSaxException(e))
-      }.get
-    }(tec).joinRight
+      try emit(XML.loadXML(source, parser))
+      catch { case e: Throwable => Halt(e)
+      }
+    }
+    new BodyParser(p, req)
+  }
+
+  implicit object XMLParserErrorHandler extends ParserFailHandler[Elem] {
+    def handle(t: Throwable): Task[Response] = t match {
+      case e: SAXException => Status.BadRequest()
+      case e => Status.InternalServerError()
+    }
+  }
+
+  /*
 
   def ignoreBody: BodyParser[Unit] = BodyParser(whileBodyChunk &>> Iteratee.ignore[BodyChunk].map(Right(_))(oec))
 
@@ -100,6 +132,14 @@ object BodyParser {
       case c =>  Emit(c::Nil, await(Get[Chunk])(go(taken, _)))
     }
     await(Get[Chunk])(go(0,_))
+  }
+
+  def comsumeUpTo(n: Int): Process1[Chunk, BodyChunk] = {
+    val p = process1.fold[Chunk, BodyChunk](BodyChunk())((c1, c2) => c2 match {
+      case c2: BodyChunk => c1 ++ (c2)
+      case _ => c1
+    })
+    takeBytes(n) |> p
   }
 
 /*
