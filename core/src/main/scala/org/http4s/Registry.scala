eb348c0e59e9715d6ec4bb7ad40e246d63befdc6
Bryce Anderson <bryce.anderson22@gmail.com>
2013-12-23 22:39:50 -0500

    add snapshot: collection.Map[K,V] method to Registry[K,V]

--- core/src/main/scala/org/http4s/Registry.scala
+++ core/src/main/scala/org/http4s/Registry.scala
@@ -4,12 +4,14 @@ import scala.collection.concurrent
 import scala.collection.concurrent.TrieMap
 
 private[http4s] trait Registry[K, V <: AnyRef] {
-  protected val registry: concurrent.Map[K, V] = TrieMap.empty
+  protected val registry: concurrent.TrieMap[K, V] = TrieMap.empty
 
   protected def register(key: K, obj: V): obj.type = {
     registry.update(key, obj)
     obj
   }
+
+  def snapshot: collection.Map[K, V] = registry.readOnlySnapshot()
 
   protected def lookup(key: K): Option[V] = registry.get(key)
 
