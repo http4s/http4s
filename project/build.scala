79b01548bb6e9f75cef4115e8bc7e408d41fdfb4
Ross A. Baker <ross@rossabaker.com>
2014-04-23 19:34:26 -0400

    OCD dependency sort'n'purge.

--- project/build.scala
+++ project/build.scala
@@ -3,7 +3,7 @@ import sbt._
 object Http4sDependencies {
   lazy val base64              = "net.iharder"               % "base64"                  % "2.3.8"
   lazy val blaze               = "org.http4s"               %% "blaze"                   % "0.2.0-SNAPSHOT"
-  lazy val typesafeConfig      = "com.typesafe"              % "config"                  % "1.0.0"
+  lazy val config              = "com.typesafe"              % "config"                  % "1.0.0"
   lazy val javaxServletApi     = "javax.servlet"             % "javax.servlet-api"       % "3.0.1"
   lazy val jettyServer         = "org.eclipse.jetty"         % "jetty-server"            % "9.1.0.v20131115"
   lazy val jettyServlet        = "org.eclipse.jetty"         % "jetty-servlet"           % jettyServer.revision
@@ -13,8 +13,6 @@ object Http4sDependencies {
   lazy val jspApi              = "javax.servlet.jsp"         % "javax.servlet.jsp-api"   % "2.3.1" // YourKit hack
   lazy val junit               = "junit"                     % "junit"                   % "4.11"
   lazy val logbackClassic      = "ch.qos.logback"            % "logback-classic"         % "1.0.9"
-  lazy val npn_api             = "org.eclipse.jetty.npn"     % "npn-api"                 % npn_version
-  lazy val npn_boot            = "org.mortbay.jetty.npn"     % "npn-boot"                % npn_version
   lazy val parboiled           = "org.parboiled"            %% "parboiled"               % "2.0.0-RC1"
   lazy val rl                  = "org.scalatra.rl"          %% "rl"                      % "0.4.10"
   lazy val scalaloggingSlf4j_2_10 = "com.typesafe"          %% "scalalogging-slf4j"      % "1.1.0"
@@ -22,8 +20,5 @@ object Http4sDependencies {
   lazy val scalameter          = "com.github.axel22"        %% "scalameter"              % "0.5-M2"
   lazy val scalatest           = "org.scalatest"            %% "scalatest"               % "2.1.3"
   lazy val scalazStream        = "org.scalaz.stream"        %% "scalaz-stream"           % "0.4.1"
-
   def scalaReflect(sv: String) = "org.scala-lang"            % "scala-reflect"           % sv
-
-  val npn_version = "8.1.2.v20120308"
 }
