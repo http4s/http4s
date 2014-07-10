2be29078ba835c6424de319ae4d20ee154f346b0
Ross A. Baker <ross@rossabaker.com>
2014-07-09 21:37:35 -0400

    Fix to blaze-0.2.0.

--- project/Http4sBuild.scala
+++ project/Http4sBuild.scala
@@ -34,7 +34,7 @@ object Http4sKeys {
 
 object Http4sDependencies {
   lazy val base64              = "net.iharder"               % "base64"                  % "2.3.8"
-  lazy val blaze               = "org.http4s"               %% "blaze-http"              % "0.2.0-SNAPSHOT"
+  lazy val blaze               = "org.http4s"               %% "blaze-http"              % "0.2.0"
   lazy val config              = "com.typesafe"              % "config"                  % "1.0.0"
   lazy val javaxServletApi     = "javax.servlet"             % "javax.servlet-api"       % "3.0.1"
   lazy val jettyServer         = "org.eclipse.jetty"         % "jetty-server"            % "9.1.4.v20140401"
