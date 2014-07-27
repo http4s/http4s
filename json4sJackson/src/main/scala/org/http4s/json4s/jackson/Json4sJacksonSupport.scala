d9e5ca4a605be88c720fa39adc53f302208e5502
Ross A. Baker <ross@rossabaker.com>
2014-07-27 00:14:56 -0400

    Tidy up json4s support sources.

--- json4sJackson/src/main/scala/org/http4s/json4s/jackson/Json4sJacksonSupport.scala
+++ json4sJackson/src/main/scala/org/http4s/json4s/jackson/Json4sJacksonSupport.scala
@@ -1,5 +1,13 @@
 package org.http4s.json4s.jackson
 
+import org.http4s.json4s.Json4sWritableInstances
+import org.json4s.JsonAST.JValue
+import org.json4s.jackson._
+
 trait Json4sJacksonSupport extends Json4sJacksonWritableInstances
 
 object Json4sJacksonSupport extends Json4sJacksonSupport
+
+trait Json4sJacksonWritableInstances extends Json4sWritableInstances {
+  override implicit protected def jValueToString(jValue: JValue): String = compactJson(renderJValue(jValue))
+}
--- json4sNative/src/main/scala/org/http4s/json4s/native/Json4sNativeSupport.scala
+++ json4sNative/src/main/scala/org/http4s/json4s/native/Json4sNativeSupport.scala
@@ -1,5 +1,13 @@
 package org.http4s.json4s.native
 
+import org.http4s.json4s.Json4sWritableInstances
+import org.json4s.JsonAST.JValue
+import org.json4s.native._
+
 trait Json4sNativeSupport extends Json4sNativeWritableInstances
 
 object Json4sNativeSupport extends Json4sNativeSupport
+
+trait Json4sNativeWritableInstances extends Json4sWritableInstances {
+  override protected def jValueToString(jValue: JValue): String = compactJson(renderJValue(jValue))
+}
