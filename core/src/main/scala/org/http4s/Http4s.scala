9fda88584fb9ba4c77c43688951c891c33c19838
Ross A. Baker <ross@rossabaker.com>
2014-08-04 19:02:14 -0400

    Split up Http4s trait.

--- core/src/main/scala/org/http4s/Http4s.scala
+++ core/src/main/scala/org/http4s/Http4s.scala
@@ -1,14 +1,30 @@
 package org.http4s
 
-trait Http4s extends EntityDecoderInstances
-  with MessageSyntax
+trait Http4s
+  extends Http4sInstances
+  with Http4sFunctions
+  with Http4sSyntax
+
+object Http4s extends Http4s
+
+trait Http4sInstances
+  extends scalaz.std.AllInstances
+  with util.TaskInstances
+  with EntityDecoderInstances
   with WritableInstances
   with StatusInstances
   with CharsetRangeInstances
-  with QValueFunctions
   with QValueInstances
-  with util.CaseInsensitiveStringSyntax
-  with util.TaskInstances
-  with scalaz.std.AllInstances
 
-object Http4s extends Http4s
+object Http4sInstances extends Http4sInstances
+
+trait Http4sFunctions
+  extends QValueFunctions
+
+object Http4sFunctions extends Http4sFunctions
+
+trait Http4sSyntax
+  extends util.CaseInsensitiveStringSyntax
+  with MessageSyntax
+
+object Http4sSyntax extends Http4sSyntax
