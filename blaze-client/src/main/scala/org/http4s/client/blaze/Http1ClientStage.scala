http4s/http4s

commit eb4b89f1a0d3f4f6e1b813c002b2dbed34962078

Fixes issue with blaze client and the request line encoding.
Closes #60. Thanks @diagonal-e for the bug report!

blaze-client/src/main/scala/org/http4s/client/blaze/Http1ClientStage.scala
@@ -96,7 +96,7 @@ class Http1ClientStage(protected val timeout: Duration = 60.seconds)
 
   private def encodeRequestLine(req: Request, writer: Writer): writer.type = {
     val uri = req.uri
-    writer ~ req.method ~ ' ' ~ uri.path ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
+    writer ~ req.method ~ ' ' ~ uri.toString ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
     if (getHttpMinor(req) == 1 && Host.from(req.headers).isEmpty) { // need to add the host header for HTTP/1.1
       uri.host match {
         case Some(host) =>

blaze-client/src/test/scala/org/http4s/client/blaze/Http1ClientStageSpec.scala
@@ -1,0 +1,48 @@
+package org.http4s
+package client.blaze
+
+import java.nio.charset.StandardCharsets
+
+import org.http4s.blaze.SeqTestHead
+import org.http4s.blaze.pipeline.LeafBuilder
+import org.specs2.mutable.Specification
+
+import java.nio.ByteBuffer
+
+import org.specs2.time.NoTimeConversions
+import scodec.bits.ByteVector
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+import scalaz.\/-
+
+// TODO: this needs more tests
+class Http1ClientStageSpec extends Specification with NoTimeConversions {
+
+  def getSubmission(req: Request, resp: String): String = {
+    val tail = new Http1ClientStage()
+    val h = new SeqTestHead(List(ByteBuffer.wrap(resp.getBytes(StandardCharsets.US_ASCII))))
+    LeafBuilder(tail).base(h)
+
+    val result = tail.runRequest(req).run
+    h.stageShutdown()
+    val buff = Await.result(h.result, 30.seconds)
+    new String(ByteVector(buff).toArray, StandardCharsets.US_ASCII)
+  }
+
+  "Http1ClientStage" should {
+    "Submit a request line with a query" in {
+      val resp = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\ndone"
+      val uri = "http://www.foo.com/huh?foo=bar"
+      val \/-(parsed) = Uri.fromString(uri)
+      val req = Request(uri = parsed)
+
+      val response = getSubmission(req, resp).split("\r\n")
+      val statusline = response(0)
+
+      statusline must_== "GET " + uri + " HTTP/1.1"
+    }
+  }
+
+}

blaze-client/src/test/scala/org/http4s/client/blaze/BlazeHttp1ClientSpec.scala
@@ -10,6 +10,7 @@ import org.specs2.time.NoTimeConversions
 
 import scala.concurrent.duration._
 
+// TODO: this should have a more comprehensive test suite
 class BlazeHttp1ClientSpec extends Http4sSpec with NoTimeConversions with After {
 
   "Blaze Simple Http1 Client" should {
@@ -38,7 +39,7 @@ class BlazeHttp1ClientSpec extends Http4sSpec with NoTimeConversions with After 
       val resp = Request(GET, uri("https://github.com/")).on(Status.Ok)(EntityDecoder.text).run
       //      println(resp.copy(body = halt))
 
-      resp.status.code must be_==(200)
+      resp.status.code must_==(200)
     }
 
     "Repeat a simple http request" in {

build.sbt
@@ -13,7 +13,7 @@ lazy val `blaze-core` = project.dependsOn(core)
 
 lazy val `blaze-server` = project.dependsOn(`blaze-core` % "compile;test->test", server)
 
-lazy val `blaze-client` = project.dependsOn(`blaze-core`, client % "compile;test->test")
+lazy val `blaze-client` = project.dependsOn(`blaze-core` % "compile;test->test", client % "compile;test->test")
 
 lazy val servlet = project.dependsOn(server)
 

commit 479d4d9e6123bb34de24bfae6e607f5725e3165d

Fix bug in blaze-client introduced by me in commit eb4b89f1a0d3f4f6e1b813c002b2dbed34962078
Clearly the test coverage is lacking or this would have been caught.

blaze-client/src/main/scala/org/http4s/client/blaze/Http1ClientStage.scala
@@ -96,7 +96,7 @@ class Http1ClientStage(protected val timeout: Duration = 60.seconds)
 
   private def encodeRequestLine(req: Request, writer: Writer): writer.type = {
     val uri = req.uri
-    writer ~ req.method ~ ' ' ~ uri ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
+    writer ~ req.method ~ ' ' ~ uri.copy(scheme = None, authority = None) ~ ' ' ~ req.httpVersion ~ '\r' ~ '\n'
     if (getHttpMinor(req) == 1 && Host.from(req.headers).isEmpty) { // need to add the host header for HTTP/1.1
       uri.host match {
         case Some(host) =>

core/src/main/scala/org/http4s/Uri.scala
@@ -194,18 +194,24 @@ case class Uri(
   override def render[W <: Writer](writer: W): writer.type = this match {
     case Uri(Some(s), Some(a), "/", None, None) =>
       renderSchemeAndAuthority(writer, s, a)
+
     case Uri(Some(s), Some(a), path, params, fragment) =>
       renderSchemeAndAuthority(writer, s, a)
       writer.append(path)
       renderParamsAndFragment(writer, params, fragment)
+
     case Uri(Some(s), None, path, params, fragment) =>
       renderScheme(writer, s)
       writer.append(path)
       renderParamsAndFragment(writer, params, fragment)
+
+    case Uri(None, Some(a), path, params, fragment) =>
+      writer ~ a ~ path
+      renderParamsAndFragment(writer, params, fragment)
+      
     case Uri(None, None, path, params, fragment) =>
       writer.append(path)
       renderParamsAndFragment(writer, params, fragment)
-    case _ => writer
   }
 }
 
@@ -240,14 +246,29 @@ object Uri extends UriFunctions {
   case class Authority(
     userInfo: Option[UserInfo] = None,
     host: Host = RegName("localhost"),
-    port: Option[Int] = None) {
+    port: Option[Int] = None) extends Renderable {
+
+    override def render[W <: Writer](writer: W): writer.type = this match {
+      case Authority(Some(u), h, None)    => writer ~ u ~ '@' ~ h
+      case Authority(Some(u), h, Some(p)) => writer ~ u ~ '@' ~ h ~ ':' ~ p
+      case Authority(None, h, Some(p))    => writer ~ h ~ ':' ~ p
+      case Authority(_, h, _)             => writer ~ h
+      case _                              => writer
+    }
   }
 
-  sealed trait Host {
+  sealed trait Host extends Renderable {
     final def value: String = this match {
       case RegName(h) => h.toString
       case IPv4(a)    => a.toString
       case IPv6(a)    => a.toString
+    }
+
+    override def render[W <: Writer](writer: W): writer.type = this match {
+      case RegName(n) => writer ~ n
+      case IPv4(a)    => writer ~ a
+      case IPv6(a)    => writer ~ '[' ~ a ~ ']'
+      case _          => writer
     }
   }
   case class RegName(host: CaseInsensitiveString) extends Host
@@ -258,28 +279,12 @@ object Uri extends UriFunctions {
   object IPv4 { def apply(address: String) = new IPv4(address.ci) }
   object IPv6 { def apply(address: String) = new IPv6(address.ci) }
 
-  private def renderAuthority(writer: Writer, a: Authority): writer.type = a match {
-    case Authority(Some(u), h, None)    => writer ~ u ~ '@'; renderHost(writer, h)
-    case Authority(Some(u), h, Some(p)) => writer ~ u ~ '@'; renderHost(writer, h) ~ ':' ~ p
-    case Authority(None, h, Some(p))    => renderHost(writer, h) ~ ':' ~ p
-    case Authority(_, h, _)             => renderHost(writer, h)
-    case _                              => writer
-  }
-
-  private def renderHost(writer: Writer, h: Host): writer.type = h match {
-    case RegName(n) => writer ~ n
-    case IPv4(a)    => writer ~ a
-    case IPv6(a)    => writer ~ '[' ~ a ~ ']'
-    case _          => writer
-  }
-
   private def renderScheme(writer: Writer, s: Scheme): writer.type =
     writer ~ s ~ ":"
 
-  private def renderSchemeAndAuthority(writer: Writer, s: Scheme, a: Authority): writer.type = {
-    val w = renderScheme(writer, s) ~ "//"
-    renderAuthority(w, a)
-  }
+  private def renderSchemeAndAuthority(writer: Writer, s: Scheme, a: Authority): writer.type =
+    renderScheme(writer, s) ~ "//" ~ a
+
 
   private def renderParamsAndFragment(writer: Writer, p: Option[Query], f: Option[Fragment]): writer.type = {
     if (p.isDefined) writer ~ '?' ~ p.get

blaze-client/src/test/scala/org/http4s/client/blaze/Http1ClientStageSpec.scala
@@ -32,10 +32,14 @@ class Http1ClientStageSpec extends Specification with NoTimeConversions {
   }
 
   "Http1ClientStage" should {
+
+    // Common throw away response
+    val resp = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\ndone"
+
     "Submit a request line with a query" in {
-      val resp = "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\ndone"
-      val uri = "http://www.foo.com/huh?foo=bar"
-      val \/-(parsed) = Uri.fromString(uri)
+
+      val uri = "/huh?foo=bar"
+      val \/-(parsed) = Uri.fromString("http://www.foo.com" + uri)
       val req = Request(uri = parsed)
 
       val response = getSubmission(req, resp).split("\r\n")