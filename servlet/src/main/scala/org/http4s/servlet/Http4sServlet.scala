c74f8bd3246c3776a297be204ad748f7e1c725fc
Ross A. Baker <ross@rossabaker.com>
2014-10-27 14:06:31 -0400

    Read servlet requests asynchronously. #15

--- servlet/src/main/scala/org/http4s/servlet/Http4sServlet.scala
+++ servlet/src/main/scala/org/http4s/servlet/Http4sServlet.scala
@@ -1,45 +1,58 @@
 package org.http4s
 package servlet
 
+import scodec.bits.ByteVector
 import server._
 
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 import java.net.InetAddress
 
 import scala.collection.JavaConverters._
-import javax.servlet.{ServletConfig, AsyncContext}
-
-import Http4sServlet._
-import util.CaseInsensitiveString._
+import javax.servlet.{ServletContext, ReadListener, ServletConfig, AsyncContext}
 
 import scala.concurrent.duration.Duration
 import scalaz.concurrent.Task
+import scalaz.stream.async.boundedQueue
 import scalaz.stream.io._
 import scalaz.{-\/, \/-}
 import scala.util.control.NonFatal
 import org.parboiled2.ParseError
 import com.typesafe.scalalogging.slf4j.LazyLogging
 
-class Http4sServlet(service: HttpService, asyncTimeout: Duration = Duration.Inf, chunkSize: Int = DefaultChunkSize)
-            extends HttpServlet with LazyLogging {
+class Http4sServlet(service: HttpService,
+                    asyncTimeout: Duration = Duration.Inf,
+                    chunkSize: Int = 4096,
+                    maxChunks: Int = 8)
+            extends HttpServlet with LazyLogging
+{
+  import Http4sServlet._
 
   private val asyncTimeoutMillis = if (asyncTimeout.isFinite) asyncTimeout.toMillis else -1  // -1 == Inf
 
   private[this] var serverSoftware: ServerSoftware = _
+  private[this] var inputStreamReader: BodyReader = _
 
   override def init(config: ServletConfig) {
-    serverSoftware = ServerSoftware(config.getServletContext.getServerInfo)
+    val servletContext = config.getServletContext
+    serverSoftware = ServerSoftware(servletContext.getServerInfo)
+    val servletApiVersion = ServletApiVersion(servletContext)
+    logger.info(s"Detected Servlet API version ${servletApiVersion}")
+
+    inputStreamReader = if (servletApiVersion >= ServletApiVersion(3, 1))
+      asyncInputStreamReader(chunkSize, maxChunks)
+    else
+      syncInputStreamReader(chunkSize)
   }
+
 
   override def service(servletRequest: HttpServletRequest, servletResponse: HttpServletResponse) {
     try {
-      val request = toRequest(servletRequest)
-      request match {
+      val ctx = servletRequest.startAsync()
+      toRequest(servletRequest) match {
         case -\/(e) =>
           // TODO make more http4sy
           servletResponse.sendError(HttpServletResponse.SC_BAD_REQUEST, e.sanitized)
         case \/-(req) =>
-          val ctx = servletRequest.startAsync()
           ctx.setTimeout(asyncTimeoutMillis)
           handle(req, ctx)
       }
@@ -98,7 +111,7 @@ class Http4sServlet(service: HttpService, asyncTimeout: Duration = Duration.Inf,
       uri = uri,
       httpVersion = version,
       headers = toHeaders(req),
-      body = chunkR(req.getInputStream).map(f => f(chunkSize)).eval,
+      body = inputStreamReader(req),
       attributes = AttributeMap(
         Request.Keys.PathInfoCaret(req.getServletPath.length),
         Request.Keys.Remote(InetAddress.getByName(req.getRemoteAddr)),
@@ -115,6 +128,42 @@ class Http4sServlet(service: HttpService, asyncTimeout: Duration = Duration.Inf,
   }
 }
 
-object Http4sServlet {
+object Http4sServlet extends LazyLogging {
   private[servlet] val DefaultChunkSize = Http4sConfig.getInt("org.http4s.servlet.default-chunk-size")
+
+  private type BodyReader = HttpServletRequest => EntityBody
+
+  private def asyncInputStreamReader(chunkSize: Int, maxChunks: Int)(req: HttpServletRequest): EntityBody = {
+    val queue = boundedQueue[ByteVector](maxChunks)
+    val in = req.getInputStream
+
+    in.setReadListener(new ReadListener {
+      override def onDataAvailable(): Unit = {
+        logger.debug("data is available on servlet input stream")
+        val buff = new Array[Byte](chunkSize)
+        do {
+          val len = in.read(buff)
+          if (len > 0) {
+            val copy = new Array[Byte](len)
+            System.arraycopy(buff, 0, copy, 0, len)
+            val chunk = ByteVector.view(copy)
+            logger.debug(s"enqueuing ${len} byte chunk to request body")
+            queue.enqueueOne(chunk).run
+          }
+        } while (in.isReady)
+      }
+
+      override def onAllDataRead(): Unit = {
+        logger.debug("closing servlet input stream")
+        queue.close.runAsync { cb => }
+      }
+
+      override def onError(t: Throwable): Unit = queue.fail(t).run
+    })
+
+    queue.dequeue
+  }
+
+  private def syncInputStreamReader(chunkSize: Int)(req: HttpServletRequest): EntityBody =
+    chunkR(req.getInputStream).map(_(chunkSize)).eval
 }
