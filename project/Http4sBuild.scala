b06443177b57f39ac8cc67cc6e4471878c7f4492
Ross A. Baker <ross@rossabaker.com>
2014-11-04 21:04:41 -0500

    slf4s was something else, this is log4s.

--- project/Http4sBuild.scala
+++ project/Http4sBuild.scala
@@ -46,6 +46,7 @@ object Http4sDependencies {
   lazy val json4sNative        = "org.json4s"               %% "json4s-native"           % json4sCore.revision
   lazy val jspApi              = "javax.servlet.jsp"         % "javax.servlet.jsp-api"   % "2.3.1" // YourKit hack
   lazy val junit               = "junit"                     % "junit"                   % "4.11"
+  lazy val log4s               = "org.log4s"                %% "log4s"                   % "1.1.1"
   lazy val logbackClassic      = "ch.qos.logback"            % "logback-classic"         % "1.0.9"
   lazy val parboiled           = "org.parboiled"            %% "parboiled"               % "2.0.0"
   lazy val rl                  = "org.scalatra.rl"          %% "rl"                      % "0.4.10"
@@ -55,7 +56,6 @@ object Http4sDependencies {
   lazy val scalazSpecs2        = "org.typelevel"            %% "scalaz-specs2"           % "0.3.0"
   lazy val scalazStream        = "org.scalaz.stream"        %% "scalaz-stream"           % "0.5a"
   lazy val scodecBits          = "org.typelevel"            %% "scodec-bits"             % "1.0.4"
-  lazy val slf4s               = "org.log4s"                %% "log4s"                   % "1.1.1"
   lazy val tomcatCatalina      = "org.apache.tomcat"         % "tomcat-catalina"         % "7.0.53"
   lazy val tomcatCoyote        = "org.apache.tomcat"         % "tomcat-coyote"           % tomcatCatalina.revision
 }
