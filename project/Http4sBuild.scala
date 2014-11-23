1cc0394166342d9543d9292e736c0fb9c1139b8b
Ross A. Baker <ross@rossabaker.com>
2014-11-22 23:19:09 -0500

    Upgrade to jawn-streamz-0.3.1-SNAPSHOT.
    This moves to something that doesn't require bintray to resolve.

--- project/Http4sBuild.scala
+++ project/Http4sBuild.scala
@@ -40,7 +40,7 @@ object Http4sDependencies {
   lazy val http4sWebsocket     = "org.http4s"               %% "http4s-websocket"        % "0.1.1"
   lazy val javaxServletApi     = "javax.servlet"             % "javax.servlet-api"       % "3.1.0"
   lazy val jawnParser          = "org.spire-math"           %% "jawn-parser"             % "0.7.0"
-  lazy val jawnStreamz         = "com.rossabaker"           %% "jawn-streamz"            % "0.2.0"
+  lazy val jawnStreamz         = "org.http4s"               %% "jawn-streamz"            % "0.3.1-SNAPSHOT"
   lazy val jettyServer         = "org.eclipse.jetty"         % "jetty-server"            % "9.2.5.v20141112"
   lazy val jettyServlet        = "org.eclipse.jetty"         % "jetty-servlet"           % jettyServer.revision
   lazy val json4sCore          = "org.json4s"               %% "json4s-core"             % "3.2.11"
