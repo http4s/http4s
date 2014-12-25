b8b7c3f9f7afb51d528ef224e0a838b0f9f331f5
Ross A. Baker <ross@rossabaker.com>
2014-12-25 14:19:46 -0500

    Integrate Twirl with example service.

--- twirl/src/main/scala/org/http4s/twirl/TwirlInstances.scala
+++ twirl/src/main/scala/org/http4s/twirl/TwirlInstances.scala
@@ -6,20 +6,23 @@ import MediaType._
 import play.twirl.api._
 
 trait TwirlInstances {
-  implicit def htmlContentEncoder(implicit charset: Charset): EntityEncoder[Html] = contentEncoder(`text/html`)
+  implicit def htmlContentEncoder(implicit charset: Charset = Charset.`UTF-8`): EntityEncoder[Html] =
+    contentEncoder(`text/html`)
 
   /**
    * Note: Twirl uses a media type of `text/javascript`.  This is obsolete, so we instead return
    * [[`application/javascript`]].
    */
-  implicit def jsContentEncoder(implicit charset: Charset): EntityEncoder[JavaScript] =
+  implicit def jsContentEncoder(implicit charset: Charset = Charset.`UTF-8`): EntityEncoder[JavaScript] =
     contentEncoder(`application/javascript`)
 
-  implicit def xmlContentEncoder(implicit charset: Charset): EntityEncoder[Xml] = contentEncoder(`application/xml`)
+  implicit def xmlContentEncoder(implicit charset: Charset = Charset.`UTF-8`): EntityEncoder[Xml] =
+    contentEncoder(`application/xml`)
 
-  implicit def txtContentEncoder(implicit charset: Charset): EntityEncoder[Txt] = contentEncoder(`text/plain`)
+  implicit def txtContentEncoder(implicit charset: Charset = Charset.`UTF-8`): EntityEncoder[Txt] =
+    contentEncoder(`text/plain`)
 
-  private def contentEncoder[C <: Content](mediaType: MediaType)(implicit charset: Charset): EntityEncoder[C] =
+  private def contentEncoder[C <: Content](mediaType: MediaType)(implicit charset: Charset = Charset.`UTF-8`): EntityEncoder[C] =
     EntityEncoder.stringEncoder(charset).contramap[C](content => content.body)
       .withContentType(`Content-Type`(mediaType, charset))
 }
