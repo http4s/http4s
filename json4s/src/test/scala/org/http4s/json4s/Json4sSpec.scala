892f9595bfac59e6d1f8b35de40234ecb609848d
Ross A. Baker <ross@rossabaker.com>
2015-01-17 18:24:50 -0500

    Test the jsonEncoder for json4s.

--- json4s/src/test/scala/org/http4s/json4s/Json4sSpec.scala
+++ json4s/src/test/scala/org/http4s/json4s/Json4sSpec.scala
@@ -1,8 +1,24 @@
-package org.http4s.json4s
+package org.http4s
+package json4s
 
+import org.http4s.EntityEncoderSpec._
+import org.http4s.Header.`Content-Type`
 import org.http4s.jawn.JawnDecodeSupportSpec
 import org.json4s.JValue
+import org.json4s.JsonAST.{JField, JString, JObject}
 
 trait Json4sSpec[J] extends JawnDecodeSupportSpec[JValue] { self: Json4sInstances[J] =>
   testJsonDecoder(json)
+
+  "json encoder" should {
+    val json: JValue = JObject(JField("test", JString("json4s")))
+
+    "have json content type" in {
+      jsonEncoder.headers.get(`Content-Type`) must_== Some(`Content-Type`(MediaType.`application/json`))
+    }
+
+    "write compact JSON" in {
+      writeToString(json) must equal ("""{"test":"json4s"}""")
+    }
+  }
 }
