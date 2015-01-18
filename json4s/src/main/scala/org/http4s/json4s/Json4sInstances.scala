6a8ae1254a0e6c9b57803d60b4bbd0e9aefa1a48
Ross A. Baker <ross@rossabaker.com>
2015-01-17 20:12:26 -0500

    Build json4s codec for its Reader and Writer.

--- json4s/src/main/scala/org/http4s/json4s/Json4sInstances.scala
+++ json4s/src/main/scala/org/http4s/json4s/Json4sInstances.scala
@@ -3,11 +3,19 @@ package json4s
 
 import org.http4s.Header.`Content-Type`
 import org.json4s.JsonAST.JValue
-import org.json4s.JsonMethods
+import org.json4s.{MappingException, Writer, Reader, JsonMethods}
 import _root_.jawn.support.json4s.Parser.facade
 
 trait Json4sInstances[J] {
   implicit lazy val json: EntityDecoder[JValue] = jawn.jawnDecoder(facade)
+
+  def jsonOf[A](implicit reader: Reader[A]): EntityDecoder[A] =
+    json.flatMapR { json =>
+      try DecodeResult.success(reader.read(json))
+      catch {
+        case e: MappingException => DecodeResult.failure(ParseFailure("Could not map JSON", e.msg))
+      }
+    }
 
   protected def jsonMethods: JsonMethods[J]
 
@@ -17,4 +25,7 @@ trait Json4sInstances[J] {
       // Look into replacing after https://github.com/non/jawn/issues/6#issuecomment-65018736
       jsonMethods.compact(jsonMethods.render(json))
     }.withContentType(`Content-Type`(MediaType.`application/json`))
+
+  def jsonEncoderOf[A](implicit writer: Writer[A]): EntityEncoder[A] =
+    jsonEncoder.contramap[A](writer.write)
 }
