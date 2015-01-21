65f9cf72da1a4ada436105c760109e3aa5c80350
Bryce Anderson <bryce.anderson22@gmail.com>
2015-01-21 09:46:17 -0500

    Add exceedingly verbose debug logging
    This is for issue #192 and should be reverted when that is addressed.

--- blaze-core/src/main/scala/org/http4s/blaze/Http1Stage.scala
+++ blaze-core/src/main/scala/org/http4s/blaze/Http1Stage.scala
@@ -130,16 +130,28 @@ trait Http1Stage { self: TailStage[ByteBuffer] =>
 
           def go(): Unit = try {
             doParseContent(currentBuffer) match {
-              case Some(result) => cb(\/-(ByteVector(result)))
-              case None if contentComplete() => cb(-\/(Terminated(End)))
+              case Some(result) =>
+                logger.debug(s"Decode successful: ${result}")
+                cb(\/-(ByteVector(result)))
+
+              case None if contentComplete() =>
+                logger.debug("Body Complete.")
+                cb(-\/(Terminated(End)))
+
               case None =>
+                logger.debug("Buffer underflow.")
                 channelRead().onComplete {
                   case Success(b)   =>
                     currentBuffer = BufferTools.concatBuffers(currentBuffer, b)
                     go()
 
-                  case Failure(EOF) => cb(-\/(eofCondition))
-                  case Failure(t)   => cb(-\/(t))
+                  case Failure(EOF) =>
+                    logger.debug("Failed to read body.")
+                    cb(-\/(eofCondition))
+
+                  case Failure(t)   =>
+                    logger.debug(t)("Unexpected error reading body.")
+                    cb(-\/(t))
                 }
             }
           } catch {
@@ -173,13 +185,17 @@ trait Http1Stage { self: TailStage[ByteBuffer] =>
 
   /** Cleans out any remaining body from the parser */
   final protected def drainBody(buffer: ByteBuffer): Future[ByteBuffer] = {
+    logger.debug("Draining body.")
     if (!contentComplete()) {
       while(!contentComplete() && doParseContent(buffer).nonEmpty) { /* we just discard the results */ }
 
       if (!contentComplete()) channelRead().flatMap(newBuffer => drainBody(concatBuffers(buffer, newBuffer)))
       else Future.successful(buffer)
     }
-    else Future.successful(buffer)
+    else {
+      logger.debug("Body drained.")
+      Future.successful(buffer)
+    }
   }
 }
 
