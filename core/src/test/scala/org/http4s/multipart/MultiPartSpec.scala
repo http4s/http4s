a515a87b78e8cb227f6c6d2090a7a419d835fa54
Jonathan Ferguson <jono@spiralarm.com>
2015-05-28 14:16:59 +1000

    Multipart
    make sure we can have binary data in Multipart

--- core/src/test/scala/org/http4s/multipart/MultiPartSpec.scala
+++ core/src/test/scala/org/http4s/multipart/MultiPartSpec.scala
@@ -1,15 +1,18 @@
 package org.http4s
 package multipart
 
-import org.http4s._
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
+
+import org.http4s._
 import org.http4s.MediaType._
 import org.http4s.headers._
 import org.http4s.Http4s._
 import org.http4s.Uri._
 import org.http4s.util._
 import org.http4s.Status.Ok
-
 import scodec.bits.ByteVector
 import org.http4s.EntityEncoder._
 import Entity._
@@ -17,26 +20,27 @@ import scalaz.stream.Process
 import org.specs2.Specification
 import org.specs2.scalaz.DisjunctionMatchers
 
+
 class MultipartSpec extends Specification  with DisjunctionMatchers {
 
+  implicit def mpe: EntityEncoder[Multipart] = MultipartEntityEncoder
+    
+  
   def is = s2"""
     Multipart form data can be 
       encoded and decoded example A       $encodeAndDecodeA
       encoded and decoded example B       $encodeAndDecodeB
+      encoded and decoded example C       $encodeAndDecodeC
      """
-
-  def encodeAndDecodeA = {
-
-   implicit def mpe: EntityEncoder[Multipart] = MultipartEntityEncoder
-
-    val url = Uri(
+  val url = Uri(
       scheme = Some(CaseInsensitiveString("https")),
       authority = Some(Authority(host = RegName("example.com"))),
       path = "/path/to/some/where")
 
-    val txtToEntity: String => EntityEncoder.Entity = in =>
-        EntityEncoder.Entity(Process.emit(in).map(s => ByteVector(s.getBytes)))
-
+  val txtToEntity: String => EntityEncoder.Entity = in =>
+      EntityEncoder.Entity(Process.emit(in).map(s => ByteVector(s.getBytes)))
+   
+  def encodeAndDecodeA = {
 
     val ctf1       = Some(`Content-Type`(`text/plain`))
     val ef1        = txtToEntity("Text_Field_1")
@@ -61,17 +65,6 @@ class MultipartSpec extends Specification  with DisjunctionMatchers {
 
   def encodeAndDecodeB = {
 
-   implicit def mpe: EntityEncoder[Multipart] = MultipartEntityEncoder
-
-    val url   = Uri(
-      scheme    = Some(CaseInsensitiveString("https")),
-      authority = Some(Authority(host = RegName("example.com"))),
-      path      = "/path/to/some/where")
-
-    val txtToEntity: String => EntityEncoder.Entity = in =>
-        EntityEncoder.Entity(Process.emit(in).map(s => ByteVector(s.getBytes)))
-
-
     val ef1        = txtToEntity("Text_Field_1")
     val field1     = FormData(Name("field1"), None, ef1)
     val multipart  = Multipart(List(field1))
@@ -91,5 +84,56 @@ class MultipartSpec extends Specification  with DisjunctionMatchers {
     result must beRightDisjunction(multipart)
   }
 
+  def encodeAndDecodeC = {
+
+    val path       = "./core/src/test/resources/Animated_PNG_example_bouncing_beach_ball.png"
+    val file       = new File(path)
+    
+    val ef1        = txtToEntity("Text_Field_1")
+    val field1     = FormData(Name("field1"), None, ef1)
+    
+    val ctf2       = Some(`Content-Type`(`image/png`))
+    val ef2        = fileToEntity(file,65557)
+    val field2     = FormData(Name("image"), ctf2, ef2)
+    
+    
+    val multipart  = Multipart(List(field1,field2))
+    
+    val entity     = MultipartEntityEncoder.toEntity(multipart)
+    val body       = entity.run.body.runLog.run.fold(ByteVector.empty)((acc,x) => acc ++ x )
+    val request    = Request(method  = Method.POST,
+                             uri     = url,
+                             body    = Process.emit(body),
+                             headers = multipart.headers )
+          
+    val decoder    = MultipartEntityDecoder.decoder
+    val decoded    = decoder.decode(request)
+    val result     = decoded.run.run
+
+    
+//    Very evil way to confirm we are getting back sensible bytes.    
+//    result.map { m =>
+//      val part:FormData = m.parts.drop(1).head.asInstanceOf[FormData]
+//      part.content.body.map{ bv =>
+//        val out = File.createTempFile("prefix", "suffix")
+//        val fos = new FileOutputStream(out )
+//        fos.write(bv.toArray)
+//        fos.flush()
+//        fos.close()
+//        println(out.getCanonicalPath)
+//        println(out.getAbsoluteFile)
+//      }.run
+//    }
+    
+    result must beRightDisjunction(multipart)
+  }
+
+  private def fileToEntity(f: File,length:Int): Entity = {
+    val fis   = new FileInputStream(f)
+    val array = new Array[Byte](length)
+    fis.read(array)
+    fis.close()
+    Entity(body = Process.emit(ByteVector(array)))
+  }  
   
 }
