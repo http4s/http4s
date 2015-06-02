05d75493f9d47ecd63949e7f1a600854782ad922
Jonathan Ferguson <jono@spiralarm.com>
2015-06-02 16:29:51 +1000

    Capture header parameters in Multipart bodies

--- core/src/main/scala/org/http4s/multipart/MultipartParser.scala
+++ core/src/main/scala/org/http4s/multipart/MultipartParser.scala
@@ -4,92 +4,73 @@ import org.parboiled2._
 import org.parboiled2.CharPredicate._
 
 final case class Pair(key:String,value:String)
-final case class BPart(headers:Seq[Pair],content:String)
+final case class PHeader(key:String,value:String, parameters:Option[Seq[Pair]])
+final case class BPart(headers:Seq[PHeader],content:String)
 class MultipartParser(val input: ParserInput, val boundary:Boundary) extends Parser {
 
   def Octet               = rule { "\u0000" - "\u00FF" }
   //TODO: This is a bit naughty
+  //This is a nicer way to write it val NL = rule { optional('\r') ~ '\n' } from 
+  //https://github.com/sirthias/parboiled2/blob/master/examples/src/main/scala/org/parboiled2/examples/CsvParser.scala#L3
   def CRLF                = rule { str("\r\n") | str("\n") }
-  def Dash                = rule { str("--") }
   
+  def Dash                = rule { str("--") }
   //http://www.rfc-editor.org/std/std68.txt
   def LWS                 = rule { anyOf(" \t") }  
-  //def AltBoundaryChar     = rule { "'" | "(" | ")" |
-  //                                 "+" | "_" | "," | "-" | "." |
-  //                                 "/" | ":" | "=" | "?"}
-  //def BCharsNoSpace       = rule { Alpha | Digit | AltBoundaryChar }
-  //def BChars              = rule { BCharsNoSpace | " "}
-  //TODO: should be 0 to 69
-  //def Boundary            = rule { (1 to 69) times BChars ~ BCharsNoSpace }
-  def DashBoundary        = rule { Dash ~ str(boundary.value) }
+  def OLWS                = rule { optional(LWS)}
+  def DashBoundary        = rule { Dash ~ boundary.value }
   def Delimiter           = rule { CRLF ~ DashBoundary }
   def TransportPadding    = rule { zeroOrMore(LWS) }
-
   def CloseDelimiter      = rule { Delimiter ~ Dash }
   def DiscardText         = rule { zeroOrMore( !DashBoundary ~ ANY) }
   def Preamble            = DiscardText 
   def Epilogue            = DiscardText
   // See https://www.ietf.org/rfc/rfc2045.txt Section 5.1
-  def Header              = rule { (capture(HeaderName)  ~ ':' ~ capture(HeaderValue))  ~> ((k,v) => Pair(k,v)) }
-  def HeaderName          = rule { oneOrMore(Alpha | '-' )    }
-  def HeaderValue         = rule { oneOrMore(!CRLF ~ Octet)     }
+  def Key          = rule { oneOrMore(Alpha | '-' )    }
+  def Value         = rule { oneOrMore(!HeaderDelimiter ~ Octet)     }
+  def HeaderDelimiter     = rule { CRLF |  ";"   | "\"" }
+  def Header              = rule { 
+                                  (capture(Key)  ~ ':' ~ capture(Value)) ~ 
+                                  optional(";" ~ oneOrMore(HeaderParameters).separatedBy(";")) ~> ((k,v,p) => PHeader(k,v,p)) 
+                            }
+
+  def Parameters =  rule {oneOrMore(HeaderParameters).separatedBy(";")} 
+  def HeaderParameters    = rule {  OLWS  ~ (capture(Key)  ~  OLWS ~  '=' ~  OLWS ~ optional("\"") ~ capture(Value)) ~ optional("\"") ~  OLWS ~> ((k,v) => Pair(k,v))  } 
+  
   def MimePartHeaders     = rule { zeroOrMore(Header ~ CRLF ) }
   def BodyEnd             = rule {CRLF ~ DashBoundary}
   def BodyContent         = rule { capture(zeroOrMore(!BodyEnd ~ ANY)) }
   def BodyPart            = rule { (MimePartHeaders ~ CRLF ~ BodyContent) ~> ((h,b) => BPart(h,b)) }
-   //oneOrMore(field).separatedBy(ctx.fieldDelimiter)
-  
-  //                            OptionalFrontMatter            ~
-  //                   optional(CRLF           ~ Epilogue)    
   def Encapsulation       = rule { CRLF ~ DashBoundary ~ TransportPadding ~ CRLF ~ BodyPart }
-  
   def MultipartBody       = rule {
                             Preamble       ~
                             DashBoundary   ~ TransportPadding          ~ CRLF ~ 
                             BodyPart       ~ zeroOrMore(Encapsulation) ~
                             CloseDelimiter ~ TransportPadding          ~
-                            Epilogue
+                            Epilogue       ~ EOI
                           }
-  //Working 
-  //def Building           = rule {  DashBoundary  ~ TransportPadding ~ CRLF}
-  //def Building           = rule { MimePartHeaders  }
-  //def Building           = rule { MimePartHeaders ~ CRLF }
-  //def Building           = rule { MimePartHeaders ~ CRLF ~ zeroOrMore(Octet)}
-  //def Building           = rule { BodyPart}
-  //def Building = rule {DashBoundary   ~ TransportPadding          ~ CRLF ~  BodyPart       ~ zeroOrMore(Encapsulation) }
-  //Not working as expected 
-  //    def Building           = rule { CRLF ~ DashBoundary ~ TransportPadding ~ CRLF ~ zeroOrMore(BodyPart).separatedBy(CRLF ~ DashBoundary ~ TransportPadding ~ CRLF  ) }
   
 }
 
 object Test extends App {
   
-// Working 
-//      val building       = """Content-Disposition: form-data; name="text"
-//Content-Type: text/plain
-//
-//I AM A MOOSE
-//I am some content
-//""""
-      val building        = """------WebKitFormBoundarycaZFo8IAKVROTEeD
+  val building        = """
+------WebKitFormBoundarycaZFo8IAKVROTEeD
 Content-Disposition: form-data; name="text"
-Content-Type: text/plain
 
 I AM A MOOSE
 ------WebKitFormBoundarycaZFo8IAKVROTEeD
 Content-Disposition: form-data; name="file1"; filename="Graph_Databases_2e_Neo4j.pdf"
-Content-Type: text/plain
+Content-Type: application/pdf
 
-
-MORE CONTENTS
 
 ------WebKitFormBoundarycaZFo8IAKVROTEeD
 Content-Disposition: form-data; name="file2"; filename="DataTypesALaCarte.pdf"
-Content-Type: text/plain
+Content-Type: application/pdf
 
-EVEN MORE CONTENTS
+
 ------WebKitFormBoundarycaZFo8IAKVROTEeD--
-"""
+"""""
   
       
       
