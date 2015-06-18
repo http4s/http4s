6d9517cb454172158251f5775fb3d453a3e4fd7e
Bryce Anderson <bryce.anderson22@gmail.com>
2015-06-18 08:36:11 -0400

    Cleanup staticcontent package
    * Moved some of the CacheStrategy types into their own file
    * Added lots of doc strings
    * Used a ConcurrentHashMap instead of synchronizing on a HashMap
    
    This PR should be binary compatible.

---
+++ server/src/main/scala/org/http4s/server/staticcontent/NoopCacheStrategy.scala
@@ -0,0 +1,10 @@
+package org.http4s.server.staticcontent
+
+import org.http4s._
+import scalaz.concurrent.Task
+
+
+/** Cache strategy that doesn't cache anything, ever. */
+object NoopCacheStrategy extends CacheStrategy {
+  override def cache(path: Uri, resp: Response): Task[Response] = Task.now(resp)
+}
