2194a6783a7dbb71314f63747d053cd4c6da81e4
Jonathan Ferguson <jono@spiralarm.com>
2015-06-26 15:02:45 +1000

    Tidy up of InstrumentHandler,
    tried to remove as much null/var as possible.

--- jetty/src/main/scala/org/http4s/server/jetty/InstrumentedHandler.scala
+++ jetty/src/main/scala/org/http4s/server/jetty/InstrumentedHandler.scala
@@ -34,102 +34,17 @@ import com.codahale.metrics.MetricRegistry.{name => registryName}
  *
  * <a href="https://github.com/http4s/http4s/issues/204">See http4s/http4s#204</a>.
  */
-case class InstrumentedHandler(registry:MetricRegistry , prefix:Option[String] = None)  extends HandlerWrapper {
+protected case class InstrumentedHandler(registry:MetricRegistry , _prefix:Option[String] = None) extends HandlerWrapper {
 
-  var name:String                = null
-  // the requests handled by this handler, excluding active
-  var requests:Timer             = null
-  // the number of dispatches seen by this handler, excluding active
-  var dispatches:Timer           = null
-  // the number of active requests
-  var activeRequests:Counter     = null
-  // the number of active dispatches
-  var activeDispatches:Counter   = null
-  // the number of requests currently suspended.
-  var activeSuspended:Counter    = null
-  // the number of requests that have been asynchronously dispatched
-  var asyncDispatches:Meter      = null
-  // the number of requests that expired while suspended
-  var asyncTimeouts:Meter        = null
-  var responses:Array[Meter]     = null
-  var getRequests:Timer          = null
-  var postRequests:Timer         = null
-  var headRequests:Timer         = null
-  var putRequests:Timer          = null
-  var deleteRequests:Timer       = null
-  var optionsRequests:Timer      = null
-  var traceRequests:Timer        = null
-  var connectRequests:Timer      = null
-  var moveRequests:Timer         = null
-  var otherRequests:Timer        = null
-  var listener:AsyncListener     = null
+  var name:String                               = null
 
-
-    override protected def doStart  {
-        super.doStart()
-
-        val _prefix = prefix.fold(registryName(getHandler().getClass(), name) )(p => registryName(p, name))
-
-        requests         = registry.timer(registryName(_prefix,   "requests"))
-        dispatches       = registry.timer(registryName(_prefix,   "dispatches"))
-        activeRequests   = registry.counter(registryName(_prefix, "active-requests"))
-        activeDispatches = registry.counter(registryName(_prefix, "active-dispatches"))
-        activeSuspended  = registry.counter(registryName(_prefix, "active-suspended"))
-        asyncDispatches  = registry.meter(registryName(_prefix,   "async-dispatches"))
-        asyncTimeouts    = registry.meter(registryName(_prefix,   "async-timeouts"))
-
-        responses = Array[Meter](
-                registry.meter(registryName(_prefix, "1xx-responses")), // 1xx
-                registry.meter(registryName(_prefix, "2xx-responses")), // 2xx
-                registry.meter(registryName(_prefix, "3xx-responses")), // 3xx
-                registry.meter(registryName(_prefix, "4xx-responses")), // 4xx
-                registry.meter(registryName(_prefix, "5xx-responses"))) // 5xx
-
-                
-        getRequests       = registry.timer(registryName(_prefix, "get-requests"))
-        postRequests      = registry.timer(registryName(_prefix, "post-requests"))
-        headRequests      = registry.timer(registryName(_prefix, "head-requests"))
-        putRequests       = registry.timer(registryName(_prefix, "put-requests"))
-        deleteRequests    = registry.timer(registryName(_prefix, "delete-requests"))
-        optionsRequests   = registry.timer(registryName(_prefix, "options-requests"))
-        traceRequests     = registry.timer(registryName(_prefix, "trace-requests"))
-        connectRequests   = registry.timer(registryName(_prefix, "connect-requests"))
-        moveRequests      = registry.timer(registryName(_prefix, "move-requests"))
-        otherRequests     = registry.timer(registryName(_prefix, "other-requests"))
-
-        registry.register(registryName(_prefix, "percent-4xx-1m"), new RatioGauge() {
-            protected def getRatio() = {
-              Ratio.of(responses(3).getOneMinuteRate(),
-                       requests.getOneMinuteRate())
-            }
-        })
-        registry.register(registryName(_prefix, "percent-4xx-5m"), new RatioGauge() {
-            protected def getRatio() = 
-              Ratio.of(responses(3).getFiveMinuteRate(),requests.getFiveMinuteRate())
-        })
-        registry.register(registryName(_prefix, "percent-4xx-15m"), new RatioGauge() {
-            protected def getRatio() = 
-              Ratio.of(responses(3).getFifteenMinuteRate(),requests.getFifteenMinuteRate())
-            
-        })
-        registry.register(registryName(_prefix, "percent-5xx-1m"), new RatioGauge() {
-            protected def getRatio() = 
-              Ratio.of(responses(4).getOneMinuteRate(),requests.getOneMinuteRate())
-            
-        })
-        registry.register(registryName(_prefix, "percent-5xx-5m"), new RatioGauge() {
-            protected def getRatio() = 
-              Ratio.of(responses(4).getFiveMinuteRate(),requests.getFiveMinuteRate())
-        })
-        registry.register(registryName(_prefix, "percent-5xx-15m"), new RatioGauge() {
-            protected def getRatio() = 
-              Ratio.of(responses(4).getFifteenMinuteRate(),requests.getFifteenMinuteRate())
-        })
-
-        this.listener = new AsyncListener() {
+  private val registerTimer:String   => Timer   = name => registry.timer(registryName(prefix,   name))
+  private val registerMeter:String   => Meter   = name => registry.meter(registryName(prefix,   name))
+  private val registerCounter:String => Counter = name => registry.counter(registryName(prefix,   name))
+  private val listener:AsyncListener            = new AsyncListener() {
             def onTimeout(event:AsyncEvent )   = asyncTimeouts.mark()
             def onStartAsync(event:AsyncEvent) = event.getAsyncContext().addListener(this)
-            def onError(event:AsyncEvent )     = () 
+            def onError(event:AsyncEvent )     = ()
             def onComplete(event:AsyncEvent)   = {
                 val state:AsyncContextState =  event.getAsyncContext().asInstanceOf[AsyncContextState]
                 val request:Request =  state.getRequest().asInstanceOf[Request]
@@ -139,15 +54,76 @@ case class InstrumentedHandler(registry:MetricRegistry , prefix:Option[String] =
                 }
             }
         }
-    }
+
+  private lazy val prefix            = _prefix.fold(registryName(getHandler().getClass(), name) )(p => registryName(p, name))
+  // the requests handled by this handler, excluding active
+  private lazy val requests          = registerTimer("requests")
+  // the number of dispatches seen by this handler, excluding active
+  private lazy val  dispatches       = registerTimer("dispatches")
+  // the number of active requests
+  private lazy val  activeRequests   = registerCounter("active-requests")
+  // the number of active dispatches
+  private lazy val  activeDispatches = registerCounter("active-dispatches")
+  // the number of requests currently suspended.
+  private lazy val  activeSuspended  = registerCounter("active-suspended")
+  // the number of requests that have been asynchronously dispatched
+  private lazy val  asyncDispatches  = registerMeter("async-dispatches")
+  // the number of requests that expired while suspended
+  private lazy val  asyncTimeouts    = registerMeter(   "async-timeouts")
+  private lazy val  responses        = Array[Meter](
+                                        registerMeter( "1xx-responses"), // 1xx
+                                        registerMeter( "2xx-responses"), // 2xx
+                                        registerMeter( "3xx-responses"), // 3xx
+                                        registerMeter( "4xx-responses"), // 4xx
+                                        registerMeter( "5xx-responses")) // 5xx
+  private lazy val  getRequests      = registerTimer("get-requests")
+  private lazy val  postRequests     = registerTimer( "post-requests")
+  private lazy val  headRequests     = registerTimer( "head-requests")
+  private lazy val  putRequests      = registerTimer( "put-requests")
+  private lazy val  deleteRequests   = registerTimer( "delete-requests")
+  private lazy val  optionsRequests  = registerTimer( "options-requests")
+  private lazy val  traceRequests    = registerTimer( "trace-requests")
+  private lazy val  connectRequests  = registerTimer( "connect-requests")
+  private lazy val  moveRequests     = registerTimer( "move-requests")
+  private lazy val  otherRequests    = registerTimer( "other-requests")
+
+  override protected def doStart  {
+      super.doStart()
+      registry.register(registryName(prefix, "percent-4xx-1m"), new RatioGauge() {
+          protected def getRatio() = {
+            Ratio.of(responses(3).getOneMinuteRate(),
+                     requests.getOneMinuteRate())
+          }
+      })
+      registry.register(registryName(prefix, "percent-4xx-5m"), new RatioGauge() {
+          protected def getRatio() =
+            Ratio.of(responses(3).getFiveMinuteRate(),requests.getFiveMinuteRate())
+      })
+      registry.register(registryName(prefix, "percent-4xx-15m"), new RatioGauge() {
+          protected def getRatio() =
+            Ratio.of(responses(3).getFifteenMinuteRate(),requests.getFifteenMinuteRate())
+
+      })
+      registry.register(registryName(prefix, "percent-5xx-1m"), new RatioGauge() {
+          protected def getRatio() =
+            Ratio.of(responses(4).getOneMinuteRate(),requests.getOneMinuteRate())
+
+      })
+      registry.register(registryName(prefix, "percent-5xx-5m"), new RatioGauge() {
+          protected def getRatio() =
+            Ratio.of(responses(4).getFiveMinuteRate(),requests.getFiveMinuteRate())
+      })
+      registry.register(registryName(prefix, "percent-5xx-15m"), new RatioGauge() {
+          protected def getRatio() =
+            Ratio.of(responses(4).getFifteenMinuteRate(),requests.getFifteenMinuteRate())
+      })
+  }
 
     override def handle(path:String,
                         request:Request,
                         httpRequest:HttpServletRequest,
                         httpResponse:HttpServletResponse):Unit = {
-
         activeDispatches.inc()
-
         val state = request.getHttpChannelState()
         val start = if (state.isInitial()) {
             // new request
@@ -159,7 +135,7 @@ case class InstrumentedHandler(registry:MetricRegistry , prefix:Option[String] =
             if (state.getState() == HttpChannelState.State.DISPATCHED) {
                 asyncDispatches.mark()
             }
-            System.currentTimeMillis()            
+            System.currentTimeMillis()
         }
 
         try {
@@ -183,7 +159,7 @@ case class InstrumentedHandler(registry:MetricRegistry , prefix:Option[String] =
         }
     }
 
-    def requestTimer( method:String) = HttpMethod.fromString(method) match  {
+    private def requestTimer( method:String) = HttpMethod.fromString(method) match  {
       case GET     => getRequests
       case POST    => postRequests
       case PUT     => putRequests
@@ -196,12 +172,12 @@ case class InstrumentedHandler(registry:MetricRegistry , prefix:Option[String] =
       case default => otherRequests
     }
 
-    def updateResponses(request:Request) = {
-        val response = request.getResponse().getStatus() / 100
-        if (response >= 1 && response <= 5) {
-            responses(response - 1).mark()
-        }
-        activeRequests.dec()
+  private def updateResponses(request:Request) = {
+      val response = request.getResponse().getStatus() / 100
+      if (response >= 1 && response <= 5) {
+          responses(response - 1).mark()
+      }
+      activeRequests.dec()
         val elapsedTime = System.currentTimeMillis() - request.getTimeStamp()
         requests.update(elapsedTime, TimeUnit.MILLISECONDS)
         requestTimer(request.getMethod()).update(elapsedTime, TimeUnit.MILLISECONDS)
