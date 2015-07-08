5cf23c253a1aad12d06ce2dd24d733f75da2ed6c
Ross A. Baker <ross@rossabaker.com>
2015-07-08 19:17:01 -0400

    Test, and squash bugs, in the Router.

--- server/src/main/scala/org/http4s/server/Router.scala
+++ server/src/main/scala/org/http4s/server/Router.scala
@@ -5,18 +5,24 @@ import org.http4s.{Status, Response, Request}
 
 import scalaz.concurrent.Task
 
-case class Router (mappings: Seq[(String, HttpService)]) extends HttpService {
-  lazy val prefixedMappings: Seq[(String, HttpService)] = mappings.map { case (prefix, service) =>
-    val transformed =
-      if (prefix.isEmpty || prefix == "/") service
-      else URITranslation.translateRoot(prefix)(service)
-    prefix -> transformed
-  }
-
+class Router private (mappings: Seq[(String, HttpService)]) extends HttpService {
   // TODO A more efficient implementation does not require much imagination
   def apply(req: Request): Task[Response] = {
-    prefixedMappings.find { case (prefix, _) =>
+    HttpService.empty(req)
+    val service = mappings.find { case (prefix, _) =>
       req.pathInfo.startsWith(prefix)
-    }.fold({_: Request => Task.now(Response(Status.NotFound))})(_._2)(req)
+    }.fold(HttpService.empty)(_._2 orElse HttpService.empty)
+    service(req)
   }
+}
+
+object Router {
+  def apply(mappings: Seq[(String, HttpService)]): Router = new Router(
+    mappings.sortBy(_._1.length).reverse.map { case (prefix, service) =>
+      val transformed =
+        if (prefix.isEmpty || prefix == "/") service
+        else URITranslation.translateRoot(prefix)(service)
+      prefix -> transformed
+    }
+  )
 }
