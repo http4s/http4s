3e8ee9ea06d4cbae3bd5fc98e81537fc897d6b2c
Ross A. Baker <ross@rossabaker.com>
2015-07-15 00:17:37 -0400

    Scaladoc the new aliases and methods.

--- server/src/main/scala/org/http4s/server/package.scala
+++ server/src/main/scala/org/http4s/server/package.scala
@@ -4,20 +4,41 @@ import scalaz.Kleisli
 import scalaz.concurrent.Task
 
 package object server {
+  /**
+   * A Service wraps a function of request type [[A]] to a Task that runs
+   * to esponse type [[B]].  By wrapping the `Service`, we can compose them
+   * using Kleisli operations.
+   */
   type Service[A, B] = Kleisli[Task, A, B]
 
   object Service {
+    /**
+     * Lifts an unwrapped function that returns a Task into a [[Service]].
+     * No effort is made to provide a default response if a [[PartialFunction]]
+     * is passed in.
+     *
+     * @see [[HttpService.apply]]
+     */
     def lift[A, B](f: A => Task[B]): Service[A, B] = Kleisli.kleisli(f)
   }
 
+  /**
+   * A [[Service]] that returns an optional result.
+   */
   type PartialService[A, B] = Service[A, Option[B]]
 
+  /**
+   * A [[Service]] that produces a Task to compute a [[Response]] from a
+   * [[Request]].  An HttpService can be run on any supported http4s
+   * server backend, such as Blaze, Jetty, or Tomcat.
+   */
   type HttpService = Service[Request, Response]
+
   object HttpService {
     /**
-     * Lifts a partial function to a service.  The resulting service will
-     * return a Task containing `None` where the partial function is not
-     * defined, and Some result where it is.
+     * Lifts a partial function to an `HttpService`, answering with a [[Response]]
+     * with status [[Status.NotFound]] for any requests where the function
+     * is undefined.
      */
     def apply(pf: PartialFunction[Request, Task[Response]]): HttpService =
       Service.lift {
@@ -31,6 +52,16 @@ package object server {
   }
 
   implicit class HttpServiceSyntax(val service: HttpService) extends AnyVal {
+    /**
+     * Returns a service that delegates the request to another service if this
+     * service responds with status [[Status.NotFound]].
+     */
+    /*
+     * TODO This could be generalized to Service if we had an Empty typeclass.  This has
+     * been added to alleycats and proposed for scalaz-contrib, but is not yet available
+     * in a project we depend on.  A Monoid is unacceptable, because we can't define a
+     * reasonable Eq/Equal typeclass for a Response due to the effects of the body.
+     */
     def orElse(that: HttpService): HttpService = Service.lift { req: Request =>
       service(req).flatMap {
         case resp: Response if resp.status == Status.NotFound =>
@@ -41,7 +72,18 @@ package object server {
     }
   }
 
+  /**
+   * A middleware is a function of one [[Service]] to another, possibly of a
+   * different [[Request]] and [[Response]] type.  http4s comes with several
+   * middlewares for composing common functionality into services.
+   *
+   * @tparam A the request type of the original service
+   * @tparam B the response type of the original service
+   * @tparam C the request type of the resulting service
+   * @tparam D the response type of the original service
+   */
   type Middleware[A, B, C, D] = Service[A, B] => Service[C, D]
+
   object Middleware {
     def apply[A, B, C, D](f: (C, Service[A, B]) => Task[D]): Middleware[A, B, C, D] = {
       service => Service.lift {
@@ -50,6 +92,9 @@ package object server {
     }
   }
 
+  /**
+   * An HTTP middleware converts an [[HttpService]] to another.
+   */
   type HttpMiddleware = Middleware[Request, Response, Request, Response]
 }
 
