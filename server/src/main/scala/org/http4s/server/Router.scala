8dea1902f8bd5ba68fbc94e0b9153fefbf13b15d
Rodolfo Hansen <rhansen@kitsd.com>
2015-08-25 11:58:34 -0500

    Expose the fallthrough implicit to use when defining a route

--- server/src/main/scala/org/http4s/server/Router.scala
+++ server/src/main/scala/org/http4s/server/Router.scala
@@ -1,21 +1,36 @@
 package org.http4s
 package server
 
-import middleware.URITranslation.{translateRoot => translate}
-import syntax.ServiceOps
-
 object Router {
 
+  import Service.{withFallback => fallback}
+  import middleware.URITranslation.{translateRoot => translate}
+
+  /**
+    * Defines an HttpService based on list of mappings.
+    * @see define
+    */
   def apply(mappings: (String, HttpService)*): HttpService = define(mappings:_*)()
 
-  def define(mappings: (String, HttpService)*)(default: HttpService = HttpService.empty): HttpService =
+  /**
+    * Defines an HttpService based on list of mappings,
+    * a default Service to be used when none in the list match incomming requests,
+    * and an implicit Fallthrough which decides whether a request was matched.
+    *
+    * The mappings are processed in descending order (longest first) of prefix length.
+    */
+  def define(mappings: (String, HttpService)*)
+            (default: HttpService = HttpService.empty)
+            (implicit fallthrough: Fallthrough[Response]): HttpService =
     mappings.sortBy(_._1.length).foldLeft(default) {
       case (acc, (prefix, service)) =>
-        if (prefix.isEmpty || prefix == "/") service orElse acc
+        if (prefix.isEmpty || prefix == "/") fallback(acc)(service)(fallthrough)
         else HttpService.lift {
           req => (
-            if (req.pathInfo.startsWith(prefix)) translate(prefix)(service) orElse acc
-            else acc
+            if (req.pathInfo.startsWith(prefix))
+              fallback(acc)(translate(prefix)(service))(fallthrough)
+            else
+              acc
           ) (req)
         }
     }
