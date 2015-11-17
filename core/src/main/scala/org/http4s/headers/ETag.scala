0ce2754eb735d2acb94e2c45758392335a448f99
Bryce Anderson <bryce.anderson22@gmail.com>
2015-11-17 10:43:15 -0500

    Fix parsing of `entity-tag` rule
    fixes #446
    
    Parsing of the `entity-tag` rule was previously malformed and only
    accepted alpha-numeric characters.
    
    Changes include ETAG and IF_MODIFIED_SINCE parsers. Notably, the
    IF_MATCH header doesn't exhist and would use the same parsing bits.

--- core/src/main/scala/org/http4s/headers/ETag.scala
+++ core/src/main/scala/org/http4s/headers/ETag.scala
@@ -3,11 +3,19 @@ package headers
 
 import org.http4s.util.Writer
 
-object ETag extends HeaderKey.Internal[ETag] with HeaderKey.Singleton
+object ETag extends HeaderKey.Internal[ETag] with HeaderKey.Singleton {
+  case class EntityTag(tag: String, weak: Boolean = false) {
+    override def toString() = {
+      if (weak) "W/\"" + tag + '"'
+      else "\"" + tag + '"'
+    }
+  }
+}
 
-final case class ETag(tag: String) extends Header.Parsed {
+final case class ETag(tag: ETag.EntityTag) extends Header.Parsed {
   def key: HeaderKey = ETag
-  override def value: String = tag
+  override def value: String = tag.toString()
   override def renderValue(writer: Writer): writer.type = writer.append(value)
 }
+
 
