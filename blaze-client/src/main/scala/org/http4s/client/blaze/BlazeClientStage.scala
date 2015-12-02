a5112df59c20a639fb861748bc3d5ccce40ac1e6
Bryce Anderson <bryce.anderson22@gmail.com>
2015-12-02 16:48:13 -0500

    Add a finalizer to the BlazeClientStage trait
    Problem: If a body isn't used the socket may never be released.
    Solution: Adding a finalizer will ensure that the stage shuts down and presumably performs resource cleanup.

--- blaze-client/src/main/scala/org/http4s/client/blaze/BlazeClientStage.scala
+++ blaze-client/src/main/scala/org/http4s/client/blaze/BlazeClientStage.scala
@@ -2,6 +2,8 @@ package org.http4s.client.blaze
 
 import java.nio.ByteBuffer
 
+import scala.util.control.NonFatal
+
 import org.http4s.blaze.pipeline.TailStage
 import org.http4s.{Request, Response}
 
@@ -13,4 +15,15 @@ trait BlazeClientStage extends TailStage[ByteBuffer] {
   def isClosed(): Boolean
 
   def shutdown(): Unit
+
+  override protected def finalize(): Unit = {
+    try if (!isClosed()) {
+      logger.warn("BlazeClientStage was not disconnected and could result in a resource leak")
+      shutdown()
+      super.finalize()
+    } catch {
+      case NonFatal(t) =>
+        logger.error(t)("Failure finalizing the client stage")
+    }
+  }
 }
