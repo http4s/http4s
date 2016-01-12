8df1c48789f4124e8e5bfa550d211af17c355a10
Mateusz Bilski <mateusz.bilski@gmail.com>
2016-01-12 13:09:18 +0100

    simple dispatch client impl

---
+++ dispatch-client/src/test/scala/org/http4s/client/dispatch/DispatchClientSpec.scala
@@ -0,0 +1,6 @@
+package org.http4s.client.dispatch
+
+import org.htt4s.client.dispatch.DispatchClient
+import org.http4s.client.ClientRouteTestBattery
+
+class DispatchClientSpec extends ClientRouteTestBattery("Dispatch Client", DispatchClient())
