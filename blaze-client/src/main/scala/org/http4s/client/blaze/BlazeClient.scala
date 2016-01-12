084d5b7dd518d5478c1fd3a114d45a2ecf35d454
Ross A. Baker <ross@rossabaker.com>
2016-01-12 11:31:46 -0500

    Run the invalidation tasks on error/EOF.

--- blaze-client/src/main/scala/org/http4s/client/blaze/BlazeClient.scala
+++ blaze-client/src/main/scala/org/http4s/client/blaze/BlazeClient.scala
@@ -3,6 +3,7 @@ package client
 package blaze
 
 import org.http4s.blaze.pipeline.Command
+import org.log4s.getLogger
 
 import scala.concurrent.duration.Duration
 import scalaz.concurrent.Task
@@ -10,9 +11,19 @@ import scalaz.{-\/, \/-}
 
 /** Blaze client implementation */
 object BlazeClient {
+  private[this] val logger = getLogger
+
   def apply[A <: BlazeConnection](manager: ConnectionManager[A], idleTimeout: Duration, requestTimeout: Duration): Client = {
     Client(Service.lift { req =>
       val key = RequestKey.fromRequest(req)
+
+      // If we can't invalidate a connection, it shouldn't tank the subsequent operation,
+      // but it should be noisy.
+      def invalidate(connection: A): Task[Unit] =
+        manager.invalidate(connection).handle {
+          case e => logger.error(e)("Error invalidating connection")
+        }
+
       def loop(connection: A, flushPrelude: Boolean): Task[DisposableResponse] = {
         // Add the timeout stage to the pipeline
         val ts = new ClientTimeoutStage(idleTimeout, requestTimeout, bits.ClientTickWheel)
@@ -26,12 +37,14 @@ object BlazeClient {
             Task.now(DisposableResponse(r, dispose))
 
           case -\/(Command.EOF) =>
-            manager.invalidate(connection)
-            manager.borrow(key).flatMap(loop(_, flushPrelude))
+            invalidate(connection).flatMap { _ =>
+              loop(connection, flushPrelude)
+            }
 
           case -\/(e) =>
-            manager.invalidate(connection)
-            Task.fail(e)
+            invalidate(connection).flatMap { _ =>
+              Task.fail(e)
+            }
         }
       }
       val flushPrelude = !req.body.isHalt
