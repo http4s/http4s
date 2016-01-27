a350675232d132a598964990b891748cdef8653f
Ross A. Baker <ross@rossabaker.com>
2016-01-27 00:37:58 -0500

    Implement mountFilter for servlet backends.
    Closes #472.

---
+++ servlet/src/main/scala/org/http4s/servlet/DefaultFilter.scala
@@ -0,0 +1,22 @@
+package org.http4s.servlet
+
+import javax.servlet._
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
+/**
+  * Mainly a convenience for our servlet examples, but, hey, why not.
+  */
+trait DefaultFilter extends Filter {
+  override def init(filterConfig: FilterConfig): Unit = {}
+
+  override def destroy(): Unit = {}
+
+  override def doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain): Unit =
+    (request, response) match {
+      case (httpReq: HttpServletRequest, httpRes: HttpServletResponse) =>
+        doHttpFilter(httpReq, httpRes, chain)
+      case _ =>
+    }
+
+  def doHttpFilter(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain): Unit
+}
