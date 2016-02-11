2bda93cc14afc11e175e255f148ac57e69417d6f
Ross A. Baker <ross@rossabaker.com>
2016-02-10 22:49:01 -0500

    writeTo* as Task was a bridge too far.

--- core/src/test/scala/org/http4s/EntityEncoderSpec.scala
+++ core/src/test/scala/org/http4s/EntityEncoderSpec.scala
@@ -16,20 +16,22 @@ import scalaz.stream.Process
 import util.byteVector._
 
 object EntityEncoderSpec {
-  def writeToString[A](a: A)(implicit W: EntityEncoder[A]): Task[String] =
+  def writeToString[A](a: A)(implicit W: EntityEncoder[A]): String =
     Process.eval(W.toEntity(a))
       .collect { case EntityEncoder.Entity(body, _ ) => body }
       .flatMap(identity)
       .fold1Monoid
       .pipe(utf8Decode)
       .runLastOr("")
+      .unsafePerformSync
 
-  def writeToByteVector[A](a: A)(implicit W: EntityEncoder[A]): Task[ByteVector] =
+  def writeToByteVector[A](a: A)(implicit W: EntityEncoder[A]): ByteVector =
     Process.eval(W.toEntity(a))
       .collect { case EntityEncoder.Entity(body, _ ) => body }
       .flatMap(identity)
       .fold1Monoid
       .runLastOr(ByteVector.empty)
+      .unsafePerformSync
 }
 
 class EntityEncoderSpec extends Http4sSpec {
@@ -37,11 +39,11 @@ class EntityEncoderSpec extends Http4sSpec {
 
   "EntityEncoder" should {
     "render strings" in {
-      writeToString("pong") must returnValue("pong")
+      writeToString("pong") must_== "pong"
     }
 
     "render single characters" in {
-      prop { char: Char => writeToString(char) must returnValue(Character.toString(char)) }
+      prop { char: Char => writeToString(char) must_== Character.toString(char) }
     }
 
     "calculate the content length of strings" in {
@@ -50,27 +52,27 @@ class EntityEncoderSpec extends Http4sSpec {
 
     "render byte arrays" in {
       val hello = "hello"
-      writeToString(hello.getBytes(StandardCharsets.UTF_8)) must returnValue(hello)
+      writeToString(hello.getBytes(StandardCharsets.UTF_8)) must_== hello
     }
 
     "render bytes" in {
-      prop { byte: Byte => writeToByteVector(byte) must returnValue(ByteVector(byte)) }
+      prop { byte: Byte => writeToByteVector(byte) must_== ByteVector(byte) }
     }
 
     "render futures" in {
       import scala.concurrent.ExecutionContext.Implicits.global
       val hello = "Hello"
-      writeToString(Future(hello)) must returnValue(hello)
+      writeToString(Future(hello)) must_== hello
     }
 
     "render Tasks" in {
       val hello = "Hello"
-      writeToString(Task.now(hello)) must returnValue(hello)
+      writeToString(Task.now(hello)) must_== hello
     }
 
     "render processes" in {
       val helloWorld = Process("hello", "world")
-      writeToString(helloWorld) must returnValue("helloworld")
+      writeToString(helloWorld) must_== "helloworld"
     }
 
     "render processes with chunked transfer encoding" in {
@@ -103,19 +105,19 @@ class EntityEncoderSpec extends Http4sSpec {
         val w = new FileWriter(tmpFile)
         try w.write("render files test")
         finally w.close()
-        writeToString(tmpFile) must returnValue("render files test")
+        writeToString(tmpFile) must_== "render files test"
       }
       finally tmpFile.delete()
     }
 
     "render input streams" in {
       val inputStream = new ByteArrayInputStream(("input stream").getBytes(StandardCharsets.UTF_8))
-      writeToString(inputStream) must returnValue("input stream")
+      writeToString(inputStream) must_== "input stream"
     }
 
     "render readers" in {
       val reader = new StringReader("string reader")
-      writeToString(reader) must returnValue("string reader")
+      writeToString(reader) must_== "string reader"
     }
 
     "give the content type" in {
