bbc03dbda85268595f17b4415b930d361a0c5a5b
Ross A. Baker <ross@rossabaker.com>
2016-02-14 21:21:39 -0500

    Upgrade to circe-0.3.0.

--- project/Http4sBuild.scala
+++ project/Http4sBuild.scala
@@ -46,7 +46,7 @@ object Http4sBuild extends Build {
   lazy val alpnBoot            = "org.mortbay.jetty.alpn"    % "alpn-boot"               % "8.1.4.v20150727"
   lazy val asyncHttp           = "org.asynchttpclient"       % "async-http-client"       % "2.0.0-RC7"
   lazy val blaze               = "org.http4s"               %% "blaze-http"              % "0.11.0"
-  lazy val circeJawn           = "io.circe"                 %% "circe-jawn"              % "0.2.0"
+  lazy val circeJawn           = "io.circe"                 %% "circe-jawn"              % "0.3.0"
   lazy val gatlingTest         = "io.gatling"                % "gatling-test-framework"  % "2.1.6"
   lazy val gatlingHighCharts   = "io.gatling.highcharts"     % "gatling-charts-highcharts" % gatlingTest.revision
   lazy val http4sWebsocket     = "org.http4s"               %% "http4s-websocket"        % "0.1.3"
