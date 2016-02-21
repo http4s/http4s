5ee2a9f371f85adbbbef64c04f04e6658234c5a6
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2016-02-21 13:13:52 -0700

    Improve error message for unsupported media type decoding error
    - List ALL supported media types when failing to decode using a composite decoder (build with `orElse`)
    - changes to message wording
    - use renderString on MediaRanges instead of standard case class to String to produce nicer message for logging
    - use logging message for http response since it does not contain any sensitive information (I think?)

--- core/src/main/scala/org/http4s/MessageFailure.scala
+++ core/src/main/scala/org/http4s/MessageFailure.scala
@@ -84,7 +84,7 @@ sealed case class InvalidMessageBodyFailure(details: String, override val cause:
 sealed abstract class UnsupportedMediaTypeFailure(expected: Set[MediaRange]) extends DecodeFailure with NoStackTrace {
   override def toHttpResponse(httpVersion: HttpVersion): Task[Response] =
     Response(Status.UnsupportedMediaType, httpVersion)
-      .withBody(s"""Please specify a media type in the following ranges: ${expected.mkString(",")}""")
+      .withBody(message)
 }
 
 /** Indicates that a [[Message]] attempting to be decoded has no [[MediaType]] and no
@@ -92,12 +92,17 @@ sealed abstract class UnsupportedMediaTypeFailure(expected: Set[MediaRange]) ext
 final case class MediaTypeMissing(expected: Set[MediaRange])
   extends UnsupportedMediaTypeFailure(expected)
 {
-  val message = s"Decoder is unable to decode a Message without a MediaType. Expected media ranges: $expected"
+  val expectedMsg = expected.map(_.renderString).mkString(", ")
+  val message = s"No media type specified in Content-Type header. Expected one of the following media ranges: $expectedMsg"
 }
 
 /** Indicates that no [[EntityDecoder]] matches the [[MediaType]] of the [[Message]] being decoded */
 final case class MediaTypeMismatch(messageType: MediaType, expected: Set[MediaRange])
   extends UnsupportedMediaTypeFailure(expected)
 {
-  def message = s"$messageType is not a supported media type. Decoder accepts one of the following media ranges: $expected"
+  def message = {
+    val actual = messageType.renderString
+    val expectedMsg = expected.map(_.renderString).mkString(", ")
+    s"$actual is not a supported media type. Expected one of the following media ranges: $expectedMsg"
+  }
 }
