b1da10e5e19f55eaac993f4f82d7dbbe4cbdc867
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2016-02-21 15:09:07 -0700

    [SI-527] Remove QValue from MediaRange and MediaType
    Instead define `MediaRangeAndQValue` for use with the `Accept` header

--- dsl/src/test/scala/org/http4s/dsl/ResponseGeneratorSpec.scala
+++ dsl/src/test/scala/org/http4s/dsl/ResponseGeneratorSpec.scala
@@ -1,6 +1,5 @@
 package org.http4s.dsl
 
-import org.http4s.Header
 import org.http4s.headers.{Accept, `Content-Length`, `Content-Type`}
 import org.http4s.Headers
 import org.http4s.MediaRange
@@ -13,18 +12,18 @@ class ResponseGeneratorSpec extends Specification {
   "Add the EntityEncoder headers along with a content-length header" in {
     val body = "foo"
     val resultheaders = Ok(body)(EntityEncoder.stringEncoder).run.headers
-    EntityEncoder.stringEncoder.headers.foldLeft(true must_== true) { (old, h) =>
-      old and (resultheaders.exists(_ == h) must_== true)
+    EntityEncoder.stringEncoder.headers.foldLeft(ok) { (old, h) =>
+      old and (resultheaders.exists(_ == h) must_=== true)
     }
 
-    resultheaders.get(`Content-Length`) must_== Some(`Content-Length`(body.getBytes().length))
+    resultheaders.get(`Content-Length`) must_=== Some(`Content-Length`(body.getBytes().length))
   }
 
   "Not duplicate headers when not provided" in {
     val w = EntityEncoder.encodeBy[String](EntityEncoder.stringEncoder.headers.put(Accept(MediaRange.`audio/*`)))(
                             EntityEncoder.stringEncoder.toEntity(_))
 
-    Ok("foo")(w).run.headers.get(Accept).get.values.list must_== List(MediaRange.`audio/*`)
+    Ok("foo")(w).run.headers.get(Accept).get.values.list must_=== List(MediaRange.`audio/*`)
   }
 
   "Explicitly added headers have priority" in {
@@ -33,7 +32,7 @@ class ResponseGeneratorSpec extends Specification {
     )
 
     Ok("foo", Headers(`Content-Type`(MediaType.`application/json`)))(w)
-      .run.headers.get(`Content-Type`).get must_== `Content-Type`(MediaType.`application/json`)
+      .run.headers.get(`Content-Type`) must_=== Some(`Content-Type`(MediaType.`application/json`))
   }
 
 }
