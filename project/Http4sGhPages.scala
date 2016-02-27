72f8625805bbac703669fa331181946c330907d4
Ross A. Baker <ross@rossabaker.com>
2016-02-27 10:38:28 -0500

    Strip tabs.

--- project/Http4sGhPages.scala
+++ project/Http4sGhPages.scala
@@ -10,21 +10,21 @@ import Http4sBuild.apiVersion
 // Copied from sbt-ghpages to avoid blowing away the old API
 // https://github.com/sbt/sbt-ghpages/issues/10
 object Http4sGhPages {
-	def cleanSiteForRealz(dir: File, git: GitRunner, s: TaskStreams, apiVersion: (Int, Int)): Unit ={
-		val toClean = IO.listFiles(dir).collect {
-			case f if f.getName == "api" => new java.io.File(f, s"${apiVersion._1}.${apiVersion._2}")
-			case f if f.getName != ".git" => f
-		}.map(_.getAbsolutePath).toList
-		if (!toClean.isEmpty)
-			git(("rm" :: "-r" :: "-f" :: "--ignore-unmatch" :: toClean) :_*)(dir, s.log)
-		()
-	}
+  def cleanSiteForRealz(dir: File, git: GitRunner, s: TaskStreams, apiVersion: (Int, Int)): Unit ={
+    val toClean = IO.listFiles(dir).collect {
+      case f if f.getName == "api" => new java.io.File(f, s"${apiVersion._1}.${apiVersion._2}")
+      case f if f.getName != ".git" => f
+    }.map(_.getAbsolutePath).toList
+    if (!toClean.isEmpty)
+      git(("rm" :: "-r" :: "-f" :: "--ignore-unmatch" :: toClean) :_*)(dir, s.log)
+    ()
+  }
 
-	def cleanSite0 = (updatedRepository, gitRunner, streams, Http4sBuild.apiVersion) map { (dir, git, s, v) =>
-		cleanSiteForRealz(dir, git, s, v)
-	}
+  def cleanSite0 = (updatedRepository, gitRunner, streams, Http4sBuild.apiVersion) map { (dir, git, s, v) =>
+    cleanSiteForRealz(dir, git, s, v)
+  }
 
-	def synchLocal0 = (privateMappings, updatedRepository, ghpagesNoJekyll, gitRunner, streams, apiVersion) map {
+  def synchLocal0 = (privateMappings, updatedRepository, ghpagesNoJekyll, gitRunner, streams, apiVersion) map {
     (mappings, repo, noJekyll, git, s, v) =>
       // TODO - an sbt.Synch with cache of previous mappings to make this more efficient. */
       val betterMappings = mappings map { case (file, target) => (file, repo / target) }
@@ -34,5 +34,5 @@ object Http4sGhPages {
       IO.copy(betterMappings)
       if(noJekyll) IO.touch(repo / ".nojekyll")
       repo
-	}
+  }
 }
