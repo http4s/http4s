9a9c337839782d7d5888074f084a4e363c48448b
Bryce Anderson <bryce.anderson22@gmail.com>
2016-03-08 08:54:33 -0500

    Clean up a doc string for `HttpService`

--- core/src/main/scala/org/http4s/HttpService.scala
+++ core/src/main/scala/org/http4s/HttpService.scala
@@ -35,8 +35,9 @@ object HttpService {
   /** The default 'Not Found' response used when lifting a partial function
     * to a [[HttpService]] or general 'not handled' results.
     *
-    * This particular instance is tagged with an so that it interacts appropriately
-    * attribute to play well with the default [[Fallthrough]] behavior.
+    * This [[Response]] is tagged with the [[Fallthrough]] attribute so composed
+    * services will have the opportunity to handle the request.
+    * See [[Fallthrough]] for more details.
     */
   val notFound: Task[Response] = Task.now(Response(Status.NotFound)
                                              .withAttribute(Fallthrough.fallthroughKey, ())
