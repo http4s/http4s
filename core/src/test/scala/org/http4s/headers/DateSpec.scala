f9812412bf859261828b7d3dd8eef243e594fe4b
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2016-08-15 18:33:18 -0400

    Format Date header using the GMT timezone

--- core/src/test/scala/org/http4s/headers/DateSpec.scala
+++ core/src/test/scala/org/http4s/headers/DateSpec.scala
@@ -1,6 +1,20 @@
 package org.http4s
 package headers
 
+import java.time.{Instant, ZoneId, ZonedDateTime}
+
 class DateSpec extends HeaderLaws {
   checkAll("Date", headerLaws(Date))
+
+  val gmtDate = ZonedDateTime.of(1994, 11, 6, 8, 49, 37, 0, ZoneId.of("GMT"))
+
+  "render" should {
+    "format GMT date according to RFC 1123" in {
+      Date(Instant.from(gmtDate)).renderString must_== "Date: Sun, 06 Nov 1994 08:49:37 GMT"
+    }
+    "format UTC date according to RFC 1123" in {
+      val utcDate = ZonedDateTime.of(1994, 11, 6, 8, 49, 37, 0, ZoneId.of("UTC"))
+      Date(Instant.from(utcDate)).renderString must_== "Date: Sun, 06 Nov 1994 08:49:37 GMT"
+    }
+  }
 }
