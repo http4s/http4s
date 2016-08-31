c587f2d5106f242667a46431e51901798fe8eecf
Andrew Mohrland <amohrland@gmail.com>
2016-08-31 09:11:01 -0700

    use implicit instead of explicit nospaces

--- zipkin-core/src/main/scala/org/http4s/zipkin/core/interpreters/collector/Http.scala
+++ zipkin-core/src/main/scala/org/http4s/zipkin/core/interpreters/collector/Http.scala
@@ -1,16 +1,17 @@
 package org.http4s.zipkin.core.interpreters.collector
 
 import argonaut.Argonaut._
+import org.http4s.argonaut._
 import org.http4s.Uri.RegName
 import org.http4s.client.Client
 import org.http4s.zipkin.core.algebras.Collector
-import org.http4s.zipkin.core.interpreters._
 import org.http4s.zipkin.core.ZipkinInfo
 import org.http4s.{MediaType, Method, Request, Uri}
 
 import scalaz.concurrent.Task
 
 final case class Http(client: Client) extends Collector {
+  private[this] val logger = org.log4s.getLogger
 
   override def send(zipkinInfo: ZipkinInfo): Unit = {
     val uri = Uri(
@@ -20,7 +21,7 @@ final case class Http(client: Client) extends Collector {
       )),
       path = "/api/v1/spans"
     )
-    val payload = List(zipkinInfo).asJson.nospaces
+    val payload = List(zipkinInfo).asJson
     val request = Request(Method.POST, uri)
       .withContentType(Option(MediaType.`application/json`))
       .withBody(payload)
