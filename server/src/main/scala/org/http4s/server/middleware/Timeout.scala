2130833da21ca02d54e4139eb0de0e1d5c7c5122
Cody Allen <cody.allen@one.verizon.com>
2016-09-30 11:01:40 -0400

    Allow custom scheduler for timeout middleware
    This is an attempt to fix #726.

--- server/src/main/scala/org/http4s/server/middleware/Timeout.scala
+++ server/src/main/scala/org/http4s/server/middleware/Timeout.scala
@@ -2,23 +2,27 @@ package org.http4s
 package server
 package middleware
 
+import org.http4s.util.threads.threadFactory
+
 import scala.concurrent.duration._
 import scalaz.concurrent.Task
-import java.util.concurrent.{TimeUnit, ScheduledThreadPoolExecutor}
+import java.util.concurrent.{Executors, ScheduledExecutorService, TimeUnit}
 import scalaz.\/-
 import scalaz.syntax.monad._
 
 object Timeout {
 
   // TODO: this should probably be replaced with a low res executor to save clock cycles
-  private val ec = new ScheduledThreadPoolExecutor(1)
+  private val defaultScheduler: ScheduledExecutorService = Executors.newScheduledThreadPool(
+    1, threadFactory(name = l => s"http4s-timeout-scheduler-$l", daemon = true))
 
   val DefaultTimeoutResponse = Response(Status.InternalServerError)
     .withBody("The service timed out.")
 
-  private def timeoutResp(timeout: Duration, response: Task[Response]): Task[Response] = Task.async[Task[Response]] { cb =>
+  private def timeoutResp(timeout: Duration, response: Task[Response], customScheduler: Option[ScheduledExecutorService]): Task[Response] = Task.async[Task[Response]] { cb =>
     val r = new Runnable { override def run(): Unit = cb(\/-(response)) }
-    ec.schedule(r, timeout.toNanos, TimeUnit.NANOSECONDS)
+    val scheduler = customScheduler.getOrElse(defaultScheduler)
+    scheduler.schedule(r, timeout.toNanos, TimeUnit.NANOSECONDS)
     ()
   }.join
 
@@ -33,15 +37,18 @@ object Timeout {
 
   /** Transform the service to return a RequestTimeOut [[Status]] after the supplied Duration
     * @param timeout Duration to wait before returning the RequestTimeOut
+    * @param customScheduler a custom scheduler to use for timing out the request.
+    *   If `None` is provided, then a default daemon thread scheduler will be used.
     * @param service [[HttpService]] to transform
     */
-  def apply(timeout: Duration, response: Task[Response] = DefaultTimeoutResponse)(service: HttpService): HttpService = {
-    if (timeout.isFinite()) apply(timeoutResp(timeout, response))(service)
+  def apply(timeout: Duration, response: Task[Response] = DefaultTimeoutResponse, customScheduler: Option[ScheduledExecutorService] = None)(service: HttpService): HttpService = {
+    if (timeout.isFinite()) apply(timeoutResp(timeout, response, customScheduler))(service)
     else service
   }
 
   /** Transform the service to return a RequestTimeOut [[Status]] after 30 seconds
     * @param service [[HttpService]] to transform
     */
+  @deprecated("Use the overload that also has response and customScheduler parameters (with default values provided)", "0.15")
   def apply(service: HttpService): HttpService = apply(30.seconds)(service)
 }
