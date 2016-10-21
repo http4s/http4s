97987c95c98cb48d0c3055763a09a6f9b48a9551
Bjørn Madsen <bm@aeons.dk>
2016-10-21 13:12:20 +0200

    Update jawn module to use jawnfs2 instead of jawnstreamz

--- jawn/src/test/scala/org/http4s/jawn/JawnDecodeSupportSpec.scala
+++ jawn/src/test/scala/org/http4s/jawn/JawnDecodeSupportSpec.scala
@@ -2,26 +2,25 @@ package org.http4s
 package jawn
 
 trait JawnDecodeSupportSpec[J] extends Http4sSpec with JawnInstances {
-  def testJsonDecoder(decoder: EntityDecoder[J]) = {
+  def testJsonDecoder(decoder: EntityDecoder[J]) =
     "json decoder" should {
       "return right when the entity is valid" in {
-        val resp = Response(Status.Ok).withBody("""{"valid": true}""").run
-        decoder.decode(resp, strict = false).run.run must beXorRight
+        val resp = Response(Status.Ok).withBody("""{"valid": true}""").unsafeRun
+        decoder.decode(resp, strict = false).value.unsafeRun must beRight
       }
 
       "return a ParseFailure when the entity is invalid" in {
-        val resp = Response(Status.Ok).withBody("""garbage""").run
-        decoder.decode(resp, strict = false).run.run must beXorLeft.like {
+        val resp = Response(Status.Ok).withBody("""garbage""").unsafeRun
+        decoder.decode(resp, strict = false).value.unsafeRun must beLeft.like {
           case MalformedMessageBodyFailure("Invalid JSON", _) => ok
         }
       }
 
       "return a ParseFailure when the entity is empty" in {
-        val resp = Response(Status.Ok).withBody("").run
-        decoder.decode(resp, strict = false).run.run must beXorLeft.like {
+        val resp = Response(Status.Ok).withBody("").unsafeRun
+        decoder.decode(resp, strict = false).value.unsafeRun must beLeft.like {
           case MalformedMessageBodyFailure("Invalid JSON: empty body", _) => ok
         }
       }
     }
-  }
 }
