58f3c429d4728a721fb98440ce068718ae4ffaae
Bjørn Madsen <bm@aeons.dk>
2016-10-24 10:14:58 +0200

    Port argonaut module to fs2

--- project/Http4sBuild.scala
+++ project/Http4sBuild.scala
@@ -53,12 +53,8 @@ object Http4sBuild {
     }
 
   lazy val alpnBoot            = "org.mortbay.jetty.alpn"    % "alpn-boot"               % "8.1.9.v20160720"
-  def argonaut(scalazVersion: String) = "io.argonaut"       %% "argonaut"                % scalazCrossBuild("6.1", scalazVersion)
-  def argonautShapeless(scalazVersion: String) = "com.github.alexarchambault" %% "argonaut-shapeless_6.1" %
-    (VersionNumber(scalazVersion).numbers match {
-      case Seq(7, 1, _*) => "1.1.0"
-      case Seq(7, 2, _*) => "1.1.1"
-    })
+  lazy val argonaut            = "io.argonaut"              %% "argonaut"                % "6.2-M3"
+  lazy val argonautShapeless   = "com.github.alexarchambault" %%"argonaut-shapeless"     % "1.2.0-M1"
   lazy val asyncHttpClient     = "org.asynchttpclient"       % "async-http-client"       % "2.0.12"
   lazy val blaze               = "org.http4s"               %% "blaze-http"              % "0.12.1"
   lazy val catsKernelLaws      = "org.typelevel"            %% "cats-kernel-laws"        % catsLaws.revision
