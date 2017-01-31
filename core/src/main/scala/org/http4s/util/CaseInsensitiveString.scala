0314abf8a17f6c65dbdb7fa35fd4abe72fccf215
Ross A. Baker <ross@rossabaker.com>
2017-01-31 15:03:00 -0500

    Cache CaseInsensitiveString hashCode without lazy
    We don't even need to keep the folded String around.

--- core/src/main/scala/org/http4s/util/CaseInsensitiveString.scala
+++ core/src/main/scala/org/http4s/util/CaseInsensitiveString.scala
@@ -5,9 +5,14 @@ import java.util.Locale
 sealed class CaseInsensitiveString private (val value: String) extends CharSequence {
   import CaseInsensitiveString._
 
-  private lazy val folded = value.toLowerCase(Locale.ROOT)
+  private var hash = 0
 
-  override def hashCode(): Int = folded.##
+  override def hashCode(): Int = {
+    if (hash == 0 && value.length > 0) {
+      hash = value.toLowerCase(Locale.ROOT).hashCode
+    }
+    hash
+  }
 
   override def equals(obj: Any): Boolean = obj match {
     case that: CaseInsensitiveString => value.equalsIgnoreCase(that.value)
