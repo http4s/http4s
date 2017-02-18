d5df4c69cf9d7d31bcadc1b2b0b5e4a9254c03d1
Brad Fritz <brad@fewerhassles.com>
2017-02-17 20:10:40 -0500

    Remove blaze-client calls to github.com
    Leaves one external SSL call and replaces the other specs with
    calls to an internal Jetty server.  Hoping this will reduce the
    number of Travis CI timeouts while talking to external services.
    
    This is suggestion #2 from @rossabaker [from Gitter](https://gitter.im/http4s/http4s?at=58a73413aa800ee52cabed70).
    
    Refs #817, #858

--- blaze-client/src/test/scala/org/http4s/client/blaze/ExternalBlazeHttp1ClientSpec.scala
+++ blaze-client/src/test/scala/org/http4s/client/blaze/ExternalBlazeHttp1ClientSpec.scala
@@ -1,7 +1,6 @@
 package org.http4s.client.blaze
 
 import scala.concurrent.duration._
-import scalaz.concurrent.Task
 
 import org.http4s._
 
@@ -22,31 +21,5 @@ class ExternalBlazeHttp1ClientSpec extends Http4sSpec {
 
   step {
     simpleClient.shutdown.run
-  }
-
-  private val pooledClient = PooledHttp1Client()
-
-  "RecyclingHttp1Client" should {
-    def fetchBody = pooledClient.toService(_.as[String]).local { uri: Uri => Request(uri = uri) }
-
-    "Make simple https requests" in {
-      val resp = fetchBody.run(uri("https://github.com/")).run
-      resp.length mustNotEqual 0
-    }
-
-    "Repeat a simple https request" in {
-      val f = (0 until 10).map(_ => Task.fork {
-        val resp = fetchBody.run(uri("https://github.com/"))
-        resp.map(_.length)
-      })
-
-      foreach(Task.gatherUnordered(f).run) { length =>
-        length mustNotEqual 0
-      }
-    }
-  }
-
-  step {
-    pooledClient.shutdown.run
   }
 }
