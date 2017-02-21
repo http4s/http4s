0d8bf28f7f918c3b476ef6f799efa6f84ac251cd
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-02-21 12:29:45 -0500

    Added option for compatibility with typelevel scala

--- project/Http4sBuild.scala
+++ project/Http4sBuild.scala
@@ -80,8 +80,8 @@ object Http4sBuild {
   lazy val quasiquotes         = "org.scalamacros"          %% "quasiquotes"             % "2.1.0"
   lazy val reactiveStreamsTck  = "org.reactivestreams"       % "reactive-streams-tck"    % "1.0.0"
   lazy val scalacheck          = "org.scalacheck"           %% "scalacheck"              % "1.13.4"
-  def scalaCompiler(sv: String) = "org.scala-lang"           % "scala-compiler"          % sv
-  def scalaReflect(sv: String) = "org.scala-lang"            % "scala-reflect"           % sv
+  def scalaCompiler(so: String, sv: String)     = so                      % "scala-compiler"            % sv
+  def scalaReflect(so: String, sv: String)      = so                      % "scala-reflect"             % sv
   lazy val scalaXml            = "org.scala-lang.modules"   %% "scala-xml"               % "1.0.5"
   def scalazCore(version: String)               = "org.scalaz"           %% "scalaz-core"               % version
   def scalazScalacheckBinding(version: String)  = "org.scalaz"           %% "scalaz-scalacheck-binding" % version
