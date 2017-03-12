8c08db5d41ebd844c628b6b4a1f6e81de6514df1
Andrew Gustafson <andrew.gustafson@digital.hmrc.gov.uk>
2017-03-12 22:21:39 +0000

    Refacrored

--- core/src/main/scala/org/http4s/util/package.scala
+++ core/src/main/scala/org/http4s/util/package.scala
@@ -4,7 +4,6 @@ import java.nio.{ByteBuffer, CharBuffer}
 
 import fs2._
 import fs2.util.Attempt
-import org.http4s.util.chunk
 import scodec.bits.ByteVector
 
 import scala.util.control.NonFatal
@@ -12,210 +11,30 @@ import scala.util.control.NonFatal
 package object util {
   def decode[F[_]](charset: Charset): Pipe[F, Byte, String] = {
     val decoder = charset.nioCharset.newDecoder
-    val avgBytesPerChar = math.ceil(1.0 / decoder.averageCharsPerByte().toDouble).toInt
     val maxCharsPerByte = math.ceil(decoder.maxCharsPerByte().toDouble).toInt
-    val bufferSize = 8 * avgBytesPerChar + 1
+    val avgBytesPerChar = math.ceil(1.0 / decoder.averageCharsPerByte().toDouble).toInt
+    val charBufferSize = 128
 
-    def push(chunkMaybe: Option[NonEmptyChunk[Byte]], carryOver: ByteVector): (ByteVector, Option[String]) = {
-      val (nextBuffer, eof) = chunkMaybe match {
-        case Some(chunk) =>
-          (ByteVector(chunk.toArray), false)
+    _.repeatPull[String] {
+      _.awaitN(charBufferSize * avgBytesPerChar, allowFewer = true).optional.flatMap {
         case None =>
-          (ByteVector.empty, true)
-      }
-      val in = carryOver ++ nextBuffer
-      val byteBuffer = in.toByteBuffer
-      val charBuffer = CharBuffer.allocate(in.size.toInt + 1)
-      decoder.decode(byteBuffer, charBuffer, eof)
-      val nextByteVector = if (eof) {
-        decoder.flush(charBuffer)
-        ByteVector.empty
-      } else {
-        ByteVector.view(byteBuffer.slice)
-      }
-      (nextByteVector, Some(charBuffer.flip().toString))
-    }
-
-    _.rechunkN(bufferSize, allowFewer = true)
-//      .chunks
-//      .map(chunk => ByteVector(chunk.toArray))
-//      .noneTerminate
-      .repeatPull[String] {
-        _.awaitN(bufferSize, allowFewer = true).optional.flatMap {
-          case None =>
-            val charBuffer = CharBuffer.allocate(bufferSize)
-            decoder.decode(ByteBuffer.allocate(0), charBuffer, true)
-            decoder.flush(charBuffer)
-            val outputString = charBuffer.flip().toString
-            if (outputString.isEmpty) Pull.done
-            else Pull.output1(outputString) as Handle.empty
-          case Some((chunks, handle)) =>
-            val chunk = chunks.flatMap(_.toList)
-            val byteVector = ByteVector(chunk.toArray)
-            val byteBuffer = byteVector.toByteBuffer
-            val charBuffer = CharBuffer.allocate(byteVector.size.toInt * maxCharsPerByte)
-            decoder.decode(byteBuffer, charBuffer, false)
-            val nextByteVector = ByteVector.view(byteBuffer.slice)
-            val nextHandle = handle.push(Chunk.bytes(nextByteVector.toArray))
-            Pull.output1(charBuffer.flip().toString) as nextHandle
-        }
-
-      /*
-        _.receive[String, Handle[F, (ByteVector, Byte)]] {
-          case (chunk, handle) =>
-            println(s"handling chunk ${new String(chunk.toArray)}")
-            val byteVector = ByteVector(chunk.toArray)
-            val byteBuffer = byteVector.toByteBuffer
-            val charBuffer = CharBuffer.allocate(byteVector.size.toInt + 1)
-            if (chunk.isEmpty) {
-              println(s"empty chunk")
-              decoder.decode(ByteBuffer.allocate(0), charBuffer, true)
-              decoder.flush(charBuffer)
-              Pull.done
-            } else {
-              decoder.decode(byteBuffer, charBuffer, false)
-              //            val nextHandle = if (eof) {
-              //              decoder.flush(charBuffer)
-              //              handle
-              //            } else {
-              //            }
-              val nextByteVector = ByteVector.view(byteBuffer.slice)
-              Pull.output1(charBuffer.flip().toString) as handle.map(nextByteVector -> _)
-            }
-            */
-
-
-          /*
-          case None =>
-            println("handling none")
-            val charBuffer = CharBuffer.allocate(bufferSize)
-            decoder.decode(ByteBuffer.allocate(0), charBuffer, true)
-            decoder.flush(charBuffer)
-            val outputString = charBuffer.flip().toString
-            println(s"flushed all: '$outputString'")
-            if (outputString.isEmpty) {
-              println("well we're done")
-              Pull.done
-            }
-            else Pull.output1(outputString) as Handle.empty
-          case Some((chunk, handle)) =>
-            println(s"handling chunk ${new String(chunk.toArray)}")
-            val byteVector = ByteVector(chunk.toArray)
-            val byteBuffer = byteVector.toByteBuffer
-            val charBuffer = CharBuffer.allocate(byteVector.size.toInt + 1)
-            if (chunk.isEmpty) {
-              println(s"empty chunk")
-              decoder.decode(ByteBuffer.allocate(0), charBuffer, true)
-              decoder.flush(charBuffer)
-              Pull.done
-            } else {
-              decoder.decode(byteBuffer, charBuffer, false)
-              //            val nextHandle = if (eof) {
-              //              decoder.flush(charBuffer)
-              //              handle
-              //            } else {
-              //            }
-              val nextByteVector = ByteVector.view(byteBuffer.slice)
-              val nextHandle = handle.push(Chunk.bytes(nextByteVector.toArray))
-              Pull.output1(charBuffer.flip().toString) as nextHandle
-            }
-            */
-      }
-      /*
-      .fold[(ByteVector, Option[String])]((ByteVector.empty, None)) {
-        case ((byteVector, strMaybe), chunkMaybe) =>
-          push(chunkMaybe, byteVector)
-      }
-      */
-    /*
-    val decoder = charset.nioCharset.newDecoder
-    var carryOver = ByteVector.empty
-
-    def push(chunk: ByteVector, eof: Boolean) = {
-      val in = carryOver ++ chunk
-      val byteBuffer = in.toByteBuffer
-      val charBuffer = CharBuffer.allocate(in.size.toInt + 1)
-      decoder.decode(byteBuffer, charBuffer, eof)
-      if (eof) decoder.flush(charBuffer)
-      else carryOver = ByteVector.view(byteBuffer.slice)
-      charBuffer.flip().toString
-    }
-
-    // A ByteVector can now be longer than Int.MaxValue, but the CharBuffer
-    // above cannot.  We need to split enormous chunks just in case.
-    def breakBigChunks(): Process1[ByteVector, ByteVector] =
-      receive1[ByteVector, ByteVector] { chunk =>
-        def loop(chunk: ByteVector): Process1[ByteVector, ByteVector] =
-          chunk.splitAt(Long.MaxValue - 1L) match {
-            case (bv, ByteVector.empty) =>
-              emit(bv) ++ breakBigChunks()
-            case (bv, tail) =>
-              emit(bv) ++ loop(tail)
-          }
-        loop(chunk)
-      }
-
-    def go(): Process1[ByteVector, String] = receive1[ByteVector, String] { chunk =>
-      val s = push(chunk, false)
-      val sChunk = if (s.nonEmpty) emit(s) else halt
-      sChunk ++ go()
-    }
-
-    def flush() = {
-      val s = push(ByteVector.empty, true)
-      if (s.nonEmpty) emit(s) else halt
-    }
-
-    breakBigChunks() pipe go() onComplete flush()
-  }
-    */
-
-
-
-
-      /*
-      .mapAccumulate[Option[ByteBuffer], String](None) {
-        case (remainingBytesMaybe, Some(chunk)) =>
-//          println(s"Handling Some(${chunk.toArray.mkString(",")}) with remaining bytes ${remainingBytesMaybe.map(_.duplicate().array().mkString(","))}")
-          //val byteBuffer = remainingBytesMaybe.map(remaining => ByteBuffer.wrap(remaining.array() ++ chunk.toArray[Byte])).getOrElse(ByteBuffer.wrap(chunk.toArray))
-          //val byteBuffer = remainingBytesMaybe.map(buffer => buffer.put(chunk.toArray, buffer.limit(), chunk.size)).getOrElse(ByteBuffer.wrap(chunk.toArray))
-          //val byteBuffer = remainingBytesMaybe.map(_.put(chunk.toArray)).getOrElse(ByteBuffer.wrap(chunk.toArray))
-          val byteBuffer = remainingBytesMaybe.map { remainingBuffer =>
-            val newBuffer = ByteBuffer.allocate(remainingBuffer.remaining() + chunk.size)
-            val out = newBuffer.put(remainingBuffer).put(chunk.toArray)
-            println(s"out is: ${out.array().mkString(",")}")
-            out
-//            val remainingBytes = Array.ofDim[Byte](buffer.remaining())
-//            ByteBuffer.wrap(buffer.get(remainingBytes) ++ buffer.put(chunk.toArray))
-          }.getOrElse(ByteBuffer.wrap(chunk.toArray))
-          println(s"pre-decode byte buffer details: limit ${byteBuffer.limit()}, position ${byteBuffer.position()}, array ${byteBuffer.array().mkString(",")}")
-          val charBuffer = CharBuffer.allocate(2 * byteBuffer.remaining() * maxCharsPerByte)
-//          println(s"decoding ${byteBuffer.duplicate().array().mkString(",")}")
-          val decodeResult = decoder.decode(byteBuffer, charBuffer, false)
-          val outputString = charBuffer.flip().toString
-          println(s"output string: '$outputString' from decode: $decodeResult")
-          println(s"post-decode byte buffer details: limit ${byteBuffer.limit()}, position ${byteBuffer.position()}, array ${byteBuffer.array().mkString(",")}\n")
-          (Some(byteBuffer).filter(_.hasRemaining), outputString)
-        case (remainingBytesMaybe, None) =>
-//          println(s"Handling None with remaining ${remainingBytesMaybe.map(_.duplicate().array().mkString(","))}")
-          val byteBuffer = remainingBytesMaybe.getOrElse(ByteBuffer.allocate(0))
-          val charBuffer = CharBuffer.allocate(2 * byteBuffer.remaining() * maxCharsPerByte)
-//          println(s"decoding ${byteBuffer.duplicate().array().mkString(",")}")
-          decoder.decode(byteBuffer, charBuffer, true)
+          val charBuffer = CharBuffer.allocate(1)
+          decoder.decode(ByteBuffer.allocate(0), charBuffer, true)
           decoder.flush(charBuffer)
-          (None, charBuffer.flip().toString)
-      }.map(_._2)
-      */
-      /*
-      .map { chunk =>
-        val byteBuffer = ByteBuffer.wrap(chunk.toArray)
-        val charBuffer = CharBuffer.allocate(chunk.size * maxCharsPerByte)
-        val result = decoder.decode(byteBuffer, charBuffer, false)
-        val flipped = charBuffer.flip()
-        println(s"result: $result, CharBuffer: $flipped. ByteBuffer has remaining ${byteBuffer.hasRemaining}")
-        flipped.toString
+          val outputString = charBuffer.flip().toString
+          if (outputString.isEmpty) Pull.done
+          else Pull.output1(outputString) as Handle.empty
+        case Some((chunks, handle)) =>
+          val chunk = chunks.flatMap(_.toList)
+          val byteVector = ByteVector(chunk.toArray)
+          val byteBuffer = byteVector.toByteBuffer
+          val charBuffer = CharBuffer.allocate(byteVector.size.toInt * maxCharsPerByte)
+          decoder.decode(byteBuffer, charBuffer, false)
+          val nextByteVector = ByteVector.view(byteBuffer.slice)
+          val nextHandle = handle.push(Chunk.bytes(nextByteVector.toArray))
+          Pull.output1(charBuffer.flip().toString) as nextHandle
       }
-      */
+    }
   }
 
   /** Constructs an assertion error with a reference back to our issue tracker. Use only with head hung low. */
--- tests/src/test/scala/org/http4s/util/DecodeSpec.scala
+++ tests/src/test/scala/org/http4s/util/DecodeSpec.scala
@@ -1,23 +1,12 @@
 package org.http4s
 package util
 
-import java.nio.{ByteBuffer, CharBuffer}
 import java.nio.charset.StandardCharsets
 
 import fs2._
 import fs2.text.utf8Decode
-import org.scalacheck.Gen
 
 class DecodeSpec extends Http4sSpec {
-  /*
-  0x8140 to 0xa0fe	Reserved for user-defined characters 造字
-  0xa140 to 0xa3bf	"Graphical characters" 圖形碼
-  0xa3c0 to 0xa3fe	Reserved, not for user-defined characters
-  0xa440 to 0xc67e	Frequently used characters 常用字
-  */
-//  val big5CharGen: Gen[Char] = ???
-  // Gen.infiniteStream(Gen.alphaChar).map(_.take(Int.MaxValue))
-
   "decode" should {
     "be consistent with utf8Decode" in prop { (s: String, chunkSize: Int) =>
       (chunkSize > 0) ==> {
@@ -52,75 +41,5 @@ class DecodeSpec extends Http4sSpec {
       }
     }
 
-
-    "handle simple stuff" in {
-      val inputString = "Hello"
-      val charset = Charset.`UTF-8`.nioCharset
-      val (input1, input2) = inputString.splitAt(2)
-      println(s"inputs are '$input1', '$input2'")
-      val inputBytes = inputString.getBytes(charset)
-      val decoder = charset.newDecoder
-
-      val cb1out = decoder.decode(ByteBuffer.wrap(input1.getBytes(charset)))
-      val cb2out = decoder.decode(ByteBuffer.wrap(input2.getBytes(charset)))
-
-      cb1out.toString must_== input1
-      cb2out.toString must_== input2
-    }
-
-    "handle chunked bytes" in { //prop { (cs: Charset, s: String/*, chunkSize: Int*/) =>
-      /*(
-        cs.nioCharset.canEncode && cs.nioCharset.name != "x-COMPOUND_TEXT" //&&
-        //chunkSize >= 1 && chunkSize <= (math.ceil(cs.nioCharset.newDecoder().maxCharsPerByte().toDouble) + 1)
-      ) ==> {*/
-//      prop { (chunkSize: Int) =>
-        //val inputString = "Hello"
-        //val cs = Charset.`UTF-16`
-        /*
-        val nioCharset = cs.nioCharset
-        val inputBytes = s.getBytes(nioCharset)
-        val source = Stream.emits(inputBytes)
-        val decoded = decode(cs, chunkSize)(source).toList.combineAll
-        decoded must_== new String(inputBytes, nioCharset)
-        */
-      val cs = Charset.fromNioCharset(java.nio.charset.Charset.forName("UTF-32"))
-      val s = "the quick brown fox jumped over the lazy dog"
-        println(s"charset $cs, max chars ${cs.nioCharset.newDecoder().maxCharsPerByte()}, avg chars ${cs.nioCharset.newDecoder().averageCharsPerByte()}")
-      val strBytes = s.getBytes(cs.nioCharset)
-      println(s"str bytes: ${strBytes.mkString(",")}")
-      val source: Stream[Pure, Byte] = Stream.chunk(Chunk.bytes(strBytes)).pure
-        val expected = new String(source.toVector.toArray, cs.nioCharset)
-        !expected.contains("\ufffd") ==> {
-          // \ufffd means we generated a String unrepresentable by the cs
-          val decoded = decode(cs)(source).toList.combineAll
-          decoded must_== expected
-        }
-      //}.setGen(Gen.chooseNum[Int](1, cs.nioCharset.newDecoder().maxCharsPerByte().toInt + 1))
-      //}
-    }.set(minTestsOk = 1)//.pretty1(charset => s"$charset (with max chars ${charset.nioCharset.newDecoder().maxCharsPerByte()})")
-      //.set(maxDiscardRatio = 10)
-      //.setGen1(Gen.const(Charset.fromString("UTF-32").right.get))
-      //.setGen2("Hello")
-
-    /*
-    "handle" in prop { (chars: scala.Stream[Char], chunkSize: Int) =>
-      val cs = Charset.fromString("Big5")
-      val nioCharset = cs.nioCharset
-      val source: Stream[Pure, Byte] = Stream.emits {
-        nioCharset.encode(CharBuffer.wrap(chars.toArray)).array()
-        //new String(chars.toArray).getBytes(cs.nioCharset)
-          .grouped(chunkSize)
-          .map(Chunk.bytes)
-          .toSeq
-      }.flatMap(Stream.chunk).pure
-      val expected = new String(source.toVector.toArray, nioCharset)
-      !expected.contains("\ufffd") ==> {
-        // \ufffd means we generated a String unrepresentable by the charset
-        val decoded = decode(cs)(source).toList.combineAll
-        println(s"$cs $chunkSize results for: $decoded")
-        decoded must_== expected
-      }
-    }.setGen1(big5CharGen).setGen2(Gen.posNum[Int])
-    */
   }
 }
