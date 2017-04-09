a7a3fc4aabeaad88d5d3e6644cef6edfd7124e31
Christopher Davenport <chris@christopherdavenport.tech>
2017-04-09 16:28:02 -0400

    Termination Tests, and Asynchronous Process Halting

--- tests/src/test/scala/org/http4s/util/ProcessAppSpec.scala
+++ tests/src/test/scala/org/http4s/util/ProcessAppSpec.scala
@@ -4,24 +4,20 @@ package util
 import scalaz.concurrent.Task
 import scalaz.stream.Process
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+
 class ProcessAppSpec extends Http4sSpec {
 
   "ProcessApp" should {
-    "Terminate Server on a Initial Process" in {
+    "Terminate Server on a Process Failure" in {
       val myError = new Throwable("Bad Initial Process")
       class TestApp extends ProcessApp {
         override def main(args: List[String]): Process[Task, Unit] = {
           Process.fail(myError)
-        }
-      }
-      new TestApp().main(Array.empty[String]) should_== (())
-    }
-
-    "Terminate Server on a Bad Second Value" in {
-      val myError = new Throwable("Bad Secondary Process")
-      class TestApp extends ProcessApp {
-        override def main(args: List[String]): Process[Task, Unit] = {
-          Process.emit(()).append(Process.fail(myError))
         }
       }
       new TestApp().main(Array.empty[String]) should_== (())
@@ -36,7 +32,7 @@ class ProcessAppSpec extends Http4sSpec {
       new TestApp().main(Array.empty[String]) should_== (())
     }
 
-    "Terminate Server on Bad Task" in {
+    "Terminate Server on a Bad Task" in {
       val myError = new Throwable("Bad Task")
       class TestApp extends ProcessApp {
         override def main(args: List[String]): Process[Task, Unit] = {
@@ -46,13 +42,27 @@ class ProcessAppSpec extends Http4sSpec {
       new TestApp().main(Array.empty[String]) should_== (())
     }
 
-    "Terminate On A Valid Task" in {
+    "Terminate Server on a Valid Task" in {
+      var touched = false
       class TestApp extends ProcessApp {
         override def main(args: List[String]): Process[Task, Unit] = {
-          Process.emit(Task("Valid Task"))
+          Process.eval(Task{touched = true; ()})
         }
       }
       new TestApp().main(Array.empty[String]) should_== (())
+    }
+
+    "requestShutdown Shuts Down a Server From A Separate Thread" in {
+      class TestApp extends ProcessApp {
+        override def main(args: List[String]): Process[Task, Unit] = {
+          Process.constant(())
+        }
+      }
+      val testApp = new TestApp()
+      val testAppF = Future(testApp.main(Array.empty[String]))
+      testApp.requestShutdown.unsafePerformSync
+
+      Await.result(testAppF, 60 seconds) should_== (())
     }
   }
 
