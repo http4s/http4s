dcaabf3cae2f65750b35d85c7645b02330b775ea
Andrew Gustafson <andrew.gustafson@digital.hmrc.gov.uk>
2017-04-19 10:58:27 +1000

    Update ChunkAggregator to use fs2; add ChunkAggregatorSpec

--- server/src/main/scala/org/http4s/server/middleware/ChunkAggregator.scala
+++ server/src/main/scala/org/http4s/server/middleware/ChunkAggregator.scala
@@ -1,43 +1,20 @@
-/* TODO fs2 port what replaces unemit?
 package org.http4s
 package server
 package middleware
 
-import scala.annotation.tailrec
-
 import fs2._
+import fs2.interop.cats._
 import org.http4s.headers._
 
 object ChunkAggregator {
-
-  @tailrec
-  private[ChunkAggregator] def reduce(acc: Chunk[Byte], chunks: Seq[Chunk[Byte]]): List[Chunk[Byte]] = {
-    println(s"reducing...")
-    if (chunks.tail.nonEmpty) reduce(acc ++ chunks.head, chunks.tail)
-    else (acc ++ chunks.head) :: Nil
-  }
-
-  private[ChunkAggregator] def compact(body: EntityBody): List[ByteVector] = {
-    val (chunks, tail) = body.unemit
-    println(s"unemitted: $chunks; $tail")
-    if (chunks.nonEmpty &&
-      tail.isInstanceOf[Halt] &&
-      (tail.asInstanceOf[Halt].cause eq End)) reduce(ByteVector.empty, chunks)
-    else Nil
-  }
-
-  def apply(service: HttpService): HttpService = service.map {
+  def apply(service: HttpService): HttpService = service.flatMapF[MaybeResponse] {
     case response: Response =>
-      val chunks = compact(response.body)
-      println(s"chunks: non-empty ${chunks.nonEmpty}")
-      if (chunks.nonEmpty) {
-        val length = chunks.head.length
-        println(s"length is: $length")
-        val h = response.headers.put(`Content-Length`(length))
-        response.copy(body = emitAll(chunks), headers = h)
+      response.body.runLog.map { bytes =>
+        if (bytes.nonEmpty) {
+          val h = response.headers.put(`Content-Length`(bytes.length.toLong))
+          response.copy(body = Stream.emits(bytes), headers = h)
+        } else response
       }
-      else response
-    case Pass => Pass
+    case Pass => Task.now(Pass)
   }
 }
-*/
--- server/src/test/scala/org/http4s/server/middleware/ChunkAggregatorSpec.scala
+++ server/src/test/scala/org/http4s/server/middleware/ChunkAggregatorSpec.scala
@@ -1,49 +1,52 @@
 package org.http4s.server.middleware
 
+import fs2._
 import org.http4s._
 import org.http4s.dsl._
-import org.http4s.headers.`Content-Length`
-import org.http4s.util.CaseInsensitiveString
 import org.specs2.matcher.MatchResult
-import scodec.bits.ByteVector
-
-import scalaz.stream.Cause.End
-import scalaz.stream.Process
-import scalaz.stream.Process.{Emit, Halt}
 
 class ChunkAggregatorSpec extends Http4sSpec {
 
   "ChunkAggregator" should {
     "handle an empty body" in {
       checkResponse(EmptyBody) { response =>
-        response.body.runLog.unsafePerformSync must_=== Vector.empty
+        response.body.runLog.unsafeValue() must_=== Some(Vector.empty)
       }
+    }
+
+    "handle a Pass" in {
+      val service: HttpService = HttpService.lift(_ => Pass.now)
+      ChunkAggregator(service).run(Request()).unsafeValue() must beSome(Pass)
     }
 
     "handle a single chunk" in {
       val str = encodeUtf8String("hiya")
-      checkResponse(Process.emit(str)) { response =>
-        response.body.runLog.unsafePerformSync must_=== Vector(str)
+      checkResponse(Stream.emits(str)) { response =>
+        response.contentLength must_=== Some(4L)
+        response.body.runLog.unsafeValue() must_=== Some(str.toVector)
       }
     }
 
     "handle multiple chunks" in {
-      val strings = Seq("the", " quick", " brown", " fox").map(encodeUtf8String)
-      checkResponse(Process.emitAll(strings)) { response =>
-        //response.headers.get(CaseInsensitiveString("Content-Length")) must_== Some(`Content-Length`(19L))
-        println(response.headers)
-        response.body.runLog.unsafePerformSync must_=== Vector(encodeUtf8String("the quick brown fox"))
+      val strings = Seq("the", " quick", " brown", " fox").map(str => Stream.emits[Task, Byte](encodeUtf8String(str)))
+      val body = strings.tail.foldLeft(strings.head)(_ ++ _)
+      checkResponse(body) { response =>
+        response.contentLength must_=== Some(19L)
+        response.body.runLog.unsafeValue() must_=== Some(encodeUtf8String("the quick brown fox").toVector)
       }
     }
   }
 
   def checkResponse(body: EntityBody)(responseCheck: Response => MatchResult[Any]): MatchResult[Any] = {
-    val service: HttpService = HttpService.lift { _ => Ok().withBody(body ++ Process.halt) }
-    val response = ChunkAggregator(service).run(Request()).unsafePerformSync.orNotFound
-    response.status must_== Ok
-    responseCheck(response)
+    val service: HttpService = HttpService.lift { _ => Ok().withBody(body) }
+    ChunkAggregator(service).run(Request()).unsafeValue() must beSome.like {
+      case maybeResponse: MaybeResponse =>
+        val response = maybeResponse.orNotFound
+        response.status must_== Ok
+        responseCheck(response)
+    }
   }
 
-  def encodeUtf8String(str: String): ByteVector = ByteVector.encodeUtf8(str).fold(throw _, identity)
+  def encodeUtf8String(str: String): Seq[Byte] = str.getBytes.toSeq
 
 }
