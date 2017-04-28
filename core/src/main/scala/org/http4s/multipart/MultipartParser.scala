c03a33556ce5ce576235f3a8d4d52b07e497dbe2
Ross A. Baker <ross@rossabaker.com>
2017-04-28 02:35:27 -0400

    Fix various issues with multipart parser

--- core/src/main/scala/org/http4s/multipart/MultipartParser.scala
+++ core/src/main/scala/org/http4s/multipart/MultipartParser.scala
@@ -5,7 +5,7 @@ import scala.language.postfixOps
 
 import scodec.bits.ByteVector
 
-import scalaz.{-\/, \/-, \/}
+import scalaz.{-\/, \/-, \/, State}
 import scalaz.stream.{process1, Process, Process1, Writer1}
 
 /** A low-level multipart-parsing pipe.  Most end users will prefer EntityDecoder[Multipart]. */
@@ -19,7 +19,10 @@ object MultipartParser {
 
   private final case class Out[+A](a: A, tail: Option[ByteVector] = None)
 
-  def parse(boundary: Boundary): Writer1[Headers, ByteVector, ByteVector] = {
+  def parse(boundary: Boundary): Writer1[Headers, ByteVector, ByteVector] =
+    parse(boundary, 10 * 1024)
+
+  def parse(boundary: Boundary, headerLimit: Long): Writer1[Headers, ByteVector, ByteVector] = {
     val boundaryBytes = boundary.toByteVector
     val startLine = DASHDASH ++ boundaryBytes
     val endLine = startLine ++ DASHDASH
@@ -51,6 +54,10 @@ object MultipartParser {
       leading map handle getOrElse receive1(handle)
     }
 
+    def takeUpTo(n: Long, pf: => ParseFailure): Process1[ByteVector, ByteVector] =
+      if (n > 0) receive1(bv => emit(bv) ++ takeUpTo(n - bv.size, pf))
+      else fail(pf)
+
     def receiveCollapsedLine(leading: Option[ByteVector]): Process1[ByteVector, Out[ByteVector]] = {
       receiveLine(leading) pipe process1.fold(Out(ByteVector.empty)) {
         case (acc, -\/(partial)) => acc.copy(acc.a ++ partial)
@@ -59,6 +66,25 @@ object MultipartParser {
     }
 
     def start: Writer1[Headers, ByteVector, ByteVector] = {
+      val partHeaderTooLong =
+        ParseFailure("Invalid multipart entity", s"Part header was longer than ${headerLimit}-byte limit")
+
+      logger.info("Exepected " + expected)
+
+      def preamble(leading: Option[ByteVector]): Process1[ByteVector, Option[ByteVector]] =
+        body(leading.map(_.compact), expected).flatMap {
+          case Out(chunk, None) =>
+            logger.debug(s"preamble chunk: $chunk")
+            halt
+          case Out(ByteVector.empty, tail) =>
+            logger.trace(s"Resuming preamble with $tail.")
+            emit(tail)
+          case Out(chunk, tail) =>
+            logger.debug(s"Last preamble chunk: $chunk.")
+            logger.trace(s"Resuming with $tail.")
+            emit(tail)
+        }
+
       def beginPart(leading: Option[ByteVector]): Process1[ByteVector, Option[ByteVector]] = {
         def isStartLine(line: ByteVector): Boolean =
           line.startsWith(startLine) && isTransportPadding(line.drop(startLine.size))
@@ -78,22 +104,32 @@ object MultipartParser {
             logger.debug("Found end line. Halting.")
             halt
           }
-          else {
-            logger.trace(s"Discarding prelude: $line")
+          else if (line.nonEmpty) {
+            logger.trace(s"Discarding preamble: $line")
             beginPart(tail)
+          }
+          else {
+            halt
           }
         }
       }
 
-      def go(leading: Option[ByteVector]): Writer1[Headers, ByteVector, ByteVector] = {
-        for {
+      def headers(leading: Option[ByteVector]): Process1[ByteVector, Out[Headers]] = {
+        takeUpTo(headerLimit, partHeaderTooLong) pipe (for {
           tail <- beginPart(leading)
           headerPair <- header(tail, expected)
           Out(headers, tail2) = headerPair
           _ = logger.debug(s"Headers: $headers")
           spacePair <- receiveCollapsedLine(tail2)
           tail3 = spacePair.tail // eat the space between header and content
-          part <- emit(-\/(headers)) ++ body(tail3.map(_.compact), expected).flatMap {
+        } yield Out(headers, tail3))
+      }
+
+      def go(leading: Option[ByteVector]): Writer1[Headers, ByteVector, ByteVector] = {
+        for {
+          headerPair <- headers(leading)
+          Out(headers, tail) = headerPair
+          part <- emit(-\/(headers)) ++ body(tail.map(_.compact), expected).flatMap {
             case Out(chunk, None) =>
               logger.debug(s"Chunk: $chunk")
               emit(\/-(chunk))
@@ -108,7 +144,11 @@ object MultipartParser {
         } yield part
       }
 
-      go(None)
+      // RFC1341 says we can tolerate a multipart body that begins
+      // with an enacpsulation line NOT initiated by a CRLF.  If we
+      // supply our own to start the preamble, we are tolerant.  If
+      // it's in the body, it gets eaten as part of the preamble.
+      preamble(Some(CRLF)).flatMap(go)
     }
 
     def header(leading: Option[ByteVector], expected: ByteVector): Process1[ByteVector, Out[Headers]] = {
@@ -165,7 +205,7 @@ object MultipartParser {
        * @param remainder the part of the next boundary we're looking for on the next read.
        */
       def mid(found: ByteVector, remainder: ByteVector): Process1[ByteVector, Out[ByteVector]] = {
-        logger.trace(s"mid: $found remainder")
+        logger.trace(s"mid: $found $remainder")
         if (remainder.isEmpty) {
           // found should start with a CRLF, because mid is called with it.
           emit(Out(ByteVector.empty, Some(found.drop(2))))
@@ -173,6 +213,7 @@ object MultipartParser {
           receive1Or[ByteVector, Out[ByteVector]](
             fail(new MalformedMessageBodyFailure("Part was not terminated"))) { bv =>
             val (remFront, remBack) = remainder splitAt bv.length
+            logger.trace(s"remFront = $remFront; remBack = $remBack; bv = $bv")
             if (bv startsWith remFront) {
               // If remBack is nonEmpty, then the progress toward our match
               // is represented by bv, and we'll loop back to read more to
