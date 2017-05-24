30b0facd9d135d30bcf77e64c04457fbe9fc194a
Bjørn Madsen <bm@aeons.dk>
2017-05-25 00:36:35 +0200

    Twirl module compiles and tests

--- twirl/src/test/scala/org/http4s/twirl/TwirlSpec.scala
+++ twirl/src/test/scala/org/http4s/twirl/TwirlSpec.scala
@@ -1,11 +1,11 @@
 package org.http4s
 package twirl
 
-import org.scalacheck.Arbitrary
-import org.scalacheck.Gen
-import play.twirl.api.{JavaScript, Html, Txt, Xml}
-import headers.`Content-Type`
-import Status.Ok
+import cats.effect.IO
+import org.http4s.Status.Ok
+import org.http4s.headers.`Content-Type`
+import org.scalacheck.{Arbitrary, Gen}
+import play.twirl.api.{Html, JavaScript, Txt, Xml}
 
 class TwirlSpec extends Http4sSpec {
   implicit val arbCharset: Arbitrary[Charset] = Arbitrary {
@@ -21,49 +21,49 @@ class TwirlSpec extends Http4sSpec {
 
   "HTML encoder" should {
     "return Content-Type text/html with proper charset" in prop { implicit cs: Charset =>
-      val headers = EntityEncoder[Html].headers
+      val headers = EntityEncoder[IO, Html].headers
       headers.get(`Content-Type`) must_== Some(`Content-Type`(MediaType.`text/html`, Some(cs)))
     }
 
     "render the body" in prop { implicit cs: Charset =>
-      val resp = Response(Ok).withBody(html.test())
-      text.decode(resp.unsafeRun, strict = false).value.unsafeRun must beRight("<h1>test html</h1>")
+      val resp = Response[IO](Ok).withBody(html.test())
+      text[IO].decode(resp.unsafeRunSync, strict = false).value.unsafeRunSync must beRight("<h1>test html</h1>")
     }
   }
 
   "JS encoder" should {
     "return Content-Type application/javascript with proper charset" in prop { implicit cs: Charset =>
-      val headers = EntityEncoder[JavaScript].headers
+      val headers = EntityEncoder[IO, JavaScript].headers
       headers.get(`Content-Type`) must_== Some(`Content-Type`(MediaType.`application/javascript`, Some(cs)))
     }
 
     "render the body" in prop { implicit cs: Charset =>
-      val resp = Response(Ok).withBody(js.test())
-      text.decode(resp.unsafeRun, strict = false).value.unsafeRun must beRight(""""test js"""")
+      val resp = Response[IO](Ok).withBody(js.test())
+      text[IO].decode(resp.unsafeRunSync, strict = false).value.unsafeRunSync must beRight(""""test js"""")
     }
   }
 
   "Text encoder" should {
     "return Content-Type text/plain with proper charset" in prop { implicit cs: Charset =>
-      val headers = EntityEncoder[Txt].headers
+      val headers = EntityEncoder[IO, Txt].headers
       headers.get(`Content-Type`) must_== Some(`Content-Type`(MediaType.`text/plain`, Some(cs)))
     }
 
     "render the body" in prop { implicit cs: Charset =>
-      val resp = Response(Ok).withBody(txt.test())
-      text.decode(resp.unsafeRun, strict = false).value.unsafeRun must beRight("""test text""")
+      val resp = Response[IO](Ok).withBody(txt.test())
+      text[IO].decode(resp.unsafeRunSync, strict = false).value.unsafeRunSync must beRight("""test text""")
     }
   }
 
   "XML encoder" should {
     "return Content-Type application/xml with proper charset" in prop { implicit cs: Charset =>
-      val headers = EntityEncoder[Xml].headers
+      val headers = EntityEncoder[IO, Xml].headers
       headers.get(`Content-Type`) must_== Some(`Content-Type`(MediaType.`application/xml`, Some(cs)))
     }
 
     "render the body" in prop { implicit cs: Charset =>
-      val resp = Response(Ok).withBody(_root_.xml.test())
-      text.decode(resp.unsafeRun, strict = false).value.unsafeRun must beRight("<test>test xml</test>")
+      val resp = Response[IO](Ok).withBody(_root_.xml.test())
+      text[IO].decode(resp.unsafeRunSync, strict = false).value.unsafeRunSync must beRight("<test>test xml</test>")
     }
   }
 }
