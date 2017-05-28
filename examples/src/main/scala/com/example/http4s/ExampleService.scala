8b5b9ff60408c5adafce5283f3fe1f29bfec9e03
Bjørn Madsen <bm@aeons.dk>
2017-05-28 16:57:34 +0200

    Examples module compiles
    Though with stuff that depends on server still commented out.

--- examples/src/main/scala/com/example/http4s/ExampleService.scala
+++ examples/src/main/scala/com/example/http4s/ExampleService.scala
@@ -1,40 +1,32 @@
 package com.example.http4s
 
-import java.util.concurrent.ExecutorService
+import _root_.io.circe.Json
+import cats._
+import cats.effect.IO
+import cats.implicits._
+import fs2._
+import org.http4s.MediaType._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.dsl._
+import org.http4s.headers._
+import org.http4s.twirl._
+
 import scala.concurrent._
 import scala.concurrent.duration._
 
-import cats.implicits._
-import fs2._
-import fs2.interop.cats._
-import _root_.io.circe.Json
-import org.http4s._
-import org.http4s.Uri
-import org.http4s.MediaType._
-import org.http4s.dsl._
-import org.http4s.headers._
-import org.http4s.circe._
-// TODO fs2 port import org.http4s.multipart._
-import org.http4s.scalaxml._
-import org.http4s.server._
-//import org.http4s.server.middleware.PushSupport._
-//import org.http4s.server.middleware.authentication._
-import org.http4s.twirl._
-
 object ExampleService {
 
-  /*
   // A Router can mount multiple services to prefixes.  The request is passed to the
   // service with the longest matching prefix.
-  def service(implicit ec: ExecutionContext = ExecutionContext.global): HttpService = Router(
-    "" -> rootService,
-    "/auth" -> authService,
-    "/science" -> ScienceExperiments.service
-  )
-   */
+  //  def service(implicit ec: ExecutionContext = ExecutionContext.global): HttpService[IO] = Router(
+  //    "" -> rootService,
+  //    "/auth" -> authService,
+  //    "/science" -> ScienceExperiments.service
+  //  )
 
-  def rootService(implicit ec: ExecutionContext = ExecutionContext.global) = HttpService[Task] {
-    case req @ GET -> Root =>
+  def rootService(implicit ec: ExecutionContext = ExecutionContext.global) = HttpService[IO] {
+    case GET -> Root =>
       // Supports Play Framework template -- see src/main/twirl.
       Ok(html.index())
 
@@ -46,120 +38,119 @@ object ExampleService {
       // EntityEncoder allows for easy conversion of types to a response body
       Ok("pong")
 
-    case GET -> Root / "future" =>
-      // EntityEncoder allows rendering asynchronous results as well
-      Ok(Task.fromFuture(Future("Hello from the future!"))(Strategy.fromExecutionContext(ec), ec))
+  case GET -> Root / "future" =>
+    // EntityEncoder allows rendering asynchronous results as well
+    Ok(IO.fromFuture(Eval.always(Future("Hello from the future!"))))
 
-    case GET -> Root / "streaming" =>
-      // Its also easy to stream responses to clients
-      implicit val s = Strategy.fromExecutionContext(ec)
-      Ok(dataStream(100))
+  case GET -> Root / "streaming" =>
+    // Its also easy to stream responses to clients
+    Ok(dataStream(100))
 
-    case req @ GET -> Root / "ip" =>
-      // Its possible to define an EntityEncoder anywhere so you're not limited to built in types
-      val json = Json.obj("origin" -> Json.fromString(req.remoteAddr.getOrElse("unknown")))
-      Ok(json)
+  case req @ GET -> Root / "ip" =>
+    // Its possible to define an EntityEncoder anywhere so you're not limited to built in types
+    val json = Json.obj("origin" -> Json.fromString(req.remoteAddr.getOrElse("unknown")))
+    Ok(json)
 
-    case req @ GET -> Root / "redirect" =>
-      // Not every response must be Ok using a EntityEncoder: some have meaning only for specific types
-      TemporaryRedirect(uri("/http4s/"))
+  case GET -> Root / "redirect" =>
+    // Not every response must be Ok using a EntityEncoder: some have meaning only for specific types
+    TemporaryRedirect(uri("/http4s/"))
 
-    case GET -> Root / "content-change" =>
-      // EntityEncoder typically deals with appropriate headers, but they can be overridden
-      Ok("<h2>This will have an html content type!</h2>")
-        .withContentType(Some(`Content-Type`(`text/html`)))
+  case GET -> Root / "content-change" =>
+    // EntityEncoder typically deals with appropriate headers, but they can be overridden
+    Ok("<h2>This will have an html content type!</h2>")
+      .withContentType(Some(`Content-Type`(`text/html`)))
 
-    case req @ GET -> "static" /: path =>
-      // captures everything after "/static" into `path`
-      // Try http://localhost:8080/http4s/static/nasa_blackhole_image.jpg
-      // See also org.http4s.server.staticcontent to create a mountable service for static content
-      StaticFile.fromResource(path.toString, Some(req)).fold(NotFound())(Task.now)
+  case req @ GET -> "static" /: path =>
+    // captures everything after "/static" into `path`
+    // Try http://localhost:8080/http4s/static/nasa_blackhole_image.jpg
+    // See also org.http4s.server.staticcontent to create a mountable service for static content
+    StaticFile.fromResource(path.toString, Some(req)).fold(NotFound())(IO.pure(_))
 
-    ///////////////////////////////////////////////////////////////
-    //////////////// Dealing with the message body ////////////////
-    case req @ POST -> Root / "echo" =>
-      // The body can be used in the response
-      Ok(req.body).map(_.putHeaders(`Content-Type`(`text/plain`)))
+  ///////////////////////////////////////////////////////////////
+  //////////////// Dealing with the message body ////////////////
+  case req @ POST -> Root / "echo" =>
+    // The body can be used in the response
+    Ok(req.body).map(_.putHeaders(`Content-Type`(`text/plain`)))
 
-    case req @ GET -> Root / "echo" =>
-      Ok(html.submissionForm("echo data"))
+  case GET -> Root / "echo" =>
+    Ok(html.submissionForm("echo data"))
 
-    case req @ POST -> Root / "echo2" =>
-      // Even more useful, the body can be transformed in the response
-      Ok(req.body.drop(6))
-        .putHeaders(`Content-Type`(`text/plain`))
+  case req @ POST -> Root / "echo2" =>
+    // Even more useful, the body can be transformed in the response
+    Ok(req.body.drop(6))
+      .putHeaders(`Content-Type`(`text/plain`))
 
-    case req @ GET -> Root / "echo2" =>
-      Ok(html.submissionForm("echo data"))
+  case GET -> Root / "echo2" =>
+    Ok(html.submissionForm("echo data"))
 
-    case req @ POST -> Root / "sum"  =>
-      // EntityDecoders allow turning the body into something useful
-      req.decode[UrlForm] { data =>
-        data.values.get("sum") match {
-          case Some(Seq(s, _*)) =>
-            val sum = s.split(' ').filter(_.length > 0).map(_.trim.toInt).sum
-            Ok(sum.toString)
+  case req @ POST -> Root / "sum"  =>
+    // EntityDecoders allow turning the body into something useful
+    req.decode[UrlForm] { data =>
+      data.values.get("sum") match {
+        case Some(Seq(s, _*)) =>
+          val sum = s.split(' ').filter(_.length > 0).map(_.trim.toInt).sum
+          Ok(sum.toString)
 
-          case None => BadRequest(s"Invalid data: " + data)
-        }
-      } handleWith {    // We can handle errors using Task methods
-        case e: NumberFormatException => BadRequest("Not an int: " + e.getMessage)
+        case None => BadRequest(s"Invalid data: " + data)
       }
+    } handleErrorWith {    // We can handle errors using Task methods
+      case e: NumberFormatException => BadRequest("Not an int: " + e.getMessage)
+    }
 
-    case req @ GET -> Root / "sum" =>
-      Ok(html.submissionForm("sum"))
+  case GET -> Root / "sum" =>
+    Ok(html.submissionForm("sum"))
 
-    ///////////////////////////////////////////////////////////////
-    ////////////////////// Blaze examples /////////////////////////
+  ///////////////////////////////////////////////////////////////
+  ////////////////////// Blaze examples /////////////////////////
 
-    // You can use the same service for GET and HEAD. For HEAD request,
-    // only the Content-Length is sent (if static content)
-    case req @ GET -> Root / "helloworld" =>
-      helloWorldService
-    case req @ HEAD -> Root / "helloworld" =>
-      helloWorldService
+  // You can use the same service for GET and HEAD. For HEAD request,
+  // only the Content-Length is sent (if static content)
+  case GET -> Root / "helloworld" =>
+    helloWorldService
+  case HEAD -> Root / "helloworld" =>
+    helloWorldService
 
-    // HEAD responses with Content-Lenght, but empty content
-    case req @ HEAD -> Root / "head" =>
-      Ok("").putHeaders(`Content-Length`(1024))
+  // HEAD responses with Content-Lenght, but empty content
+  case HEAD -> Root / "head" =>
+    Ok("").putHeaders(`Content-Length`(1024))
 
-    // Response with invalid Content-Length header generates
-    // an error (underflow causes the connection to be closed)
-    case req @ GET -> Root / "underflow" =>
-      Ok("foo").putHeaders(`Content-Length`(4))
+  // Response with invalid Content-Length header generates
+  // an error (underflow causes the connection to be closed)
+  case GET -> Root / "underflow" =>
+    Ok("foo").putHeaders(`Content-Length`(4))
 
-    // Response with invalid Content-Length header generates
-    // an error (overflow causes the extra bytes to be ignored)
-    case req @ GET -> Root / "overflow" =>
-      Ok("foo").putHeaders(`Content-Length`(2))
+  // Response with invalid Content-Length header generates
+  // an error (overflow causes the extra bytes to be ignored)
+  case GET -> Root / "overflow" =>
+    Ok("foo").putHeaders(`Content-Length`(2))
 
-    ///////////////////////////////////////////////////////////////
-    //////////////// Form encoding example ////////////////////////
-    case req @ GET -> Root / "form-encoded" =>
-      Ok(html.formEncoded())
+  ///////////////////////////////////////////////////////////////
+  //////////////// Form encoding example ////////////////////////
+  case GET -> Root / "form-encoded" =>
+    Ok(html.formEncoded())
 
-    case req @ POST -> Root / "form-encoded" =>
-      // EntityDecoders return a Task[A] which is easy to sequence
-      req.decode[UrlForm] { m =>
-        val s = m.values.mkString("\n")
-        Ok(s"Form Encoded Data\n$s")
-      }
+  case req @ POST -> Root / "form-encoded" =>
+    // EntityDecoders return a Task[A] which is easy to sequence
+    req.decode[UrlForm] { m =>
+      val s = m.values.mkString("\n")
+      Ok(s"Form Encoded Data\n$s")
+    }
 
-    ///////////////////////////////////////////////////////////////
-    //////////////////////// Server Push //////////////////////////
-      /*
-    case req @ GET -> Root / "push" =>
-      // http4s intends to be a forward looking library made with http2.0 in mind
-      val data = <html><body><img src="image.jpg"/></body></html>
-      Ok(data)
-        .withContentType(Some(`Content-Type`(`text/html`)))
-        .push("/image.jpg")(req)
-       */
+  ///////////////////////////////////////////////////////////////
+  //////////////////////// Server Push //////////////////////////
+    /*
+  case req @ GET -> Root / "push" =>
+    // http4s intends to be a forward looking library made with http2.0 in mind
+    val data = <html><body><img src="image.jpg"/></body></html>
+    Ok(data)
+      .withContentType(Some(`Content-Type`(`text/html`)))
+      .push("/image.jpg")(req)
+     */
 
-    case req @ GET -> Root / "image.jpg" =>
-      StaticFile.fromResource("/nasa_blackhole_image.jpg", Some(req))
-        .map(Task.now)
-        .getOrElse(NotFound())
+  case req @ GET -> Root / "image.jpg" =>
+    StaticFile.fromResource("/nasa_blackhole_image.jpg", Some(req))
+      .map(IO.pure)
+      .getOrElse(NotFound())
 
     ///////////////////////////////////////////////////////////////
     //////////////////////// Multi Part //////////////////////////
@@ -176,15 +167,14 @@ object ExampleService {
        */
   }
 
-  def helloWorldService = Ok("Hello World!")
-
-  implicit val defaultStrategy = Strategy.fromExecutionContext(scala.concurrent.ExecutionContext.global)
   implicit val defaultScheduler = Scheduler.fromFixedDaemonPool(1)
 
+  def helloWorldService: IO[Response[IO]] = Ok("Hello World!")
+
   // This is a mock data source, but could be a Process representing results from a database
-  def dataStream(n: Int): Stream[Task, String] = {
+  def dataStream(n: Int)(implicit ec: ExecutionContext): Stream[IO, String] = {
     val interval = 100.millis
-    val stream = time.awakeEvery[Task](interval)(Task.asyncInstance, defaultScheduler)
+    val stream = time.awakeEvery[IO](interval)
       .map(_ => s"Current system time: ${System.currentTimeMillis()} ms\n")
       .take(n.toLong)
 
@@ -207,5 +197,5 @@ object ExampleService {
     case req @ GET -> Root / "protected" as user =>
       Ok(s"This page is protected using HTTP authentication; logged in as $user")
   })
-   */
+  */
 }
