019d298352905a15a088eca9bede8b261cfa044f
Bjørn Madsen <bm@aeons.dk>
2017-06-03 20:38:19 +0200

    Jetty module compiles and tests

--- jetty/src/test/scala/org/http4s/server/jetty/JettyServerSpec.scala
+++ jetty/src/test/scala/org/http4s/server/jetty/JettyServerSpec.scala
@@ -2,6 +2,8 @@ package org.http4s
 package server
 package jetty
 
+import cats.effect.IO
+
 class JettyServerSpec extends ServerAddressSpec {
-  val builder = JettyBuilder
+  val builder = JettyBuilder[IO]
 }
