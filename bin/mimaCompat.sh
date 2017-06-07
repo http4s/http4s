7fd2d0d8f2bb9b2647b0441730a48a3a13891ab3
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-06-07 18:11:13 -0400

    Change To Special Build Only

--- bin/mimaCompat.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. $TRAVIS_BUILD_DIR/bin/setup.sh
-
-sbt $SBT_EXTRA_OPTS 'set scalazVersion in ThisBuild := System.getenv("SCALAZ_VERSION")' ++$TRAVIS_SCALA_VERSION "mimaReportBinaryIssues"
