53156090702f54b2f5a0633100cc778f9649bc82
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-06-16 08:42:24 -0400

    Test case demonstrating #1209

--- scala-xml/src/test/scala/scalaxml/ScalaXmlSpec.scala
+++ scala-xml/src/test/scala/scalaxml/ScalaXmlSpec.scala
@@ -1,7 +1,7 @@
 package org.http4s
 package scalaxml
 
-import fs2.{Stream, Task}
+import fs2.{Strategy, Stream, Task}
 import org.http4s.Status.Ok
 
 import scala.xml.Elem
@@ -14,13 +14,22 @@ class ScalaXmlSpec extends Http4sSpec {
 
   "xml" should {
     val server: Request => Task[Response] = { req =>
-      req.decode { elem: Elem => Response(Ok).withBody(elem.label) }
+      req.decode[Elem] { elem =>
+        Response(Ok).withBody(elem.label) }
     }
 
     "parse the XML" in {
       val resp = server(Request(body = strBody("<html><h1>h1</h1></html>"))).unsafeRun
       resp.status must_==(Ok)
       getBody(resp.body) must_== ("html".getBytes)
+    }
+
+    "parse XML in parallel" in {
+      // https://github.com/http4s/http4s/issues/1209
+      val pool = Strategy.fromFixedDaemonPool(5)
+      val resp = Task.parallelTraverse(0 to 5)(_ => server(Request(body = strBody("""<?xml version="1.0" encoding="UTF-8" standalone="yes"?><html><h1>h1</h1></html>"""))))(pool).unsafeRun
+      resp.forall(_.status must_==(Ok))
+      resp.forall(x => getBody(x.body) must_== ("html".getBytes))
     }
 
     "return 400 on parse error" in {
