1380853bc36fea06b59dc2e4e5934aba0e35480f
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-07-18 14:53:50 -0400

    Move StreamApp to be jvm only

--- core/js/src/main/scala/org/log4s/Logger.scala
+++ core/js/src/main/scala/org/log4s/Logger.scala
@@ -4,4 +4,5 @@ case class Logger(name: String) {
   def warn(s: => String): Unit = {}
   def trace(s: => String): Unit = {}
   def error(t: Throwable)(s: => String): Unit = {}
+  def error(t: Any)(s: => String): Unit = {}
 }
