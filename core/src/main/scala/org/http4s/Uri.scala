dec74ab9e61ae1d2f65fba0721202224e752e4ae
Christopher Davenport <Chris@ChristopherDavenport.tech>
2017-07-21 09:56:36 -0400

    Switch to Fragment Directly
    General use case appears to exchange a URI fragment, so rather than an option, pass the Fragment/String directly with copy serving the pupose of clearing the fragment.

--- core/src/main/scala/org/http4s/Uri.scala
+++ core/src/main/scala/org/http4s/Uri.scala
@@ -34,7 +34,7 @@ final case class Uri(
 
   def withPath(path: Path): Uri = copy(path = path)
 
-  def withFragment(fragment: Option[Fragment]): Uri = copy(fragment = fragment)
+  def withFragment(fragment: Fragment): Uri = copy(fragment = Option(fragment))
 
   def /(newSegment: Path): Uri = {
     val encoded = UrlCodingUtils.pathEncode(newSegment)
