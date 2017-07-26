43e57f8bac914048cc9ff86d5c35745183e133cb
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-07-26 14:31:37 -0700

    Indirect fix for #1281

--- servlet/src/main/scala/org/http4s/servlet/syntax/ServletContextSyntax.scala
+++ servlet/src/main/scala/org/http4s/servlet/syntax/ServletContextSyntax.scala
@@ -7,17 +7,20 @@ import javax.servlet.{ServletContext, ServletRegistration}
 import org.http4s.server.AsyncTimeoutSupport
 import org.http4s.util.threads.DefaultExecutionContext
 
+import scala.concurrent.ExecutionContext
+
 trait ServletContextSyntax {
   implicit def ToServletContextOps(self: ServletContext): ServletContextOps = new ServletContextOps(self)
 }
 
 final class ServletContextOps private[syntax](val self: ServletContext) extends AnyVal {
   /** Wraps an HttpService and mounts it as a servlet */
-  def mountService(name: String, service: HttpService, mapping: String = "/*"): ServletRegistration.Dynamic = {
+  def mountService(name: String, service: HttpService, mapping: String = "/*",
+                   executionContext: ExecutionContext = DefaultExecutionContext): ServletRegistration.Dynamic = {
     val servlet = new Http4sServlet(
       service = service,
       asyncTimeout = AsyncTimeoutSupport.DefaultAsyncTimeout,
-      executionContext = DefaultExecutionContext,
+      executionContext = executionContext,
       servletIo = servletIo
     )
     val reg = self.addServlet(name, servlet)
