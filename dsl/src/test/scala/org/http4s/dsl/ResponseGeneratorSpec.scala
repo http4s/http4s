be6bb1b6eeea77730b8cb6b05e116ffdcd0bf00c
Ross A. Baker <ross@rossabaker.com>
2017-08-03 08:15:50 -0400

    Fix master

--- dsl/src/test/scala/org/http4s/dsl/ResponseGeneratorSpec.scala
+++ dsl/src/test/scala/org/http4s/dsl/ResponseGeneratorSpec.scala
@@ -13,7 +13,7 @@ class ResponseGeneratorSpec extends Http4sSpec {
       old and (resultheaders.exists(_ == h) must_=== true)
     }
 
-    resultheaders.get(`Content-Length`) must_=== `Content-Length`.fromLong(body.getBytes.length.toLong).toOption
+    resultheaders.get(`Content-Length`) must_=== `Content-Length`.fromLong(body.getBytes.length.toLong).right.toOption
   }
 
   "Not duplicate headers when not provided" in {
