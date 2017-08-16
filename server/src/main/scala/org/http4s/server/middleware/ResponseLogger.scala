bac181074e5c81a6f1d90f3dad7da484c8a2fc15
Christopher Davenport <Chris@ChristopherDavenport.tech>
2017-08-16 09:29:55 -0400

    Added Header Redaction to Logger

--- server/src/main/scala/org/http4s/server/middleware/ResponseLogger.scala
+++ server/src/main/scala/org/http4s/server/middleware/ResponseLogger.scala
@@ -5,6 +5,7 @@ package middleware
 import fs2._
 import fs2.interop.cats._
 import cats.implicits._
+import org.http4s.util.CaseInsensitiveString
 import org.log4s.getLogger
 
 /**
@@ -13,12 +14,16 @@ import org.log4s.getLogger
 object ResponseLogger {
   private[this] val logger = getLogger
 
-  def apply(logHeaders: Boolean, logBody: Boolean)(service: HttpService)(implicit strategy: Strategy): HttpService =
+  def apply(
+             logHeaders: Boolean,
+             logBody: Boolean,
+             redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains
+           )(service: HttpService)(implicit strategy: Strategy): HttpService =
     Service.lift { req => service(req).flatMap {
         case Pass => Pass.now
         case response: Response =>
           if (!logBody) {
-            Logger.logMessage(response)(logHeaders, logBody)(logger)(strategy) >> Task(response)
+            Logger.logMessage(response)(logHeaders, logBody, redactHeadersWhen)(logger)(strategy) >> Task(response)
           } else {
             fs2.async.unboundedQueue[Task, Byte].map { queue =>
               val newBody = Stream.eval(queue.size.get)
@@ -28,7 +33,7 @@ object ResponseLogger {
                 body = response.body
                   .observe(queue.enqueue)
                   .onFinalize {
-                    Logger.logMessage(response.copy(body = newBody))(logHeaders, logBody)(logger)(strategy)
+                    Logger.logMessage(response.copy(body = newBody))(logHeaders, logBody, redactHeadersWhen)(logger)(strategy)
                   }
               )
             }
