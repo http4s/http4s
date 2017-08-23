607795c2ba0648830b6b2b4c166d35d730e5b46e
Ross A. Baker <ross@rossabaker.com>
2017-08-23 12:11:16 -0400

    Preserve `orNotFound(A)` for Scala 2.10 / Scalaz

--- core/src/main/scala/org/http4s/syntax/ServiceSyntax.scala
+++ core/src/main/scala/org/http4s/syntax/ServiceSyntax.scala
@@ -1,6 +1,7 @@
 package org.http4s
 package syntax
 
+import scalaz.concurrent.Task
 import scalaz.Kleisli
 
 trait ServiceSyntax {
@@ -9,6 +10,9 @@ trait ServiceSyntax {
 }
 
 final class ServiceOps[A](self: Service[A, MaybeResponse]) {
+  def orNotFound(a: A): Task[Response] =
+    orNotFound.run(a)
+
   def orNotFound: Service[A, Response] =
     Kleisli(a => self.run(a).map(_.orNotFound))
 }
