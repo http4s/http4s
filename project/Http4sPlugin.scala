9ddd1a48dec7808d33aabb2c2af27bf26ad9db12
Ross A. Baker <ross@rossabaker.com>
2017-08-23 14:03:35 -0400

    Upgrade to blaze-0.12.7

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -102,7 +102,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val alpnBoot                         = "org.mortbay.jetty.alpn" %  "alpn-boot"                 % "8.1.11.v20170118"
   lazy val argonaut                         = "io.argonaut"            %% "argonaut"                  % "6.2"
   lazy val asyncHttpClient                  = "org.asynchttpclient"    %  "async-http-client"         % "2.0.34"
-  lazy val blaze                            = "org.http4s"             %% "blaze-http"                % "0.12.6"
+  lazy val blaze                            = "org.http4s"             %% "blaze-http"                % "0.12.7"
   lazy val circeGeneric                     = "io.circe"               %% "circe-generic"             % circeJawn.revision
   lazy val circeJawn                        = "io.circe"               %% "circe-jawn"                % "0.8.0"
   lazy val circeLiteral                     = "io.circe"               %% "circe-literal"             % circeJawn.revision
