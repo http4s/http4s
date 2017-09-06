26b57f69516b5115e76436be6f5b4690430e3b4f
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2017-09-05 21:00:38 -0300

    Split the platform dependent bits of Part

--- core/jvm/src/main/scala/org/http4s/multipart/PlatformPart.scala
+++ core/jvm/src/main/scala/org/http4s/multipart/PlatformPart.scala
@@ -1,27 +1,19 @@
 package org.http4s
 package multipart
 
-import cats.effect.Sync
-import fs2.Stream
-import fs2.text.utf8Encode
 import java.io.{File, FileInputStream, InputStream}
 import java.net.URL
+
+import cats.effect.Sync
+import fs2.Stream
+import fs2.io.readInputStream
+import fs2.text.utf8Encode
 import org.http4s.headers.`Content-Disposition`
 import org.http4s.util.CaseInsensitiveString
 import org.http4s.{EmptyBody, Header, Headers}
 
-final case class Part[F[_]](headers: Headers, body: Stream[F, Byte]) {
-  def name: Option[String] = headers.get(`Content-Disposition`).flatMap(_.parameters.get("name"))
-}
-
-object Part extends PlatformPart {
-  def empty[F[_]]: Part[F] =
-    Part(Headers.empty, EmptyBody)
-
-  def formData[F[_]: Sync](name: String, value: String, headers: Header*): Part[F] =
-    Part(
-      `Content-Disposition`("form-data", Map("name" -> name)) +: headers,
-      Stream.emit(value).through(utf8Encode))
+trait PlatformPart {
+  private val ChunkSize = 8192
 
   def fileData[F[_]: Sync](name: String, file: File, headers: Header*): Part[F] =
     fileData(name, file.getName, new FileInputStream(file), headers: _*)
@@ -41,7 +33,7 @@ object Part extends PlatformPart {
       entityBody)
 
   // The InputStream is passed by name, and we open it in the by-name
-  // argument in callers, so we can avoid lifting into an effect.  Exposing
+  // argument in callers, so we can avoid lifting into a Task.  Exposing
   // this API publicly would invite unsafe use, and the `EntityBody` version
   // should be safe.
   private def fileData[F[_]](name: String, filename: String, in: => InputStream, headers: Header*)(
