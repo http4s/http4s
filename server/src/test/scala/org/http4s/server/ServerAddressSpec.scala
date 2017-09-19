09d4b10fc8f633bf1deb4d4ab3729c5e56574322
Ross A. Baker <ross@rossabaker.com>
2017-09-18 22:16:48 -0400

    Test which thread pool requests route and run on

--- server/src/test/scala/org/http4s/server/ServerAddressSpec.scala
+++ server/src/test/scala/org/http4s/server/ServerAddressSpec.scala
@@ -6,18 +6,5 @@ import org.specs2.mutable.After
 import scalaz.{\/-, -\/}
 import scalaz.concurrent.Task
 
-trait ServerAddressSpec extends Http4sSpec {
-  def builder: ServerBuilder
-
-  trait ServerContext extends After {
-    val address = new InetSocketAddress(0)
-    val server = Task.fork(builder.bindSocketAddress(address).start).unsafePerformSync
-    def after = server.shutdown.unsafePerformSync
-  }
-
-  "A server configured with port 0" should {
-    "know its local port after start" in new ServerContext {
-      server.address.getPort must be_> (0)
-    }
-  }
+trait ServerAddressSpec extends ServerSpec {
 }
--- tomcat/src/test/scala/org/http4s/tomcat/TomcatServerSpec.scala
+++ tomcat/src/test/scala/org/http4s/tomcat/TomcatServerSpec.scala
@@ -2,6 +2,12 @@ package org.http4s
 package server
 package tomcat
 
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory
+
 class TomcatServerSpec extends ServerAddressSpec {
   val builder = TomcatBuilder
+
+  // Prevents us from loading jar and war URLs, but lets us
+  // run Tomcat twice in the same JVM.  This makes me grumpy.
+  TomcatURLStreamHandlerFactory.disable()
 }
