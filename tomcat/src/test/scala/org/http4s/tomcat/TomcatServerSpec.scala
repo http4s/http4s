8fa61d137c4bd1248b64aa0ce2ac4293d93b0779
Travis CI <ross@rossabaker.com>
2017-10-04 11:19:21 -0400

    Fix TomcatServerSpec so it can run multiple times

--- tomcat/src/test/scala/org/http4s/tomcat/TomcatServerSpec.scala
+++ tomcat/src/test/scala/org/http4s/tomcat/TomcatServerSpec.scala
@@ -4,16 +4,14 @@ package tomcat
 
 import cats.effect.IO
 import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory
-import org.specs2.specification.Before
 
-class TomcatServerSpec extends ServerSpec with Before {
-  def before =
-    // Prevents us from loading jar and war URLs, but lets us
-    // run Tomcat twice in the same JVM.  This makes me grumpy.
-    //
-    // Needs to run before the server is initialized in the superclass.
-    // This also makes me grumpy.
-    TomcatURLStreamHandlerFactory.disable()
-
+class TomcatServerSpec extends {
+  // Prevents us from loading jar and war URLs, but lets us
+  // run Tomcat twice in the same JVM.  This makes me grumpy.
+  //
+  // Needs to run before the server is initialized in the superclass.
+  // This also makes me grumpy.
+  val _ = TomcatURLStreamHandlerFactory.disable()
+} with ServerSpec {
   def builder = TomcatBuilder[IO]
 }
