d8c897dca8e98b745d1bfccc0412ffc86548d182
Calvin Brown <calvin.brown@banno.com>
2017-11-06 06:22:10 -0600

    Updating deprecation warning

--- server/src/main/scala/org/http4s/server/syntax/package.scala
+++ server/src/main/scala/org/http4s/server/syntax/package.scala
@@ -6,7 +6,7 @@ import cats.data.Kleisli
 import cats.implicits._
 
 package object syntax {
-  @deprecated("Import `cats.implicits._` and `org.http4s.implicits._` and use `service1 <+> service2` instead. Ensure -Ypartial-unification is enabled.", "0.16")
+  @deprecated("Import `cats.implicits._` and use `service1 <+> service2` instead. Ensure -Ypartial-unification is enabled.", "0.16")
   final implicit class ServiceOps[F[_], A, B](val service: Kleisli[F, A, B])(
       implicit B: Semigroup[F[B]]) {
     def ||(fallback: Kleisli[F, A, B]) = orElse(fallback)
