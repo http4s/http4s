b86d654e69f94a51bb17a9de5a0aeadea4be64e0
Ross A. Baker <ross@rossabaker.com>
2017-11-09 21:22:12 -0500

    Fix compilation failure in Scala 2.10

--- server/src/test/scala/org/http4s/server/middleware/CSRFSpec.scala
+++ server/src/test/scala/org/http4s/server/middleware/CSRFSpec.scala
@@ -32,12 +32,12 @@ class CSRFSpec extends Http4sSpec {
       "validate for the correct csrf token" in {
         val response = (for {
           token <- csrf.generateToken
-          res <- csrf.validate(dummyService)(
+          res <- csrf.validate(dummyService).orNotFound(
             dummyRequest
               .addCookie(Cookie(csrf.cookieName, token))
               .putHeaders(Header(csrf.headerName, token))
           )
-        } yield res).unsafePerformSync.orNotFound
+        } yield res).unsafePerformSync
 
         response.status must_== Status.Ok
       }
@@ -45,11 +45,11 @@ class CSRFSpec extends Http4sSpec {
       "not validate for token missing in cookie" in {
         val response = (for {
           token <- csrf.generateToken
-          res <- csrf.validate(dummyService)(
+          res <- csrf.validate(dummyService).orNotFound(
             dummyRequest
               .putHeaders(Header(csrf.headerName, token))
           )
-        } yield res).unsafePerformSync.orNotFound
+        } yield res).unsafePerformSync
 
         response.status must_== Status.Unauthorized
       }
@@ -57,31 +57,31 @@ class CSRFSpec extends Http4sSpec {
       "not validate for token missing in header" in {
         val response = (for {
           token <- csrf.generateToken
-          res <- csrf.validate(dummyService)(
+          res <- csrf.validate(dummyService).orNotFound(
             dummyRequest
               .addCookie(Cookie(csrf.cookieName, token))
           )
-        } yield res).unsafePerformSync.orNotFound
+        } yield res).unsafePerformSync
 
         response.status must_== Status.Unauthorized
       }
 
       "not validate if token is missing in both" in {
-        val response = csrf.validate(dummyService)(dummyRequest).unsafePerformSync
+        val response = csrf.validate(dummyService).orNotFound(dummyRequest).unsafePerformSync
 
-        response.orNotFound.status must_== Status.Unauthorized
+        response.status must_== Status.Unauthorized
       }
 
       "not validate for different tokens" in {
         val response = (for {
           token1 <- csrf.generateToken
           token2 <- csrf.generateToken
-          res <- csrf.validate(dummyService)(
+          res <- csrf.validate(dummyService).orNotFound(
             dummyRequest
               .addCookie(Cookie(csrf.cookieName, token1))
               .putHeaders(Header(csrf.headerName, token2))
           )
-        } yield res).unsafePerformSync.orNotFound
+        } yield res).unsafePerformSync
 
         response.status must_== Status.Unauthorized
       }
@@ -89,7 +89,7 @@ class CSRFSpec extends Http4sSpec {
       "not return the same token to mitigate BREACH" in {
         val (response, originalToken) = (for {
           token <- csrf.generateToken
-          res <- csrf.validate(dummyService)(
+          res <- csrf.validate(dummyService).apply(
             dummyRequest
               .addCookie(Cookie(csrf.cookieName, token))
               .putHeaders(Header(csrf.headerName, token))
@@ -107,8 +107,8 @@ class CSRFSpec extends Http4sSpec {
       "not return a token for a failed CSRF check" in {
         val response = (for {
           token <- csrf.generateToken
-          res <- csrf.validate(dummyService)(dummyRequest)
-        } yield res.orNotFound).unsafePerformSync
+          res <- csrf.validate(dummyService).orNotFound(dummyRequest)
+        } yield res).unsafePerformSync
 
         response.status must_== Status.Unauthorized
         !HCookie
