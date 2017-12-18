a311b7c78dce3fb55d2b8910c0369585d0fd1e98
Robert Soeldner <r.soeldner@gmail.com>
2017-12-18 20:53:25 +0100

    Fix to correct deprecation version

--- core/src/main/scala/org/http4s/websocket/Websocket.scala
+++ core/src/main/scala/org/http4s/websocket/Websocket.scala
@@ -4,8 +4,8 @@ import fs2._
 import org.http4s.websocket.WebsocketBits.WebSocketFrame
 
 private[http4s] final case class Websocket[F[_]](
-    @deprecatedName('read, "0.18-M6") send: Stream[F, WebSocketFrame],
-    @deprecatedName('write, "0.18-M6") receive: Sink[F, WebSocketFrame]
+    @deprecatedName('read, "0.18.0-M7") send: Stream[F, WebSocketFrame],
+    @deprecatedName('write, "0.18.0-M7") receive: Sink[F, WebSocketFrame]
 ) {
   def read: Stream[F, WebSocketFrame] = send
   def write: Sink[F, WebSocketFrame] = receive
