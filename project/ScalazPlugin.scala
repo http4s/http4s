3c0bdd2dcb7706b04e9f8a4482a40a604cf02013
Ross A. Baker <ross@rossabaker.com>
2017-12-20 23:07:59 -0500

    Upgrade to scalaz-7.2.18

--- project/ScalazPlugin.scala
+++ project/ScalazPlugin.scala
@@ -19,7 +19,7 @@ object ScalazPlugin extends AutoPlugin {
   override def trigger = allRequirements
 
   override lazy val projectSettings = Seq(
-    scalazVersion := sys.env.get("SCALAZ_VERSION").getOrElse("7.2.17"),
+    scalazVersion := sys.env.get("SCALAZ_VERSION").getOrElse("7.2.18"),
     scalazVersionRewriter := scalazVersionRewriters.default,
     version := scalazVersionRewriter.value(version.value, scalazVersion.value),
     unmanagedSourceDirectories in Compile +=
