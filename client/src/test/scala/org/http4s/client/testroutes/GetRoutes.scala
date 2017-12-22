9630d2cb25d557dafe4b7b7563625ff3f4916a7c
Ross A. Baker <ross@rossabaker.com>
2017-12-22 13:42:42 -0500

    Eliminate Thread.sleep from GetRoutes

--- client/src/test/scala/org/http4s/client/testroutes/GetRoutes.scala
+++ client/src/test/scala/org/http4s/client/testroutes/GetRoutes.scala
@@ -1,22 +1,25 @@
-package org.http4s.client.testroutes
+package org.http4s
+package client.testroutes
 
 import cats.effect._
-import fs2._
-import org.http4s.Response
-import org.http4s.Status._
 import cats.implicits._
+import fs2._
+import org.http4s.Status._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
 
 object GetRoutes {
   val SimplePath = "/simple"
   val ChunkedPath = "/chunked"
   val DelayedPath = "/delayed"
 
-  val getPaths: Map[String, Response[IO]] = {
+  def getPaths(implicit ec: ExecutionContext): Map[String, Response[IO]] =
     Map(
       SimplePath -> Response[IO](Ok).withBody("simple path"),
       ChunkedPath -> Response[IO](Ok).withBody(
         Stream.emits("chunk".toSeq.map(_.toString)).covary[IO]),
-      DelayedPath -> IO(Thread.sleep(1000L)) *> Response[IO](Ok).withBody("delayed path")
+      DelayedPath ->
+        Http4sSpec.TestScheduler.sleep_[IO](1.second).run *>
+          Response[IO](Ok).withBody("delayed path")
     ).mapValues(_.unsafeRunSync())
-  }
 }
