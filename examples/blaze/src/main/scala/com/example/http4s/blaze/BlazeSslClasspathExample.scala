0d839289d988a95440bd9429ed8ceb4c56194e2c
Chris Davenport <chris@christopherdavenport.tech>
2017-12-29 15:08:29 -0500

    Apply Scalafmt To New Files

--- examples/blaze/src/main/scala/com/example/http4s/blaze/BlazeSslClasspathExample.scala
+++ examples/blaze/src/main/scala/com/example/http4s/blaze/BlazeSslClasspathExample.scala
@@ -4,6 +4,6 @@ import cats.effect.IO
 import com.example.http4s.ssl.SslClasspathExample
 import org.http4s.server.blaze.BlazeBuilder
 
-object BlazeSslClasspathExample extends SslClasspathExample[IO]{
+object BlazeSslClasspathExample extends SslClasspathExample[IO] {
   def builder: BlazeBuilder[IO] = BlazeBuilder[IO]
 }
--- examples/src/main/scala/com/example/http4s/ssl/SslClasspathExample.scala
+++ examples/src/main/scala/com/example/http4s/ssl/SslClasspathExample.scala
@@ -12,25 +12,25 @@ import java.security.{KeyStore, Security}
 
 abstract class SslClasspathExample[F[_]: Effect] extends StreamApp[F] {
 
-  def loadContextFromClasspath(keystorePassword: String, keyManagerPass: String): F[SSLContext] = Sync[F].delay {
+  def loadContextFromClasspath(keystorePassword: String, keyManagerPass: String): F[SSLContext] =
+    Sync[F].delay {
 
-    val ksStream = this.getClass.getResourceAsStream("/server.jks")
-    val ks = KeyStore.getInstance("JKS")
-    ks.load(ksStream, keystorePassword.toCharArray)
-    ksStream.close()
+      val ksStream = this.getClass.getResourceAsStream("/server.jks")
+      val ks = KeyStore.getInstance("JKS")
+      ks.load(ksStream, keystorePassword.toCharArray)
+      ksStream.close()
 
-    val kmf = KeyManagerFactory.getInstance(
-      Option(Security.getProperty("ssl.KeyManagerFactory.algorithm"))
-        .getOrElse(KeyManagerFactory.getDefaultAlgorithm))
+      val kmf = KeyManagerFactory.getInstance(
+        Option(Security.getProperty("ssl.KeyManagerFactory.algorithm"))
+          .getOrElse(KeyManagerFactory.getDefaultAlgorithm))
 
-    kmf.init(ks, keyManagerPass.toCharArray)
+      kmf.init(ks, keyManagerPass.toCharArray)
 
-    val context = SSLContext.getInstance("TLS")
-    context.init(kmf.getKeyManagers, null, null)
+      val context = SSLContext.getInstance("TLS")
+      context.init(kmf.getKeyManagers, null, null)
 
-    context
-  }
-
+      context
+    }
 
   def builder: ServerBuilder[F] with SSLContextSupport[F]
 
