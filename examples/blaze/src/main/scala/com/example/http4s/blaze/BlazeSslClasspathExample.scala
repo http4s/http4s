046700786049106b0a08a5e2b55d3b5cf434c970
Chris Davenport <chris@christopherdavenport.tech>
2017-12-29 14:51:48 -0500

    Adds Example for SSL Pulled Directly from ClassPath

---
+++ examples/blaze/src/main/scala/com/example/http4s/blaze/BlazeSslClasspathExample.scala
@@ -0,0 +1,9 @@
+package com.example.http4s.blaze
+
+import cats.effect.IO
+import com.example.http4s.ssl.SslClasspathExample
+import org.http4s.server.blaze.BlazeBuilder
+
+object BlazeSslClasspathExample extends SslClasspathExample[IO]{
+  def builder: BlazeBuilder[IO] = BlazeBuilder[IO]
+}
---
+++ examples/src/main/scala/com/example/http4s/ssl/SslClasspathExample.scala
@@ -0,0 +1,48 @@
+package com.example.http4s.ssl
+
+import cats.effect.{Effect, Sync}
+import javax.net.ssl.{KeyManagerFactory, SSLContext}
+
+import com.example.http4s.ExampleService
+import fs2.StreamApp.ExitCode
+import fs2.{Scheduler, Stream, StreamApp}
+import org.http4s.server.middleware.HSTS
+import org.http4s.server.{SSLContextSupport, ServerBuilder}
+import java.security.{KeyStore, Security}
+
+abstract class SslClasspathExample[F[_]: Effect] extends StreamApp[F] {
+
+  def somethingElse(keystorePassword: String, keyManagerPass: String): F[SSLContext] = Sync[F].delay {
+
+    val ksStream = this.getClass.getResourceAsStream("/server.jks")
+    val ks = KeyStore.getInstance("JKS")
+    ks.load(ksStream, keystorePassword.toCharArray)
+    ksStream.close()
+
+    val kmf = KeyManagerFactory.getInstance(
+      Option(Security.getProperty("ssl.KeyManagerFactory.algorithm"))
+        .getOrElse(KeyManagerFactory.getDefaultAlgorithm))
+
+    kmf.init(ks, keyManagerPass.toCharArray)
+
+    val context = SSLContext.getInstance("TLS")
+    context.init(kmf.getKeyManagers, null, null)
+
+    context
+  }
+
+
+  def builder: ServerBuilder[F] with SSLContextSupport[F]
+
+  def stream(args: List[String], requestShutdown: F[Unit]): Stream[F, ExitCode] =
+    for {
+      scheduler <- Scheduler(corePoolSize = 2)
+      context <- Stream.eval(somethingElse("password", "secure"))
+      exitCode <- builder
+        .withSSLContext(context)
+        .bindHttp(8443, "0.0.0.0")
+        .mountService(HSTS(new ExampleService[F].service(scheduler)), "/http4s")
+        .serve
+    } yield exitCode
+
+}
