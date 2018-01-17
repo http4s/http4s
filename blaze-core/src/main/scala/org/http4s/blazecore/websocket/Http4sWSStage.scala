8ff33d5b7a406122a2543ebeb2efde77a3a3dfe3
Chris Davenport <chris@christopherdavenport.tech>
2018-01-17 16:50:19 -0500

    Clean Up Imports Etc

--- blaze-core/src/main/scala/org/http4s/blazecore/websocket/Http4sWSStage.scala
+++ blaze-core/src/main/scala/org/http4s/blazecore/websocket/Http4sWSStage.scala
@@ -3,7 +3,6 @@ package blazecore
 package websocket
 
 import java.util.concurrent.atomic.AtomicReference
-
 import cats.effect._
 import cats.implicits._
 import fs2._
@@ -12,10 +11,8 @@ import org.http4s.blaze.pipeline.{Command, LeafBuilder, TailStage, TrunkBuilder}
 import org.http4s.blaze.pipeline.stages.SerializingStage
 import org.http4s.blaze.util.Execution.{directec, trampoline}
 import org.http4s.websocket.WebsocketBits._
-
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
-import cats.effect.IO
 
 class Http4sWSStage[F[_]](ws: ws4s.Websocket[F])(implicit F: Effect[F], val ec: ExecutionContext)
     extends TailStage[WebSocketFrame] {
@@ -23,6 +20,7 @@ class Http4sWSStage[F[_]](ws: ws4s.Websocket[F])(implicit F: Effect[F], val ec: 
   def name: String = "Http4s WebSocket Stage"
 
   private val deadSignal: AtomicReference[Boolean] = new AtomicReference(false)
+
 
   //////////////////////// Source and Sink generators ////////////////////////
 
@@ -81,7 +79,9 @@ class Http4sWSStage[F[_]](ws: ws4s.Websocket[F])(implicit F: Effect[F], val ec: 
     // Effect to send a close to the other endpoint
     val sendClose: F[Unit] = F.delay(sendOutboundCommand(Command.Disconnect))
 
-    val wsStream = inputstream.to(ws.receive).onFinalize(onStreamFinalize)
+    val wsStream = inputstream
+      .to(ws.receive)
+      .onFinalize(onStreamFinalize)
       .mergeHaltR(ws.send.onFinalize(onStreamFinalize).to(snk).drain)
       .interruptWhen(Stream.repeatEval(F.delay(deadSignal.get()))) // Check The Status of the deadSignal Atomic Ref
       .onFinalize(sendClose)
