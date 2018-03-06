fc49745a9987df1d2488c37ee9e8a11215f9a245
Chris Davenport <chris@christopherdavenport.tech>
2018-03-06 17:09:06 -0500

    Add Partial Implementatation Of Single Large Parser

--- prometheus-server-metrics/src/main/scala/org/http4s/server/prometheus/PrometheusMetrics.scala
+++ prometheus-server-metrics/src/main/scala/org/http4s/server/prometheus/PrometheusMetrics.scala
@@ -9,39 +9,44 @@ import org.http4s._
 import org.http4s.server.HttpMiddleware
 
 object PrometheusMetrics {
-  private case class GeneralServicMetrics(
-      activeRequests: Gauge,
-      totalRequests: Histogram,
-      headers: Histogram,
-      abnormalTerminations: Histogram,
-      serviceErrors: Histogram
-  )
 
-  private case class RequestHistograms(
-      getReq: Histogram,
-      postReq: Histogram,
-      putReq: Histogram,
-      headReq: Histogram,
-      moveReq: Histogram,
-      optionsReq: Histogram,
-      traceReq: Histogram,
-      connectReq: Histogram,
-      deleteReq: Histogram,
-      otherReq: Histogram
-  )
+  /**
+  * org_http4s_response_duration_seconds{labels=method,serving_phase} - Histogram
+  *
+  * org_http4s_active_request_total - Gauge 
+  *
+  * org_http4s_response_total{labels=method,code} - Counter
+  * 
+  * org_http4s_abnormal_terminations_total{labels=termination_type} - Counter
+  **/
 
-  private case class ResponseHistograms(
-      resp1xx: Histogram,
-      resp2xx: Histogram,
-      resp3xx: Histogram,
-      resp4xx: Histogram,
-      resp5xx: Histogram
-  )
+  private def reportRequestMethod(m: Method): String = m match {
+    case Method.GET => "get"
+    case Method.PUT => "put"
+    case Method.POST => "post"
+    case Method.HEAD => "head"
+    case Method.MOVE => "move"
+    case Method.OPTIONS => "options"
+    case Method.TRACE => "trace"
+    case Method.CONNECT => "connect"
+    case Method.DELETE => "delete"
+    case _ => "other"
+  }
+
+  private def reportResponseCode(status: Status): String =
+    status.code match {
+      case hundreds if hundreds < 200 => "1xx"
+      case twohundreds if twohundreds < 300 => "2xx"
+      case threehundreds if threehundreds < 400 => "3xx"
+      case fourhundreds if fourhundreds < 500 => "4xx"
+      case _ => "5xx"
+    }
 
   private case class ServiceMetrics(
-      generalMetrics: GeneralServicMetrics,
-      requestHist: RequestHistograms,
-      responseHist: ResponseHistograms
+    requestDuration: Histogram,
+    activeRequests: Gauge,
+    responseCount: Counter,
+    abnormalTerminations: Counter
   )
 
   private def metricsService[F[_]: Sync](
@@ -52,35 +57,29 @@ object PrometheusMetrics {
   ): OptionT[F, Response[F]] = OptionT {
     for {
       initialTime <- Sync[F].delay(System.nanoTime())
-      _ <- Sync[F].delay(serviceMetrics.generalMetrics.activeRequests.inc())
+      _ <- Sync[F].delay(serviceMetrics.activeRequests.inc())
       responseAtt <- service(req).value.attempt
       headersElapsed <- Sync[F].delay(System.nanoTime())
-      _ <- Sync[F].delay(
-        serviceMetrics.generalMetrics.headers
-          .observe(((headersElapsed - initialTime) / 1000000000.0).toDouble))
-      respOpt <- metricsServiceHandler[F](req.method, initialTime, serviceMetrics, responseAtt)
-    } yield respOpt
+      result <- responseAtt.fold(
+        e => onServiceError(req.method, initialTime, serviceMetrics) *> Sync[F].raiseError[Option[Response[F]]](e),
+        _.fold(
+          onEmpty[F](req.method, initialTime, serviceMetrics).as(Option.empty[Response[F]])
+        )(
+          onResponse(req.method, initialTime, serviceMetrics)(_).some.pure[F]
+        )
+      )
+    } yield result
   }
 
-  private def metricsServiceHandler[F[_]: Sync](
-      m: Method,
-      start: Long,
-      serviceMetrics: ServiceMetrics,
-      e: Either[Throwable, Option[Response[F]]]
-  ): F[Option[Response[F]]] =
-    e.fold(
-      e => onServiceError(m, start, serviceMetrics) *> Sync[F].raiseError[Option[Response[F]]](e),
-      _.fold(
-        Sync[F]
-          .delay(serviceMetrics.generalMetrics.activeRequests.dec())
-          .as(Option.empty[Response[F]])
-      )(
-        onResponse(m, start, serviceMetrics)(_).some.pure[F]
-      )
-    )
+  private def onEmpty[F[_]: Sync](m: Method, start: Long, headerTime: Long, serviceMetrics: ServiceMetrics): F[Unit] = ???
 
-  private def onResponse[F[_]: Sync](m: Method, start: Long, serviceMetrics: ServiceMetrics)(
-      r: Response[F]): Response[F] = {
+  private def onResponse[F[_]: Sync](
+    m: Method, 
+    start: Long, 
+    serviceMetrics: ServiceMetrics
+  )(
+    r: Response[F]
+  ): Response[F] = {
     val newBody = r.body
       .onFinalize {
         for {
@@ -123,114 +122,29 @@ object PrometheusMetrics {
         sm.generalMetrics.serviceErrors.observe(SimpleTimer.elapsedSecondsFromNanos(begin, end)))
     } yield ()
 
-  private def requestMetrics[F[_]: Sync](
-      rh: RequestHistograms,
-      totalReqs: Histogram,
-      activeReqs: Gauge)(m: Method, begin: Long, end: Long): F[Unit] = Sync[F].delay {
-    val histogram = requestHistogram(rh, m)
-    val elapsed = SimpleTimer.elapsedSecondsFromNanos(begin, end)
-    histogram.observe(elapsed)
-    totalReqs.observe(elapsed)
-    activeReqs.dec()
-  }
-
-  private def responseMetrics[F[_]: Sync](
-      rh: ResponseHistograms,
-      status: Status,
-      begin: Long,
-      end: Long): F[Unit] = Sync[F].delay {
-    responseHistogram(rh, status).observe(SimpleTimer.elapsedSecondsFromNanos(begin, end))
-  }
-
-  private def requestHistogram[F[_]: Sync](rt: RequestHistograms, method: Method): Histogram =
-    method match {
-      case Method.GET => rt.getReq
-      case Method.POST => rt.postReq
-      case Method.PUT => rt.putReq
-      case Method.HEAD => rt.headReq
-      case Method.MOVE => rt.moveReq
-      case Method.OPTIONS => rt.optionsReq
-      case Method.TRACE => rt.traceReq
-      case Method.CONNECT => rt.connectReq
-      case Method.DELETE => rt.deleteReq
-      case _ => rt.otherReq
-    }
-
-  private def responseHistogram(responseTimers: ResponseHistograms, status: Status): Histogram =
-    status.code match {
-      case hundreds if hundreds < 200 => responseTimers.resp1xx
-      case twohundreds if twohundreds < 300 => responseTimers.resp2xx
-      case threehundreds if threehundreds < 400 => responseTimers.resp3xx
-      case fourhundreds if fourhundreds < 500 => responseTimers.resp4xx
-      case _ => responseTimers.resp5xx
-    }
-
   def apply[F[_]: Effect](
+      c: CollectorRegistry,
       prefix: String = "org_http4s_server",
-      c: CollectorRegistry): F[HttpMiddleware[F]] = Sync[F].delay {
-    val generalServiceMetrics = GeneralServicMetrics(
-      activeRequests = Gauge
+      emptyResponseHandler: Option[Status] = Status.NotFound.some,
+      errorResponseHandler: Throwable => Option[Status] = e => Status.InternalServerError.some
+      ): F[HttpMiddleware[F]] = Sync[F].delay {
+    
+    val serviceMetrics =
+      ServiceMetrics(
+        requestDuration = Histogram
+        .build()
+        .name(prefix + "_" + "requests")
+        .help("Request Duration.")
+        .labelNames("method", "code")
+        .register(c),
+        activeRequests = Gauge
         .build()
         .name(prefix + "_" + "active_requests")
         .help("Total Active Requests.")
         .register(c),
-      totalRequests =
-        Histogram.build().name(prefix + "_" + "requests").help("Total Requests.").register(c),
-      headers =
-        Histogram.build().name(prefix + "_" + "headers_times").help("Header Timings.").register(c),
-      abnormalTerminations = Histogram
-        .build()
-        .name(prefix + "_" + "abnormal_terminations")
-        .help("Abnormal Terminations.")
-        .register(c),
-      serviceErrors =
-        Histogram.build().name(prefix + "_" + "service_errors").help("Service Errors.").register(c)
-    )
-    val requestHistograms = RequestHistograms(
-      getReq =
-        Histogram.build().name(prefix + "_" + "get_requests").help("GET Requests.").register(c),
-      postReq =
-        Histogram.build().name(prefix + "_" + "post_requests").help("POST Requests.").register(c),
-      putReq =
-        Histogram.build().name(prefix + "_" + "put_requests").help("PUT Requests.").register(c),
-      headReq =
-        Histogram.build().name(prefix + "_" + "head_requests").help("HEAD Requests.").register(c),
-      moveReq =
-        Histogram.build().name(prefix + "_" + "move_requests").help("MOVE Requests.").register(c),
-      optionsReq = Histogram
-        .build()
-        .name(prefix + "_" + "options_requests")
-        .help("OPTIONS Requests.")
-        .register(c),
-      traceReq =
-        Histogram.build().name(prefix + "_" + "trace_requests").help("TRACE Requests.").register(c),
-      connectReq = Histogram
-        .build()
-        .name(prefix + "_" + "connect_requests")
-        .help("CONNECT Requests.")
-        .register(c),
-      deleteReq = Histogram
-        .build()
-        .name(prefix + "_" + "delete_requests")
-        .help("DELETE Requests.")
-        .register(c),
-      otherReq =
-        Histogram.build().name(prefix + "_" + "other_requests").help("Other Requests.").register(c)
-    )
-    val responseHistograms = ResponseHistograms(
-      resp1xx =
-        Histogram.build().name(prefix + "_" + "1xx_responses").help("1xx Responses.").register(c),
-      resp2xx =
-        Histogram.build().name(prefix + "_" + "2xx_responses").help("2xx Responses.").register(c),
-      resp3xx =
-        Histogram.build().name(prefix + "_" + "3xx_responses").help("3xx Responses.").register(c),
-      resp4xx =
-        Histogram.build().name(prefix + "_" + "4xx_responses").help("4xx Responses.").register(c),
-      resp5xx =
-        Histogram.build().name(prefix + "_" + "5xx_responses").help("5xx Responses.").register(c)
-    )
-    val serviceMetrics =
-      ServiceMetrics(generalServiceMetrics, requestHistograms, responseHistograms)
+        responseCount = ???,
+        abnormalTerminations = ???
+      )
 
     { service: HttpService[F] =>
       Kleisli(metricsService[F](serviceMetrics, service)(_))
