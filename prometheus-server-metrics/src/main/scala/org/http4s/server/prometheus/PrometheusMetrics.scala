3abdb293686459b81e88a8fb8fd32275d9b6fbae
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-03-05 20:27:01 -0500

    Add Alternative Implementation for Prometheus Metrics

--- prometheus-server-metrics/src/main/scala/org/http4s/server/prometheus/PrometheusMetrics.scala
+++ prometheus-server-metrics/src/main/scala/org/http4s/server/prometheus/PrometheusMetrics.scala
@@ -9,12 +9,22 @@ import org.http4s._
 import org.http4s.server.HttpMiddleware
 
 object PrometheusMetrics {
+
+  private case class SingleMetricRun(
+    activeRequests: Gauge,
+    totalRequests: Histogram.Timer,
+    requestTypeTimer: Histogram.Timer,
+    headers: Histogram.Timer,
+    abnormalTerminations: Counter,
+    serviceErrors: Counter,
+  )
+
   private case class GeneralServicMetrics(
     activeRequests: Gauge,
     totalRequests: Histogram,
     headers: Histogram,
-    abnormalTerminations: Histogram,
-    serviceErrors: Histogram,
+    abnormalTerminations: Counter,
+    serviceErrors: Counter,
   )
 
   private case class RequestHistograms(
@@ -30,135 +40,100 @@ object PrometheusMetrics {
     otherReq: Histogram
   )
 
-  private case class ResponseHistograms(
-    resp1xx: Histogram,
-    resp2xx: Histogram,
-    resp3xx: Histogram,
-    resp4xx: Histogram,
-    resp5xx: Histogram
-  )
-
   private case class ServiceMetrics(
     generalMetrics: GeneralServicMetrics,
-    requestHist: RequestHistograms,
-    responseHist: ResponseHistograms
+    requestHist: RequestHistograms
   )
 
   private def metricsService[F[_]: Sync](
-    serviceMetrics: ServiceMetrics, 
+    singleMetricRun: SingleMetricRun, 
     service: HttpService[F]
   )(
     req: Request[F]
   ): OptionT[F, Response[F]] = OptionT{
     for {
-      initialTime <- Sync[F].delay(System.nanoTime())
-      _ <- Sync[F].delay(serviceMetrics.generalMetrics.activeRequests.inc())
       responseAtt <- service(req).value.attempt
-      headersElapsed <- Sync[F].delay(System.nanoTime())
-      _ <- Sync[F].delay(serviceMetrics.generalMetrics.headers.observe((headersElapsed - initialTime).toDouble))
-      respOpt <- metricsServiceHandler[F](req.method, initialTime, serviceMetrics, responseAtt)
+      _ <- Sync[F].delay(singleMetricRun.headers.observeDuration)
+      respOpt <- metricsServiceHandler[F](singleMetricRun, responseAtt)
     } yield respOpt
   }
 
   private def metricsServiceHandler[F[_]: Sync](
-    m: Method, 
-    start: Long,
-    serviceMetrics: ServiceMetrics,
+    s: SingleMetricRun,
     e: Either[Throwable, Option[Response[F]]]
   ): F[Option[Response[F]]] = {
     e.fold(
-      e => onServiceError(m, start, serviceMetrics) *> Sync[F].raiseError[Option[Response[F]]](e),
+      e => onServiceError(s) *> Sync[F].raiseError[Option[Response[F]]](e),
       _.fold(
-        Sync[F].delay(serviceMetrics.generalMetrics.activeRequests.dec()).as(Option.empty[Response[F]])
+        Sync[F].delay(s.activeRequests.dec()).as(Option.empty[Response[F]])
       )(
-        onResponse(m, start, serviceMetrics)(_).some.pure[F]
+        onResponse(s)(_).some.pure[F]
       )
     )
   }
 
 
-  private def onResponse[F[_]: Sync](m: Method, start: Long, serviceMetrics: ServiceMetrics)(r: Response[F]): Response[F] = {
+  private def onResponse[F[_]: Sync](s: SingleMetricRun)(r: Response[F]): Response[F] = {
     val newBody = r.body
-      .onFinalize{
-        for {
-          finalBodyTime <- Sync[F].delay(System.nanoTime)
-          _ <- requestMetrics(
-            serviceMetrics.requestHist, 
-            serviceMetrics.generalMetrics.totalRequests, 
-            serviceMetrics.generalMetrics.activeRequests
-          )(m, start, finalBodyTime)
-          _ <- responseMetrics(
-            serviceMetrics.responseHist,
-            r.status,
-            start,
-            finalBodyTime
-          )
-        } yield ()
-      }.handleErrorWith(e => 
-        for {
-          terminationTime <- Stream.eval(Sync[F].delay(System.nanoTime))
-          _ <- Stream.eval(Sync[F].delay(
-            serviceMetrics.generalMetrics.abnormalTerminations.observe((terminationTime - start).toDouble)
-          ))
-          result <- Stream.raiseError[Byte](e).covary[F]
-        } yield result
+      .onFinalize(requestMetrics(s))
+      .handleErrorWith(e =>
+        Stream.eval(Sync[F].delay(
+          s.abnormalTerminations.inc()
+        )) *> Stream.raiseError[Byte](e)
       )
     r.copy(body = newBody)
   }
 
-  private def onServiceError[F[_]: Sync](m: Method, begin: Long, sm: ServiceMetrics): F[Unit] = for {
-    end <- Sync[F].delay(System.nanoTime)
-    _ <- requestMetrics(
-      sm.requestHist,
-      sm.generalMetrics.totalRequests,
-      sm.generalMetrics.activeRequests
-    )(m, begin, end)
-    _ <- Sync[F].delay(sm.generalMetrics.serviceErrors.observe((end - begin).toDouble))
+  private def onServiceError[F[_]: Sync](s: SingleMetricRun): F[Unit] = for {
+    _ <- requestMetrics(s)
+    _ <- Sync[F].delay(s.serviceErrors.inc())
   } yield ()
 
 
 
-  private def requestMetrics[F[_]: Sync](rh: RequestHistograms, totalReqs: Histogram, activeReqs: Gauge)(m: Method, begin: Long, end: Long): F[Unit] = Sync[F].delay{
-    val histogram = requestHistogram(rh, m)
-    val elapsed = (end - begin).toDouble
-    histogram.observe(elapsed)
-    totalReqs.observe(elapsed)
-    activeReqs.dec()
+  private def requestMetrics[F[_]: Sync](s: SingleMetricRun): F[Unit] = Sync[F].delay{
+    s.requestTypeTimer.observeDuration()
+    s.totalRequests.observeDuration()
+    s.activeRequests.dec()
   }
 
-  private def responseMetrics[F[_]: Sync](rh: ResponseHistograms, status: Status, begin: Long, end: Long): F[Unit] = Sync[F].delay{
-    responseHistogram(rh, status).observe((end - begin).toDouble)
-  }
+  private def requestHistogram[F[_]: Sync](rt: RequestHistograms, method: Method): F[Histogram.Timer] = Sync[F].delay(method match {
+    case Method.GET => rt.getReq.startTimer
+    case Method.POST => rt.postReq.startTimer
+    case Method.PUT => rt.putReq.startTimer
+    case Method.HEAD => rt.headReq.startTimer
+    case Method.MOVE => rt.moveReq.startTimer
+    case Method.OPTIONS => rt.optionsReq.startTimer
+    case Method.TRACE => rt.traceReq.startTimer
+    case Method.CONNECT => rt.connectReq.startTimer
+    case Method.DELETE => rt.deleteReq.startTimer
+    case _ => rt.otherReq.startTimer
+  })
 
-  private def requestHistogram[F[_]: Sync](rt: RequestHistograms, method: Method): Histogram = method match {
-    case Method.GET => rt.getReq
-    case Method.POST => rt.postReq
-    case Method.PUT => rt.putReq
-    case Method.HEAD => rt.headReq
-    case Method.MOVE => rt.moveReq
-    case Method.OPTIONS => rt.optionsReq
-    case Method.TRACE => rt.traceReq
-    case Method.CONNECT => rt.connectReq
-    case Method.DELETE => rt.deleteReq
-    case _ => rt.otherReq
-  }
+    private def startedRun[F[_]: Sync](serviceMetrics: ServiceMetrics, method: Method): F[SingleMetricRun] = for {
+    requestTypeTimer <- requestHistogram(serviceMetrics.requestHist, method)
+    result <- Sync[F].delay{
+      serviceMetrics.generalMetrics.activeRequests.inc()
 
-  private def responseHistogram(responseTimers: ResponseHistograms, status: Status): Histogram =
-    status.code match {
-      case hundreds if hundreds < 200 => responseTimers.resp1xx
-      case twohundreds if twohundreds < 300 => responseTimers.resp2xx
-      case threehundreds if threehundreds < 400 => responseTimers.resp3xx
-      case fourhundreds if fourhundreds < 500 => responseTimers.resp4xx
-      case _ => responseTimers.resp5xx
-}
+      SingleMetricRun(
+        serviceMetrics.generalMetrics.activeRequests,
+        serviceMetrics.generalMetrics.totalRequests.startTimer,
+        requestTypeTimer,
+        serviceMetrics.generalMetrics.headers.startTimer,
+        serviceMetrics.generalMetrics.abnormalTerminations,
+        serviceMetrics.generalMetrics.serviceErrors
+      )
+
+    }
+  } yield result
 
   def apply[F[_]: Effect](prefix: String = "org_http4s_server", c: CollectorRegistry): F[HttpMiddleware[F]] = Sync[F].delay{
     val generalServiceMetrics = GeneralServicMetrics(
       activeRequests = Gauge.build().name(prefix + "_" + "active_requests").help("Total Active Requests.").register(c),
       totalRequests = Histogram.build().name(prefix + "_" + "requests").help("Total Requests.").register(c),
       headers = Histogram.build().name(prefix + "_" + "headers_times").help("Header Timings.").register(c),
-      abnormalTerminations = Histogram.build().name(prefix + "_" + "abnormal_terminations").help("Abnormal Terminations.").register(c),
-      serviceErrors = Histogram.build().name(prefix + "_" + "service_errors").help("Service Errors.").register(c)
+      abnormalTerminations = Counter.build().name(prefix + "_" + "abnormal_terminations").help("Abnormal Terminations.").register(c),
+      serviceErrors = Counter.build().name(prefix + "_" + "service_errors").help("Service Errors.").register(c)
     )
     val requestHistograms = RequestHistograms(
       getReq = Histogram.build().name(prefix + "_" + "get_requests").help("GET Requests.").register(c),
@@ -172,16 +147,13 @@ object PrometheusMetrics {
       deleteReq = Histogram.build().name(prefix + "_" + "delete_requests").help("DELETE Requests.").register(c),
       otherReq = Histogram.build().name(prefix + "_" + "other_requests").help("Other Requests.").register(c)
     )
-    val responseHistograms = ResponseHistograms(
-      resp1xx = Histogram.build().name(prefix + "_" + "1xx_responses").help("1xx Responses.").register(c),
-      resp2xx = Histogram.build().name(prefix + "_" + "2xx_responses").help("2xx Responses.").register(c),
-      resp3xx = Histogram.build().name(prefix + "_" + "3xx_responses").help("3xx Responses.").register(c),
-      resp4xx = Histogram.build().name(prefix + "_" + "4xx_responses").help("4xx Responses.").register(c),
-      resp5xx = Histogram.build().name(prefix + "_" + "5xx_responses").help("5xx Responses.").register(c)
-    )
-    val serviceMetrics = ServiceMetrics(generalServiceMetrics, requestHistograms, responseHistograms)
+    val serviceMetrics = ServiceMetrics(generalServiceMetrics, requestHistograms)
     
-    { service: HttpService[F] => Kleisli(metricsService[F](serviceMetrics, service)(_)) }
+    { service: HttpService[F] => Kleisli(req => 
+        OptionT.liftF(startedRun[F](serviceMetrics, req.method)).flatMap( singleMetricRun => 
+          metricsService[F](singleMetricRun, service)(req)
+        ))
+    }
   }
 
 }
