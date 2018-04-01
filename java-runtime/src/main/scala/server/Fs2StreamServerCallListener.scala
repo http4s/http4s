32eaa302344d91145e8d947090f11639624484b1
Gary Coady <gcoady@gilt.com>
2018-04-01 13:36:21 +0100

    Terminate server stream with onComplete/onHalfClose.

--- java-runtime/src/main/scala/server/Fs2StreamServerCallListener.scala
+++ java-runtime/src/main/scala/server/Fs2StreamServerCallListener.scala
@@ -2,23 +2,28 @@ package org.lyranthe.fs2_grpc.java_runtime.server
 
 import cats.arrow.FunctionK
 import cats.effect._
+import cats.implicits._
 import io.grpc._
 import fs2._
 
 import scala.concurrent.ExecutionContext
 
 class Fs2StreamServerCallListener[F[_], Request, Response] private (
-    queue: async.mutable.Queue[IO, Request],
+    queue: async.mutable.Queue[IO, Option[Request]],
     val call: Fs2ServerCall[F, Request, Response])(implicit F: Effect[F])
     extends ServerCall.Listener[Request]
     with Fs2ServerCallListener[F, Stream[F, ?], Request, Response] {
   override def onMessage(message: Request): Unit = {
     call.call.request(1)
-    queue.enqueue1(message).unsafeRunSync()
+    queue.enqueue1(message.some).unsafeRunSync()
   }
 
+  override def onHalfClose(): Unit = queue.enqueue1(none).unsafeRunSync()
+
+  override def onComplete(): Unit = queue.enqueue1(none).unsafeRunSync()
+
   override def source: Stream[F, Request] =
-    queue.dequeue.translate(FunctionK.lift(F.liftIO _))
+    queue.dequeue.unNoneTerminate.translate(FunctionK.lift(F.liftIO _))
 }
 
 object Fs2StreamServerCallListener {
@@ -27,7 +32,7 @@ object Fs2StreamServerCallListener {
         implicit F: Effect[F],
         ec: ExecutionContext): Fs2StreamServerCallListener[F, Request, Response] = {
       async
-        .unboundedQueue[IO, Request]
+        .unboundedQueue[IO, Option[Request]]
         .map(new Fs2StreamServerCallListener[F, Request, Response](_, Fs2ServerCall[F, Request, Response](call)))
         .unsafeRunSync()
     }
