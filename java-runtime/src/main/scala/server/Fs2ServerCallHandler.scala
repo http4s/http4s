2d3e0d8f8024063999b701440f78f523ba5a1e97
Ross A. Baker <ross@rossabaker.com>
2018-04-03 19:16:12 -0400

    Fix build for Scala 2.11

--- java-runtime/src/main/scala/server/Fs2ServerCallHandler.scala
+++ java-runtime/src/main/scala/server/Fs2ServerCallHandler.scala
@@ -11,44 +11,52 @@ class Fs2ServerCallHandler[F[_]](val dummy: Boolean = false) extends AnyVal {
   def unaryToUnaryCall[Request, Response](implementation: (Request, Metadata) => F[Response])(
       implicit F: Effect[F],
       ec: ExecutionContext): ServerCallHandler[Request, Response] =
-    (call: ServerCall[Request, Response], headers: Metadata) => {
-      val listener = Fs2UnaryServerCallListener[F].unsafeCreate(call)
-      listener.unsafeUnaryResponse(headers, _ flatMap { request =>
-        implementation(request, headers)
-      })
-      listener
+    new ServerCallHandler[Request, Response] {
+      def startCall(call: ServerCall[Request, Response], headers: Metadata): ServerCall.Listener[Request] = {
+        val listener = Fs2UnaryServerCallListener[F].unsafeCreate(call)
+        listener.unsafeUnaryResponse(headers, _ flatMap { request =>
+          implementation(request, headers)
+        })
+        listener
+      }
     }
 
   def unaryToStreamingCall[Request, Response](implementation: (Request, Metadata) => Stream[F, Response])(
       implicit F: Effect[F],
       ec: ExecutionContext): ServerCallHandler[Request, Response] =
-    (call: ServerCall[Request, Response], headers: Metadata) => {
-      val listener = Fs2UnaryServerCallListener[F].unsafeCreate(call)
-      listener.unsafeStreamResponse(new Metadata(),
-                                    v =>
-                                      Stream.eval(v) flatMap { request =>
-                                        implementation(request, headers)
-                                    })
-      listener
+    new ServerCallHandler[Request, Response] {
+      def startCall(call: ServerCall[Request, Response], headers: Metadata): ServerCall.Listener[Request] = {
+        val listener = Fs2UnaryServerCallListener[F].unsafeCreate(call)
+        listener.unsafeStreamResponse(new Metadata(),
+                                      v =>
+                                        Stream.eval(v) flatMap { request =>
+                                          implementation(request, headers)
+                                      })
+        listener
+      }
     }
 
   def streamingToUnaryCall[Request, Response](implementation: (Stream[F, Request], Metadata) => F[Response])(
       implicit F: Effect[F],
       ec: ExecutionContext): ServerCallHandler[Request, Response] =
-    (call: ServerCall[Request, Response], headers: Metadata) => {
-      val listener = Fs2StreamServerCallListener[F].unsafeCreate(call)
-      listener.unsafeUnaryResponse(headers, implementation(_, headers))
-      listener
+    new ServerCallHandler[Request, Response] {
+      def startCall(call: ServerCall[Request, Response], headers: Metadata): ServerCall.Listener[Request] = {
+        val listener = Fs2StreamServerCallListener[F].unsafeCreate(call)
+        listener.unsafeUnaryResponse(headers, implementation(_, headers))
+        listener
+      }
     }
 
   def streamingToStreamingCall[Request, Response](
       implementation: (Stream[F, Request], Metadata) => Stream[F, Response])(
       implicit F: Effect[F],
       ec: ExecutionContext): ServerCallHandler[Request, Response] =
-    (call: ServerCall[Request, Response], headers: Metadata) => {
-      val listener = Fs2StreamServerCallListener[F].unsafeCreate(call)
-      listener.unsafeStreamResponse(headers, implementation(_, headers))
-      listener
+    new ServerCallHandler[Request, Response] {
+      def startCall(call: ServerCall[Request, Response], headers: Metadata): ServerCall.Listener[Request] = {
+        val listener = Fs2StreamServerCallListener[F].unsafeCreate(call)
+        listener.unsafeStreamResponse(headers, implementation(_, headers))
+        listener
+      }
     }
 }
 
