8a36e939dd716ff8af5407c8602a17208bdc89b2
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2018-04-23 21:10:17 -0300

    Removed log4s stubs

--- core/js/src/main/scala/org/log4s/package.scala
+++
@@ -1,7 +0,0 @@
-package org
-
-// Poor's man implementation of log4s for scala.js
-package object log4s {
-  def getLogger: Logger = Logger(this.getClass.getName)
-  def getLogger[A](clazz: Class[A]): Logger = Logger(clazz.getName)
-}
