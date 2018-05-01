801b58ce8dd2d068c64008858b5b6325ba2e14fd
Brian P. Holt <bholt@dwolla.com>
2018-05-01 01:53:05 -0500

    copy server logging middleware to client module

---
+++ client/src/main/scala/org/http4s/client/middleware/Logger.scala
@@ -0,0 +1,78 @@
+package org.http4s
+package client
+package middleware
+
+import cats.effect._
+import fs2._
+import org.http4s.util.CaseInsensitiveString
+import org.log4s.{Logger ⇒ SLogger}
+
+/**
+  * Simple Middleware for Logging All Requests and Responses
+  */
+object Logger {
+  def apply[F[_]: Effect](
+      logHeaders: Boolean,
+      logBody: Boolean,
+      redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains
+  )(client: Client[F]): Client[F] =
+    Client.fromHttpService(
+      ResponseLogger(logHeaders, logBody, redactHeadersWhen)(
+        RequestLogger(logHeaders, logBody, redactHeadersWhen)(
+          client.toHttpService
+        )
+      )
+    )
+
+  def logMessage[F[_], A <: Message[F]](message: A)(
+      logHeaders: Boolean,
+      logBody: Boolean,
+      redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains)(
+      logger: SLogger)(implicit F: Effect[F]): F[Unit] = {
+
+    val charset = message.charset
+    val isBinary = message.contentType.exists(_.mediaType.binary)
+    val isJson = message.contentType.exists(mT =>
+      mT.mediaType == MediaType.`application/json` || mT.mediaType == MediaType.`application/hal+json`)
+
+    val isText = !isBinary || isJson
+
+    def prelude = message match {
+      case Request(method, uri, httpVersion, _, _, _) =>
+        s"$httpVersion $method $uri"
+
+      case Response(status, httpVersion, _, _, _) =>
+        s"$httpVersion $status"
+    }
+
+    val headers =
+      if (logHeaders)
+        message.headers.redactSensitive(redactHeadersWhen).toList.mkString("Headers(", ", ", ")")
+      else ""
+
+    val bodyStream = if (logBody && isText) {
+      message.bodyAsText(charset.getOrElse(Charset.`UTF-8`))
+    } else if (logBody) {
+      message.body
+        .fold(new StringBuilder)((sb, b) => sb.append(java.lang.Integer.toHexString(b & 0xff)))
+        .map(_.toString)
+    } else {
+      Stream.empty.covary[F]
+    }
+
+    val bodyText = if (logBody) {
+      bodyStream.fold("")(_ + _).map(text => s"""body="$text"""")
+    } else {
+      Stream("").covary[F]
+    }
+
+    if (!logBody && !logHeaders) F.unit
+    else {
+      bodyText
+        .map(body => s"$prelude $headers $body")
+        .map(text => logger.info(text))
+        .compile
+        .drain
+    }
+  }
+}
---
+++ client/src/main/scala/org/http4s/client/middleware/ResponseLogger.scala
@@ -0,0 +1,53 @@
+package org.http4s
+package client
+package middleware
+
+import cats.data.Kleisli
+import cats.effect._
+import cats.implicits._
+import fs2._
+import org.http4s.util.CaseInsensitiveString
+import org.log4s.getLogger
+import scala.concurrent.ExecutionContext
+
+/**
+  * Simple middleware for logging responses as they are processed
+  */
+object ResponseLogger {
+  private[this] val logger = getLogger
+
+  def apply[F[_]](
+      logHeaders: Boolean,
+      logBody: Boolean,
+      redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains
+  )(service: HttpService[F])(
+      implicit F: Effect[F],
+      ec: ExecutionContext = ExecutionContext.global): HttpService[F] =
+    Kleisli { req =>
+      service(req).semiflatMap { response =>
+        if (!logBody)
+          Logger.logMessage[F, Response[F]](response)(logHeaders, logBody, redactHeadersWhen)(
+            logger) *> F.delay(response)
+        else
+          async.refOf[F, Vector[Segment[Byte, Unit]]](Vector.empty[Segment[Byte, Unit]]).map {
+            vec =>
+              val newBody = Stream
+                .eval(vec.get)
+                .flatMap(v => Stream.emits(v).covary[F])
+                .flatMap(c => Stream.segment(c).covary[F])
+
+              response.copy(
+                body = response.body
+                // Cannot Be Done Asynchronously - Otherwise All Chunks May Not Be Appended Previous to Finalization
+                  .observe(_.segments.flatMap(s => Stream.eval_(vec.modify(_ :+ s))))
+                  .onFinalize {
+                    Logger.logMessage[F, Response[F]](response.withBodyStream(newBody))(
+                      logHeaders,
+                      logBody,
+                      redactHeadersWhen)(logger)
+                  }
+              )
+          }
+      }
+    }
+}
