ee927f52b25810ccc5ab4d04b3d39dda068da766
Andi Miller <andi@andimiller.net>
2018-05-10 22:10:11 +0100

    Add an OkHttp client

---
+++ okhttp-client/src/test/scala/org/http4s/client/okhttp/OkHttpClientSpec.scala
@@ -0,0 +1,11 @@
+package org.http4s
+package client
+package okhttp
+
+import cats.effect.{Effect, IO}
+
+class OkHttpClientSpec
+    extends ClientRouteTestBattery(
+      "OkHttp",
+      OkHttp()(Effect[IO], Http4sSpec.TestExecutionContext)
+    )
