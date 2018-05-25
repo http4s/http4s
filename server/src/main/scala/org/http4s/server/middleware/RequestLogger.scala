a2e9e1df29b199bf1dec474729f2f7a7efabb279
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-05-24 20:02:24 -0400

    Add Request Logging on Error and Unmatched

--- server/src/main/scala/org/http4s/server/middleware/RequestLogger.scala
+++ server/src/main/scala/org/http4s/server/middleware/RequestLogger.scala
@@ -42,15 +42,25 @@ object RequestLogger {
             )
 
             val response = service(changedRequest)
-            response.map { resp =>
-              resp.withBodyStream(
-                resp.body.onFinalize(
-                  Logger.logMessage[F, Request[F]](req.withBodyStream(newBody))(
+
+            response.attempt.flatMap {
+              case Left(e) => 
+                OptionT.liftF(Logger.logMessage[F, Request[F]](req.withBodyStream(newBody))(
                     logHeaders,
                     logBody,
-                    redactHeadersWhen)(logger)
+                    redactHeadersWhen)(logger)) *>
+                Sync[OptionT[F, ?]].raiseError[Response[F]](e)
+              case Right(resp) =>
+                Sync[OptionT[F, ?]].pure(
+                  resp.withBodyStream(
+                    resp.body.onFinalize(
+                      Logger.logMessage[F, Request[F]](req.withBodyStream(newBody))(
+                      logHeaders,
+                      logBody,
+                      redactHeadersWhen)(logger)
+                    )
+                  )
                 )
-              )
             }
           }
     }
