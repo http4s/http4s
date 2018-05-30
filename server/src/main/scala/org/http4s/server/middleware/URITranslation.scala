eb3f305c6e831dcd7fc1a4d58580a1b4f3d6f69e
Tom Wadeson <tom@tomwadeson.com>
2018-05-30 07:15:38 +0100

    Parameterize `URITranslation` middleware

--- server/src/main/scala/org/http4s/server/middleware/URITranslation.scala
+++ server/src/main/scala/org/http4s/server/middleware/URITranslation.scala
@@ -3,17 +3,18 @@ package server
 package middleware
 
 import cats.Functor
+import cats.data.Kleisli
 
 object URITranslation {
-  def translateRoot[F[_]: Functor](prefix: String)(
-      @deprecatedName('service) routes: HttpRoutes[F]): HttpRoutes[F] = {
+  def translateRoot[F[_], G[_]: Functor, A](prefix: String)(
+      @deprecatedName('service) http: Kleisli[F, Request[G], A]): Kleisli[F, Request[G], A] = {
     val newCaret = prefix match {
       case "/" => 0
       case x if x.startsWith("/") => x.length
       case x => x.length + 1
     }
 
-    routes.local { req: Request[F] =>
+    http.local { req: Request[G] =>
       val oldCaret = req.attributes
         .get(Request.Keys.PathInfoCaret)
         .getOrElse(0)
