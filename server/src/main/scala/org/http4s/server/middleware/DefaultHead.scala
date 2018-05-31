0fbbe08e38b0dcdb536c1043ccd843239a0838b8
Conor Reedy <ofrequisiteessence@gmail.com>
2018-05-31 16:18:33 -0400

    Place request match in the top level to ensure we're not doing more work on lower levels.  Bring back headAsTruncatedGet with updated signature.

--- server/src/main/scala/org/http4s/server/middleware/DefaultHead.scala
+++ server/src/main/scala/org/http4s/server/middleware/DefaultHead.scala
@@ -14,18 +14,24 @@ import cats.implicits._
   * requiring more optimization should implement their own HEAD handler.
   */
 object DefaultHead {
-  def apply[F[_], G[_], B](@deprecatedName('service) http: Kleisli[F, Request[G], B])(
-    implicit F: MonoidK[F],
-    G: Functor[G]): Kleisli[F, Request[G], B] = {
+  def apply[F[_]: Functor, G[_]](@deprecatedName('service) http: Http[F, G])(
+    implicit F: MonoidK[F]): Http[F, G] = {
     Kleisli { req =>
-      http(req) <+> {
-        req.method match {
-          case Method.HEAD => {
-            http(req.withMethod(Method.GET).withEmptyBody)
-          }
-          case _ => http(req) 
+      req.method match {
+        case Method.HEAD => {
+          (http <+> headAsTruncatedGet(http))(req)
+        }
+        case _ => {
+          http(req)
         }
       }
+    }
+  }
+
+  private def headAsTruncatedGet[F[_]: Functor, G[_]](http: Http[F, G]): Http[F, G] = {
+    Kleisli { req =>
+      val getReq = req.withMethod(Method.GET)
+      http(getReq).map(response => response.copy(body = response.body.drain))
     }
   }
 }
