60e3103fdd9e1712c287142e7c7c6529c7416af4
Chris Davenport <chris@christopherdavenport.tech>
2018-07-05 13:30:34 -0400

    Add StaticHeaders middleware

---
+++ server/src/main/scala/org/http4s/server/middleware/StaticHeaders.scala
@@ -0,0 +1,17 @@
+package org.http4s.server.middleware
+
+import cats.Functor
+import cats.data.{Kleisli, NonEmptyList}
+import org.http4s.{CacheDirective, Header, Headers, HttpService}
+import org.http4s.headers.`Cache-Control`
+
+object StaticHeaders {
+
+  def apply[F[_]: Functor](headers: Headers)(service: HttpService[F]): HttpService[F] = 
+    Kleisli { req =>
+      service(req).map(resp => resp.copy(headers = headers ++ resp.headers))
+    }
+
+  private val noCacheHeader: Header = `Cache-Control`(NonEmptyList.of(CacheDirective.`no-cache`()))
+  def `no-cache`[F[_]: Functor](service: HttpService[F]): HttpService[F] = StaticHeaders[F](Headers(noCacheHeader))(service)
+}
