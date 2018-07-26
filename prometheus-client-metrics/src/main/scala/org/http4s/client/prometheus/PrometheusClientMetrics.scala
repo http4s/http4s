99cc76f8f76b2ca4b1b3d7107bfb770f5f55b039
Zach Cox <zach.cox@banno.com>
2018-07-26 15:07:24 -0500

    make destination more generic, with attribute impl

--- prometheus-client-metrics/src/main/scala/org/http4s/client/prometheus/PrometheusClientMetrics.scala
+++ prometheus-client-metrics/src/main/scala/org/http4s/client/prometheus/PrometheusClientMetrics.scala
@@ -18,14 +18,8 @@ object PrometheusClientMetrics {
       case _ => "5xx"
     }
 
-  /** The value of this key in the request's attributes is used as the value for the destination metric label. */
-  val Destination = AttributeKey[String]
-  val EmptyDestination = ""
-  private def reportDestination(attributes: AttributeMap, destination: String): String =
-    attributes.get(Destination).getOrElse(destination)
-
-  private case class ClientMetrics(
-      destination: String,
+  private case class ClientMetrics[F[_]](
+      destination: Request[F] => String,
       responseDuration: Histogram,
       activeRequests: Gauge,
       responseCounter: Counter,
@@ -33,7 +27,7 @@ object PrometheusClientMetrics {
   )
 
   private def metricsClient[F[_]: Sync](
-      metrics: ClientMetrics,
+      metrics: ClientMetrics[F],
       client: Client[F]
   )(
       request: Request[F]
@@ -42,7 +36,7 @@ object PrometheusClientMetrics {
       start <- Sync[F].delay(System.nanoTime())
       _ <- Sync[F].delay(
         metrics.activeRequests
-          .labels(reportDestination(request.attributes, metrics.destination))
+          .labels(metrics.destination(request))
           .inc())
       responseAttempt <- client.open(request).attempt
       end <- Sync[F].delay(System.nanoTime())
@@ -54,14 +48,14 @@ object PrometheusClientMetrics {
       )
     } yield result
 
-  private def onClientError[F[_]: Sync](request: Request[F], metrics: ClientMetrics): F[Unit] =
+  private def onClientError[F[_]: Sync](request: Request[F], metrics: ClientMetrics[F]): F[Unit] =
     Sync[F].delay {
       //not updating responseDuration or responseCounter, since we did not receive a response
       metrics.activeRequests
-        .labels(reportDestination(request.attributes, metrics.destination))
+        .labels(metrics.destination(request))
         .dec()
       metrics.clientErrorsCounter
-        .labels(reportDestination(request.attributes, metrics.destination))
+        .labels(metrics.destination(request))
         .inc()
     }
 
@@ -70,31 +64,31 @@ object PrometheusClientMetrics {
       response: Response[F],
       start: Long,
       end: Long,
-      metrics: ClientMetrics): F[Unit] =
+      metrics: ClientMetrics[F]): F[Unit] =
     Sync[F].delay {
       metrics.responseDuration
         .labels(
-          reportDestination(request.attributes, metrics.destination),
+          metrics.destination(request),
           reportStatus(response.status))
         .observe(SimpleTimer.elapsedSecondsFromNanos(start, end))
       metrics.responseCounter
         .labels(
-          reportDestination(request.attributes, metrics.destination),
+          metrics.destination(request),
           reportStatus(response.status))
         .inc()
       metrics.activeRequests
-        .labels(reportDestination(request.attributes, metrics.destination))
+        .labels(metrics.destination(request))
         .dec()
     }
 
   def apply[F[_]: Sync](
       c: CollectorRegistry,
       prefix: String = "org_http4s_client",
-      destination: String = EmptyDestination
+      destination: Request[F] => String = {_: Request[F] => ""}
   ): Kleisli[F, Client[F], Client[F]] =
     Kleisli { client =>
       Sync[F].delay {
-        val clientMetrics = ClientMetrics(
+        val clientMetrics = ClientMetrics[F](
           destination = destination,
           responseDuration = Histogram
             .build()
@@ -124,8 +118,21 @@ object PrometheusClientMetrics {
         client.copy(open = Kleisli(metricsClient[F](clientMetrics, client)(_)))
       }
     }
+}
+
+object DestinationAttribute {
+
+  /** The value of this key in the request's attributes is used as the value for the destination metric label. */
+  val Destination = AttributeKey[String]
+
+  val EmptyDestination = ""
+
+  /** The returned function can be used when creating the PrometheusClientMetrics middleware, to extract destination from request attributes. */
+  def getDestination[F[_]](default: String = EmptyDestination): Request[F] => String =
+    _.attributes.get(Destination).getOrElse(default)
 
   /** Client middleware that sets the destination attribute of every request to the specified value. */
   def setRequestDestination[F[_]: Sync](client: Client[F], destination: String): Client[F] =
     client.copy(open = Kleisli(r => client.open(r.withAttribute(Destination, destination))))
+
 }
