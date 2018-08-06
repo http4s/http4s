43523b079ca22595fcfabc987c53cd63b8451319
Luis del Toro <luis.deltoromarzal@springer.com>
2018-08-06 16:05:31 +0200

    Reformat code to comply with scalafmt

--- client-metrics/src/main/scala/org/http4s/client/metrics/Metrics.scala
+++ client-metrics/src/main/scala/org/http4s/client/metrics/Metrics.scala
@@ -10,10 +10,11 @@ import org.http4s.client.{Client, DisposableResponse}
 
 object Metrics {
   def apply[F[_]: Sync: Timer](
-    registry: MetricRegistry,
-    prefix: String = "org.http4s.client",
-    destination: Request[F] => Option[String] = { _: Request[F] => None})
-  (client: Client[F]): Client[F] = {
+      registry: MetricRegistry,
+      prefix: String = "org.http4s.client",
+      destination: Request[F] => Option[String] = { _: Request[F] =>
+        None
+      })(client: Client[F]): Client[F] = {
 
     def withMetrics()(req: Request[F]): F[DisposableResponse[F]] = {
       val namespace = destination(req).map(d => s"${prefix}.${d}").getOrElse(s"${prefix}.default")
@@ -22,8 +23,12 @@ object Metrics {
         _ <- Sync[F].delay(registry.counter(s"${namespace}.active-requests").inc())
         resp <- client.open(req)
         now <- Timer[F].clockMonotonic(TimeUnit.NANOSECONDS)
-        _ <- Sync[F].delay(registry.timer(s"${namespace}.requests.headers").update(now - start, TimeUnit.NANOSECONDS))
-        _ <- Sync[F].delay(destination(req).map(d => registry.counter(s"${prefix}.${d}").inc()).getOrElse(Unit))
+        _ <- Sync[F].delay(
+          registry
+            .timer(s"${namespace}.requests.headers")
+            .update(now - start, TimeUnit.NANOSECONDS))
+        _ <- Sync[F].delay(
+          destination(req).map(d => registry.counter(s"${prefix}.${d}").inc()).getOrElse(Unit))
         iResp <- Sync[F].delay(instrumentResponse(start, namespace, resp))
       } yield iResp
     }
@@ -46,9 +51,12 @@ object Metrics {
       registry.timer(s"${namespace}.requests.total").update(elapsed, TimeUnit.NANOSECONDS)
       status.code match {
         case hundreds if hundreds < 200 => registry.counter(s"${namespace}.1xx-responses").inc()
-        case twohundreds if twohundreds < 300 => registry.counter(s"${namespace}.2xx-responses").inc()
-        case threehundreds if threehundreds < 400 => registry.counter(s"${namespace}.3xx-responses").inc()
-        case fourhundreds if fourhundreds < 500 => registry.counter(s"${namespace}.4xx-responses").inc()
+        case twohundreds if twohundreds < 300 =>
+          registry.counter(s"${namespace}.2xx-responses").inc()
+        case threehundreds if threehundreds < 400 =>
+          registry.counter(s"${namespace}.3xx-responses").inc()
+        case fourhundreds if fourhundreds < 500 =>
+          registry.counter(s"${namespace}.4xx-responses").inc()
         case _ => registry.counter(s"${namespace}.5xx-responses").inc()
       }
     }
