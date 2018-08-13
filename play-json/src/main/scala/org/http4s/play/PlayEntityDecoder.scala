556497fbbd08ad293a4e61c77561a107a3603d8f
julien <julien.truffaut@linklaters.com>
2018-08-13 13:47:35 +0100

    rename some implicits from circe to play

--- play-json/src/main/scala/org/http4s/play/PlayEntityDecoder.scala
+++ play-json/src/main/scala/org/http4s/play/PlayEntityDecoder.scala
@@ -8,7 +8,7 @@ import play.api.libs.json.Reads
   * Derive [[EntityDecoder]] if implicit [[Reads]] is in the scope without need to explicitly call `jsonOf`
   */
 trait PlayEntityDecoder {
-  implicit def circeEntityDecoder[F[_]: Sync, A: Reads]: EntityDecoder[F, A] = jsonOf[F, A]
+  implicit def playEntityDecoder[F[_]: Sync, A: Reads]: EntityDecoder[F, A] = jsonOf[F, A]
 }
 
 object PlayEntityDecoder extends PlayEntityDecoder
--- play-json/src/main/scala/org/http4s/play/PlayEntityEncoder.scala
+++ play-json/src/main/scala/org/http4s/play/PlayEntityEncoder.scala
@@ -8,7 +8,7 @@ import org.http4s.EntityEncoder
   * Derive [[EntityEncoder]] if implicit [[Writes]] is in the scope without need to explicitly call `jsonEncoderOf`
   */
 trait PlayEntityEncoder {
-  implicit def circeEntityEncoder[F[_]: Applicative, A: Writes]: EntityEncoder[F, A] =
+  implicit def playEntityEncoder[F[_]: Applicative, A: Writes]: EntityEncoder[F, A] =
     jsonEncoderOf(EntityEncoder.stringEncoder[F], implicitly, implicitly)
 }
 
