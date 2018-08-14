575b2f53e608ac8788d9a5d43b2681432509293e
Bjørn Madsen <bm@aeons.dk>
2018-08-14 13:54:57 +0200

    Readd deprecated URITranslation

--- server/src/main/scala/org/http4s/server/middleware/URITranslation.scala
+++ server/src/main/scala/org/http4s/server/middleware/URITranslation.scala
@@ -2,33 +2,22 @@ package org.http4s
 package server
 package middleware
 
-import cats.{Applicative, Functor}
-import cats.data.{Kleisli, OptionT}
+import cats.Functor
 
+@deprecated("Use org.http4s.server.middleware.UriTranslation instead", since = "0.18.16")
 object URITranslation {
-  def translateRoot[F[_]: Applicative](prefix: String)(service: HttpService[F]): HttpService[F] =
-    if (prefix.isEmpty || prefix == "/") service
-    else {
-      val (slashedPrefix, unslashedPrefix) =
-        if (prefix.startsWith("/")) (prefix, prefix.substring(1))
-        else (s"/$prefix", prefix)
-
-      val newCaret = slashedPrefix.length
-
-      Kleisli { req: Request[F] =>
-        val shouldTranslate =
-          req.pathInfo.startsWith(unslashedPrefix) || req.pathInfo.startsWith(slashedPrefix)
-
-        if (shouldTranslate) service(setCaret(req, newCaret))
-        else OptionT.none
-      }
-
+  def translateRoot[F[_]: Functor](prefix: String)(service: HttpService[F]): HttpService[F] = {
+    val newCaret = prefix match {
+      case "/" => 0
+      case x if x.startsWith("/") => x.length
+      case x => x.length + 1
     }
 
-  private def setCaret[F[_]: Functor](req: Request[F], newCaret: Int): Request[F] = {
-    val oldCaret = req.attributes
-      .get(Request.Keys.PathInfoCaret)
-      .getOrElse(0)
-    req.withAttribute(Request.Keys.PathInfoCaret(oldCaret + newCaret))
+    service.local { req: Request[F] =>
+      val oldCaret = req.attributes
+        .get(Request.Keys.PathInfoCaret)
+        .getOrElse(0)
+      req.withAttribute(Request.Keys.PathInfoCaret(oldCaret + newCaret))
+    }
   }
 }
