1aecf7bc21f56791eb0a4caabd0b9d02f5ac232c
kmeredith <kevin.meredith@banno.com>
2018-09-06 14:02:03 -0400

    Format code per 'test:scalafmt'.

--- client/src/main/scala/org/http4s/client/middleware/Retry.scala
+++ client/src/main/scala/org/http4s/client/middleware/Retry.scala
@@ -40,7 +40,8 @@ object Retry {
           policy(req, Left(e), attempts) match {
             case Some(duration) =>
               // info instead of error(e), because e is not discarded
-              logger.info(e)(s"Request threw an exception on attempt #$attempts. Retrying after $duration")
+              logger.info(e)(
+                s"Request threw an exception on attempt #$attempts. Retrying after $duration")
               nextAttempt(req, attempts, duration, None)
             case None =>
               logger.info(e)(
