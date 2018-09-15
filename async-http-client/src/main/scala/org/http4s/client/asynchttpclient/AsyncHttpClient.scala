e17f1aa8658496777d99b2b39577eb037983bf26
Ross A. Baker <ross@rossabaker.com>
2018-09-14 23:09:54 -0400

    Remove ExecutionContext argument to async-http-client

--- async-http-client/src/main/scala/org/http4s/client/asynchttpclient/AsyncHttpClient.scala
+++ async-http-client/src/main/scala/org/http4s/client/asynchttpclient/AsyncHttpClient.scala
@@ -15,12 +15,14 @@ import org.asynchttpclient.AsyncHandler.State
 import org.asynchttpclient.handler.StreamedAsyncHandler
 import org.asynchttpclient.request.body.generator.{BodyGenerator, ReactiveStreamsBodyGenerator}
 import org.asynchttpclient.{Request => AsyncRequest, Response => _, _}
+import org.http4s.internal.invokeCallback
 import org.http4s.util.threads._
+import org.log4s.getLogger
 import org.reactivestreams.Publisher
 import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionContext
 
 object AsyncHttpClient {
+  private[this] val logger = getLogger
 
   val defaultConfig = new DefaultAsyncHttpClientConfig.Builder()
     .setMaxConnectionsPerHost(200)
@@ -38,8 +40,7 @@ object AsyncHttpClient {
     * @param ec The ExecutionContext to run responses on
     */
   def apply[F[_]: Timer](config: AsyncHttpClientConfig = defaultConfig)(
-      implicit F: ConcurrentEffect[F],
-      ec: ExecutionContext): Client[F] = {
+      implicit F: ConcurrentEffect[F]): Client[F] = {
     val client = new DefaultAsyncHttpClient(config)
     Client(
       Kleisli { req =>
@@ -62,12 +63,11 @@ object AsyncHttpClient {
     */
   def stream[F[_]](config: AsyncHttpClientConfig = defaultConfig)(
       implicit F: ConcurrentEffect[F],
-      ec: ExecutionContext,
       timer: Timer[F]): Stream[F, Client[F]] =
     Stream.bracket(F.delay(apply(config)))(_.shutdown)
 
-  private def asyncHandler[F[_]: Timer](
-      cb: Callback[DisposableResponse[F]])(implicit F: ConcurrentEffect[F], ec: ExecutionContext) =
+  private def asyncHandler[F[_]: Timer](cb: Callback[DisposableResponse[F]])(
+      implicit F: ConcurrentEffect[F]) =
     new StreamedAsyncHandler[Unit] {
       var state: State = State.CONTINUE
       var dr: DisposableResponse[F] =
@@ -89,7 +89,7 @@ object AsyncHttpClient {
             // callback, rather than waiting for onComplete, or else we'll
             // buffer the entire response before we return it for
             // streaming consumption.
-            ec.execute(new Runnable { def run(): Unit = cb(Right(dr)) })
+            invokeCallback(logger)(cb(Right(dr)))
           }
           .runAsync(_ => IO.unit)
           .unsafeRunSync()
@@ -110,7 +110,7 @@ object AsyncHttpClient {
       }
 
       override def onThrowable(throwable: Throwable): Unit =
-        ec.execute(new Runnable { def run(): Unit = cb(Left(throwable)) })
+        invokeCallback(logger)(cb(Left(throwable)))
 
       override def onCompleted(): Unit = {
         // Don't close here.  onStream may still be being called.
