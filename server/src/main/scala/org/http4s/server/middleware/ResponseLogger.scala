b729806ed4b3b32b62b717051e4e2865ca2c86d7
ang-mic <angelos.michos@gmail.com>
2018-09-17 23:37:18 +0100

    Fixes compilation issue intruduced by Cats 1.4.0

--- server/src/main/scala/org/http4s/server/middleware/ResponseLogger.scala
+++ server/src/main/scala/org/http4s/server/middleware/ResponseLogger.scala
@@ -24,35 +24,37 @@ object ResponseLogger {
       implicit F: Effect[F],
       ec: ExecutionContext = ExecutionContext.global): HttpService[F] =
     Kleisli { req =>
-      service(req)
-        .semiflatMap { response =>
-          if (!logBody)
-            Logger.logMessage[F, Response[F]](response)(logHeaders, logBody, redactHeadersWhen)(
-              logger) *> F.delay(response)
-          else
-            async.refOf[F, Vector[Segment[Byte, Unit]]](Vector.empty[Segment[Byte, Unit]]).map {
-              vec =>
-                val newBody = Stream
-                  .eval(vec.get)
-                  .flatMap(v => Stream.emits(v).covary[F])
-                  .flatMap(c => Stream.segment(c).covary[F])
+      OptionT(
+        service(req)
+          .semiflatMap { response =>
+            if (!logBody)
+              Logger.logMessage[F, Response[F]](response)(logHeaders, logBody, redactHeadersWhen)(
+                logger) *> F.delay(response)
+            else
+              async.refOf[F, Vector[Segment[Byte, Unit]]](Vector.empty[Segment[Byte, Unit]]).map {
+                vec =>
+                  val newBody = Stream
+                    .eval(vec.get)
+                    .flatMap(v => Stream.emits(v).covary[F])
+                    .flatMap(c => Stream.segment(c).covary[F])
 
-                response.copy(
-                  body = response.body
-                  // Cannot Be Done Asynchronously - Otherwise All Chunks May Not Be Appended Previous to Finalization
-                    .observe(_.segments.flatMap(s => Stream.eval_(vec.modify(_ :+ s))))
-                    .onFinalize {
-                      Logger.logMessage[F, Response[F]](response.withBodyStream(newBody))(
-                        logHeaders,
-                        logBody,
-                        redactHeadersWhen)(logger)
-                    }
-                )
-            }
-        }
-        .orElse(OptionT(F.delay(logger.info("service returned None")).as(None)))
-        .handleErrorWith(t =>
-          OptionT.liftF(F.delay(logger.info(s"service raised an error: ${t.getClass}")) *> F
-            .raiseError[Response[F]](t)))
+                  response.copy(
+                    body = response.body
+                    // Cannot Be Done Asynchronously - Otherwise All Chunks May Not Be Appended Previous to Finalization
+                      .observe(_.segments.flatMap(s => Stream.eval_(vec.modify(_ :+ s))))
+                      .onFinalize {
+                        Logger.logMessage[F, Response[F]](response.withBodyStream(newBody))(
+                          logHeaders,
+                          logBody,
+                          redactHeadersWhen)(logger)
+                      }
+                  )
+              }
+          }
+          .orElse(OptionT(F.delay(logger.info("service returned None")).as(None)))
+          .value
+          .handleErrorWith(t =>
+            F.delay(logger.info(s"service raised an error: ${t.getClass}")) *> F
+              .raiseError[Option[Response[F]]](t)))
     }
 }
