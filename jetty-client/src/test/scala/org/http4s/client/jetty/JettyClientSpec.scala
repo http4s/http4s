e2f45179ea521d0078d4c493434eb02b2b8c4bf4
Ross A. Baker <ross@rossabaker.com>
2018-09-22 01:39:39 -0400

    I still can't sleep

--- jetty-client/src/test/scala/org/http4s/client/jetty/JettyClientSpec.scala
+++ jetty-client/src/test/scala/org/http4s/client/jetty/JettyClientSpec.scala
@@ -2,8 +2,8 @@ package org.http4s
 package client
 package jetty
 
-import cats.effect.{IO, Resource}
+import cats.effect.IO
 
 class JettyClientSpec extends ClientRouteTestBattery("JettyClient") {
-  override def clientResource = Resource.make(JettyClient[IO]())(_.shutdown)
+  override def clientResource = JettyClient.resource[IO]()
 }
