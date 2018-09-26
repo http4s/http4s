1fa6ef78648a8150cc453d0366ce136f630a5879
Scala steward <scala-steward@timepit.eu>
2018-09-26 07:01:11 +0200

    Update jawn-fs2 to 0.13.0-RC1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -297,7 +297,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val gatlingHighCharts                = "io.gatling.highcharts"  %  "gatling-charts-highcharts" % gatlingTest.revision
   lazy val http4sWebsocket                  = "org.http4s"             %% "http4s-websocket"          % "0.2.1"
   lazy val javaxServletApi                  = "javax.servlet"          %  "javax.servlet-api"         % "3.1.0"
-  lazy val jawnFs2                          = "org.http4s"             %% "jawn-fs2"                  % "0.13.0-M4"
+  lazy val jawnFs2                          = "org.http4s"             %% "jawn-fs2"                  % "0.13.0-RC1"
   lazy val jawnJson4s                       = "org.spire-math"         %% "jawn-json4s"               % "0.13.0"
   lazy val jawnPlay                         = "org.spire-math"         %% "jawn-play"                 % "0.13.0"
   lazy val jettyClient                      = "org.eclipse.jetty"      %  "jetty-client"              % "9.4.12.v20180830"
