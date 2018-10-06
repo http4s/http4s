d4c88016cc1da96611d6ebdfa1c222a356d198f3
Calvin Brown <calvin.brown@banno.com>
2018-10-06 12:51:41 -0500

    Finishing algebra and moving onto tests

--- core/src/main/scala/org/http4s/Static.scala
+++ core/src/main/scala/org/http4s/Static.scala
@@ -1,30 +1,55 @@
 package org.http4s
 
+
+import java.io._
+import java.net.URL
+
+
 import cats.implicits.{catsSyntaxEither => _, _}
 import cats.data.OptionT
 import cats.effect.{ContextShift, Sync}
 import fs2.Stream.empty
+import fs2.io._
 import fs2.io.file.readRange
-import java.io.File
-import org.http4s.StaticFile._
 import org.http4s.headers._
 import org.http4s.Status.NotModified
 import org.log4s.getLogger
+
 import scala.concurrent.ExecutionContext
 
 
 trait Static[F[_]] {
+
+  def fromString(
+    url: String,
+    blockingExecutionContext: ExecutionContext,
+    req: Option[Request[F]] = None): OptionT[F, Response[F]]
+
+  def fromResource(
+    name: String,
+    blockingExecutionContext: ExecutionContext,
+    req: Option[Request[F]] = None,
+    preferGzipped: Boolean = false): OptionT[F, Response[F]]
+
+
+  def fromURL(
+     url: URL,
+     blockingExecutionContext: ExecutionContext,
+     req: Option[Request[F]] = None): OptionT[F, Response[F]]
+
   def fromFile(f: File,
                blockingExecutionContext: ExecutionContext,
                start: Long = 0L,
-               end: Option[Long],
-               buffsize: Int = 8192,
+               end: Option[Long] = None,
+               buffsize: Int = Static.DefaultBufferSize,
                req: Option[Request[F]] = None,
-               etagCalculator: File => F[String]): OptionT[F, Response[F]]
+               etagCalculator: File => F[String] = Static.calcETag[F]): OptionT[F, Response[F]]
 }
 
 object Static {
   private[this] val logger = getLogger
+  private[http4s] val staticFileKey = AttributeKey[File]
+  val DefaultBufferSize = 8192
 
 
   def calcETag[F[_]: Sync]: File => F[String] =
@@ -66,6 +91,74 @@ object Static {
     } yield nm
 
   def apply[F[_]](implicit F: Sync[F], cs: ContextShift[F]) = new Static[F] {
+
+    def fromString(
+                                              url: String,
+                                              blockingExecutionContext: ExecutionContext,
+                                              req: Option[Request[F]] = None): OptionT[F, Response[F]] =
+      fromFile(new File(url), blockingExecutionContext, req = req)
+
+
+    def fromResource(
+                                                name: String,
+                                                blockingExecutionContext: ExecutionContext,
+                                                req: Option[Request[F]] = None,
+                                                preferGzipped: Boolean = false): OptionT[F, Response[F]] = {
+      val tryGzipped = preferGzipped && req.flatMap(_.headers.get(`Accept-Encoding`)).exists {
+        acceptEncoding =>
+          acceptEncoding.satisfiedBy(ContentCoding.gzip) || acceptEncoding.satisfiedBy(
+            ContentCoding.`x-gzip`)
+      }
+
+      val gzUrl: OptionT[F, URL] =
+        if (tryGzipped) OptionT.fromOption(Option(getClass.getResource(name + ".gz")))
+        else OptionT.none
+
+      gzUrl
+        .flatMap { url =>
+          // Guess content type from the name without ".gz"
+          val contentType = nameToContentType(name)
+          val headers = `Content-Encoding`(ContentCoding.gzip) :: contentType.toList
+
+          fromURL(url, blockingExecutionContext, req).map(
+            _.removeHeader(`Content-Type`).putHeaders(headers: _*))
+        }
+        .orElse(OptionT
+          .fromOption[F](Option(getClass.getResource(name)))
+          .flatMap(fromURL(_, blockingExecutionContext, req)))
+    }
+
+    def fromURL(
+       url: URL,
+       blockingExecutionContext: ExecutionContext,
+       req: Option[Request[F]] = None): OptionT[F, Response[F]] =
+      OptionT.liftF(F.delay {
+        val urlConn = url.openConnection
+        val lastmod = HttpDate.fromEpochSecond(urlConn.getLastModified / 1000).toOption
+        val ifModifiedSince = req.flatMap(_.headers.get(`If-Modified-Since`))
+        val expired = (ifModifiedSince, lastmod).mapN(_.date < _).getOrElse(true)
+
+        if (expired) {
+          val lastModHeader: List[Header] = lastmod.map(`Last-Modified`(_)).toList
+          val contentType = nameToContentType(url.getPath).toList
+          val len = urlConn.getContentLengthLong
+          val lenHeader =
+            if (len >= 0) `Content-Length`.unsafeFromLong(len)
+            else `Transfer-Encoding`(TransferCoding.chunked)
+          val headers = Headers(lenHeader :: lastModHeader ::: contentType)
+
+          Response(
+            headers = headers,
+            body =
+              readInputStream[F](F.delay(url.openStream), DefaultBufferSize, blockingExecutionContext)
+          )
+        } else {
+          urlConn.getInputStream.close()
+          Response(NotModified)
+        }
+      })
+
+
     def fromFile(f: File,
                  blockingExecutionContext: ExecutionContext,
                  start: Long = 0L,
---
+++ tests/src/test/scala/org/http4s/StaticSpec.scala
@@ -0,0 +1,145 @@
+package org.http4s
+
+import cats.effect.{IO, Sync}
+import java.io.File
+import java.nio.file.Files
+import org.http4s.Status.NotModified
+import org.http4s.headers._
+import org.specs2.matcher.MatchResult
+
+
+class StaticSpec extends Http4sSpec {
+
+  val static = Static(Sync[IO], contextShift)
+
+  "StaticFile" should {
+    "Determine the media-type based on the files extension" in {
+
+      def check(f: File, tpe: Option[MediaType]): MatchResult[Any] = {
+        val r = static.fromFile(f = f, blockingExecutionContext = testBlockingExecutionContext).value.unsafeRunSync
+
+        r must beSome[Response[IO]]
+        r.flatMap(_.headers.get(`Content-Type`)) must_== tpe.map(t => `Content-Type`(t))
+        // Other headers must be present
+        r.flatMap(_.headers.get(`Last-Modified`)).isDefined must beTrue
+        r.flatMap(_.headers.get(`Content-Length`)).isDefined must beTrue
+        r.flatMap(_.headers.get(`Content-Length`).map(_.length)) must beSome(f.length())
+      }
+
+      val tests = Seq(
+        "/Animated_PNG_example_bouncing_beach_ball.png" -> Some(MediaType.image.png),
+        "/test.fiddlefaddle" -> None)
+      forall(tests) {
+        case (p, om) =>
+          check(new File(getClass.getResource(p).toURI), om)
+      }
+    }
+
+    "handle an empty file" in {
+      val emptyFile = File.createTempFile("empty", ".tmp")
+
+      static.fromFile(f = emptyFile, blockingExecutionContext = testBlockingExecutionContext).value must returnValue(
+        beSome[Response[IO]])
+    }
+
+    "Don't send unmodified files" in {
+      val emptyFile = File.createTempFile("empty", ".tmp")
+
+      val request =
+        Request[IO]().putHeaders(`If-Modified-Since`(HttpDate.MaxValue))
+      val response = StaticFile
+        .fromFile[IO](emptyFile, testBlockingExecutionContext, Some(request))
+        .value
+        .unsafeRunSync
+      response must beSome[Response[IO]]
+      response.map(_.status) must beSome(NotModified)
+    }
+
+    "Don't send unmodified files by ETag" in {
+      val emptyFile = File.createTempFile("empty", ".tmp")
+
+      val request =
+        Request[IO]().putHeaders(
+          ETag(s"${emptyFile.lastModified().toHexString}-${emptyFile.length().toHexString}"))
+      val response = StaticFile
+        .fromFile[IO](emptyFile, testBlockingExecutionContext, Some(request))
+        .value
+        .unsafeRunSync
+      response must beSome[Response[IO]]
+      response.map(_.status) must beSome(NotModified)
+    }
+
+    "Send partial file" in {
+      def check(path: String): MatchResult[Any] = {
+        val f = new File(path)
+        val r =
+          StaticFile
+            .fromFile[IO](
+              f,
+              0,
+              1,
+              StaticFile.DefaultBufferSize,
+              testBlockingExecutionContext,
+              None,
+              StaticFile.calcETag[IO])
+            .value
+            .unsafeRunSync
+
+        r must beSome[Response[IO]]
+        // Length is only 1 byte
+        r.flatMap(_.headers.get(`Content-Length`).map(_.length)) must beSome(1)
+        // get the Body to check the actual size
+        r.map(_.body.compile.toVector.unsafeRunSync.length) must beSome(1)
+      }
+
+      val tests = List(
+        "./testing/src/test/resources/logback-test.xml",
+        "./server/src/test/resources/testresource.txt",
+        ".travis.yml")
+
+      forall(tests)(check)
+    }
+
+    "Send file larger than BufferSize" in {
+      val emptyFile = File.createTempFile("some", ".tmp")
+      emptyFile.deleteOnExit()
+
+      val fileSize = StaticFile.DefaultBufferSize * 2 + 10
+
+      val gibberish = (for {
+        i <- 0 until fileSize
+      } yield i.toByte).toArray
+      Files.write(emptyFile.toPath, gibberish)
+
+      def check(file: File): MatchResult[Any] = {
+        val r = StaticFile
+          .fromFile[IO](
+            file,
+            0,
+            fileSize.toLong - 1,
+            StaticFile.DefaultBufferSize,
+            testBlockingExecutionContext,
+            None,
+            StaticFile.calcETag[IO])
+          .value
+          .unsafeRunSync
+
+        r must beSome[Response[IO]]
+        // Length of the body must match
+        r.flatMap(_.headers.get(`Content-Length`).map(_.length)) must beSome(fileSize - 1)
+        // get the Body to check the actual size
+        val body = r.map(_.body.compile.toVector.unsafeRunSync)
+        body.map(_.length) must beSome(fileSize - 1)
+        // Verify the context
+        body.map(
+          bytes =>
+            java.util.Arrays.equals(
+              bytes.toArray,
+              java.util.Arrays.copyOfRange(gibberish, 0, fileSize - 1))) must beSome(true)
+      }
+
+      check(emptyFile)
+    }
+  }
+
+}
