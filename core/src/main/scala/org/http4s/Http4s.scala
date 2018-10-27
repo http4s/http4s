4b4a34685f09273613bca6a39734f260d6530a86
Bjørn Madsen <bm@aeons.dk>
2018-10-27 20:57:24 +0200

    Deprecate Http4s, Http4sInstances and Http4sFunctions

--- core/src/main/scala/org/http4s/Http4s.scala
+++ core/src/main/scala/org/http4s/Http4s.scala
@@ -1,9 +1,12 @@
 package org.http4s
 
+@deprecated("Use org.http4s.implicits._ instead", "0.20.0-M2")
 trait Http4s extends Http4sInstances with Http4sFunctions with syntax.AllSyntax
 
+@deprecated("Use org.http4s.implicits._ instead", "0.20.0-M2")
 object Http4s extends Http4s
 
+@deprecated("Import from or use EntityDecoder/EntityEncoder directly instead", "0.20.0-M2")
 trait Http4sInstances
     extends EntityDecoderInstances
     with HttpVersionInstances
@@ -13,8 +16,11 @@ trait Http4sInstances
     with MethodInstances
     with StatusInstances
 
+@deprecated("Import from or use EntityDecoder/EntityEncoder directly instead", "0.20.0-M2")
 object Http4sInstances extends Http4sInstances
 
+@deprecated("Use org.http4s.qvalue._ or org.http4s.Uri._ instead", "0.20.0-M2")
 trait Http4sFunctions extends QValueFunctions with UriFunctions
 
+@deprecated("Use org.http4s.qvalue._ or org.http4s.Uri._ instead", "0.20.0-M2")
 object Http4sFunctions extends Http4sFunctions
