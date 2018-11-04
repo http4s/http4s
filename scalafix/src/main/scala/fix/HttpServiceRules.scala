2ba3f64adb12db7ff98cf12fae5b47c434e3b619
amarrella <zioale91@gmail.com>
2018-11-04 17:19:35 +0000

    Using symbolmatcher in httpservice rules

--- scalafix/src/main/scala/fix/HttpServiceRules.scala
+++ scalafix/src/main/scala/fix/HttpServiceRules.scala
@@ -4,9 +4,12 @@ import scalafix.v1._
 import scala.meta._
 
 object HttpServiceRules {
-  def unapply(t: Tree): Option[Patch] = t match {
-    case t @ Type.Name("HttpService") => Some(Patch.replaceTree(t, "HttpRoutes"))
-    case t @ Term.Name("HttpService") => Some(Patch.replaceTree(t, "HttpRoutes.of"))
+  val httpService = SymbolMatcher.normalized("org/http4s/HttpService.")
+  def unapply(t: Tree)(implicit doc: SemanticDocument): Option[Patch] = t match {
+    case httpService(Term.Apply(Term.ApplyType(t, _), _)) =>
+      Some(Patch.replaceTree(t, "HttpRoutes.of"))
+    case httpService(Term.Apply(t: Term.Name, _)) => Some(Patch.replaceTree(t, "HttpRoutes.of"))
+    case Type.Apply(t @ Type.Name("HttpService"), _) => Some(Patch.replaceTree(t, "HttpRoutes"))
     case t @ Importee.Name(Name("HttpService")) => Some(Patch.replaceTree(t, "HttpRoutes"))
     case _ => None
   }
