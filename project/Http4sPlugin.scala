83107a5017561e27df73b7ccc9a2ae995542bacd
Scala steward <me@scala-steward.org>
2019-01-17 02:24:40 +0100

    Update play-json to 2.7.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -311,7 +311,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val logbackClassic                   = "ch.qos.logback"         %  "logback-classic"           % "1.2.3"
   lazy val mockito                          = "org.mockito"            %  "mockito-core"              % "2.23.4"
   lazy val okhttp                           = "com.squareup.okhttp3"   %  "okhttp"                    % "3.12.1"
-  lazy val playJson                         = "com.typesafe.play"      %% "play-json"                 % "2.6.13"
+  lazy val playJson                         = "com.typesafe.play"      %% "play-json"                 % "2.7.0"
   lazy val prometheusClient                 = "io.prometheus"          %  "simpleclient"              % "0.6.0"
   lazy val prometheusCommon                 = "io.prometheus"          %  "simpleclient_common"       % prometheusClient.revision
   lazy val prometheusHotspot                = "io.prometheus"          %  "simpleclient_hotspot"      % prometheusClient.revision
