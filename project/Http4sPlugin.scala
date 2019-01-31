bfd956e72cd283c5fe75503674d6992445afdc96
Scala steward <me@scala-steward.org>
2019-01-31 20:41:41 +0100

    Update cats-core, cats-kernel-laws, ... to 1.6.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -279,7 +279,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val asyncHttpClient                  = "org.asynchttpclient"    %  "async-http-client"         % "2.7.0"
   lazy val blaze                            = "org.http4s"             %% "blaze-http"                % "0.14.0-M12"
   lazy val boopickle                        = "io.suzaku"              %% "boopickle"                 % "1.3.0"
-  lazy val cats                             = "org.typelevel"          %% "cats-core"                 % "1.5.0"
+  lazy val cats                             = "org.typelevel"          %% "cats-core"                 % "1.6.0"
   lazy val catsEffect                       = "org.typelevel"          %% "cats-effect"               % "1.1.0"
   lazy val catsEffectLaws                   = "org.typelevel"          %% "cats-effect-laws"          % catsEffect.revision
   lazy val catsKernelLaws                   = "org.typelevel"          %% "cats-kernel-laws"          % cats.revision
