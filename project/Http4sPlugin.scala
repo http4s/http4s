5e44cabe36e16591089e36a60131e895a6c9efc8
Ross A. Baker <ross@rossabaker.com>
2019-04-26 13:36:31 -0400

    cats-2.0.0-M1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -303,7 +303,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val asyncHttpClient                  = "org.asynchttpclient"    %  "async-http-client"         % "2.8.1"
   lazy val blaze                            = "org.http4s"             %% "blaze-http"                % "0.14.0-RC1"
   lazy val boopickle                        = "io.suzaku"              %% "boopickle"                 % "1.3.0"
-  lazy val cats                             = "org.typelevel"          %% "cats-core"                 % "1.6.0"
+  lazy val cats                             = "org.typelevel"          %% "cats-core"                 % "2.0.0-M1"
   lazy val catsEffect                       = "org.typelevel"          %% "cats-effect"               % "1.1.0"
   lazy val catsEffectLaws                   = "org.typelevel"          %% "cats-effect-laws"          % catsEffect.revision
   lazy val catsKernelLaws                   = "org.typelevel"          %% "cats-kernel-laws"          % cats.revision
