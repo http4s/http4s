5bb9f57092cdd4f0bdf1d308e99aa89aa17fca8b
Ben Plommer <ben.plommer@ovoenergy.com>
2019-04-29 08:55:55 +0100

    Add imapK syntax for HttpApp and HttpRoutes

--- core/src/main/scala/org/http4s/syntax/KleisliSyntax.scala
+++ core/src/main/scala/org/http4s/syntax/KleisliSyntax.scala
@@ -2,15 +2,35 @@ package org.http4s
 package syntax
 
 import cats._
+import cats.syntax.functor._
+import cats.effect.Sync
 import cats.data.{Kleisli, OptionT}
 
 trait KleisliSyntax {
   implicit def http4sKleisliResponseSyntax[F[_]: Functor, A](
       service: Kleisli[OptionT[F, ?], A, Response[F]]): KleisliResponseOps[F, A] =
     new KleisliResponseOps[F, A](service)
+
+  implicit def http4sKleisliHttpRoutesSyntax[F[_]: Sync](
+      routes: HttpRoutes[F]): KleisliHttpRoutesOps[F] =
+    new KleisliHttpRoutesOps[F](routes)
+
+  implicit def http4sKleisliHttpAppOps[F[_]: Sync](
+      app: HttpApp[F]): KleisliHttpAppOps[F] =
+    new KleisliHttpAppOps[F](app)
 }
 
 final class KleisliResponseOps[F[_]: Functor, A](self: Kleisli[OptionT[F, ?], A, Response[F]]) {
   def orNotFound: Kleisli[F, A, Response[F]] =
     Kleisli(a => self.run(a).getOrElse(Response.notFound))
+}
+
+final class KleisliHttpRoutesOps[F[_] : Sync](self: HttpRoutes[F]) {
+  def imapK[G[_] : Sync](fk: F ~> G)(gK: G ~> F): HttpRoutes[G] =
+    HttpRoutes(request => self.run(request.mapK(gK)).mapK(fk).map(_.mapK(fk)))
+}
+
+final class KleisliHttpAppOps[F[_] : Sync](self: HttpApp[F]) {
+  def imapK[G[_] : Sync](fk: F ~> G)(gK: G ~> F): HttpApp[G] =
+    HttpApp(request => fk(self.run(request.mapK(gK)).map(_.mapK(fk))))
 }
