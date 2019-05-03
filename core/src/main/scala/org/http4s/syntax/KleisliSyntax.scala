588c89e5ba9c3d3420d5c382234a8fe3a0b016df
Ben Plommer <ben.plommer@ovoenergy.com>
2019-05-03 10:42:14 +0100

    change imapK to translate

--- core/src/main/scala/org/http4s/syntax/KleisliSyntax.scala
+++ core/src/main/scala/org/http4s/syntax/KleisliSyntax.scala
@@ -17,7 +17,7 @@ trait KleisliSyntaxBinCompat0 {
       routes: HttpRoutes[F]): KleisliHttpRoutesOps[F] =
     new KleisliHttpRoutesOps[F](routes)
 
-  implicit def http4sKleisliHttpAppOps[F[_]: Sync](app: HttpApp[F]): KleisliHttpAppOps[F] =
+  implicit def http4sKleisliHttpAppSyntax[F[_]: Sync](app: HttpApp[F]): KleisliHttpAppOps[F] =
     new KleisliHttpAppOps[F](app)
 }
 
@@ -27,11 +27,11 @@ final class KleisliResponseOps[F[_]: Functor, A](self: Kleisli[OptionT[F, ?], A,
 }
 
 final class KleisliHttpRoutesOps[F[_]: Sync](self: HttpRoutes[F]) {
-  def imapK[G[_]: Sync](fk: F ~> G)(gK: G ~> F): HttpRoutes[G] =
+  def translate[G[_]: Sync](fk: F ~> G)(gK: G ~> F): HttpRoutes[G] =
     HttpRoutes(request => self.run(request.mapK(gK)).mapK(fk).map(_.mapK(fk)))
 }
 
 final class KleisliHttpAppOps[F[_]: Sync](self: HttpApp[F]) {
-  def imapK[G[_]: Sync](fk: F ~> G)(gK: G ~> F): HttpApp[G] =
+  def translate[G[_]: Sync](fk: F ~> G)(gK: G ~> F): HttpApp[G] =
     HttpApp(request => fk(self.run(request.mapK(gK)).map(_.mapK(fk))))
 }
