e5cd8aa7d2337f8ac96becff6b22893eed491206
Ross A. Baker <ross@rossabaker.com>
2019-05-28 09:35:50 -0400

    Benchmark encodeHex rewrite

---
+++ bench/src/main/scala/org/http4s/bench/EncodeHexBench.scala
@@ -0,0 +1,23 @@
+package org.http4s
+package bench
+
+import java.util.concurrent.TimeUnit
+
+import org.openjdk.jmh.annotations._
+
+// sbt "bench/jmh:run -i 10 -wi 10 -f 2 -t 1 org.http4s.bench.EncodeHexBench"
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+class EncodeHexBench {
+  val bytes = {
+    val r = new scala.util.Random(2597)
+    val bs = new Array[Byte](8192)
+    r.nextBytes(bs)
+    bs
+  }
+
+  @Benchmark
+  def encodeHex: Array[Char] =
+    org.http4s.internal.encodeHex(bytes)
+}
