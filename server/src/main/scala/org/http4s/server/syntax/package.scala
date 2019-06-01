a11e40d19743a58a47d0cdf643f75e8598a970f0
TomTriple <thomas-hoefer@gmx.net>
2019-06-01 15:22:06 +0200

    correction
    Co-Authored-By: Diego E. Alonso Blas <diesalbla@gmail.com>

--- server/src/main/scala/org/http4s/server/syntax/package.scala
+++ server/src/main/scala/org/http4s/server/syntax/package.scala
@@ -7,7 +7,7 @@ import cats.implicits._
 
 package object syntax {
   @deprecated(
-    "Import `cats.implicits._` and use `kleisli1 <+> kleisli` instead. Ensure -Ypartial-unification is enabled.",
+    "Import `cats.implicits._` and use `kleisli1 <+> kleisli2` instead. Ensure -Ypartial-unification is enabled.",
     "0.16")
   final implicit class ServiceOps[F[_], A, B](val kleisli: Kleisli[F, A, B])(
       implicit B: Semigroup[F[B]]) {
