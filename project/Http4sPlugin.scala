25d3c61360b4f091814f52d72626eec70b84c61c
Scala Steward <me@scala-steward.org>
2019-06-03 19:25:27 +0200

    Update async-http-client to 2.10.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -287,7 +287,7 @@ object Http4sPlugin extends AutoPlugin {
 
   lazy val alpnBoot                         = "org.mortbay.jetty.alpn" %  "alpn-boot"                 % "8.1.13.v20181017"
   lazy val argonaut                         = "io.argonaut"            %% "argonaut"                  % "6.2.3"
-  lazy val asyncHttpClient                  = "org.asynchttpclient"    %  "async-http-client"         % "2.9.0"
+  lazy val asyncHttpClient                  = "org.asynchttpclient"    %  "async-http-client"         % "2.10.0"
   lazy val blaze                            = "org.http4s"             %% "blaze-http"                % "0.14.2"
   lazy val boopickle                        = "io.suzaku"              %% "boopickle"                 % "1.3.0"
   lazy val cats                             = "org.typelevel"          %% "cats-core"                 % "1.6.0"
