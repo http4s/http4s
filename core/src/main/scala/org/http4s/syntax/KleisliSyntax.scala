78ca6d052e08c8731871b88a73cdd90d5e85271e
Thomas Hoefer <mail@tomhoefer.de>
2019-06-04 18:40:31 +0200

    append 'optionT' to function name

--- core/src/main/scala/org/http4s/syntax/KleisliSyntax.scala
+++ core/src/main/scala/org/http4s/syntax/KleisliSyntax.scala
@@ -7,7 +7,7 @@ import cats.effect.Sync
 import cats.data.{Kleisli, OptionT}
 
 trait KleisliSyntax {
-  implicit def http4sKleisliResponseSyntax[F[_]: Functor, A](
+  implicit def http4sKleisliResponseSyntaxOptionT[F[_]: Functor, A](
       kleisli: Kleisli[OptionT[F, ?], A, Response[F]]): KleisliResponseOps[F, A] =
     new KleisliResponseOps[F, A](kleisli)
 }
