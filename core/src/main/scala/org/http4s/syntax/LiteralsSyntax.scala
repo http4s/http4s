9440015905d57ba67d5ac5c9c061f8ffbf3dc8e7
Ross A. Baker <ross@rossabaker.com>
2019-07-07 00:05:19 -0400

    Improved ipv6 support in Uri

--- core/src/main/scala/org/http4s/syntax/LiteralsSyntax.scala
+++ core/src/main/scala/org/http4s/syntax/LiteralsSyntax.scala
@@ -10,6 +10,7 @@ class LiteralsOps(val sc: StringContext) extends AnyVal {
   def uri(args: Any*): Uri = macro LiteralSyntaxMacros.uriInterpolator
   def scheme(args: Any*): Uri.Scheme = macro LiteralSyntaxMacros.schemeInterpolator
   def ipv4(args: Any*): Uri.Ipv4Address = macro LiteralSyntaxMacros.ipv4AddressInterpolator
+  def ipv6(args: Any*): Uri.Ipv6Address = macro LiteralSyntaxMacros.ipv6AddressInterpolator
   def mediaType(args: Any*): MediaType = macro LiteralSyntaxMacros.mediaTypeInterpolator
   def qValue(args: Any*): QValue = macro LiteralSyntaxMacros.qValueInterpolator
 }
