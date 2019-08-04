824d8db8e870a519cb70624c1bc3df8b638e8876
Bjørn Madsen <bm@aeons.dk>
2019-08-04 20:40:10 +0200

    Add explicit dependency on scala-reflect

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -300,6 +300,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val parboiled                        = "org.http4s"             %% "parboiled"                 % "2.0.1"
   lazy val quasiquotes                      = "org.scalamacros"        %% "quasiquotes"               % "2.1.0"
   lazy val scalacheck                       = "org.scalacheck"         %% "scalacheck"                % "1.14.0"
+  def scalaReflect(sv: String)              = "org.scala-lang"         %  "scala-reflect"             % sv
   def scalatagsApi(sv: String)              = "com.lihaoyi"            %% "scalatags"                 % CrossVersion.partialVersion(sv).filter(_._2 > 11).fold("0.6.8")(_ => "0.7.0")
   lazy val scalaXml                         = "org.scala-lang.modules" %% "scala-xml"                 % "1.2.0"
   lazy val specs2Core                       = "org.specs2"             %% "specs2-core"               % "4.6.0"
