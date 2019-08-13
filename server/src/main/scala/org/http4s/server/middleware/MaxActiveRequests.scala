a5fa7ec8f234dac351b6b836da2ef8edec5f194a
Chris Davenport <chris@christopherdavenport.tech>
2019-08-13 13:11:39 -0700

    Scalafmt

--- server/src/main/scala/org/http4s/server/middleware/MaxActiveRequests.scala
+++ server/src/main/scala/org/http4s/server/middleware/MaxActiveRequests.scala
@@ -21,20 +21,21 @@ object MaxActiveRequests {
       maxActive: Long,
       defaultResp: Response[F] = Response[F](status = Status.ServiceUnavailable)
   ): G[Kleisli[F, Request[F], Response[F]] => Kleisli[F, Request[F], Response[F]]] =
-    Semaphore.in[G, F](maxActive).map { sem =>
-      { http: Kleisli[F, Request[F], Response[F]] =>
-        Kleisli { a: Request[F] =>
-          sem.tryAcquire.bracketCase { bool =>
-            if (bool) http.run(a).map(resp => resp.copy(body = resp.body.onFinalize(sem.release)))
-            else defaultResp.pure[F]
-          } {
-            case (bool, ExitCase.Canceled | ExitCase.Error(_)) =>
-              if (bool) sem.release
-              else Sync[F].unit
-            case (_, ExitCase.Completed) => Sync[F].unit
+    Semaphore.in[G, F](maxActive).map {
+      sem =>
+        { http: Kleisli[F, Request[F], Response[F]] =>
+          Kleisli { a: Request[F] =>
+            sem.tryAcquire.bracketCase { bool =>
+              if (bool) http.run(a).map(resp => resp.copy(body = resp.body.onFinalize(sem.release)))
+              else defaultResp.pure[F]
+            } {
+              case (bool, ExitCase.Canceled | ExitCase.Error(_)) =>
+                if (bool) sem.release
+                else Sync[F].unit
+              case (_, ExitCase.Completed) => Sync[F].unit
+            }
           }
         }
-      }
     }
 
   def httpRoutes[F[_]: Concurrent](
@@ -53,24 +54,25 @@ object MaxActiveRequests {
     OptionT[F, ?],
     Request[F],
     Response[F]]] =
-    Semaphore.in[G, F](maxActive).map { sem =>
-      { http: Kleisli[OptionT[F, ?], Request[F], Response[F]] =>
-        Kleisli { a: Request[F] =>
-          Concurrent[OptionT[F, ?]].bracketCase(OptionT.liftF(sem.tryAcquire)) { bool =>
-            if (bool)
-              http
-                .run(a)
-                .map(resp => resp.copy(body = resp.body.onFinalize(sem.release)))
-                .orElseF(sem.release.as(None))
-            else OptionT.pure[F](defaultResp)
-          } {
-            case (bool, ExitCase.Canceled | ExitCase.Error(_)) =>
-              if (bool) OptionT.liftF(sem.release)
-              else OptionT.pure[F](())
-            case (_, ExitCase.Completed) => OptionT.pure[F](())
+    Semaphore.in[G, F](maxActive).map {
+      sem =>
+        { http: Kleisli[OptionT[F, ?], Request[F], Response[F]] =>
+          Kleisli { a: Request[F] =>
+            Concurrent[OptionT[F, ?]].bracketCase(OptionT.liftF(sem.tryAcquire)) { bool =>
+              if (bool)
+                http
+                  .run(a)
+                  .map(resp => resp.copy(body = resp.body.onFinalize(sem.release)))
+                  .orElseF(sem.release.as(None))
+              else OptionT.pure[F](defaultResp)
+            } {
+              case (bool, ExitCase.Canceled | ExitCase.Error(_)) =>
+                if (bool) OptionT.liftF(sem.release)
+                else OptionT.pure[F](())
+              case (_, ExitCase.Completed) => OptionT.pure[F](())
+            }
           }
         }
-      }
     }
 
 }
