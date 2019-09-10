b1ac33512d9fed315737c324195877d78d3ad870
Scala Steward <me@scala-steward.org>
2019-09-10 18:30:56 +0200

    Update vault to 2.0.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -310,5 +310,5 @@ object Http4sPlugin extends AutoPlugin {
   lazy val tomcatCatalina                   = "org.apache.tomcat"      %  "tomcat-catalina"           % "9.0.24"
   lazy val tomcatCoyote                     = "org.apache.tomcat"      %  "tomcat-coyote"             % tomcatCatalina.revision
   lazy val twirlApi                         = "com.typesafe.play"      %% "twirl-api"                 % "1.4.2"
-  lazy val vault                            = "io.chrisdavenport"      %% "vault"                     % "2.0.0-RC2"
+  lazy val vault                            = "io.chrisdavenport"      %% "vault"                     % "2.0.0"
 }
