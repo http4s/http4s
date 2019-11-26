f4809c1b40e4440e4c9bf3bd730ddd961ab50432
Ross A. Baker <ross@rossabaker.com>
2019-11-26 15:07:09 -0500

    Fix compilation errors in cherry-pick of #2966

--- server/src/main/scala/org/http4s/server/middleware/HttpsRedirect.scala
+++ server/src/main/scala/org/http4s/server/middleware/HttpsRedirect.scala
@@ -5,11 +5,9 @@ package middleware
 import cats.Applicative
 import cats.implicits._
 import cats.data.Kleisli
-import org.http4s.headers.{ `Content-Type`, `X-Forwarded-Proto`, Host, Location }
-import org.http4s._
-import org.http4s.dsl.io.MovedPermanently
-import org.http4s.util.CaseInsensitiveString
-import org.http4s.Uri.{ Authority, Host, RegName, Scheme }
+import org.http4s.Status.MovedPermanently
+import org.http4s.Uri.{Authority, RegName, Scheme}
+import org.http4s.headers.{Host, Location, `Content-Type`, `X-Forwarded-Proto`}
 
 import org.log4s.getLogger
 
@@ -27,15 +25,15 @@ object HttpsRedirect {
   def apply[F[_], G[_]](http: Http[F, G])(implicit F: Applicative[F]): Http[F, G] =
     Kleisli { req =>
       (req.headers.get(`X-Forwarded-Proto`), req.headers.get(Host)) match {
-        
-        case (Some(proto), Some(host)) if Scheme.fromString(proto.value).contains(Scheme.http) =>
+
+        case (Some(proto), Some(host)) if Scheme.parse(proto.value).contains(Scheme.http) =>
           logger.debug(s"Redirecting ${req.method} ${req.uri} to https on $host")
           val authority = Authority(host = RegName(host.value))
-          val location  = req.uri.copy(scheme = Some(Scheme.https), authority = Some(authority))
-          val headers   = Headers(Location(location) :: `Content-Type`(MediaType.text.xml) :: Nil)
-          val response  = Response[G](status = MovedPermanently, headers = headers)
+          val location = req.uri.copy(scheme = Some(Scheme.https), authority = Some(authority))
+          val headers = Headers(Location(location) :: `Content-Type`(MediaType.text.xml) :: Nil)
+          val response = Response[G](status = MovedPermanently, headers = headers)
           response.pure[F]
-        
+
         case _ =>
           http(req)
       }
