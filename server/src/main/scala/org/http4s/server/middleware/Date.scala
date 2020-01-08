3e084afc2f6d19af1fb97cf1563d5a2de36799ab
Chris Davenport <chris@christopherdavenport.tech>
2020-01-08 10:32:16 -0800

    Reduce Work On Header Present

--- server/src/main/scala/org/http4s/server/middleware/Date.scala
+++ server/src/main/scala/org/http4s/server/middleware/Date.scala
@@ -11,23 +11,27 @@ import scala.concurrent.duration.MILLISECONDS
 
 import java.time.Instant
 
+/**
+  * Date Middleware, adds the Date Header to All Responses generated
+  * by the service.
+ **/
 object Date {
-  def apply[G[_]: FlatMap: Clock, F[_], A](
+  def apply[G[_]: Monad: Clock, F[_], A](
       k: Kleisli[G, A, Response[F]]): Kleisli[G, A, Response[F]] =
     Kleisli { a =>
       for {
         resp <- k(a)
-        nowInstant <- Clock[G]
-          .realTime(MILLISECONDS)
-          .map(Instant.ofEpochMilli(_))
-      } yield {
-        // Starting on January 1,n 10000, this will throw an exception.
-        // The author intends to leave this problem for future generations.
-        val date = resp.headers
+        header <- resp.headers
           .get(HDate)
-          .getOrElse(HDate(HttpDate.unsafeFromInstant(nowInstant)))
+          .fold(
+            Clock[G]
+              .realTime(MILLISECONDS)
+              .map(Instant.ofEpochMilli(_))
+              .map(nowInstant => HDate(HttpDate.unsafeFromInstant(nowInstant)))
+            // Starting on January 1,n 10000, this will throw an exception.
+            // The author intends to leave this problem for future generations.
+          )(_.pure[G])
 
-        resp.putHeaders(date)
-      }
+      } yield resp.putHeaders(header)
     }
 }
