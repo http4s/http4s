8e214b1bdfa4b38f12369fa109cf724946250591
Chris Davenport <chris@christopherdavenport.tech>
2020-01-09 08:02:44 -0800

    Add Convenience Constructor for HttpDate

---
+++ tests/src/test/scala/org/http4s/HttpDateSpec.scala
@@ -0,0 +1,16 @@
+package org.http4s
+
+import cats.effect.{IO, Timer}
+import cats.effect.testing.specs2.CatsIO
+
+class HttpDateSpec extends Http4sSpec with CatsIO {
+  override val timer: Timer[IO] = Http4sSpec.TestTimer
+  "HttpDate" should {
+    "current should be extremely close to now" >> {
+      HttpDate.current[IO].map { current =>
+        val diff = HttpDate.now.epochSecond - current.epochSecond
+        diff must be_<=(1L)
+      }
+    }
+  }
+}
