7f75ce2e20c717b921a2a827be246944daf1bcbf
Bruno Fernandes <bfdes@users.noreply.github.com>
2020-01-10 19:24:43 +0000

    Fix SameSite render method (caused StackOverflow)

--- core/src/main/scala/org/http4s/SameSite.scala
+++ core/src/main/scala/org/http4s/SameSite.scala
@@ -1,13 +1,20 @@
 package org.http4s
 
+import org.http4s.SameSite._
 import org.http4s.util.{Renderable, Writer}
 
 /**
   * RFC6265 SameSite cookie attribute values.
   */
 sealed trait SameSite extends Renderable {
-  override def render(writer: Writer): writer.type =
-    writer.append(toString)
+  override def render(writer: Writer): writer.type = {
+    val str = this match {
+      case Strict => "Strict"
+      case Lax => "Lax"
+      case None => "None"
+    }
+    writer.append(str)
+  }
 }
 
 object SameSite {
