c71e16d4e9252f9524429a6a6c0e770c5b001255
Scala Steward <me@scala-steward.org>
2020-01-12 23:01:57 +0100

    Update scalatags to 0.8.4

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -298,7 +298,7 @@ object Http4sPlugin extends AutoPlugin {
   lazy val quasiquotes                      = "org.scalamacros"        %% "quasiquotes"               % "2.1.0"
   lazy val scalacheck                       = "org.scalacheck"         %% "scalacheck"                % "1.14.2"
   def scalaReflect(sv: String)              = "org.scala-lang"         %  "scala-reflect"             % sv
-  lazy val scalatagsApi                     = "com.lihaoyi"            %% "scalatags"                 % "0.8.3"
+  lazy val scalatagsApi                     = "com.lihaoyi"            %% "scalatags"                 % "0.8.4"
   lazy val scalaXml                         = "org.scala-lang.modules" %% "scala-xml"                 % "1.2.0"
   lazy val specs2Core                       = "org.specs2"             %% "specs2-core"               % "4.8.3"
   lazy val specs2Matcher                    = "org.specs2"             %% "specs2-matcher"            % specs2Core.revision
