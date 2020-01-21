c1766f6b66b07b8d53603d76afd0fec6fa3c48c7
Tomas Herman <hermant@avast.com>
2020-01-21 10:33:08 +0100

    Scalafmt

--- client/src/main/scala/org/http4s/client/ClientFailure.scala
+++ client/src/main/scala/org/http4s/client/ClientFailure.scala
@@ -5,7 +5,8 @@ import java.net.InetSocketAddress
 
 /** Indicates a failure in the client when executing a request preserving the context (requestKey and upstream address)
   */
-class ClientFailure(requestKey: RequestKey, upstream: InetSocketAddress, cause: Throwable) extends IOException(cause) {
+class ClientFailure(requestKey: RequestKey, upstream: InetSocketAddress, cause: Throwable)
+    extends IOException(cause) {
   override def getMessage(): String =
     s"Error connecting to $requestKey using address $upstream (unresolved: ${upstream.isUnresolved})"
 }
