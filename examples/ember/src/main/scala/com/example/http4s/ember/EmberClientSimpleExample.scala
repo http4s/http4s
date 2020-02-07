17138a02e51284331365261ef48ed09f05f41de1
Christopher Davenport <chris@christopherdavenport.tech>
2020-02-07 15:29:54 -0800

    Scalafmt - Makes All Code Ugly

--- examples/ember/src/main/scala/com/example/http4s/ember/EmberClientSimpleExample.scala
+++ examples/ember/src/main/scala/com/example/http4s/ember/EmberClientSimpleExample.scala
@@ -17,47 +17,57 @@ import scala.concurrent.duration._
 import java.util.concurrent.TimeUnit
 import scodec.bits.ByteVector
 
-object EmberClientSimpleExample extends IOApp{
+object EmberClientSimpleExample extends IOApp {
 
   val githubReq = Request[IO](Method.GET, uri"http://christopherdavenport.github.io/")
-  val dadJokeReq = Request[IO](Method.GET, uri"https://icanhazdadjoke.com/")  
+  val dadJokeReq = Request[IO](Method.GET, uri"https://icanhazdadjoke.com/")
   val googleReq = Request[IO](Method.GET, uri"https://www.google.com/")
   val httpBinGet = Request[IO](Method.GET, uri"https://httpbin.org/get")
   val httpBinPng = Request[IO](Method.GET, uri"https://httpbin.org/image/png")
 
   val logger = Slf4jLogger.getLogger[IO]
 
-  def run(args: List[String]) : IO[ExitCode] = {
-    EmberClientBuilder.default[IO]
+  def run(args: List[String]): IO[ExitCode] = {
+    EmberClientBuilder
+      .default[IO]
       .build
-      .use( client =>
-        logTimed(logger, "Http Only - ChristopherDavenport Site", getRequestBufferedBody(client, githubReq)) >>
-        logTimed(logger, "Json - DadJoke", client.expect[Json](dadJokeReq)) >>
-        logTimed(logger, "Https - Google", getRequestBufferedBody(client, googleReq)) >>
-        logTimed(logger, "Small Response - HttpBin Get", getRequestBufferedBody(client, httpBinGet)) >>
-        logTimed(logger, "Large Response - HttpBin PNG", getRequestBufferedBody(client, httpBinPng)) >>
-        IO(println("Done"))
-      )
+      .use(client =>
+        logTimed(
+          logger,
+          "Http Only - ChristopherDavenport Site",
+          getRequestBufferedBody(client, githubReq)) >>
+          logTimed(logger, "Json - DadJoke", client.expect[Json](dadJokeReq)) >>
+          logTimed(logger, "Https - Google", getRequestBufferedBody(client, googleReq)) >>
+          logTimed(
+            logger,
+            "Small Response - HttpBin Get",
+            getRequestBufferedBody(client, httpBinGet)) >>
+          logTimed(
+            logger,
+            "Large Response - HttpBin PNG",
+            getRequestBufferedBody(client, httpBinPng)) >>
+          IO(println("Done")))
 
   }.as(ExitCode.Success)
 
-  def getRequestBufferedBody[F[_]: Sync](client: Client[F], req: Request[F]): F[Response[F]] = {
-    client.run(req)
-      .use(resp => 
-        resp.body.compile.to(ByteVector).map(bv => resp.copy(body = Stream.chunk(Chunk.ByteVectorChunk(bv))))
-      )
-  }
+  def getRequestBufferedBody[F[_]: Sync](client: Client[F], req: Request[F]): F[Response[F]] =
+    client
+      .run(req)
+      .use(resp =>
+        resp.body.compile
+          .to(ByteVector)
+          .map(bv => resp.copy(body = Stream.chunk(Chunk.ByteVectorChunk(bv)))))
 
-  def logTimed[F[_]: Clock: Monad, A](logger: Logger[F], name: String, fa: F[A]): F[A] = {
-    timedMS(fa).flatMap{ case (time, action) =>
-      logger.info(s"Action $name took $time").as(action)
+  def logTimed[F[_]: Clock: Monad, A](logger: Logger[F], name: String, fa: F[A]): F[A] =
+    timedMS(fa).flatMap {
+      case (time, action) =>
+        logger.info(s"Action $name took $time").as(action)
     }
-  }
 
   def timedMS[F[_]: Clock: Applicative, A](fa: F[A]): F[(FiniteDuration, A)] = {
     val nowMS = Clock[F].monotonic(TimeUnit.MILLISECONDS)
-    (nowMS, fa, nowMS).mapN{
-      case (before, result, after) => 
+    (nowMS, fa, nowMS).mapN {
+      case (before, result, after) =>
         val time = (after - before).millis
         (time, result)
     }
--- examples/ember/src/main/scala/com/example/http4s/ember/EmberServerSimpleExample.scala
+++ examples/ember/src/main/scala/com/example/http4s/ember/EmberServerSimpleExample.scala
@@ -1,6 +1,5 @@
 package com.example.http4s.ember
 
-
 import fs2._
 import cats.effect._
 import cats.implicits._
@@ -11,47 +10,47 @@ import org.http4s.circe._
 import _root_.io.circe._
 import _root_.org.http4s.ember.server.EmberServerBuilder
 
-object EmberServerSimpleExample extends IOApp{
+object EmberServerSimpleExample extends IOApp {
 
-  def run(args: List[String]) : IO[ExitCode] = {
+  def run(args: List[String]): IO[ExitCode] = {
     val host = "0.0.0.0"
     val port = 8080
     for {
       // Server Level Resources Here
-      server <- EmberServerBuilder.default[IO]
+      server <- EmberServerBuilder
+        .default[IO]
         .withHost(host)
         .withPort(port)
         .withHttpApp(service[IO])
         .build
     } yield server
   }.use(server =>
-    IO.delay(println(s"Server Has Started at ${server.address}")) >> 
-    IO.never.as(ExitCode.Success)
-  )
+    IO.delay(println(s"Server Has Started at ${server.address}")) >>
+      IO.never.as(ExitCode.Success))
 
-  
-
-  def service[F[_]: Sync] : HttpApp[F] = {
-    val dsl = new Http4sDsl[F]{}
+  def service[F[_]: Sync]: HttpApp[F] = {
+    val dsl = new Http4sDsl[F] {}
     import dsl._
-    
-    HttpRoutes.of[F]{
-      case req @ POST -> Root => 
-        for {
-          json <- req.decodeJson[Json]
-          resp <- Ok(json)
-        } yield resp
-      case GET -> Root => 
-        Ok(Json.obj("root" -> Json.fromString("GET")))
-      case GET -> Root / "hello" / name => 
-        Ok(show"Hi $name!")
-      case GET -> Root / "chunked" =>
-        val body = Stream("This IS A CHUNK\n")
-          .covary[F]
-          .repeat
-          .take(100)
-          .through(fs2.text.utf8Encode[F])
-        Ok(body).map(_.withContentType(headers.`Content-Type`(MediaType.text.plain)))
-    }.orNotFound
+
+    HttpRoutes
+      .of[F] {
+        case req @ POST -> Root =>
+          for {
+            json <- req.decodeJson[Json]
+            resp <- Ok(json)
+          } yield resp
+        case GET -> Root =>
+          Ok(Json.obj("root" -> Json.fromString("GET")))
+        case GET -> Root / "hello" / name =>
+          Ok(show"Hi $name!")
+        case GET -> Root / "chunked" =>
+          val body = Stream("This IS A CHUNK\n")
+            .covary[F]
+            .repeat
+            .take(100)
+            .through(fs2.text.utf8Encode[F])
+          Ok(body).map(_.withContentType(headers.`Content-Type`(MediaType.text.plain)))
+      }
+      .orNotFound
   }
 }
