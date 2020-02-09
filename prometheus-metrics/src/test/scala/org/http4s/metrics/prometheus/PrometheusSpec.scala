39ef4341b91ee10d7a0e8beaac27393ef1575875
Kevin Meredith <kevin.m.meredith@gmail.com>
2020-02-09 00:35:07 -0500

    Add tests for 'classifierFMethodWithOptionallyExcludedPath'.

---
+++ prometheus-metrics/src/test/scala/org/http4s/metrics/prometheus/PrometheusSpec.scala
@@ -0,0 +1,59 @@
+package org.http4s.metrics.prometheus
+
+import cats.effect.IO
+import cats.implicits._
+import java.util.UUID
+import org.http4s._
+import Prometheus.classifierFMethodWithOptionallyExcludedPath
+
+class PrometheusSpec extends Http4sSpec {
+
+  "classifierFMethodWithOptionallyExcludedPath" should {
+    "properly exclude UUIDs" in prop {
+      (method: Method, uuid: UUID) => {
+        val request: Request[IO] = Request[IO](
+          method = method,
+          uri    = Uri.unsafeFromString(s"/users/$uuid/comments")
+        )
+
+        val excludeUUIDs: String => Boolean = {
+          str: String =>
+            Either
+              .catchOnly[IllegalArgumentException](UUID.fromString(str))
+              .isRight
+        }
+
+        val classifier: Request[IO] => Option[String] =
+          classifierFMethodWithOptionallyExcludedPath(
+            excludeUUIDs
+          )
+
+        val result: Option[String] =
+          classifier(request)
+
+        val expected: Option[String] =
+          Some(method.name.toLowerCase() + "_" + "users_*_comments")
+
+        result ==== expected
+      }
+    }
+    "return '$method' if the path is '/'" in prop {
+      method: Method =>
+        val request: Request[IO] = Request[IO](
+          method = method,
+          uri    = uri"""/"""
+        )
+
+        val classifier: Request[IO] => Option[String] =
+          classifierFMethodWithOptionallyExcludedPath(
+            _ => true
+          )
+
+        val result: Option[String] =
+          classifier(request)
+
+        result ==== Some(method.name.toLowerCase)
+    }
+  }
+  
+}
