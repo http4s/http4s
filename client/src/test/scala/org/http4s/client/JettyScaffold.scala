22145291322a61785f2db0748b84175e061e6556
satorg <satorg@gmail.com>
2020-04-02 01:33:34 -0700

    fix failing local unit tests

--- client/src/test/scala/org/http4s/client/JettyScaffold.scala
+++ client/src/test/scala/org/http4s/client/JettyScaffold.scala
@@ -66,7 +66,7 @@ class JettyScaffold private (num: Int, secure: Boolean) {
       server.start()
 
       val address = new InetSocketAddress(
-        InetAddress.getLocalHost(),
+        InetAddress.getLoopbackAddress,
         server.getConnectors.head.asInstanceOf[ServerConnector].getLocalPort)
 
       (address, server)
