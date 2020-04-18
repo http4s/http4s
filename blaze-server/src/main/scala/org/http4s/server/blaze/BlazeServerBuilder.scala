1e585374ae9237fb4839752114fb6ec34a9a2a48
alberto.adami <alberto.adami.7@gmail.com>
2020-04-18 21:45:33 +0200

    run scalafmt

--- blaze-server/src/main/scala/org/http4s/server/blaze/BlazeServerBuilder.scala
+++ blaze-server/src/main/scala/org/http4s/server/blaze/BlazeServerBuilder.scala
@@ -380,7 +380,10 @@ class BlazeServerBuilder[F[_]](
 }
 
 object BlazeServerBuilder {
-  def apply[F[_]](implicit F: ConcurrentEffect[F], timer: Timer[F], executionContext: ExecutionContext): BlazeServerBuilder[F] =
+  def apply[F[_]](
+      implicit F: ConcurrentEffect[F],
+      timer: Timer[F],
+      executionContext: ExecutionContext): BlazeServerBuilder[F] =
     new BlazeServerBuilder(
       socketAddress = defaults.SocketAddress,
       executionContext = executionContext,
