ad8796baa0ca155fdcff12a152e75cb0c3cd1cf5
Jichao Ouyang <oyanglulu@gmail.com>
2020-05-06 22:55:32 +1000

    refactor

--- finagle/src/main/scala/org/http4s/client/finagle/Finagle.scala
+++ finagle/src/main/scala/org/http4s/client/finagle/Finagle.scala
@@ -4,6 +4,7 @@ package finagle
 import client._
 import cats.effect._
 import cats.syntax.functor._
+import cats.syntax.apply._
 import com.twitter.finagle.{Http,Service}
 import com.twitter.finagle.http.{Request => Req, Response=>Resp, Method, RequestBuilder}
 import com.twitter.util.{Return, Throw, Future}
@@ -14,12 +15,18 @@ import fs2.{
 }
 import cats.Functor
 import com.twitter.util.Promise
+import cats.Applicative
 
 object Finagle {
 
   def mkClient[F[_]](dest: String)(
-    implicit F: ConcurrentEffect[F]): Resource[F, Client[F]] = mkResource(Http.newService(dest))
+    implicit F: ConcurrentEffect[F]): Resource[F, Client[F]] = mkClient(Http.newService(dest))
 
+  def mkClient[F[_]](svc: Service[Req, Resp])(
+    implicit F: ConcurrentEffect[F]): Resource[F, Client[F]] = {
+    Resource.make(F.delay(svc)){_ => F.delay(())}
+    .flatMap(svc => Resource.liftF(allocate(svc)))
+  }
   def mkService[F[_]:Functor: Effect:ConcurrentEffect](route: HttpApp[F]): Service[Req,Resp] = new Service[Req,Resp] {
     def apply(req: Req) = toFuture(route.local(fromFinagleReq[F]).flatMapF(toFinagleResp[F]).run(req))
   }
@@ -33,30 +40,30 @@ object Finagle {
          }yield resp).map(toHttp4sResp)
         })
 
-  private def mkResource[F[_]](svc: Service[Req, Resp])(
-    implicit F: ConcurrentEffect[F]): Resource[F, Client[F]] = {
-    Resource.make(F.delay(svc)){_ => F.delay(())}
-    .flatMap(svc => Resource.liftF(allocate(svc)))
-  }
   private def fromFinagleReq[F[_]](req: Req): Request[F] = {
-    val method: org.http4s.Method = org.http4s.Method.fromString(req.method.name).getOrElse(org.http4s.Method.GET)
+    import org.http4s.{Method, Header}
+    val method = Method.fromString(req.method.name).getOrElse(Method.GET)
     val uri = Uri.unsafeFromString(req.uri)
-    val headers = Headers(req.headerMap.toList.map{case (name, value) => org.http4s.Header(name, value)})
+    val headers = Headers(req.headerMap.toList.map{case (name, value) => Header(name, value)})
     val body = toStream[F](req.content)
-    Request(method, uri, HttpVersion.`HTTP/1.1`, headers, body)
+    val version = HttpVersion.fromVersion(req.version.major, req.version.minor).getOrElse(HttpVersion.`HTTP/1.1`)
+    Request(method, uri, version, headers, body)
   }
 
-  private def toFinagleResp[F[_]:ConcurrentEffect](resp: Response[F]): F[Resp] = {
+  private def toFinagleResp[F[_]:Concurrent](resp: Response[F]): F[Resp] = {
     import com.twitter.finagle.http.{Status}
     val status = Status(resp.status.code)
     val headers = resp.headers.toList.map(h => (h.name.toString, h.value))
     val finagleResp = Resp(status)
     finagleResp.headerMap.addAll(headers)
-    val bodyUpdate = resp.body.chunks.map(_.toArray).evalMap{ a=>
-      val out = (finagleResp.writer.write(com.twitter.finagle.http.Chunk.fromByteArray(a).content))
-      toF(out)
-    }.compile.drain.map(_ => toF((finagleResp.writer.close())))
-    ConcurrentEffect[F].runCancelable(bodyUpdate)(_=>IO.unit).to[F].as(finagleResp)
+    val writeBody = if (resp.isChunked) {
+      finagleResp.setChunked(true)
+      Concurrent[F].start(streamBody(resp.body, finagleResp.writer).compile.drain).void
+    } else {
+      resp.as[Array[Byte]].map{Buf.ByteArray.Owned(_)}
+        .map(finagleResp.content = _).void
+    }
+    writeBody *> Applicative[F].pure(finagleResp)
   }
 
   private def toFinagleReq[F[_]](req: Request[F])(implicit F: ConcurrentEffect[F]):F[Req] = {
@@ -65,20 +72,23 @@ object Finagle {
     val reqBuilder = RequestBuilder().url(req.uri.toString).addHeaders(reqheaders)
     if (req.isChunked) {
       val request = reqBuilder.build(method, None)
-        request.headerMap.remove("Transfer-Encoding")
-        val writer = request.chunkWriter
-        request.setChunked(true)
-        val bodyUpdate = req.body.chunks.map(_.toArray).evalMap{ a=>
-          val out = (writer.write(com.twitter.finagle.http.Chunk.fromByteArray(a)))
-          toF(out)
-        }.compile.drain.map(_ => toF((writer.close())))
-        ConcurrentEffect[F].runCancelable(bodyUpdate)(_=>IO.unit).to[F].as(request)
+      request.headerMap.remove("Transfer-Encoding")
+      request.setChunked(true)
+      Concurrent[F].start(streamBody(req.body, request.writer).compile.drain) *>
+      Applicative[F].pure(request)
     }else{
-        req.as[Array[Byte]].map{b=>
-          val body = if (b.isEmpty) None else Some(Buf.ByteArray.Owned(b))
-          reqBuilder.build(method, body)
-        }
+      req.as[Array[Byte]].map{b=>
+        val body = if (b.isEmpty) None else Some(Buf.ByteArray.Owned(b))
+        reqBuilder.build(method, body)
+      }
     }
+  }
+
+  private def streamBody[F[_]:Async](body: Stream[F, Byte], writer: Writer[Buf]): Stream[F, Unit] ={
+    import com.twitter.finagle.http.{Chunk}
+      (body.chunks.map(a=>Chunk.fromByteArray(a.toArray).content).evalMap{ a=>
+      toF(writer.write(a))
+    }) ++ Stream.eval { toF(writer.close()) }
   }
 
   private def toStream[F[_]](buf: Buf): Stream[F, Byte] = {
