0a9040b29ebbb7d26281c8a5d5889fa8a844a1e1
Ross A. Baker <ross@rossabaker.com>
2020-05-09 17:33:50 -0400

    sbt-header with better defaults

--- project/Http4sOrgPlugin.scala
+++ project/Http4sOrgPlugin.scala
@@ -5,6 +5,8 @@ import sbt._
 import sbt.Keys._
 
 import com.typesafe.tools.mima.plugin.MimaPlugin.autoImport._
+import de.heikoseeberger.sbtheader.{AutomateHeaderPlugin, LicenseDetection, LicenseStyle}
+import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport._
 import java.lang.{Runtime => JRuntime}
 import _root_.io.chrisdavenport.sbtmimaversioncheck.MimaVersionCheck
 import org.scalafmt.sbt.ScalafmtPlugin
@@ -19,18 +21,46 @@ object Http4sOrgPlugin extends AutoPlugin {
   override def requires = MimaVersionCheck && ScalafmtPlugin
 
   override lazy val projectSettings: Seq[Setting[_]] =
+    organizationSettings ++
+    scalaSettings ++
+    javaSettings ++
+    headerSettings
+
+  val organizationSettings: Seq[Setting[_]] =
     Seq(
       organization := "org.http4s",
+      organizationName := "http4s.org"
+    )
 
+  val scalaSettings: Seq[Setting[_]] =
+    Seq(
       scalacOptions ++=
         Seq(
           "-Ybackend-parallelism",
           math.min(JRuntime.getRuntime.availableProcessors, 16).toString
-        ),
+        )
+    )
 
+  val javaSettings: Seq[Setting[_]] =
+    Seq(
       javacOptions ++=
         Seq(
           "-Xlint:all",
         )
+    )
+
+  val headerSettings: Seq[Setting[_]] =
+    Seq(
+      headerLicenseStyle := LicenseStyle.SpdxSyntax,
+      headerLicense := {
+        val current = java.time.Year.now().getValue
+        val copyrightYear = startYear.value.fold(current.toString)(start => s"$start-$current")
+        LicenseDetection(
+          licenses.value.toList,
+          organizationName.value,
+          Some(copyrightYear),
+          headerLicenseStyle.value
+        )
+      }
     )
 }
