a69d3dbed69b318d53e96f5a113ea63a4ac832e2
Kevin Meredith <kevin.m.meredith@gmail.com>
2020-06-07 00:46:03 -0400

    Add org.http4s.client.middleware.Logger#logMessage.

--- client/src/main/scala/org/http4s/client/middleware/Logger.scala
+++ client/src/main/scala/org/http4s/client/middleware/Logger.scala
@@ -77,4 +77,59 @@ object Logger {
       .map(body => s"$prelude${spaced(headers)}${spaced(body)}")
       .flatMap(log)
   }
+
+  def logMessage[F[_], A <: Message[F]](message: A)(
+    logHeaders: Boolean,
+    logBodyText: Option[String => F[String]],
+    redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains)(
+    log: String => F[Unit])(implicit F: Sync[F]): F[Unit] = {
+
+    val charset = message.charset
+    val isBinary = message.contentType.exists(_.mediaType.binary)
+    val isJson = message.contentType.exists(mT =>
+      mT.mediaType == MediaType.application.json || mT.mediaType.subType.endsWith("+json"))
+
+    val isText = !isBinary || isJson
+
+    def prelude =
+      message match {
+        case Request(method, uri, httpVersion, _, _, _) =>
+          s"$httpVersion $method $uri"
+
+        case Response(status, httpVersion, _, _, _) =>
+          s"$httpVersion $status"
+      }
+
+    val headers: String =
+      if (logHeaders)
+        message.headers.redactSensitive(redactHeadersWhen).toList.mkString("Headers(", ", ", ")")
+      else ""
+
+    val bodyText: F[String] = logBodyText match {
+      case Some(f) =>
+        val m: Stream[F, String] =
+          if(isText) {
+            message
+              .bodyAsText(charset.getOrElse(Charset.`UTF-8`))
+          } else {
+            message.body
+              .map(b => java.lang.Integer.toHexString(b & 0xff))
+          }
+
+        m.compile
+         .string
+         .flatMap(f)
+         .handleError {case _ => "N/A"}
+         .map(text => s"""body="$text"""")
+
+      case None =>
+        F.pure("")
+    }
+
+    def spaced(x: String): String = if (x.isEmpty) x else s" $x"
+
+    bodyText
+      .map(body => s"$prelude${spaced(headers)}${spaced(body)}")
+      .flatMap(log)
+  }
 }
