d73cca92fd1d314440dd5572042c04ee28dc9985
Kevin Meredith <kevin.m.meredith@gmail.com>
2020-06-11 01:23:49 -0400

    Return org.http4s.internal.Logger#logMessage to its original definition.

--- core/src/main/scala/org/http4s/internal/Logger.scala
+++ core/src/main/scala/org/http4s/internal/Logger.scala
@@ -19,10 +19,48 @@ object Logger {
       logBody: Boolean,
       redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains)(
       log: String => F[Unit])(implicit F: Sync[F]): F[Unit] = {
-    val logBodyText: Option[String => F[String]] =
-      if (logBody) Some(F.pure) else None
+    val charset = message.charset
+    val isBinary = message.contentType.exists(_.mediaType.binary)
+    val isJson = message.contentType.exists(mT =>
+      mT.mediaType == MediaType.application.json || mT.mediaType.subType.endsWith("+json"))
 
-    logMessageWithBodyText[F, A](message)(logHeaders, logBodyText, redactHeadersWhen)(log)
+    val isText = !isBinary || isJson
+
+    def prelude =
+      message match {
+        case Request(method, uri, httpVersion, _, _, _) =>
+          s"$httpVersion $method $uri"
+
+        case Response(status, httpVersion, _, _, _) =>
+          s"$httpVersion $status"
+      }
+
+    val headers =
+      if (logHeaders)
+        message.headers.redactSensitive(redactHeadersWhen).toList.mkString("Headers(", ", ", ")")
+      else ""
+
+    val bodyStream =
+      if (logBody && isText)
+        message.bodyAsText(charset.getOrElse(Charset.`UTF-8`))
+      else if (logBody)
+        message.body
+          .map(b => java.lang.Integer.toHexString(b & 0xff))
+      else
+        Stream.empty.covary[F]
+
+    val bodyText =
+      if (logBody)
+        bodyStream.compile.string
+          .map(text => s"""body="$text"""")
+      else
+        F.pure("")
+
+    def spaced(x: String): String = if (x.isEmpty) x else s" $x"
+
+    bodyText
+      .map(body => s"$prelude${spaced(headers)}${spaced(body)}")
+      .flatMap(log)
   }
 
   def logMessageWithBodyText[F[_], A <: Message[F]](message: A)(
