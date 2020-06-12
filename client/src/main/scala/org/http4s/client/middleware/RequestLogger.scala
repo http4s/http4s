294f26df582839a597a443127939b4044f40cc52
kmeredith <kevin.meredith@banno.com>
2020-06-12 14:33:31 -0400

    Define org.http4s.client.middleware.RequestLogger#apply in terms of impl.

--- client/src/main/scala/org/http4s/client/middleware/RequestLogger.scala
+++ client/src/main/scala/org/http4s/client/middleware/RequestLogger.scala
@@ -26,40 +26,8 @@ object RequestLogger {
       logBody: Boolean,
       redactHeadersWhen: CaseInsensitiveString => Boolean = Headers.SensitiveHeaders.contains,
       logAction: Option[String => F[Unit]] = None
-  )(client: Client[F]): Client[F] = {
-    val log = logAction.getOrElse { (s: String) =>
-      Sync[F].delay(logger.info(s))
-    }
-    Client { req =>
-      if (!logBody)
-        Resource.liftF(Logger
-          .logMessage[F, Request[F]](req)(logHeaders, logBody, redactHeadersWhen)(log(_))) *> client
-          .run(req)
-      else
-        Resource.suspend {
-          Ref[F].of(Vector.empty[Chunk[Byte]]).map { vec =>
-            val newBody = Stream
-              .eval(vec.get)
-              .flatMap(v => Stream.emits(v).covary[F])
-              .flatMap(c => Stream.chunk(c).covary[F])
-
-            val changedRequest = req.withBodyStream(
-              req.body
-              // Cannot Be Done Asynchronously - Otherwise All Chunks May Not Be Appended Previous to Finalization
-                .observe(_.chunks.flatMap(s => Stream.eval_(vec.update(_ :+ s))))
-                .onFinalizeWeak(
-                  Logger.logMessage[F, Request[F]](req.withBodyStream(newBody))(
-                    logHeaders,
-                    logBody,
-                    redactHeadersWhen)(log(_))
-                )
-            )
-
-            client.run(changedRequest)
-          }
-        }
-    }
-  }
+  )(client: Client[F]): Client[F] =
+    impl[F](logHeaders, Left(logBody), redactHeadersWhen, logAction)(client)
 
   def impl[F[_]: Concurrent](
       logHeaders: Boolean,
