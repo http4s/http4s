01ea59473120cb8a53afc5e8ed6d1034b0119d02
Scala Steward <me@scala-steward.org>
2020-06-12 20:25:47 +0200

    Update fs2-io, fs2-reactive-streams to 2.4.2

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -215,7 +215,7 @@ object Http4sPlugin extends AutoPlugin {
     val cryptobits = "1.3"
     val disciplineSpecs2 = "1.1.0"
     val dropwizardMetrics = "4.1.9"
-    val fs2 = "2.4.1"
+    val fs2 = "2.4.2"
     val jawn = "1.0.0"
     val jawnFs2 = "1.0.0"
     val jetty = "9.4.28.v20200408"
