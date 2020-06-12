d9fda4884e2e332fd389a356a1ed9a9aadffdeea
kmeredith <kevin.meredith@banno.com>
2020-06-12 14:46:58 -0400

    Add org.http4s.server.middleware.RequestLogger#impl.

--- server/src/main/scala/org/http4s/server/middleware/RequestLogger.scala
+++ server/src/main/scala/org/http4s/server/middleware/RequestLogger.scala
@@ -83,6 +83,73 @@ object RequestLogger {
     }
   }
 
+  private def impl[G[_], F[_]](
+      logHeaders: Boolean,
+      logBodyText: Either[Boolean, Stream[F, Byte] => Option[F[String]]],
+      fk: F ~> G,
+      redactHeadersWhen: CaseInsensitiveString => Boolean,
+      logAction: Option[String => F[Unit]]
+  )(http: Http[G, F])(implicit
+      F: Concurrent[F],
+      G: Bracket[G, Throwable]
+  ): Http[G, F] = {
+    val log = logAction.fold { (s: String) =>
+      Sync[F].delay(logger.info(s))
+    }(identity)
+
+    def logMessage(r: Request[F]): F[Unit] =
+      logBodyText match {
+        case Left(bool) =>
+          Logger.logMessage[F, Request[F]](r)(logHeaders, bool, redactHeadersWhen)(log(_))
+        case Right(f) =>
+          org.http4s.internal.Logger
+            .logMessageWithBodyText[F, Request[F]](r)(logHeaders, f, redactHeadersWhen)(log(_))
+      }
+
+    val logBody: Boolean = logBodyText match {
+      case Left(bool) => bool
+      case Right(_) => true
+    }
+
+    Kleisli { req =>
+      if (!logBody) {
+        def logAct = logMessage(req)
+        // This construction will log on Any Error/Cancellation
+        // The Completed Case is Unit, as we rely on the semantics of G
+        // As None Is Successful, but we oly want to log on Some
+        http(req)
+          .guaranteeCase {
+            case ExitCase.Canceled => fk(logAct)
+            case ExitCase.Error(_) => fk(logAct)
+            case ExitCase.Completed => G.unit
+          } <* fk(logAct)
+      } else
+        fk(Ref[F].of(Vector.empty[Chunk[Byte]]))
+          .flatMap { vec =>
+            val newBody = Stream
+              .eval(vec.get)
+              .flatMap(v => Stream.emits(v).covary[F])
+              .flatMap(c => Stream.chunk(c).covary[F])
+
+            val changedRequest = req.withBodyStream(
+              req.body
+              // Cannot Be Done Asynchronously - Otherwise All Chunks May Not Be Appended Previous to Finalization
+                .observe(_.chunks.flatMap(c => Stream.eval_(vec.update(_ :+ c))))
+            )
+            def logRequest: F[Unit] =
+              logMessage(req.withBodyStream(newBody))
+            val response: G[Response[F]] =
+              http(changedRequest)
+                .guaranteeCase {
+                  case ExitCase.Completed => G.unit
+                  case _ => fk(logRequest)
+                }
+                .map(resp => resp.withBodyStream(resp.body.onFinalizeWeak(logRequest)))
+            response
+          }
+    }
+  }
+
   def httpApp[F[_]: Concurrent](
       logHeaders: Boolean,
       logBody: Boolean,
