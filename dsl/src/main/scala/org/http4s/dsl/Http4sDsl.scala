92e1d215e0be2489d479423e5515fc6dc90398de
Ross A. Baker <ross@rossabaker.com>
2020-06-27 19:33:17 -0400

    Restore binary compatibility

--- dsl/src/main/scala/org/http4s/dsl/Http4sDsl.scala
+++ dsl/src/main/scala/org/http4s/dsl/Http4sDsl.scala
@@ -10,7 +10,34 @@ import cats.arrow.FunctionK
 import org.http4s.Method
 import org.http4s.dsl.impl._
 
-trait Http4sDsl2[F[_], G[_]] extends Responses[F, G] with request
+trait Http4sDsl2[F[_], G[_]] extends RequestDsl with Statuses with Responses[F, G] {
+  val Path: impl.Path.type = impl.Path
+  val Root: impl.Root.type = impl.Root
+  val / : impl./.type = impl./
+  val :? : impl.:?.type = impl.:?
+  val ~ : impl.~.type = impl.~
+  val -> : impl.->.type = impl.->
+  val /: : impl./:.type = impl./:
+  val +& : impl.+&.type = impl.+&
+
+  /**
+    * Alias for `->`.
+    *
+    * Note: Due to infix operation precedence, `→` has a lower priority than `/`. So you have to use parentheses in
+    * pattern matching when using this operator.
+    *
+    * For example:
+    * {{{
+    *   (request.method, Path(request.path)) match {
+    *     case Method.GET → (Root / "test.json") => ...
+    * }}}
+    */
+  val → : impl.->.type = impl.->
+
+  val IntVar: impl.IntVar.type = impl.IntVar
+  val LongVar: impl.LongVar.type = impl.LongVar
+  val UUIDVar: impl.UUIDVar.type = impl.UUIDVar
+}
 
 trait Http4sDsl[F[_]] extends Http4sDsl2[F, F] {
   val liftG: FunctionK[F, F] = FunctionK.id[F]
