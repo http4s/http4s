b5e168579fc5e3dcf0da6fb324128cf4e50ec414
Nelu Snegur <nelu.snegur@gmail.com>
2020-07-15 08:52:10 +0200

    Model Access-Control-Allow-Headers header

--- tests/src/test/scala/org/http4s/parser/SimpleHeadersSpec.scala
+++ tests/src/test/scala/org/http4s/parser/SimpleHeadersSpec.scala
@@ -30,6 +30,21 @@ class SimpleHeadersSpec extends Http4sSpec {
       HttpHeaderParser.parseHeader(bad) must beLeft
     }
 
+    "parse Access-Control-Allow-Headers" in {
+      val header = `Access-Control-Allow-Headers`(
+        NonEmptyList.of(
+          CIString("Accept"),
+          CIString("Expires"),
+          CIString("X-Custom-Header"),
+          CIString("*")
+        )
+      )
+      HttpHeaderParser.parseHeader(header.toRaw) must beRight(header)
+
+      val invalidHeader = Header(header.name.toString, "(non-token-name), non[&token]name")
+      HttpHeaderParser.parseHeader(invalidHeader) must beLeft
+    }
+
     "parse Connection" in {
       val header = Connection(CIString("closed"))
       HttpHeaderParser.parseHeader(header.toRaw) must beRight(header)
