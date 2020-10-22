a139106a2d319de1fa27f07a5c7757565cee86da
Ross A. Baker <ross@rossabaker.com>
2020-10-21 22:36:41 -0400

    Adds CollectionCompat trait for Dotty

---
+++ core/src/main/scala-0.27/org/http4s/internal/CollectionCompat.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2013-2020 http4s.org
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.http4s.internal
+
+import scala.collection.immutable
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+private[http4s] object CollectionCompat {
+  type LazyList[A] = Stream[A]
+  val LazyList = Stream
+
+  def pairsToMultiParams[K, V](map: collection.Seq[(K, Option[V])]): Map[K, immutable.Seq[V]] =
+    if (map.isEmpty) Map.empty
+    else {
+      val m = mutable.Map.empty[K, ListBuffer[V]]
+      map.foreach {
+        case (k, None) => m.getOrElseUpdate(k, new ListBuffer)
+        case (k, Some(v)) => m.getOrElseUpdate(k, new ListBuffer) += v
+      }
+      m.toMap.mapValues(_.toList)
+    }
+
+  def mapValues[K, A, B](map: collection.Map[K, A])(f: A => B): Map[K, B] =
+    map.mapValues(f).toMap
+
+  val CollectionConverters = scala.collection.JavaConverters
+}
