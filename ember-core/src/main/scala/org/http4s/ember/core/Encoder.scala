cdc0616b1d7fa69759a6e20da2af480d663db28a
Christopher Davenport <chris@christopherdavenport.tech>
2020-10-27 19:10:21 -0700

    Chunked Default to Default To Blaze Semantics

--- ember-core/src/main/scala/org/http4s/ember/core/Encoder.scala
+++ ember-core/src/main/scala/org/http4s/ember/core/Encoder.scala
@@ -16,10 +16,12 @@ private[ember] object Encoder {
 
   private val SPACE = " "
   private val CRLF = "\r\n"
+  val chunkedTansferEncodingHeaderRaw = "Transfer-Encoding: chunked"
+  
   def respToBytes[F[_]: Sync](
       resp: Response[F],
       writeBufferSize: Int = 32 * 1024): Stream[F, Byte] = {
-    val chunked = resp.isChunked
+    var chunked = resp.isChunked
     val initSection = {
       var appliedContentLength = false
       val stringBuilder = new StringBuilder()
@@ -42,7 +44,8 @@ private[ember] object Encoder {
         ()
       }
       if (!chunked && !appliedContentLength) {
-        stringBuilder.append(`Content-Length`.zero.renderString).append(CRLF)
+        stringBuilder.append(chunkedTansferEncodingHeaderRaw).append(CRLF)
+        chunked = true
         ()
       }
       // Final CRLF terminates headers and signals body to follow.
@@ -58,8 +61,9 @@ private[ember] object Encoder {
         .flatMap(Stream.chunk)
   }
 
+  
   def reqToBytes[F[_]: Sync](req: Request[F], writeBufferSize: Int = 32 * 1024): Stream[F, Byte] = {
-    val chunked = req.isChunked
+    var chunked = req.isChunked
     val initSection = {
       var appliedContentLength = false
       val stringBuilder = new StringBuilder()
@@ -94,7 +98,8 @@ private[ember] object Encoder {
       }
 
       if (!chunked && !appliedContentLength) {
-        stringBuilder.append(`Content-Length`.zero.renderString).append(CRLF)
+        stringBuilder.append(chunkedTansferEncodingHeaderRaw).append(CRLF)
+        chunked = true
         ()
       }
 
