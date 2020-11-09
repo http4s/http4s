61acee5b6a3918c3fbe1d4f814184c6f8d9e1070
Ross A. Baker <ross@rossabaker.com>
2020-11-09 11:28:18 -0500

    fs2-2.4.5

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -247,7 +247,7 @@ object Http4sPlugin extends AutoPlugin {
     val cryptobits = "1.3"
     val disciplineSpecs2 = "1.1.1"
     val dropwizardMetrics = "4.1.14"
-    val fs2 = "2.4.4"
+    val fs2 = "2.4.5"
     val jawn = "1.0.0"
     val jawnFs2 = "1.0.0"
     val jetty = "9.4.34.v20201102"
