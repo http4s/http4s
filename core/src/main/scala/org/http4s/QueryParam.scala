3a3ffdcb047e0a3c9c95ea67101819456d4c219a
Ross A. Baker <ross@rossabaker.com>
2020-11-08 21:53:52 -0500

    Undo AbstractFunction1 MiMa hack from #3815

--- core/src/main/scala/org/http4s/QueryParam.scala
+++ core/src/main/scala/org/http4s/QueryParam.scala
@@ -12,11 +12,10 @@ import cats.implicits._
 import java.time.{Instant, LocalDate, ZonedDateTime}
 import java.time.format.{DateTimeFormatter, DateTimeParseException}
 import java.time.temporal.TemporalAccessor
-import scala.runtime.AbstractFunction1
 
 final case class QueryParameterKey(value: String) extends AnyVal
 
-object QueryParameterKey extends AbstractFunction1[String, QueryParameterKey] {
+object QueryParameterKey {
   implicit lazy val showInstance: Show[QueryParameterKey] =
     Show.show(_.value)
 
@@ -29,7 +28,7 @@ object QueryParameterKey extends AbstractFunction1[String, QueryParameterKey] {
 
 final case class QueryParameterValue(value: String) extends AnyVal
 
-object QueryParameterValue extends AbstractFunction1[String, QueryParameterValue] {
+object QueryParameterValue {
   implicit lazy val showInstance: Show[QueryParameterValue] =
     Show.show(_.value)
 
