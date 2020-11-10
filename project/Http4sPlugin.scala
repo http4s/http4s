08eecfbaa3075ccfd67ea0174fe50d5f1de5812a
Ross A. Baker <ross@rossabaker.com>
2020-11-09 20:25:13 -0500

    blaze-0.14.14

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -240,7 +240,7 @@ object Http4sPlugin extends AutoPlugin {
     // error-prone merge conflicts in the dependencies below.
     val argonaut = "6.2.5"
     val asyncHttpClient = "2.10.5"
-    val blaze = "0.14.13"
+    val blaze = "0.14.14"
     val boopickle = "1.3.3"
     val cats = "2.2.0"
     val catsEffect = "2.2.0"
