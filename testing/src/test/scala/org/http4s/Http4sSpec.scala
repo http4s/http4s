c9c08316e17671aa5c9e14bd606a63c3f684d48a
Domas Poliakas <domas@poliakas.dev>
2020-11-11 21:34:13 +0100

    Fixed resource allocation

--- testing/src/test/scala/org/http4s/Http4sSpec.scala
+++ testing/src/test/scala/org/http4s/Http4sSpec.scala
@@ -98,7 +98,8 @@ trait Http4sSpec
   }
 
   def withResource[A](r: Resource[IO, A])(fs: A => Fragments): Fragments =
-    r.use(a => IO(fs(a))).unsafeRunSync()
+    r.allocated.map { case (r, release) => fs(r).append(step(release.unsafeRunSync())) }.unsafeRunSync()
+    // r.use(a => IO(fs(a))).unsafeRunSync()
 
   /** These tests are flaky on Travis.  Use sparingly and with great shame. */
   def skipOnCi(f: => Result): Result =
