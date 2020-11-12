387512443af5b45fece02d3639bc695fa715785a
Andrey Kovbovich <akovbovich@gmail.com>
2020-11-12 23:16:30 +0300

    make scalatags encoder more generic

--- scalatags/src/main/scala/org/http4s/scalatags/ScalatagsInstances.scala
+++ scalatags/src/main/scala/org/http4s/scalatags/ScalatagsInstances.scala
@@ -7,15 +7,15 @@
 package org.http4s
 package scalatags
 
-import _root_.scalatags.Text.TypedTag
+import _root_.scalatags.generic.Frag
 import org.http4s.headers.`Content-Type`
 
 trait ScalatagsInstances {
-  implicit def scalatagsEncoder[F[_]](implicit
-      charset: Charset = DefaultCharset): EntityEncoder[F, TypedTag[String]] =
+  implicit def scalatagsEncoder[F[_], C <: Frag[_, String]](implicit
+      charset: Charset = DefaultCharset): EntityEncoder[F, C] =
     contentEncoder(MediaType.text.html)
 
-  private def contentEncoder[F[_], C <: TypedTag[String]](mediaType: MediaType)(implicit
+  private def contentEncoder[F[_], C <: Frag[_, String]](mediaType: MediaType)(implicit
       charset: Charset): EntityEncoder[F, C] =
     EntityEncoder
       .stringEncoder[F]
