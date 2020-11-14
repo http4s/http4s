a707cbbaa0e24b7a8a7bb9a7ca58fd5b048bd1d3
Harpreet Singh <singhh@tcd.ie>
2020-11-14 13:33:23 +0000

    Use http4s collection converters (#3849)

--- async-http-client/src/main/scala/org/http4s/client/asynchttpclient/AsyncHttpClientStats.scala
+++ async-http-client/src/main/scala/org/http4s/client/asynchttpclient/AsyncHttpClientStats.scala
@@ -4,12 +4,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.http4s.client.asynchttpclient
+package org.http4s
+package client
+package asynchttpclient
 
 import cats.effect.Sync
-import org.asynchttpclient.{ClientStats, HostStats}
-
-import scala.jdk.CollectionConverters.MapHasAsScala
+import org.asynchttpclient.{ClientStats, HostStats, Response => _}
+import org.http4s.internal.CollectionCompat.CollectionConverters._
 
 class AsyncHttpClientStats[F[_]: Sync](private val underlying: ClientStats)(implicit F: Sync[F]) {
 
