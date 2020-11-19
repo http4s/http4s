d70acd7956af4f1e6675f35b417696c25c6a78c9
Domas Poliakas <domas@poliakas.dev>
2020-11-19 20:24:47 +0100

    KleisliSyntaxBinCompat flattened into KleisliSyntax

--- core/src/main/scala/org/http4s/syntax/package.scala
+++ core/src/main/scala/org/http4s/syntax/package.scala
@@ -7,7 +7,7 @@
 package org.http4s
 
 package object syntax {
-  object all extends AllSyntaxBinCompat
+  object all extends AllSyntax
   object kleisli extends KleisliSyntax with KleisliSyntaxBinCompat0 with KleisliSyntaxBinCompat1
   object literals extends LiteralsSyntax
   @deprecated("Use cats.foldable._", "0.18.5")
