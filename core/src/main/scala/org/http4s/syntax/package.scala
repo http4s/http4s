96775b355afe59fdcbaa8a543397003d7a22bcc0
Domas Poliakas <domas@poliakas.dev>
2020-11-19 20:46:15 +0100

    Missed one usage of KleisliSyntax to amend

--- core/src/main/scala/org/http4s/syntax/package.scala
+++ core/src/main/scala/org/http4s/syntax/package.scala
@@ -8,7 +8,7 @@ package org.http4s
 
 package object syntax {
   object all extends AllSyntax
-  object kleisli extends KleisliSyntax with KleisliSyntaxBinCompat0 with KleisliSyntaxBinCompat1
+  object kleisli extends KleisliSyntax 
   object literals extends LiteralsSyntax
   @deprecated("Use cats.foldable._", "0.18.5")
   object nonEmptyList extends NonEmptyListSyntax
