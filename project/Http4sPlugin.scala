a76bedc246d7546d2caf692968aa4bb0befb784f
Ross A. Baker <ross@rossabaker.com>
2020-12-08 22:36:30 -0500

    Back out Scala 2.13.4 to unblock the others

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -23,7 +23,7 @@ object Http4sPlugin extends AutoPlugin {
 
   override def requires = Http4sOrgPlugin
 
-  val scala_213 = "2.13.4"
+  val scala_213 = "2.13.3"
   val scala_212 = "2.12.12"
   val scalaVersions = Seq(scala_213, scala_212)
 
