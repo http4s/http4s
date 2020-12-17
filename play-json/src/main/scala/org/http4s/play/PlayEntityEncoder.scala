db308b36faa8a700f4e07db30ba5b4b981026d4d
David Strawn <isomarcte@gmail.com>
2020-12-16 20:06:15 -0700

    Fixup Most Of The ScalaDoc Issues
    This commit corrects _most_ of the Scaladoc issues.
    
    The Scaladoc issues fell into one or more of the following categories.
    
    * Reference was not fully qualified.
        * For external links, and for _some_ internal links, Scaladoc wants a fully qualified symbol to link against, even if the given symbol is in scope.
        * For example, `[[Kleisli]]` -> `[[cats.data.Kleisli]]`.
    * Third party library is not exposing `apiURL` in the POM, or is doing so incorrectly.
        * This prevents SBT from automatically configuring the URLs for linking into the third party Scaladoc.
        * To solve this issue `ScaladocApiMapping` was added in `project/`. This contains manual code to construct the linking,
          using `https://javadoc.io` if the project does not provide another more specific URL.
    * Overloaded references
        * These are warnings caused by a reference to an overloaded symbol.
        * These were _not fixed as part of this commit_. I tried for quite some time, but I just couldn't get the disambiguation logic to work.
    * The Scaladoc was just wrong.
        * In this some cases the Scaladoc just didn't link to a real symbol, usually because of typos or change symbol names.
    
    Prior to this commit there were 35 warnings issued for Scaladoc (you need to enable `link-warnings` to see this), now there are 5 and they are all related to overloaded symbols.

--- play-json/src/main/scala/org/http4s/play/PlayEntityEncoder.scala
+++ play-json/src/main/scala/org/http4s/play/PlayEntityEncoder.scala
@@ -19,7 +19,8 @@ package org.http4s.play
 import play.api.libs.json.Writes
 import org.http4s.EntityEncoder
 
-/** Derive [[EntityEncoder]] if implicit [[Writes]] is in the scope without need to explicitly call `jsonEncoderOf`
+/** Derive [[EntityEncoder]] if implicit [[play.api.libs.json.Writes]] is in
+  * the scope without need to explicitly call `jsonEncoderOf`.
   */
 trait PlayEntityEncoder {
   implicit def playEntityEncoder[F[_], A: Writes]: EntityEncoder[F, A] =
