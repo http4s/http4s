a718e3157a053115022c92ff39909aebb6f0a01c
Jeff Lewis <lewisjkl@me.com>
2020-12-16 21:41:37 -0700

    cleanup ipv6 parser

--- core/src/main/scala/org/http4s/internal/parsing/Rfc3986.scala
+++ core/src/main/scala/org/http4s/internal/parsing/Rfc3986.scala
@@ -36,40 +36,42 @@ private[http4s] object Rfc3986 {
   val ipv6: Parser1[Ipv6Address] = {
 
     def toIpv6(lefts: collection.Seq[Short], rights: collection.Seq[Short]): Ipv6Address =
-      (lefts ++ collection.Seq.fill(8 - lefts.size - rights.size)(0.toShort) ++ rights) match {
+      lefts ++ collection.Seq.fill(8 - lefts.size - rights.size)(0.toShort) ++ rights match {
         case collection.Seq(a, b, c, d, e, f, g, h) =>
           Ipv6Address(a, b, c, d, e, f, g, h)
       }
+
+    def repN[A](n: Int, p: Parser[A], sep: Parser[Unit] = Parser.unit): Parser[List[A]] = ((p ~ sep).replicateA(n - 1) ~ p)
+      .map { case (head, tail) => head.map(_._1) :+ tail }.backtrack
+      .orElse(if (n == 1) p.map(List(_)).backtrack else repN(n - 1, p, sep).backtrack)
+
+    def repExactly[A](n: Int, p: Parser[A], sep: Parser[Unit] = Parser.unit): Parser[List[A]] = ((p ~ sep).replicateA(n - 1) ~ p)
+      .map { case (head, tail) => head.map(_._1) :+ tail }.backtrack
 
     val h16: Parser1[Short] = {
       (hexdig ~ hexdig.? ~ hexdig.? ~ hexdig.?).string.map { (s: String) => java.lang.Integer.parseInt(s, 16).toShort }
     }
 
     val colon = char(':')
+    val doubleColon = string1("::").void
+    val h16Colon = h16 <* colon
 
     val ls32: Parser1[(Short, Short)] = {
       val option1 = (h16 ~ colon.void ~ h16).map { case ((l, _), r) => (l, r) }
       val option2 = ipv4Internal { (a: Byte, b: Byte, c: Byte, d: Byte) =>
         ((a << 8) | b).toShort -> ((c << 8) | d).toShort
       }
-      option1.backtrack.orElse1(option2.backtrack)
+      option1.backtrack.orElse1(option2)
     }
-
-    val doubleColon = string1("::").void
-    val h16Colon = h16 <* colon
-
-    def repN[A](n: Int, p: Parser[A], sep: Parser[Unit] = Parser.unit): Parser[List[A]] = ((p ~ sep).replicateA(n - 1) ~ p)
-      .map { case (head, tail) => head.map(_._1) :+ tail }.backtrack
-      .orElse(if (n == 1) p.map(List(_)).backtrack else repN(n - 1, p, sep).backtrack)
 
     (repN(6, h16Colon).with1 ~ ls32)
       .map { case (ls: collection.Seq[Short], (r0: Short, r1: Short)) => toIpv6(ls, Seq(r0, r1)) }.backtrack
       .orElse1((doubleColon *> repN(4, h16Colon, Parser.unit) ~ ls32)
         .map { case (rs: List[Short], (r0: Short, r1: Short)) => toIpv6(Seq.empty, rs :+ r0 :+ r1) }).backtrack
-      .orElse1((h16.?.with1 ~ doubleColon.void ~ h16Colon ~ h16Colon ~ h16Colon ~ ls32)
-        .map { case (((((ls: Option[Short], _), r1: Short), r2: Short), r3: Short), (r4: Short, r5: Short)) => toIpv6(ls.toSeq, Seq(r1, r2, r3, r4, r5)) }).backtrack
-      .orElse1((repN(2, h16, colon.void).?.with1 ~ doubleColon.void ~ h16Colon ~ h16Colon ~ ls32)
-        .map { case ((((ls: Option[List[Short]], _), r0: Short), r1: Short), (r2: Short, r3: Short)) => toIpv6(ls.getOrElse(Seq.empty), Seq(r0, r1, r2, r3)) }).backtrack
+      .orElse1((h16.?.with1 ~ doubleColon.void ~ repExactly(3, h16Colon) ~ ls32)
+        .map { case (((ls: Option[Short], _), rs), (r4: Short, r5: Short)) => toIpv6(ls.toSeq, rs :+ r4 :+ r5) }).backtrack
+      .orElse1((repN(2, h16, colon.void).?.with1 ~ doubleColon.void ~ repExactly(2, h16Colon) ~ ls32)
+        .map { case (((ls: Option[List[Short]], _), rs), (r2: Short, r3: Short)) => toIpv6(ls.getOrElse(Seq.empty), rs :+ r2 :+ r3) }).backtrack
       .orElse1((repN(3, h16, colon.void).?.with1 ~ doubleColon ~ h16Colon ~ ls32)
         .map { case (((ls: Option[List[Short]], _), r0: Short), (r1: Short, r2: Short)) => toIpv6(ls.getOrElse(Seq.empty), Seq(r0, r1, r2)) }).backtrack
       .orElse1((repN(4, h16, colon.void).?.with1 ~ doubleColon ~ ls32)
