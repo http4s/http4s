http4s/http4s

commit 743625135c21ea347e4cf54f0763e69ac980ae32

Conditionally omit empty content-length header by request method (#4152)

blaze-core/src/main/scala/org/http4s/blazecore/Http1Stage.scala
@@ -67,24 +67,26 @@ private[http4s] trait Http1Stage[F[_]] { self: TailStage[ByteBuffer] =>
       true
     }
 
-  /** Get the proper body encoder based on the message headers */
+  /** Get the proper body encoder based on the request */
   final protected def getEncoder(
-      msg: Message[F],
+      req: Request[F],
       rr: StringWriter,
       minor: Int,
       closeOnFinish: Boolean): Http1Writer[F] = {
-    val headers = msg.headers
+    val headers = req.headers
     getEncoder(
       Connection.from(headers),
       `Transfer-Encoding`.from(headers),
       `Content-Length`.from(headers),
-      msg.trailerHeaders,
+      req.trailerHeaders,
       rr,
       minor,
-      closeOnFinish)
+      closeOnFinish,
+      Http1Stage.omitEmptyContentLength(req)
+    )
   }
 
-  /** Get the proper body encoder based on the message headers,
+  /** Get the proper body encoder based on the request,
     * adding the appropriate Connection and Transfer-Encoding headers along the way
     */
   final protected def getEncoder(
@@ -94,7 +96,8 @@ private[http4s] trait Http1Stage[F[_]] { self: TailStage[ByteBuffer] =>
       trailer: F[Headers],
       rr: StringWriter,
       minor: Int,
-      closeOnFinish: Boolean): Http1Writer[F] =
+      closeOnFinish: Boolean,
+      omitEmptyContentLength: Boolean): Http1Writer[F] =
     lengthHeader match {
       case Some(h) if bodyEncoding.forall(!_.hasChunked) || minor == 0 =>
         // HTTP 1.1: we have a length and no chunked encoding
@@ -142,7 +145,7 @@ private[http4s] trait Http1Stage[F[_]] { self: TailStage[ByteBuffer] =>
 
             case None => // use a cached chunk encoder for HTTP/1.1 without length of transfer encoding
               logger.trace("Using Caching Chunk Encoder")
-              new CachingChunkWriter(this, trailer, chunkBufferMaxSize)
+              new CachingChunkWriter(this, trailer, chunkBufferMaxSize, omitEmptyContentLength)
           }
     }
 
@@ -268,6 +271,9 @@ object Http1Stage {
   private var currentEpoch: Long = _
   private var cachedString: String = _
 
+  private val NoPayloadMethods: Set[Method] =
+    Set(Method.GET, Method.DELETE, Method.CONNECT, Method.TRACE)
+
   private def currentDate: String = {
     val now = Instant.now()
     val epochSecond = now.getEpochSecond
@@ -302,4 +308,7 @@ object Http1Stage {
       rr << Date.name << ": " << currentDate << "\r\n"
     ()
   }
+
+  private def omitEmptyContentLength[F[_]](req: Request[F]) =
+    NoPayloadMethods.contains(req.method)
 }

blaze-core/src/main/scala/org/http4s/blazecore/util/CachingChunkWriter.scala
@@ -30,7 +30,10 @@ import scala.concurrent._
 private[http4s] class CachingChunkWriter[F[_]](
     pipe: TailStage[ByteBuffer],
     trailer: F[Headers],
-    bufferMaxSize: Int)(implicit protected val F: Effect[F], protected val ec: ExecutionContext)
+    bufferMaxSize: Int,
+    omitEmptyContentLength: Boolean)(implicit
+    protected val F: Effect[F],
+    protected val ec: ExecutionContext)
     extends Http1Writer[F] {
   import ChunkWriter._
 
@@ -83,14 +86,19 @@ private[http4s] class CachingChunkWriter[F[_]](
           val hbuff = ByteBuffer.wrap(h.result.getBytes(ISO_8859_1))
           pipe.channelWrite(hbuff :: body :: Nil)
         } else {
-          h << s"Content-Length: 0\r\n\r\n"
+          if (!omitEmptyContentLength)
+            h << s"Content-Length: 0\r\n"
+          h << "\r\n"
           val hbuff = ByteBuffer.wrap(h.result.getBytes(ISO_8859_1))
           pipe.channelWrite(hbuff)
         }
-      } else if (!chunk.isEmpty) writeBodyChunk(chunk, true).flatMap { _ =>
+      } else if (!chunk.isEmpty) {
+        writeBodyChunk(chunk, true).flatMap { _ =>
+          writeTrailer(pipe, trailer)
+        }
+      } else {
         writeTrailer(pipe, trailer)
       }
-      else writeTrailer(pipe, trailer)
 
     f.map(Function.const(false))
   }

blaze-core/src/test/scala/org/http4s/blazecore/util/Http1WriterSpec.scala
@@ -117,12 +117,12 @@ class Http1WriterSpec extends Http4sSpec with CatsEffect {
 
   "CachingChunkWriter" should {
     runNonChunkedTests(tail =>
-      new CachingChunkWriter[IO](tail, IO.pure(Headers.empty), 1024 * 1024))
+      new CachingChunkWriter[IO](tail, IO.pure(Headers.empty), 1024 * 1024, false))
   }
 
   "CachingStaticWriter" should {
     runNonChunkedTests(tail =>
-      new CachingChunkWriter[IO](tail, IO.pure(Headers.empty), 1024 * 1024))
+      new CachingChunkWriter[IO](tail, IO.pure(Headers.empty), 1024 * 1024, false))
   }
 
   "FlushingChunkWriter" should {

blaze-server/src/main/scala/org/http4s/server/blaze/Http1ServerStage.scala
@@ -266,7 +266,8 @@ private[blaze] class Http1ServerStage[F[_]](
           resp.trailerHeaders,
           rr,
           parser.minorVersion(),
-          closeOnFinish)
+          closeOnFinish,
+          false)
     }
 
     unsafeRunAsync(bodyEncoder.write(rr, resp.body).recover { case EOF => true }) {

commit 4026057452c6510a165adec081113dff2a55ab8a

Restore binary compatibility broken in #4152 backport

blaze-core/src/main/scala/org/http4s/blazecore/Http1Stage.scala
@@ -86,6 +86,26 @@ private[http4s] trait Http1Stage[F[_]] { self: TailStage[ByteBuffer] =>
     )
   }
 
+  @deprecated("Preserved for binary compatibility", "0.21.16")
+  protected def getEncoder(
+      msg: Message[F],
+      rr: StringWriter,
+      minor: Int,
+      closeOnFinish: Boolean
+  ): Http1Writer[F] = {
+    val headers = msg.headers
+    getEncoder(
+      Connection.from(headers),
+      `Transfer-Encoding`.from(headers),
+      `Content-Length`.from(headers),
+      msg.trailerHeaders,
+      rr,
+      minor,
+      closeOnFinish,
+      false
+    )
+  }
+
   /** Get the proper body encoder based on the request,
     * adding the appropriate Connection and Transfer-Encoding headers along the way
     */
@@ -148,6 +168,26 @@ private[http4s] trait Http1Stage[F[_]] { self: TailStage[ByteBuffer] =>
               new CachingChunkWriter(this, trailer, chunkBufferMaxSize, omitEmptyContentLength)
           }
     }
+
+  @deprecated("Preserved for binary compatibility", "0.21.16")
+  protected def getEncoder(
+      connectionHeader: Option[Connection],
+      bodyEncoding: Option[`Transfer-Encoding`],
+      lengthHeader: Option[`Content-Length`],
+      trailer: F[Headers],
+      rr: StringWriter,
+      minor: Int,
+      closeOnFinish: Boolean
+  ): Http1Writer[F] =
+    getEncoder(
+      connectionHeader,
+      bodyEncoding,
+      lengthHeader,
+      trailer,
+      rr,
+      minor,
+      closeOnFinish,
+      false)
 
   /** Makes a [[EntityBody]] and a function used to drain the line if terminated early.
     *

blaze-core/src/main/scala/org/http4s/blazecore/util/CachingChunkWriter.scala
@@ -37,6 +37,14 @@ private[http4s] class CachingChunkWriter[F[_]](
     extends Http1Writer[F] {
   import ChunkWriter._
 
+  @deprecated("Preserved for binary compatibility", "0.21.16")
+  private[CachingChunkWriter] def this(
+      pipe: TailStage[ByteBuffer],
+      trailer: F[Headers],
+      bufferMaxSize: Int
+  )(implicit F: Effect[F], ec: ExecutionContext) =
+    this(pipe, trailer, bufferMaxSize, false)(F, ec)
+
   private[this] var pendingHeaders: StringWriter = _
   private[this] var bodyBuffer: Buffer[Chunk[Byte]] = Buffer()
   private[this] var size: Int = 0