1f41a62c73899d6c1916b898d2c8d0a6588d2905
Ross A. Baker <ross@rossabaker.com>
2021-01-08 23:02:35 -0500

    jackson-databind-2.12.1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -302,7 +302,7 @@ object Http4sPlugin extends AutoPlugin {
     val disciplineSpecs2 = "1.1.3"
     val dropwizardMetrics = "4.1.16"
     val fs2 = "2.5.0"
-    val jacksonDatabind = "2.12.0"
+    val jacksonDatabind = "2.12.1"
     val jawn = "1.0.1"
     val jawnFs2 = "1.0.0"
     val jetty = "9.4.35.v20201120"
