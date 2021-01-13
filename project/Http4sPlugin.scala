d9be381fc8eed2e6fce2faf6cf4c6a7deff0c5e3
Scala Steward <me@scala-steward.org>
2021-01-13 12:31:17 +0100

    Update cats-effect-testing-specs2 to 1.0-26-0b34520

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -291,7 +291,7 @@ object Http4sPlugin extends AutoPlugin {
     val caseInsensitive = "0.3.0"
     val cats = "2.3.1"
     val catsEffect = "3.0.0-M5"
-    val catsEffectTesting = "1.0-23-f76ace5"
+    val catsEffectTesting = "1.0-26-0b34520"
     val catsParse = "0.2.0"
     val circe = "0.13.0"
     val cryptobits = "1.3"
