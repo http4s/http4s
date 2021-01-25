850466290cb80c42924ea51ee8551443352d6163
Scala Steward <me@scala-steward.org>
2021-01-25 16:36:25 +0100

    Update simpleclient, simpleclient_common, ... to 0.10.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -316,7 +316,7 @@ object Http4sPlugin extends AutoPlugin {
     val okio = "2.10.0"
     val okhttp = "4.9.0"
     val playJson = "2.9.2"
-    val prometheusClient = "0.9.0"
+    val prometheusClient = "0.10.0"
     val reactiveStreams = "1.0.3"
     val quasiquotes = "2.1.0"
     val scalacheck = "1.15.2"
