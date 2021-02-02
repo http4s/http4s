fead663bdaf9915c49d289d61a4e4fdb3de76b0d
Raas Ahsan <raas.ahsan@gmail.com>
2021-02-02 14:40:24 -0600

    Pass maxConnections correctly

--- blaze-server/src/main/scala/org/http4s/server/blaze/BlazeServerBuilder.scala
+++ blaze-server/src/main/scala/org/http4s/server/blaze/BlazeServerBuilder.scala
@@ -400,7 +400,7 @@ class BlazeServerBuilder[F[_]] private (
               .fixedGroup(connectorPoolSize, bufferSize, channelOptions, selectorThreadFactory)
           case Nio1 =>
             NIO1SocketServerGroup
-              .fixed(connectorPoolSize, bufferSize, channelOptions, selectorThreadFactory, maxConnections)
+              .fixed(connectorPoolSize, bufferSize, channelOptions, selectorThreadFactory, maxConnections = maxConnections)
         }
       })(factory => F.delay(factory.closeGroup()))
 
