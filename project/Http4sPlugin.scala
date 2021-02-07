47f1b833866f69e1bd2b4b2cb519043cca7331c9
Ross A. Baker <ross@rossabaker.com>
2021-02-06 20:00:25 -0500

    Upgrade to ip4s-2.0.0-M1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -296,7 +296,7 @@ object Http4sPlugin extends AutoPlugin {
     val disciplineSpecs2 = "1.1.3"
     val dropwizardMetrics = "4.1.17"
     val fs2 = "2.5.0"
-    val ip4s = "1.4.1"
+    val ip4s = "2.0.0-M1"
     val jacksonDatabind = "2.12.1"
     val jawn = "1.0.3"
     val jawnFs2 = "1.0.1"
