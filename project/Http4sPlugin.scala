46640ff69569a51ca818d9641f234cdbdfde0b4d
Scala Steward <me@scala-steward.org>
2021-02-08 16:25:56 +0100

    Update netty-buffer, netty-codec-http to 4.1.59.Final

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -311,7 +311,7 @@ object Http4sPlugin extends AutoPlugin {
     val munit = "0.7.18"
     val munitCatsEffect = "0.13.0"
     val munitDiscipline = "1.0.4"
-    val netty = "4.1.58.Final"
+    val netty = "4.1.59.Final"
     val okio = "2.10.0"
     val okhttp = "4.9.1"
     val playJson = "2.10.0-RC1"
