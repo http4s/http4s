e55b3399650e83e32cb96534befe84bd562a0fb1
Fabio Labella <fabio.labella2@gmail.com>
2021-02-16 00:22:26 +0000

    Add new Header instance for Accept-Charset

--- core/src/main/scala/org/http4s/headers/Accept-Charset.scala
+++ core/src/main/scala/org/http4s/headers/Accept-Charset.scala
@@ -21,14 +21,15 @@ import cats.data.NonEmptyList
 import cats.parse.Parser
 import cats.syntax.all._
 import org.http4s.CharsetRange.{Atom, `*`}
+import org.http4s.util.Renderer
 import org.typelevel.ci.CIString
 
 object `Accept-Charset` extends HeaderKey.Internal[`Accept-Charset`] with HeaderKey.Recurring {
   implicit def headerForAcceptCharset: v2.Header[`Accept-Charset`, v2.Header.Recurring] =
     v2.Header.of(
       CIString("Accept-Charset"),
-      _ => "",
-      _ => ???
+      ac => Renderer.renderString(ac.values),
+      ParseResult.fromParser(parser, "Invalid Accept-Charset header")
     )
 
   override def parse(s: String): ParseResult[`Accept-Charset`] =
