b359d7a403bc87d86c93047361d61d3514683763
Ross A. Baker <ross@rossabaker.com>
2021-02-20 16:18:11 -0500

    Migrate boopickle

--- boopickle/src/test/scala/org/http4s/booPickle/BoopickleSuite.scala
+++ boopickle/src/test/scala/org/http4s/booPickle/BoopickleSuite.scala
@@ -65,13 +65,13 @@ class BoopickleSuite extends Http4sSuite with BooPickleInstances with Http4sLawS
 
   test("have octet-stream content type") {
     assertEquals(
-      encoder.headers.get(`Content-Type`),
+      encoder.headers.get[`Content-Type`],
       Some(`Content-Type`(MediaType.application.`octet-stream`)))
   }
 
   test("have octect-stream content type") {
     assertEquals(
-      booEncoderOf[IO, Fruit].headers.get(`Content-Type`),
+      booEncoderOf[IO, Fruit].headers.get[`Content-Type`],
       Some(`Content-Type`(MediaType.application.`octet-stream`)))
   }
 
