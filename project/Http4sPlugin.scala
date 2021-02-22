d177e04a61029c09dd9cc655337c5a2ba833a07a
Scala Steward <me@scala-steward.org>
2021-02-22 08:25:45 +0100

    Update log4cats-slf4j, log4cats-testing to 2.0.0-RC1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -295,7 +295,7 @@ object Http4sPlugin extends AutoPlugin {
     val jetty = "9.4.36.v20210114"
     val keypool = "0.4.0-M1"
     val logback = "1.2.3"
-    val log4cats = "2.0.0-M1"
+    val log4cats = "2.0.0-RC1"
     val log4s = "1.10.0-M5"
     val munit = "0.7.18"
     val munitCatsEffect = "0.13.0"
