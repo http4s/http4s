5258f311d93c7a70db97b541456465a7ebb75925
Scala Steward <me@scala-steward.org>
2021-02-26 04:22:37 +0100

    Update http2-server to 11.0.1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -291,7 +291,7 @@ object Http4sPlugin extends AutoPlugin {
     val jacksonDatabind = "2.12.1"
     val jawn = "1.1.0"
     val jawnFs2 = "2.0.0-RC2"
-    val jetty = "9.4.37.v20210219"
+    val jetty = "11.0.1"
     val keypool = "0.4.0-RC1"
     val logback = "1.2.3"
     val log4cats = "2.0.0-RC1"
