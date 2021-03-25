b3ce8759ed20fa29317e232e96e31ae863252758
Jens Grassel <jens@wegtam.com>
2021-03-25 17:58:11 +0100

    CSRF checkCSRFinHeaderAndForm fails for multipart
    - add another multipart test case

--- tests/src/test/scala/org/http4s/middleware/CSRFIntegrationSuite.scala
+++ tests/src/test/scala/org/http4s/middleware/CSRFIntegrationSuite.scala
@@ -17,6 +17,7 @@
 package org.http4s.server.middleware
 
 import java.net.ServerSocket
+import java.nio.charset.StandardCharsets
 
 import cats.arrow._
 import cats.effect._
@@ -164,6 +165,58 @@ class CSRFIntegrationSuite extends Http4sSuite {
         client <- IO.delay(EmberClientBuilder.default[IO].withBlocker(testBlocker).build)
         mf = Part.formData[IO](COOKIE_NAME, unlift(token), `Content-Type`(MediaType.text.plain))
         mp = Multipart(Vector(mf))
+        baseReq = Request[IO](
+          method = Method.POST,
+          uri = Uri(
+            scheme = Uri.Scheme.http.some,
+            authority = Uri.Authority(host = Uri.RegName("localhost"), port = port.some).some,
+            path = "/"
+          ),
+          headers = mp.headers
+        ).withEntity(mp).putHeaders(Header("Origin", s"http://localhost:$port"))
+        req = csrfProtect.embedInRequestCookie(baseReq, token)
+        fiber <- resource
+          .use(server =>
+            IO.delay(println(s"Server started at ${server.address}!")) >> client.use(
+              _.expect[String](req)))
+          .start
+        check <- fiber.join
+      } yield check
+
+    prg.assertEquals("CSRF passed!")
+  }
+
+  test("CSRF check must work for multipart forms with files") {
+    val port = CSRFIntegrationSuite.findAvailablePort(reuseAddress = true)
+    val routes = HttpRoutes.of[IO] { case Method.POST -> Root =>
+      Ok("CSRF passed!")
+    }
+
+    val prg =
+      for {
+        key <- CSRF.generateSigningKey[IO]()
+        csrfCheck = CSRF.defaultOriginCheck[IO](_, "localhost", Uri.Scheme.http, port.some)
+        csrfBuilder = CSRF[IO, IO](key, csrfCheck)
+        csrfProtect =
+          csrfBuilder
+            .withCookieName(COOKIE_NAME)
+            .withCSRFCheck(CSRF.checkCSRFinHeaderAndForm[IO, IO](COOKIE_NAME, FunctionK.id))
+            .build
+        token <- csrfProtect.generateToken[IO]
+        service = csrfProtect.validate()(Router("/" -> routes).orNotFound)
+        resource = EmberServerBuilder
+          .default[IO]
+          .withBlocker(testBlocker)
+          .withHost("localhost")
+          .withPort(port)
+          .withHttpApp(service)
+          .build
+        client <- IO.delay(EmberClientBuilder.default[IO].withBlocker(testBlocker).build)
+        mf1 = Part.formData[IO](COOKIE_NAME, unlift(token), `Content-Type`(MediaType.text.plain))
+        file: EntityBody[IO] = fs2.Stream.emits(
+          scala.util.Random.alphanumeric.take(2048).mkString.getBytes(StandardCharsets.UTF_8))
+        mf2 = Part.fileData("file", "passport.txt", file)
+        mp = Multipart(Vector(mf1, mf2))
         baseReq = Request[IO](
           method = Method.POST,
           uri = Uri(
