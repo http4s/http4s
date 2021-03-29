1296e331a90da70b41c2c512b2251286f5946ad1
Scala Steward <me@scala-steward.org>
2021-03-29 19:37:45 +0200

    Update fs2-core, fs2-io, ... to 3.0.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -286,7 +286,7 @@ object Http4sPlugin extends AutoPlugin {
     val cryptobits = "1.3"
     val disciplineCore = "1.1.4"
     val dropwizardMetrics = "4.1.18"
-    val fs2 = "3.0.0-M9"
+    val fs2 = "3.0.0"
     val ip4s = "3.0.0-RC2"
     val jacksonDatabind = "2.12.2"
     val jawn = "1.1.0"
