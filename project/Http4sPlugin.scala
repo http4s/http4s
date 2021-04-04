c3f428ed66d189b58ca66f0e7c7a306f0fc85eec
Scala Steward <me@scala-steward.org>
2021-04-04 12:24:29 +0200

    Update vault to 3.0.1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -317,7 +317,7 @@ object Http4sPlugin extends AutoPlugin {
     val tomcat = "9.0.44"
     val treehugger = "0.4.4"
     val twirl = "1.4.2"
-    val vault = "3.0.0"
+    val vault = "3.0.1"
   }
 
   lazy val asyncHttpClient                  = "org.asynchttpclient"    %  "async-http-client"         % V.asyncHttpClient
