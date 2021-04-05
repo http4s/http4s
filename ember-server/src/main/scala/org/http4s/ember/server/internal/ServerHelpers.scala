8c468c59a5d196fb572bb21d6f0f7ab6a414c6a4
sbuzzard <steve_buzzard@cable.comcast.com>
2021-04-05 09:18:04 -0400

    handle errors/close socket in ember server initial req read

--- ember-server/src/main/scala/org/http4s/ember/server/internal/ServerHelpers.scala
+++ ember-server/src/main/scala/org/http4s/ember/server/internal/ServerHelpers.scala
@@ -199,10 +199,13 @@ private[server] object ServerHelpers {
       Stream
         .unfoldLoopEval((Array.emptyByteArray, false)) { case (incoming, reused) =>
           if (incoming.isEmpty || !reused) {
-            read.map(chunkOpt =>
-              (
+            read.attempt.map {
+              case Right(chunkOpt) => (
                 Option.empty[Either[Throwable, (Request[F], Response[F])]],
-                chunkOpt.map(c => (c.toArray, true))))
+                chunkOpt.map(c => (c.toArray, true))
+              )
+              case Left(e) => (Left(e).some, None)
+            }
           } else {
             runApp(
               incoming,
