5a65b20243478350f940a268aec9595cf8375c08
Scala Steward <me@scala-steward.org>
2021-04-23 00:19:16 +0200

    Update ip4s-core, ip4s-test-kit to 3.0.2

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -287,7 +287,7 @@ object Http4sPlugin extends AutoPlugin {
     val disciplineCore = "1.1.4"
     val dropwizardMetrics = "4.1.18"
     val fs2 = "3.0.1"
-    val ip4s = "3.0.1"
+    val ip4s = "3.0.2"
     val jacksonDatabind = "2.12.3"
     val jawn = "1.1.1"
     val jawnFs2 = "2.0.1"
