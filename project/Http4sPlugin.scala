bf18f6de69518638ad0a7ea64cbe980816a9b595
Bjørn Madsen <bm@aeons.dk>
2021-05-07 14:10:52 +0200

    Update cats-parse to 0.3.3

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -278,7 +278,7 @@ object Http4sPlugin extends AutoPlugin {
     val caseInsensitive = "1.1.3"
     val cats = "2.6.0"
     val catsEffect = "2.5.0"
-    val catsParse = "0.3.2"
+    val catsParse = "0.3.3"
     val circe = "0.14.0-M5"
     val cryptobits = "1.3"
     val disciplineCore = "1.1.4"
