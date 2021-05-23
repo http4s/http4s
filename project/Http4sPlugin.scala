09338c7373033d572e17197d4e777585aadad053
Ross A. Baker <ross@rossabaker.com>
2021-05-23 01:30:47 -0400

    cats-mtl-1.2.1

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -293,7 +293,7 @@ object Http4sPlugin extends AutoPlugin {
     val caseInsensitive = "1.1.4"
     val cats = "2.6.1"
     val catsEffect = "2.5.1"
-    val catsMtl = "1.1.3"
+    val catsMtl = "1.2.1"
     val catsParse = "0.3.4"
     val circe = "0.14.0-M7"
     val cryptobits = "1.3"
