4a473fc54bbb0a4fe04762479749cbe770e6d16b
Arman Bilge <armanbilge@gmail.com>
2021-08-18 16:01:17 +0000

    Add header, head off bincompat pain

--- dom-service-worker/src/main/scala/org/http4s/dom/FetchEventContext.scala
+++ dom-service-worker/src/main/scala/org/http4s/dom/FetchEventContext.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 http4s.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.http4s
 package dom
 
@@ -7,16 +23,16 @@ import cats.syntax.all._
 import org.scalajs.dom.experimental.serviceworkers.FetchEvent
 import org.scalajs.dom.experimental.{Response => DomResponse}
 
-final case class FetchEventContext[F[_]] private (
-    clientId: String,
-    replacesClientId: String,
-    resultingClientId: String,
-    preloadResponse: F[Option[Response[F]]]
+final class FetchEventContext[F[_]] private (
+    val clientId: String,
+    val replacesClientId: String,
+    val resultingClientId: String,
+    val preloadResponse: F[Option[Response[F]]]
 )
 
 object FetchEventContext {
   private[dom] def apply[F[_]](event: FetchEvent)(implicit F: Async[F]): FetchEventContext[F] =
-    FetchEventContext(
+    new FetchEventContext(
       event.clientId,
       event.replacesClientId,
       event.resultingClientId,
