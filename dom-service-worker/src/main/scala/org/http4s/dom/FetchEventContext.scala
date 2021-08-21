28c86ee343ae866dfdae0c7ff39865385f4b3ea1
Arman Bilge <armanbilge@gmail.com>
2021-08-21 03:41:02 +0000

    Use attribute instead of context routes

--- dom-service-worker/src/main/scala/org/http4s/dom/FetchEventContext.scala
+++ dom-service-worker/src/main/scala/org/http4s/dom/FetchEventContext.scala
@@ -18,19 +18,30 @@ package org.http4s
 package dom
 
 import cats.data.OptionT
+import cats.effect.IO
+import cats.effect.SyncIO
 import cats.effect.kernel.Async
+import cats.effect.std.Supervisor
 import cats.syntax.all._
 import org.scalajs.dom.experimental.serviceworkers.FetchEvent
 import org.scalajs.dom.experimental.{Response => DomResponse}
+import org.typelevel.vault.Key
 
 final class FetchEventContext[F[_]] private (
     val clientId: String,
     val resultingClientId: String,
-    val preloadResponse: F[Option[Response[F]]]
+    val preloadResponse: F[Option[Response[F]]],
+    val supervisor: Supervisor[F]
 )
 
 object FetchEventContext {
-  private[dom] def apply[F[_]](event: FetchEvent)(implicit F: Async[F]): FetchEventContext[F] =
+  private[dom] val IOKey = Key.newKey[SyncIO, FetchEventContext[IO]].unsafeRunSync()
+
+  def apply(request: Request[IO]): IO[FetchEventContext[IO]] =
+    IO.fromEither(request.attributes.lookup(IOKey).toRight(new NoSuchElementException))
+
+  private[dom] def apply[F[_]](event: FetchEvent, supervisor: Supervisor[F])(implicit
+      F: Async[F]): FetchEventContext[F] =
     new FetchEventContext(
       event.clientId,
       event.resultingClientId,
@@ -38,6 +49,7 @@ object FetchEventContext {
         F.fromPromise(F.pure(
           // Another incorrectly typed facade :(
           event.preloadResponse.asInstanceOf[scalajs.js.Promise[scalajs.js.UndefOr[DomResponse]]]))
-          .map(_.toOption)).semiflatMap(fromResponse[F]).value
+          .map(_.toOption)).semiflatMap(fromResponse[F]).value,
+      supervisor
     )
 }
--- dom-service-worker/src/main/scala/org/http4s/dom/ServiceWorker.scala
+++ dom-service-worker/src/main/scala/org/http4s/dom/ServiceWorker.scala
@@ -23,45 +23,53 @@ import cats.effect.Async
 import cats.effect.IO
 import cats.effect.SyncIO
 import cats.effect.kernel.Deferred
+import cats.effect.std.Supervisor
 import cats.effect.unsafe.IORuntime
 import cats.syntax.all._
 import fs2.Chunk
 import fs2.Stream
 import org.scalajs.dom.crypto._
 import org.scalajs.dom.experimental.Body
-import org.scalajs.dom.experimental.Fetch
 import org.scalajs.dom.experimental.ResponseInit
+import org.scalajs.dom.experimental.serviceworkers.ExtendableEvent
 import org.scalajs.dom.experimental.serviceworkers.FetchEvent
 import org.scalajs.dom.experimental.serviceworkers.ServiceWorkerGlobalScope
 import org.scalajs.dom.experimental.{Response => DomResponse}
+import org.typelevel.vault.Key
 
 object ServiceWorker {
 
   /** Adds a listener for `FetchEvent`.
     * If the event is not intercepted by `routes` then it is treated as an ordinary request.
+    * Additional context can be retrieved via [[FetchEventContext]] including a [[Supervisor]] for running background tasks.
     * @return an action for removing the listener.
     */
-  def addFetchEventListener(routes: IO[ContextRoutes[FetchEventContext[IO], IO]])(implicit
+  def addFetchEventListener(routes: IO[HttpRoutes[IO]])(implicit
       runtime: IORuntime): SyncIO[SyncIO[Unit]] = for {
-    handler <- Deferred.in[SyncIO, IO, Either[Throwable, FetchEventListener[IO]]]
-    _ <- SyncIO(
-      routes.map(routesToListener[IO]).attempt.flatMap(handler.complete).unsafeRunAndForget())
+    handler <- Deferred.in[SyncIO, IO, Either[Throwable, HttpRoutes[IO]]]
+    _ <- SyncIO(routes.attempt.flatMap(handler.complete).unsafeRunAndForget())
     jsHandler = { event =>
-      event.respondWith {
-        OptionT
-          .liftF(handler.get.rethrow)
-          .flatMap(_(event))
-          .getOrElseF(IO.fromPromise(IO(Fetch.fetch(event.request))))
-          .unsafeToPromise()
-      }
+      event
+        .asInstanceOf[ExtendableEvent]
+        .waitUntil(
+          Supervisor[IO]
+            .use { supervisor =>
+              OptionT
+                .liftF(handler.get.rethrow)
+                .flatMap(routesToListener(_, supervisor, FetchEventContext.IOKey).apply(event))
+                .foldF(IO.unit)(res => IO(event.respondWith(res)))
+            }
+            .unsafeToPromise())
     }: scalajs.js.Function1[FetchEvent, Unit]
     _ <- SyncIO(ServiceWorkerGlobalScope.self.addEventListener("fetch", jsHandler))
   } yield SyncIO(ServiceWorkerGlobalScope.self.removeEventListener("fetch", jsHandler))
 
   private type FetchEventListener[F[_]] = Kleisli[OptionT[F, *], FetchEvent, DomResponse]
 
-  private def routesToListener[F[_]](routes: ContextRoutes[FetchEventContext[F], F])(implicit
-      F: Async[F]): FetchEventListener[F] =
+  private def routesToListener[F[_]](
+      routes: HttpRoutes[F],
+      supervisor: Supervisor[F],
+      key: Key[FetchEventContext[F]])(implicit F: Async[F]): FetchEventListener[F] =
     Kleisli { event =>
       val OptionF = Async[OptionT[F, *]]
       val req = event.request
@@ -74,7 +82,8 @@ object ServiceWorker {
             F.fromPromise(F.delay(req.asInstanceOf[Body].arrayBuffer())).map(Chunk.jsArrayBuffer))
           .covary[F]
         request = Request(method, uri, headers = headers, body = body)
-        response <- routes(ContextRequest(FetchEventContext(event), request))
+          .withAttribute(key, FetchEventContext(event, supervisor))
+        response <- routes(request)
         body <- OptionT.liftF(OptionT(response.body.chunkAll.filter(_.nonEmpty).compile.last).map {
           chunk =>
             arrayBuffer2BufferSource(chunk.toJSArrayBuffer)
