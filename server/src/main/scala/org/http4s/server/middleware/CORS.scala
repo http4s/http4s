15bc8a50af83de788f5aee174bbacb5a61a2524a
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-08-26 11:10:16 +0530

    added httpRoutes and httpApp with config parameter
    Updated CORS.scala, added functions httpRoutes and httpApp with config parameters, made the old httpRoutes and httpApp (at lines 217 and 220 respectively) call the new httpRoutes and httpApp functions respectively , fixes the issue "CORS.toHttpRoutes and CORS.toHttpApp are unconfigurable #5096 "

--- server/src/main/scala/org/http4s/server/middleware/CORS.scala
+++ server/src/main/scala/org/http4s/server/middleware/CORS.scala
@@ -215,8 +215,17 @@ object CORS {
     }
 
   def httpRoutes[F[_]: Monad](httpRoutes: HttpRoutes[F]): HttpRoutes[F] =
-    apply(httpRoutes)
+    httpRoutes(httpRoutes,CORSConfig)
 
   def httpApp[F[_]: Applicative](httpApp: HttpApp[F]): HttpApp[F] =
+    httpApp(httpApp,CORSConfig)
+ 
+  def httpRoutes[F[_]: Monad](httpRoutes: HttpRoutes[F], config: CORSConfig): HttpRoutes[F] = 
+    apply(httpRoutes)
+
+  def httpApp[F[_]: Applicative](httpApp: HttpApp[F], config: CORSConfig): HttpApp[F] = 
     apply(httpApp)
+ 
+ 
 }
+
