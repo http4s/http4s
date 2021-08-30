d7233779860889fa0cc660d90d849787c5b56bcd
rcardin <riccardo.cardin@gmail.com>
2021-08-30 22:16:02 +0200

    Introduced an ADT over `Access-Control-Max-Age` type

--- core/shared/src/main/scala/org/http4s/headers/`Access-Control-Max-Age`.scala
+++ core/shared/src/main/scala/org/http4s/headers/`Access-Control-Max-Age`.scala
@@ -7,12 +7,42 @@ import org.typelevel.ci.CIStringSyntax
 import scala.concurrent.duration.{DurationLong, FiniteDuration}
 import scala.util.Try
 
+
+/** Constructs an `Access-Control-Max-Age` header.
+  *
+  * The value of this field indicates how long the results of a preflight request (that is the information contained in the Access-Control-Allow-Methods and [[`Access-Control-Allow-Headers`]] headers) can be cached. A value of -1 will disable caching.
+  *
+  * @param age age of the response (in seconds)
+  */
+trait `Access-Control-Max-Age` {
+  def duration: Option[FiniteDuration]
+  def unsafeDuration: FiniteDuration
+}
+
 object `Access-Control-Max-Age` {
+
+  final case object NoCaching extends `Access-Control-Max-Age` {
+    override def duration: Option[FiniteDuration] = Option.empty
+
+    override def unsafeDuration: FiniteDuration = throw new UnsupportedOperationException(
+        "It's no possible to get cache duration if caching is disable")
+  }
+
+  final case class Cache private (age: Long) extends `Access-Control-Max-Age` {
+    def duration: Option[FiniteDuration] = Try(age.seconds).toOption
+    def unsafeDuration: FiniteDuration = age.seconds
+  }
+
   def fromLong(age: Long): ParseResult[`Access-Control-Max-Age`] =
     if (age >= 0)
-      ParseResult.success(apply(age))
-    else
-      ParseResult.fail("Invalid age value", s"Access-Control-Max-Age param $age must be more or equal to 0 seconds")
+      ParseResult.success(Cache.apply(age))
+    else if (age == -1) {
+      ParseResult.success(NoCaching)
+    } else {
+      ParseResult.fail(
+        "Invalid age value",
+        s"Access-Control-Max-Age param $age must be greater or equal to 0 seconds or -1")
+    }
 
   def unsafeFromDuration(age: FiniteDuration): `Access-Control-Max-Age` =
     fromLong(age.toSeconds).fold(throw _, identity)
@@ -31,16 +61,4 @@ object `Access-Control-Max-Age` {
       _.age,
       parse
     )
-}
-
-/** Constructs an `Access-Control-Max-Age` header.
-  *
-  * The value of this field indicates how long the results of a preflight request (that is the information contained in the Access-Control-Allow-Methods and [[`Access-Control-Allow-Headers`]] headers) can be cached. A value of -1 will disable caching.
-  *
-  * @param age age of the response (in seconds)
-  */
-final case class `Access-Control-Max-Age` private (age: Long) {
-  def duration: Option[FiniteDuration] = Try(age.seconds).toOption
-
-  def unsafeDuration: FiniteDuration = age.seconds
 }
