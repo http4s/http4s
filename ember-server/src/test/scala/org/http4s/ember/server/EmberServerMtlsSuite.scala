70b380caa6e0c28c2f4d2a59ee94d0ce128e04a8
gchew <geoffrey_chew@cable.comcast.com>
2021-09-02 10:32:35 -0700

    Rewrote unit tests to use Network[IO].tlsContext.fromKeyStoreResource to create TLS contexts where possible and removed the need for an explicitly defined hostname verifier

--- ember-server/src/test/scala/org/http4s/ember/server/EmberServerMtlsSuite.scala
+++ ember-server/src/test/scala/org/http4s/ember/server/EmberServerMtlsSuite.scala
@@ -17,32 +17,26 @@
 package org.http4s.ember.server
 
 import cats.effect._
+import cats.implicits._
+import fs2.io.net.Network
 import fs2.io.net.tls.TLSContext
 import fs2.io.net.tls.TLSParameters
-import java.net.URL
-import java.nio.charset.StandardCharsets
+
+import java.io.IOException
 import java.security.KeyStore
 import javax.net.ssl._
 import org.http4s._
-import org.http4s.server.Server
 import org.http4s.implicits._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.server.ServerRequestKeys
-import org.http4s.testing.ErrorReporting
-import scala.io.Source
-import scala.util.Try
 
 /** Test cases for mTLS support in Ember Server.
   */
 class EmberServerMtlsSuite extends Http4sSuite {
-  {
-    val hostnameVerifier: HostnameVerifier = new HostnameVerifier {
-      override def verify(s: String, sslSession: SSLSession): Boolean = true
-    }
 
-    // For test cases, don't do any host name verification. Certificates are self-signed and not available to all hosts
-    HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier)
-  }
+  val clientCertCn = "CN=Test,OU=Test,O=Test,L=CA,ST=CA,C=US"
+  val expectedNoAuthResponse = "success"
 
   def service[F[_]](implicit F: Async[F]): HttpApp[F] = {
     val dsl = new Http4sDsl[F] {}
@@ -79,24 +73,14 @@ class EmberServerMtlsSuite extends Http4sSuite {
       .orNotFound
   }
 
-  lazy val sslContext: SSLContext = {
-    val ks = KeyStore.getInstance("JKS")
-    ks.load(getClass.getResourceAsStream("/keystore.jks"), "password".toCharArray)
-
-    val kmf = KeyManagerFactory.getInstance("SunX509")
-    kmf.init(ks, "password".toCharArray)
-
-    val js = KeyStore.getInstance("JKS")
-    js.load(getClass.getResourceAsStream("/keystore.jks"), "password".toCharArray)
-
-    val tmf = TrustManagerFactory.getInstance("SunX509")
-    tmf.init(js)
-
-    val sc = SSLContext.getInstance("TLSv1.2")
-    sc.init(kmf.getKeyManagers, tmf.getTrustManagers, null)
-
-    sc
-  }
+  lazy val authTlsClientContext: Resource[IO, TLSContext[IO]] =
+    Resource.eval(Network[IO].tlsContext
+      .fromKeyStoreResource(
+        "keystore.jks",
+        "password".toCharArray,
+        "password".toCharArray
+      )
+    )
 
   lazy val noAuthClientContext: SSLContext = {
     val js = KeyStore.getInstance("JKS")
@@ -111,53 +95,90 @@ class EmberServerMtlsSuite extends Http4sSuite {
     sc
   }
 
-  def server(tlsParams: TLSParameters, receiveBufferSize: Int = 256 * 1024) = ResourceFixture(
-    EmberServerBuilder
-      .default[IO]
-      .withHttpApp(service[IO])
-      .withTLS(
-        TLSContext.Builder.forAsync[IO].fromSSLContext(sslContext),
-        tlsParams
-      )
-      .withReceiveBufferSize(receiveBufferSize)
-      .build)
+  lazy val noAuthTlsClientContext: Resource[IO, TLSContext[IO]] =
+    Resource.eval(
+      TLSContext.Builder.forAsync[IO].fromSSLContext(noAuthClientContext).pure[IO]
+    )
 
-  def get(server: Server, path: String, clientAuth: Boolean = true): String =
-    ErrorReporting.silenceOutputStreams {
-      val url = new URL(s"https://localhost:${server.address.getPort}$path")
-      val conn = url.openConnection().asInstanceOf[HttpsURLConnection]
-      conn.setRequestMethod("GET")
+  def fixture(tlsParams: TLSParameters, clientTlsContext: Resource[IO, TLSContext[IO]]) =
+    (server(tlsParams), client(clientTlsContext)).mapN(FunFixture.map2(_, _))
 
-      if (clientAuth)
-        conn.setSSLSocketFactory(sslContext.getSocketFactory)
-      else
-        conn.setSSLSocketFactory(noAuthClientContext.getSocketFactory)
+  def client(tlsContextResource: Resource[IO, TLSContext[IO]]) = {
+    ResourceFixture(clientResource(tlsContextResource))
+  }
 
-      Try {
-        Source.fromInputStream(conn.getInputStream, StandardCharsets.UTF_8.name).getLines().mkString
-      }.recover { case ex: Throwable =>
-        ex.getMessage
-      }.toOption
-        .getOrElse("")
-    }
+  def clientResource(tlsContextResource: Resource[IO, TLSContext[IO]]) = {
+    (for {
+      tlsContext <- tlsContextResource
+      emberClient <- EmberClientBuilder.default[IO]
+        .withTLSContext(tlsContext)
+        .withoutCheckEndpointAuthentication
+        .build
+    } yield emberClient)
+  }
 
-  server(TLSParameters(needClientAuth = true))
-    .test("Server should handle mTLS request correctly") { server =>
-      assertEquals(get(server, "/dummy", true), "CN=Test,OU=Test,O=Test,L=CA,ST=CA,C=US")
-    }
+  def server(tlsParams: TLSParameters) = {
+    ResourceFixture(serverResource(tlsParams))
+  }
 
-  server(TLSParameters(needClientAuth = true))
-    .test("Server should fail for invalid client auth") { server =>
-      assertNotEquals(get(server, "/dummy", false), "CN=Test,OU=Test,O=Test,L=CA,ST=CA,C=US")
-    }
+  def serverResource(tlsParams: TLSParameters) =
+    (for {
+      tlsContext <- authTlsClientContext
+      emberServer <- EmberServerBuilder
+        .default[IO]
+        .withHttpApp(service[IO])
+        .withTLS(
+          tlsContext,
+          tlsParams
+        )
+        .build
+    } yield emberServer)
 
-  server(TLSParameters(wantClientAuth = true))
-    .test("Server should handle mTLS request correctly with optional auth") { server =>
-      assertEquals(get(server, "/dummy", true), "CN=Test,OU=Test,O=Test,L=CA,ST=CA,C=US")
-    }
+  fixture(TLSParameters(needClientAuth = true, protocols = List("TLSv1.2").some), authTlsClientContext).test("Server should handle mTLS request correctly") { case (server, client) =>
+    import org.http4s.dsl.io._
+    import org.http4s.client.dsl.io._
 
-  server(TLSParameters(wantClientAuth = true))
-    .test("Server should handle mTLS request correctly without clientAuth") { server =>
-      assertEquals(get(server, "/noauth", false), "success")
-    }
+    val uri = Uri
+      .fromString(s"https://${server.address.getHostName}:${server.address.getPort}/dummy")
+      .toOption
+      .get
+    val request = GET(uri)
+    for {
+      response <- client.fetchAs[String](request)
+    } yield assertEquals(clientCertCn, response)
+  }
+
+  fixture(TLSParameters(needClientAuth = true, protocols = List("TLSv1.2").some), noAuthTlsClientContext).test("Server should fail for invalid client auth") { case (server, client) =>
+    client.get(s"https://${server.address.getHostName}:${server.address.getPort}/dummy")(_.status.pure[IO])
+      .intercept[IOException]
+  }
+
+  fixture(TLSParameters(wantClientAuth = true, protocols = List("TLSv1.2").some), authTlsClientContext).test("Server should handle mTLS request correctly with optional auth") { case (server, client) =>
+    import org.http4s.dsl.io._
+    import org.http4s.client.dsl.io._
+
+    val uri = Uri
+      .fromString(s"https://${server.address.getHostName}:${server.address.getPort}/dummy")
+      .toOption
+      .get
+    val request = GET(uri)
+    for {
+      response <- client.fetchAs[String](request)
+    } yield assertEquals(clientCertCn, response)
+  }
+
+  fixture(TLSParameters(wantClientAuth = true, protocols = List("TLSv1.2").some), noAuthTlsClientContext).test("Server should handle mTLS request correctly without clientAuth") { case (server, client) =>
+    import org.http4s.dsl.io._
+    import org.http4s.client.dsl.io._
+
+    val uri = Uri
+      .fromString(s"https://${server.address.getHostName}:${server.address.getPort}/noauth")
+      .toOption
+      .get
+    val request = GET(uri)
+    for {
+      response <- client.fetchAs[String](request)
+    } yield assertEquals(expectedNoAuthResponse, response)
+  }
+
 }
