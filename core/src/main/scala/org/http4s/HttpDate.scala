8599c1fead72ce8a8c5e9b9cfe94cebbeeffd0cb
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-09-18 20:21:02 +0530

    Added Hash to cats instance

--- core/src/main/scala/org/http4s/HttpDate.scala
+++ core/src/main/scala/org/http4s/HttpDate.scala
@@ -17,7 +17,7 @@
 package org.http4s
 
 import cats.Functor
-import cats.kernel.Order
+import cats.{Hash, Order}
 import cats.effect.Clock
 import cats.implicits._
 import cats.parse.{Parser, Rfc5234}
@@ -55,10 +55,13 @@ class HttpDate private (val epochSecond: Long) extends Renderable with Ordered[H
 object HttpDate {
   private val MinEpochSecond = -2208988800L
   private val MaxEpochSecond = 253402300799L
-  implicit val catsOrderForHttp4sHttpDate: Order[HttpDate] =
-    new Order[HttpDate] {
+  implicit val catsOrderForHttp4sHttpDate: Order[HttpDate] with Hash[HttpDate] =
+    new Order[HttpDate] with Hash[HttpDate] {
       override def compare(x: HttpDate, y: HttpDate): Int =
         x.epochSecond.compareTo(y.epochSecond)
+      
+      override def hash(x: HttpDate): Int =
+        x.hashCode
     }
 
   /** The earliest value reprsentable as an HTTP-date, `Mon, 01 Jan 1900 00:00:00 GMT`.
