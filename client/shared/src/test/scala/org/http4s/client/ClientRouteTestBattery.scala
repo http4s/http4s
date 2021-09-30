f843714771da57d27bc7a206d416dc21b38447c8
Arman Bilge <armanbilge@gmail.com>
2021-09-30 15:11:46 -0700

    Fix bungled rebase

--- client/shared/src/test/scala/org/http4s/client/ClientRouteTestBattery.scala
+++ client/shared/src/test/scala/org/http4s/client/ClientRouteTestBattery.scala
@@ -47,111 +47,129 @@ abstract class ClientRouteTestBattery(name: String) extends Http4sSuite with Htt
       Ok(req.body)
   }
 
-  val server = resourceSuiteFixture("server", ServerScaffold[IO](1, false, testHandler))
-  val client = resourceSuiteFixture("client", clientResource)
+  val serverClient = resourceSuiteDeferredFixture(
+    "serverClient",
+    ServerScaffold[IO](1, false, testHandler)
+      .map(() => _)
+      .product(clientResource.map(() => _))
+  )
 
   test(s"$name Repeat a simple request") {
-    val address = server().addresses.head
-    val path = GetRoutes.SimplePath
-
-    def fetchBody =
-      client().toKleisli(_.as[String]).local { (uri: Uri) =>
-        Request(uri = uri)
-      }
-
-    val url = Uri.fromString(s"http://${address.getHostName}:${address.getPort}$path").yolo
-    (0 until 10).toVector
-      .parTraverse(_ => fetchBody.run(url).map(_.length))
-      .map(_.forall(_ =!= 0))
-      .assert
-  }
-
-  test(s"$name POST an empty body") {
-    val address = server().addresses.head
-    val uri = Uri.fromString(s"http://${address.getHostName}:${address.getPort}/echo").yolo
-    val req = POST(uri)
-    val body = client().expect[String](req)
-    body.assertEquals("")
-  }
-
-  test(s"$name POST a normal body") {
-    val address = server().addresses.head
-    val uri = Uri.fromString(s"http://${address.getHostName}:${address.getPort}/echo").yolo
-    val req = POST("This is normal.", uri)
-    val body = client().expect[String](req)
-    body.assertEquals("This is normal.")
-  }
-
-  test(s"$name POST a chunked body".flaky) {
-    val address = server().addresses.head
-    val uri = Uri.fromString(s"http://${address.getHostName}:${address.getPort}/echo").yolo
-    val req = POST(Stream("This is chunked.").covary[IO], uri)
-    val body = client().expect[String](req)
-    body.assertEquals("This is chunked.")
-  }
-
-  test(s"$name POST a multipart body") {
-    val address = server().addresses.head
-    val uri = Uri.fromString(s"http://${address.getHostName}:${address.getPort}/echo").yolo
-    val multipart = Multipart[IO](Vector(Part.formData("text", "This is text.")))
-    val req = POST(multipart, uri).withHeaders(multipart.headers)
-    val body = client().expect[String](req)
-    body.map(_.contains("This is text.")).assert
-  }
-
-  GetRoutes.getPaths.toList.foreach { case (path, expected) =>
-    test(s"$name Execute GET $path") {
+    serverClient().flatMap { case (server, client) =>
       val address = server().addresses.head
-      val name = address.getHostName
-      val port = address.getPort
-      val req = Request[IO](uri = Uri.fromString(s"http://$name:$port$path").yolo)
-      client()
-        .run(req)
-        .use(resp => expected.flatMap(checkResponse(resp, _)))
+      val path = GetRoutes.SimplePath
+
+      def fetchBody =
+        client().toKleisli(_.as[String]).local { (uri: Uri) =>
+          Request(uri = uri)
+        }
+
+      val url = Uri.fromString(s"http://$address$path").yolo
+      (0 until 10).toVector
+        .parTraverse(_ => fetchBody.run(url).map(_.length))
+        .map(_.forall(_ =!= 0))
         .assert
     }
   }
 
+  test(s"$name POST an empty body") {
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val uri = Uri.fromString(s"http://$address/echo").yolo
+      val req = POST(uri)
+      val body = client().expect[String](req)
+      body.assertEquals("")
+    }
+  }
+
+  test(s"$name POST a normal body") {
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val uri = Uri.fromString(s"http://$address/echo").yolo
+      val req = POST("This is normal.", uri)
+      val body = client().expect[String](req)
+      body.assertEquals("This is normal.")
+    }
+  }
+
+  test(s"$name POST a chunked body".flaky) {
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val uri = Uri.fromString(s"http://$address/echo").yolo
+      val req = POST(Stream("This is chunked.").covary[IO], uri)
+      val body = client().expect[String](req)
+      body.assertEquals("This is chunked.")
+    }
+  }
+
+  test(s"$name POST a multipart body") {
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val uri = Uri.fromString(s"http://$address/echo").yolo
+      val multipart = Multipart[IO](Vector(Part.formData("text", "This is text.")))
+      val req = POST(multipart, uri).withHeaders(multipart.headers)
+      val body = client().expect[String](req)
+      body.map(_.contains("This is text.")).assert
+    }
+  }
+
+  GetRoutes.getPaths.toList.foreach { case (path, expected) =>
+    test(s"$name Execute GET $path") {
+      serverClient().flatMap { case (server, client) =>
+        val address = server().addresses.head
+        val req = Request[IO](uri = Uri.fromString(s"http://$address$path").yolo)
+        client()
+          .run(req)
+          .use(resp => expected.flatMap(checkResponse(resp, _)))
+          .assert
+      }
+    }
+  }
+
   test("Mitigates request splitting attack in URI path") {
-    val address = server().addresses.head
-    val name = address.getHostName
-    val port = address.getPort
-    val req = Request[IO](
-      uri = Uri(
-        authority = Uri.Authority(None, Uri.RegName(name), port = port.some).some,
-        path = Uri.Path.Root / Uri.Path.Segment.encoded(
-          "request-splitting HTTP/1.0\r\nEvil:true\r\nHide-Protocol-Version:")
-      ))
-    client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val name = address.host.toString
+      val port = address.port.value
+      val req = Request[IO](
+        uri = Uri(
+          authority = Uri.Authority(None, Uri.RegName(name), port = port.some).some,
+          path = Uri.Path.Root / Uri.Path.Segment.encoded(
+            "request-splitting HTTP/1.0\r\nEvil:true\r\nHide-Protocol-Version:")
+        ))
+      client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    }
   }
 
   test("Mitigates request splitting attack in URI RegName") {
-    val address = server().addresses.head
-    val name = address.getHostName
-    val port = address.getPort
-    val req = Request[IO](uri = Uri(
-      authority =
-        Uri.Authority(None, Uri.RegName(s"${name}\r\nEvil:true\r\n"), port = port.some).some,
-      path = path"/request-splitting"))
-    client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val name = address.host
+      val port = address.port.value
+      val req = Request[IO](uri = Uri(
+        authority =
+          Uri.Authority(None, Uri.RegName(s"${name}\r\nEvil:true\r\n"), port = port.some).some,
+        path = path"/request-splitting"))
+      client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    }
   }
 
   test("Mitigates request splitting attack in field name") {
-    val address = server().addresses.head
-    val req = Request[IO](
-      uri =
-        Uri.fromString(s"http://${address.getHostName}:${address.getPort}/request-splitting").yolo)
-      .putHeaders(Header.Raw(ci"Fine:\r\nEvil:true\r\n", "oops"))
-    client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val req = Request[IO](uri = Uri.fromString(s"http://$address/request-splitting").yolo)
+        .putHeaders(Header.Raw(ci"Fine:\r\nEvil:true\r\n", "oops"))
+      client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    }
   }
 
   test("Mitigates request splitting attack in field value") {
-    val address = server().addresses.head
-    val req = Request[IO](
-      uri =
-        Uri.fromString(s"http://${address.getHostName}:${address.getPort}/request-splitting").yolo)
-      .putHeaders(Header.Raw(ci"X-Carrier", "\r\nEvil:true\r\n"))
-    client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    serverClient().flatMap { case (server, client) =>
+      val address = server().addresses.head
+      val req = Request[IO](uri = Uri.fromString(s"http://$address/request-splitting").yolo)
+        .putHeaders(Header.Raw(ci"X-Carrier", "\r\nEvil:true\r\n"))
+      client().status(req).handleError(_ => Status.Ok).assertEquals(Status.Ok)
+    }
   }
 
   private def checkResponse(rec: Response[IO], expected: Response[IO]): IO[Boolean] = {
