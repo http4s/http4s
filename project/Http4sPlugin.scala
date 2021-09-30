2555f80fe8d4f30140cb515bf5132f38d8c1991f
Arman Bilge <armanbilge@gmail.com>
2021-09-30 00:58:23 -0700

    Bump munit-cats-effect to v1.0.6

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -267,7 +267,7 @@ object Http4sPlugin extends AutoPlugin {
     val log4cats = "2.1.1"
     val log4s = "1.10.0"
     val munit = "0.7.29"
-    val munitCatsEffect = "0.4-70-156d308"
+    val munitCatsEffect = "1.0.6"
     val munitDiscipline = "1.0.9"
     val netty = "4.1.68.Final"
     val okio = "2.10.0"
