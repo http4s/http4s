bc350bdd9176e04251b984cec12009b6402b9dba
Scala Steward <me@scala-steward.org>
2021-09-30 14:41:20 +0200

    Update munit-cats-effect-2 to 1.0.6

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -316,7 +316,7 @@ object Http4sPlugin extends AutoPlugin {
     val log4cats = "1.3.1"
     val log4s = "1.10.0"
     val munit = "0.7.29"
-    val munitCatsEffect = "1.0.5"
+    val munitCatsEffect = "1.0.6"
     val munitDiscipline = "1.0.9"
     val netty = "4.1.68.Final"
     val okio = "2.10.0"
