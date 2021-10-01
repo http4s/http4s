63c1d6b2df77960c652f9c4a065cfa1246452e0f
Scala Steward <me@scala-steward.org>
2021-10-01 20:27:09 +0200

    Update metrics-core, metrics-json to 4.2.4

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -250,7 +250,7 @@ object Http4sPlugin extends AutoPlugin {
     val crypto = "0.1.0"
     val cryptobits = "1.3"
     val disciplineCore = "1.1.5"
-    val dropwizardMetrics = "4.2.3"
+    val dropwizardMetrics = "4.2.4"
     val fs2 = "3.1.2"
     val ip4s = "3.0.3"
     val javaWebSocket = "1.5.2"
