8d3c4223859a29155a7c2b417852aa4b9debb32e
Ross A. Baker <ross@rossabaker.com>
2021-10-11 17:33:14 -0400

    Uh, can we just use fs2's gzip?

--- server/src/main/scala/org/http4s/server/middleware/GZip.scala
+++ server/src/main/scala/org/http4s/server/middleware/GZip.scala
@@ -20,22 +20,18 @@ package middleware
 
 import cats.Functor
 import cats.data.Kleisli
-import cats.effect.{Async, Ref}
+import cats.effect.Sync
 import cats.syntax.all._
-import fs2.{Chunk, Stream}
 import fs2.compression._
-import java.util.zip.Deflater
 import org.http4s.headers._
 import org.log4s.getLogger
-import scodec.bits.{BitVector, ByteOrdering, ByteVector}
-import scodec.bits
 
 object GZip {
   private[this] val logger = getLogger
 
   // TODO: It could be possible to look for F.pure type bodies, and change the Content-Length header after
   // TODO      zipping and buffering all the input. Just a thought.
-  def apply[F[_]: Functor, G[_]: Async](
+  def apply[F[_]: Functor, G[_]: Sync](
       http: Http[F, G],
       bufferSize: Int = 32 * 1024,
       level: DeflateParams.Level = DeflateParams.Level.DEFAULT,
@@ -61,7 +57,7 @@ object GZip {
     acceptEncoding.satisfiedBy(ContentCoding.gzip) || acceptEncoding.satisfiedBy(
       ContentCoding.`x-gzip`)
 
-  private def zipOrPass[F[_]: Async](
+  private def zipOrPass[F[_]: Sync](
       response: Response[F],
       bufferSize: Int,
       level: DeflateParams.Level,
@@ -71,7 +67,7 @@ object GZip {
       case resp => resp // Don't touch it, Content-Encoding already set
     }
 
-  private def zipResponse[F[_]: Async](
+  private def zipResponse[F[_]: Sync](
       bufferSize: Int,
       level: DeflateParams.Level,
       resp: Response[F]): Response[F] = {
@@ -79,59 +75,18 @@ object GZip {
     logger.trace("GZip middleware encoding content")
     // Need to add the Gzip header and trailer
 
-    val b = Stream.chunk(header) ++ Stream.eval(Ref.of(TrailerGen())).flatMap { ref =>
-      resp.body
-        .chunkLimit(bufferSize)
-        .noneTerminate
-        .evalMapAccumulate(TrailerGen()) {
-          case (TrailerGen(crc, inputLength), Some(chunk)) =>
-            val gen = TrailerGen(crc.updated(chunk.toBitVector), inputLength + chunk.size)
-            (gen, chunk).pure
-          case (gen, None) =>
-            ref.set(gen).as((gen, Chunk.empty[Byte]))
-        }
-        .flatMap(x => Stream.chunk(x._2))
-        .through(
-          Compression[F].deflate(
-            DeflateParams(
-              level = level,
-              header = ZLibParams.Header.GZIP,
-              bufferSize = bufferSize))) ++
-        Stream.eval(ref.get).flatMap { case TrailerGen(crc, inputLength) =>
-          val checksum = crc.result.toByteVector.reverse
-          val length = ByteVector.fromInt(
-            (inputLength % GZIP_LENGTH_MOD).toInt,
-            ordering = ByteOrdering.LittleEndian
-          )
-          Stream.chunk(Chunk.byteVector(checksum ++ length))
-        }
-    }
     resp
       .removeHeader[`Content-Length`]
       .putHeaders(`Content-Encoding`(ContentCoding.gzip))
-      .copy(body = b)
+      .copy(body = resp.body.through(
+        Compression[F].gzip(
+          fileName = None,
+          modificationTime = None,
+          comment = None,
+          DeflateParams(
+            bufferSize = bufferSize,
+            level = level,
+            header = ZLibParams.Header.GZIP
+          ))))
   }
-
-  private val GZIP_MAGIC_NUMBER = 0x8b1f
-  private val GZIP_LENGTH_MOD = Math.pow(2, 32).toLong
-
-  private val header: Chunk[Byte] = Chunk.array(
-    Array(
-      GZIP_MAGIC_NUMBER.toByte, // Magic number (int16)
-      (GZIP_MAGIC_NUMBER >> 8).toByte, // Magic number  c
-      Deflater.DEFLATED.toByte, // Compression method
-      0.toByte, // Flags
-      0.toByte, // Modification time (int32)
-      0.toByte, // Modification time  c
-      0.toByte, // Modification time  c
-      0.toByte, // Modification time  c
-      0.toByte, // Extra flags
-      0.toByte
-    ) // Operating system
-  )
-
-  private final case class TrailerGen(
-      val crc: bits.crc.CrcBuilder[BitVector] = bits.crc.crc32Builder,
-      val inputLength: Int = 0)
-
 }
