efa0c2c0f46ddf4dd0c7b852a19a6e3b39568d61
m3is0 <m3is0@pm.me>
2021-10-12 20:05:00 +0300

    Sec-Fetch-User header

--- laws/src/main/scala/org/http4s/laws/discipline/arbitrary.scala
+++ laws/src/main/scala/org/http4s/laws/discipline/arbitrary.scala
@@ -1036,4 +1036,7 @@ private[discipline] trait ArbitraryInstancesBinCompat0 extends ArbitraryInstance
       values <- listOf(http4sGenMediaType)
     } yield headers.`Accept-Post`(values)
   }
+
+  implicit val http4sTestingArbitraryForSecFetchUser: Arbitrary[`Sec-Fetch-User`] =
+    Arbitrary(Gen.const(`Sec-Fetch-User`.`?1`))
 }
