a8a00458d1cf189a50c30ad44d82e221a51cbe6b
Ben Plommer <ben.plommer@itv.com>
2021-10-18 15:40:35 +0100

    Fix header test

--- scalafix/output/src/main/scala/fix/HeaderTests.scala
+++ scalafix/output/src/main/scala/fix/HeaderTests.scala
@@ -4,7 +4,7 @@ import org.typelevel.ci._
 object HeaderTests {
   val k = "k"
   val foo: Header.Raw = Header.Raw(ci"key", "value")
-  val bar = Header.Raw(CaseInsensitiveString(k), "value")
+  val bar = Header.Raw(CIString(k), "value")
 
   bar match {
     case Header.Raw(_, v) => v
