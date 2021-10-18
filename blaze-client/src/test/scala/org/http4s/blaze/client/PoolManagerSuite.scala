f226706abe3e2f782729fd952ec2f733285a9260
Rafał Sumisławski <rafal.sumislawski@gmail.com>
2021-10-18 20:31:52 +0200

    reproduce a deadlock on cancelation

--- blaze-client/src/test/scala/org/http4s/blaze/client/PoolManagerSuite.scala
+++ blaze-client/src/test/scala/org/http4s/blaze/client/PoolManagerSuite.scala
@@ -19,7 +19,7 @@ package blaze
 package client
 
 import cats.effect._
-import cats.effect.concurrent.Ref
+import cats.effect.concurrent.{Ref, Semaphore}
 import cats.implicits._
 import com.comcast.ip4s._
 import fs2.Stream
@@ -161,9 +161,8 @@ class PoolManagerSuite extends Http4sSuite with AllSyntax {
         maxTotal = 1,
         maxWaitQueueLimit = 10,
         builder = _ =>
-          IO(println("allocating")) >>
-            isEstablishingConnectionsPossible.get
-              .ifM(IO(new TestConnection()), IO.raiseError(connectionFailure))
+          isEstablishingConnectionsPossible.get
+            .ifM(IO(new TestConnection()), IO.raiseError(connectionFailure))
       )
       conn1 <- pool.borrow(key)
       conn2Fiber <- pool.borrow(key).start
@@ -183,6 +182,28 @@ class PoolManagerSuite extends Http4sSuite with AllSyntax {
         .timeout(200.millis)
       // After failing to allocate conn2, the pool should attempt to allocate the conn3,
       // but it doesn't so we hit the timeoeut. Without the timeout it would be a deadlock.
+    } yield ()
+  }
+
+  test(
+    "A pool manager should not deadlock after an attempt to create a connection is canceled".fail) {
+    for {
+      isEstablishingConnectionsHangs <- Ref[IO].of(true)
+      connectionAttemptsStarted <- Semaphore[IO](0L)
+      pool <- mkPool(
+        maxTotal = 1,
+        maxWaitQueueLimit = 10,
+        builder = _ =>
+          connectionAttemptsStarted.release >>
+            isEstablishingConnectionsHangs.get.ifM(IO.never, IO(new TestConnection()))
+      )
+      conn1Fiber <- pool.borrow(key).start
+      // wait for the first connection attempt to start before we cancel it
+      _ <- connectionAttemptsStarted.acquire
+      _ <- conn1Fiber.cancel
+      _ <- isEstablishingConnectionsHangs.set(false)
+      // The first connection attempt is canceled, so it should now be possible to acquire a new connection (but it's not because curAllocated==1==maxTotal)
+      _ <- pool.borrow(key).timeout(200.millis)
     } yield ()
   }
 }
