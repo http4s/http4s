5836c73d4ad982e77c28467c82347076cb896760
Scala Steward <me@scala-steward.org>
2021-10-20 08:22:30 +0200

    Update scala3-library to 3.1.0 in series/0.22

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -27,7 +27,7 @@ object Http4sPlugin extends AutoPlugin {
 
   val scala_213 = "2.13.6"
   val scala_212 = "2.12.15"
-  val scala_3 = "3.0.2"
+  val scala_3 = "3.1.0"
 
   override lazy val globalSettings = Seq(
     isCi := sys.env.get("CI").isDefined
