2704875efdb3a794f42db43ef736c4a848d965a9
Ben Plommer <ben.plommer@itv.com>
2021-10-20 18:00:47 +0100

    Add vary header

---
+++ core/src/main/scala/org/http4s/headers/Vary.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2013 http4s.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.http4s.headers
+
+import cats.data.NonEmptyList
+import org.typelevel.ci._
+import cats.parse.Parser
+import org.http4s.Header
+import org.http4s.internal.parsing.Rfc7230
+
+sealed trait Vary
+
+object Vary extends HeaderCompanion[Vary]("Vary") {
+
+  def apply(headers: NonEmptyList[CIString]): Vary = HeaderList(headers)
+  def apply(head: CIString, tail: CIString*): Vary = HeaderList(NonEmptyList.of(head, tail: _*))
+
+  case object `*` extends Vary
+  final case class HeaderList(headers: NonEmptyList[CIString]) extends Vary
+
+  override private[http4s] val parser: Parser[Vary] =
+    Parser.char('*').as(`*`).orElse(Rfc7230.headerRep1(Rfc7230.token.map(CIString(_))).map(apply))
+
+  override implicit val headerInstance: Header[Vary, Header.Single] = createRendered {
+    case HeaderList(values) => values
+    case `*` => NonEmptyList.one(ci"*")
+  }
+}
