015a7f23078b169634a1c8ab6b4c651e72639c5b
Arman Bilge <armanbilge@gmail.com>
2021-10-23 16:25:14 +0000

    Revert "Fix url to logo", copy dark to website
    This reverts commit 57a446b447517050f6aa8004772e77cb76f4de91.

--- project/SiteConfig.scala
+++ project/SiteConfig.scala
@@ -173,7 +173,7 @@ object SiteConfig {
       .site.darkMode.disabled
       .site.topNavigationBar(
         // TODO temporary hard-code of homeURL
-        homeLink = ImageLink.external("https://http4s.org", Image.internal(Root / "images" / "http4s-logo-text-light-2.svg")),
+        homeLink = ImageLink.external("https://http4s.org", Image.internal(Root / "images" / "http4s-logo-text-dark-2.svg")),
         navLinks = apiLink.toSeq ++ Seq(
           IconLink.external("https://github.com/http4s/http4s", HeliumIcon.github, options = Styles("svg-link")),
           IconLink.external("https://discord.gg/XF3CXcMzqD", HeliumIcon.chat),
