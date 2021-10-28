0ae76d5257984a36e0f9d163702c612bb6e2847e
rcardin <riccardo.cardin@gmail.com>
2021-10-28 08:54:39 +0200

    Refactoring of the Access-Control-Allow-Methods, letting the * value to be a regular method

--- core/src/main/scala/org/http4s/headers/`Access-Control-Allow-Methods`.scala
+++ core/src/main/scala/org/http4s/headers/`Access-Control-Allow-Methods`.scala
@@ -16,47 +16,25 @@
 
 package org.http4s.headers
 
-import cats.data.NonEmptyList
+import cats.syntax.traverse._
 import org.http4s.internal.parsing.Rfc7230
 import org.http4s.{Header, Method, ParseResult}
 import org.typelevel.ci.{CIString, CIStringSyntax}
 
 /** The `Access-Control-Allow-Methods` header. */
-sealed trait `Access-Control-Allow-Methods`
+final case class `Access-Control-Allow-Methods`(methods: List[Method])
 
 object `Access-Control-Allow-Methods` {
 
-  private val Wildcard: CIString = CIString("*")
-
-  /** The value "*" only counts as a special wildcard value for requests without credentials
-    * (requests without HTTP cookies or HTTP authentication information). In requests with
-    * credentials, it is treated as the literal method name "*" without special semantics.
-    */
-  final case object AllMethods extends `Access-Control-Allow-Methods`
-
-  /** A comma-delimited list of the allowed HTTP request methods.
-    * @param methods List of allowed HTTP methods
-    */
-  final case class Methods(methods: NonEmptyList[Method]) extends `Access-Control-Allow-Methods`
-
   private[http4s] val parser =
     Rfc7230
-      .headerRep1(Rfc7230.token.map(CIString(_)))
+      .headerRep(Rfc7230.token.map(CIString(_)))
       .mapFilter { list =>
-        if (containsOnlyTheWildcard(list)) {
-          Some(AllMethods)
-        } else if (list.exists(_ == Wildcard)) {
-          Option.empty
-        } else {
-          val parsedMethodList = list.traverse { ciMethod =>
-            Method.fromString(ciMethod.toString).toOption
-          }
-          parsedMethodList.map(Methods)
+        val parsedMethodList = list.traverse { ciMethod =>
+          Method.fromString(ciMethod.toString).toOption
         }
+        parsedMethodList.map(`Access-Control-Allow-Methods`.apply)
       }
-
-  private def containsOnlyTheWildcard(list: NonEmptyList[CIString]) =
-    list.size == 1 && list.head == Wildcard
 
   def parse(s: String): ParseResult[`Access-Control-Allow-Methods`] =
     ParseResult.fromParser(parser, "Invalid Access-Control-Allow-Methods header")(s)
@@ -64,10 +42,10 @@ object `Access-Control-Allow-Methods` {
   implicit val headerInstance: Header[`Access-Control-Allow-Methods`, Header.Recurring] =
     Header.createRendered(
       ci"Access-Control-Allow-Methods",
-      {
-        case AllMethods => "*"
-        case Methods(methods) => methods.toList.mkString(",")
-      },
+      _.methods,
       parse
     )
+
+  // TODO Add the semigroup
+
 }
