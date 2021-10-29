4a1407901f434189f6a972cb1163c6072c5537fe
Scala Steward <me@scala-steward.org>
2021-10-29 06:27:54 +0200

    Update okio to 3.0.0 in series/0.22

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -302,7 +302,7 @@ object Http4sPlugin extends AutoPlugin {
     val munitCatsEffect = "1.0.6"
     val munitDiscipline = "1.0.9"
     val netty = "4.1.69.Final"
-    val okio = "2.10.0"
+    val okio = "3.0.0"
     val okhttp = "4.9.2"
     val playJson = "2.9.2"
     val prometheusClient = "0.11.0"
