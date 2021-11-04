7f7ddc7fb6dea62fd7b284a45facd70d27d8b50d
Ben Plommer <ben.plommer@itv.com>
2021-11-04 15:45:41 +0000

    Fix test name

--- scalafix-internal/input/src/main/scala/fix/Fs2SyncCompilerTest.scala
+++ scalafix-internal/input/src/main/scala/fix/Fs2SyncCompilerTest.scala
@@ -7,7 +7,7 @@ package fix
 import cats.effect._
 import fs2._
 
-object `Fs2SyncCompilerTest.scala` {
+object Fs2SyncCompilerTest {
   def usesSyncInnocently[F[_]](implicit F: Sync[F]) = F.delay(println("hi"))
   def usesSyncCompiler[F[_]](implicit F: Sync[F]) = Stream(1, 2, 3).covary[F].compile.drain // assert: Http4sFs2Linters.noFs2SyncCompiler
   def usesConcurrentCompiler[F[_]](implicit F: Concurrent[F]) = Stream(1, 2, 3).covary[F].compile.drain
