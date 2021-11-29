8856bf92d1aca83c41126137f01bbe208e569117
Scala Steward <me@scala-steward.org>
2021-11-29 23:48:26 +0100

    Update cats-core, cats-laws to 2.7.0

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -196,7 +196,7 @@ object Http4sPlugin extends AutoPlugin {
     val blaze = "0.15.2"
     val boopickle = "1.4.0"
     val caseInsensitive = "1.2.0"
-    val cats = "2.6.1"
+    val cats = "2.7.0"
     val catsEffect = "3.2.9"
     val catsParse = "0.3.5"
     val circe = "0.15.0-M1"
