b68a0e665c9a5f600f5344489831169d51db1db4
Scala Steward <me@scala-steward.org>
2021-12-08 14:30:45 +0100

    Update tomcat-catalina, tomcat-coyote, ... to 9.0.56 in series/0.22

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -282,7 +282,7 @@ object Http4sPlugin extends AutoPlugin {
     val scodecBits = "1.1.29"
     val servlet = "3.1.0"
     val slf4j = "1.7.32"
-    val tomcat = "9.0.55"
+    val tomcat = "9.0.56"
     val treehugger = "0.4.4"
     val twirl = "1.4.2"
     val vault = "2.1.13"
