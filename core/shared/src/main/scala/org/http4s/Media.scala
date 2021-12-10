54b8352542f67af3c4260a9edb673b276b69fadd
Ben Plommer <ben.plommer@itv.com>
2021-12-10 17:56:52 +0000

    Make `Media` covariant in F

--- core/shared/src/main/scala/org/http4s/Media.scala
+++ core/shared/src/main/scala/org/http4s/Media.scala
@@ -23,18 +23,10 @@ import fs2.text.decodeWithCharset
 import org.http4s.Charset.`UTF-8`
 import org.http4s.headers._
 
-trait Media[F[_]] {
+trait Media[+F[_]] {
   def body: EntityBody[F]
   def headers: Headers
   def covary[F2[x] >: F[x]]: Media[F2]
-
-  final def bodyText(implicit
-      RT: RaiseThrowable[F],
-      defaultCharset: Charset = `UTF-8`,
-  ): Stream[F, String] = {
-    val cs = charset.getOrElse(defaultCharset).nioCharset
-    body.through(decodeWithCharset(cs))
-  }
 
   final def contentType: Option[`Content-Type`] =
     headers.get[`Content-Type`]
@@ -45,36 +37,50 @@ trait Media[F[_]] {
   final def charset: Option[Charset] =
     contentType.flatMap(_.charset)
 
-  // Decoding methods
-
-  /** Decode the [[Media]] to the specified type
-    *
-    * @param decoder [[EntityDecoder]] used to decode the [[Media]]
-    * @tparam T type of the result
-    * @return the effect which will generate the `DecodeResult[T]`
-    */
-  final def attemptAs[T](implicit decoder: EntityDecoder[F, T]): DecodeResult[F, T] =
-    decoder.decode(this, strict = false)
-
-  /** Decode the [[Media]] to the specified type
-    *
-    * If no valid [[Status]] has been described, allow Ok
-    *
-    * @param decoder [[EntityDecoder]] used to decode the [[Media]]
-    * @tparam A type of the result
-    * @return the effect which will generate the A
-    */
-  final def as[A](implicit F: MonadThrow[F], decoder: EntityDecoder[F, A]): F[A] =
-    F.rethrow(attemptAs.value)
 }
 
 object Media {
+
+  implicit class InvariantOps[F[_]](private val self: Media[F]) {
+    import self._
+
+    final def bodyText(implicit
+        RT: RaiseThrowable[F],
+        defaultCharset: Charset = `UTF-8`,
+    ): Stream[F, String] = {
+      val cs = charset.getOrElse(defaultCharset).nioCharset
+      body.through(decodeWithCharset(cs))
+    }
+
+    // Decoding methods
+
+    /** Decode the [[Media]] to the specified type
+      *
+      * @param decoder [[EntityDecoder]] used to decode the [[Media]]
+      * @tparam T type of the result
+      * @return the effect which will generate the `DecodeResult[T]`
+      */
+    final def attemptAs[T](implicit decoder: EntityDecoder[F, T]): DecodeResult[F, T] =
+      decoder.decode(self, strict = false)
+
+    /** Decode the [[Media]] to the specified type
+      *
+      * If no valid [[Status]] has been described, allow Ok
+      *
+      * @param decoder [[EntityDecoder]] used to decode the [[Media]]
+      * @tparam A type of the result
+      * @return the effect which will generate the A
+      */
+    final def as[A](implicit F: MonadThrow[F], decoder: EntityDecoder[F, A]): F[A] =
+      F.rethrow(attemptAs.value)
+  }
+
   def apply[F[_]](b: EntityBody[F], h: Headers): Media[F] =
     new Media[F] {
       def body = b
 
       def headers: Headers = h
 
-      override def covary[F2[x] >: F[x]]: Media[F2] = this.asInstanceOf[Media[F2]]
+      override def covary[F2[x] >: F[x]]: Media[F2] = this
     }
 }
