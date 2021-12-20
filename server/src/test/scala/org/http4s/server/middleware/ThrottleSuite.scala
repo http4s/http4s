5f3dca00a00b10328012060f61cd9074bf447c1a
danicheg <e.danicheg@yandex.ru>
2021-12-20 20:42:45 +0300

    Use .contains in the ThrottleSuite

--- server/src/test/scala/org/http4s/server/middleware/ThrottleSuite.scala
+++ server/src/test/scala/org/http4s/server/middleware/ThrottleSuite.scala
@@ -45,7 +45,7 @@ class ThrottleSuite extends Http4sSuite {
       val takeFiveTokens: IO[List[TokenAvailability]] =
         (1 to 5).toList.traverse(_ => testee.takeToken)
       val checkTokensUpToCapacity =
-        takeFiveTokens.map(tokens => tokens.exists(_ == TokenAvailable))
+        takeFiveTokens.map(tokens => tokens.contains(TokenAvailable))
       (checkTokensUpToCapacity, testee.takeToken.map(_.isInstanceOf[TokenUnavailable]))
         .mapN(_ && _)
     }.assert
