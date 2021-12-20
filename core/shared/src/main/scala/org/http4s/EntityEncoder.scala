b9a5c59f74b7abd1338510e99751de34b8fe1d9a
Ben Plommer <ben.plommer@itv.com>
2021-12-20 15:31:36 +0000

    Use Strict for EntityEncoder.simple

--- core/shared/src/main/scala/org/http4s/EntityEncoder.scala
+++ core/shared/src/main/scala/org/http4s/EntityEncoder.scala
@@ -101,10 +101,7 @@ object EntityEncoder {
     * This constructor is a helper for types that can be serialized synchronously, for example a String.
     */
   def simple[A](hs: Header.ToRaw*)(toChunk: A => Chunk[Byte]): EntityEncoder.Pure[A] =
-    encodeBy(hs: _*) { a =>
-      val c = toChunk(a)
-      Entity(Stream.chunk(c), Some(c.size.toLong))
-    }
+    encodeBy(hs: _*)(a => Entity.strict(toChunk(a)))
 
   /** Encodes a value from its Show instance.  Too broad to be implicit, too useful to not exist. */
   def showEncoder[A](implicit charset: Charset = `UTF-8`, show: Show[A]): EntityEncoder.Pure[A] = {
