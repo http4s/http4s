51a059eba590a0edce6631384c291aae02ed6db0
yurique <i@yurique.com>
2021-12-25 04:42:47 +0200

    update scaladoc
    Co-authored-by: Arman Bilge <armanbilge@gmail.com>

--- server/shared/src/main/scala/org/http4s/server/staticcontent/FileService.scala
+++ server/shared/src/main/scala/org/http4s/server/staticcontent/FileService.scala
@@ -184,10 +184,10 @@ object FileService {
     /** Creates an instance of [[org.http4s.server.staticcontent.FileService]] configuration.
       *
       * @param systemPath path prefix to the folder from which content will be served
-      * @param pathPrefix prefix of Uri from which content will be served
       * @param fs2PathCollector function that performs the work of collecting the file or rendering the directory into a response.
-      * @param bufferSize buffer size to use for internal read buffers
+      * @param pathPrefix prefix of Uri from which content will be served
       * @param cacheStrategy strategy to use for caching purposes. Default to no caching.
+      * @param bufferSize buffer size to use for internal read buffers
       */
     def apply[F[_]](
         systemPath: String,
