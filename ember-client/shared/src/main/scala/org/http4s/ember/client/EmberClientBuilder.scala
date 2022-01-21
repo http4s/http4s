85a1e434e61d5538d91a0ed820e1c26bed883a91
Christopher Davenport <chris@christopherdavenport.tech>
2022-01-20 21:11:45 -0800

    Fix Retry Application Merge Conflict

--- ember-client/shared/src/main/scala/org/http4s/ember/client/EmberClientBuilder.scala
+++ ember-client/shared/src/main/scala/org/http4s/ember/client/EmberClientBuilder.scala
@@ -270,7 +270,7 @@ final class EmberClientBuilder[F[_]: Async] private (
             unixSocketClient(request, address)
           }
       }
-      val stackClient = Retry(retryPolicy, logRetries = false)(client)
+      val stackClient = Retry.create(retryPolicy, logRetries = false)(client)
       val iClient = new EmberClient[F](stackClient, pool)
 
       optH2.fold(iClient) { h2 =>
