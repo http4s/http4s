3eaf1b5bfb3d3cdc2412aa33a1cc2982f8bc570a
Ross A. Baker <ross@rossabaker.com>
2022-01-27 11:27:58 -0500

    nowarn on Scala.js

--- ember-core/js/src/main/scala/org/http4s/ember/core/h2/facade/Compressor.scala
+++ ember-core/js/src/main/scala/org/http4s/ember/core/h2/facade/Compressor.scala
@@ -16,11 +16,13 @@
 
 package org.http4s.ember.core.h2.facade
 
+import scala.annotation.nowarn
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 @js.native
 @JSImport("hpack.js", "compressor")
+@nowarn
 private[h2] class Compressor(options: HpackOptions) extends js.Object {
   def write(headers: js.Array[Header]): Unit = js.native
   def read(): js.typedarray.Uint8Array = js.native
