375eefd423ae4677069bd645a8cfac9e64d46eb5
Jon-Anders Teigen <jteigen@gmail.com>
2022-02-17 17:05:24 +0100

    Routing on variable segments
    Router and ContextRouter are enhanced with support for routing on
    variable path segments through the new methods `dynamic` and `of`.
    Static and dynamic routing is unified through a new type
    `{Router, ContextRouter}.Routable`.
    Capturing and transforming a segment of the path is implemented as a
    tiny middleware `{Router, ContextRouter}.Segment`.
    
    As there are no changes to existing methods, this should be
    backwards compatible. `of` and `dynamic` are source compatible with `apply` and `define`,
    with only slightly worse type-inference for static cases), and
    could replace `apply` and `define` at the next point of breaking binary compatibility.
    
    The routing is always done static, with longest match first, and then
    dynamic in the order they are defined. (Same as it used to be, but
    with the dynamic ones at the end)

--- server/src/main/scala/org/http4s/server/ContextRouter.scala
+++ server/src/main/scala/org/http4s/server/ContextRouter.scala
@@ -16,14 +16,67 @@
 
 package org.http4s.server
 
+import cats.Contravariant
+import cats.Functor
+import cats.Monad
 import cats.data.Kleisli
+import cats.data.OptionT
 import cats.effect.Sync
-import cats.syntax.semigroupk._
+import cats.syntax.all._
 import org.http4s.ContextRequest
 import org.http4s.ContextRoutes
+import org.http4s.Request
 import org.http4s.Uri
 
 object ContextRouter {
+
+  object Segment {
+    implicit def functor[F[_]: Functor, X]: Functor[Segment[F, X, *]] =
+      new Functor[Segment[F, X, *]] {
+        override def map[A, B](fa: Segment[F, X, A])(f: A => B): Segment[F, X, B] =
+          new Segment(fa.run(_, _).map(f))
+      }
+
+    implicit def contravariant[F[_], X]: Contravariant[Segment[F, *, X]] =
+      new Contravariant[Segment[F, *, X]] {
+        override def contramap[A, B](fa: Segment[F, A, X])(f: B => A): Segment[F, B, X] =
+          new Segment((b, segment) => fa.run(f(b), segment))
+      }
+
+    class Partial[A] {
+      def apply[F[_], B](run: (A, Uri.Path.Segment) => OptionT[F, B]): Segment[F, A, B] =
+        new Segment(run)
+    }
+    // partially apply for inference
+    def apply[A] = new Partial[A]
+  }
+
+  final class Segment[F[_], A, B](val run: (A, Uri.Path.Segment) => OptionT[F, B]) {
+    def apply(routes: ContextRoutes[B, F])(implicit F: Monad[F]): ContextRoutes[A, F] =
+      Kleisli { case ContextRequest(a, req) =>
+        for {
+          head <- OptionT.fromOption[F](req.pathInfo.segments.headOption)
+          b <- run(a, head)
+          caret = req.attributes.lookup(Request.Keys.PathInfoCaret).getOrElse(0)
+          response <- routes(
+            ContextRequest(b, req.withAttribute(Request.Keys.PathInfoCaret, caret + 1))
+          )
+        } yield response
+      }
+
+    def ->(routes: ContextRoutes[B, F]): Routable[F, A] =
+      Routable.Dynamic(this, routes)
+  }
+
+  object Routable {
+    final case class Static[F[_], A](tupled: (String, ContextRoutes[A, F])) extends Routable[F, A]
+    final case class Dynamic[F[_], A, B](segment: Segment[F, A, B], routes: ContextRoutes[B, F])
+        extends Routable[F, A]
+
+    implicit def tuple[F[_], A](tupled: (String, ContextRoutes[A, F])): Routable[F, A] =
+      Static(tupled)
+  }
+  sealed trait Routable[F[_], A]
 
   /** Defines an [[ContextRoutes]] based on list of mappings.
     * @see define
@@ -55,4 +108,17 @@ object ContextRouter {
           )(req)
         }
     }
+
+  def of[F[_]: Sync, A](mappings: Routable[F, A]*): ContextRoutes[A, F] =
+    dynamic(mappings: _*)(ContextRoutes.empty[A, F])
+
+  def dynamic[F[_]: Sync, A](mappings: Routable[F, A]*)(
+      default: ContextRoutes[A, F]
+  ): ContextRoutes[A, F] = {
+    val (statics, dynamic) = mappings.toList.partitionEither {
+      case Routable.Static(tuple) => Left(tuple)
+      case Routable.Dynamic(segment, route) => Right(segment(route))
+    }
+    dynamic.foldLeft(define(statics: _*)(default))(_ <+> _)
+  }
 }
