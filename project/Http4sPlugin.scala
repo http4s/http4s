d3fdd74e43d0ea72ddb4a5b073aa12418d1e4e51
Scala Steward <me@scala-steward.org>
2022-02-17 06:21:28 +0100

    Update http4s-crypto to 0.2.2

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -177,7 +177,7 @@ object Http4sPlugin extends AutoPlugin {
     val catsEffect = "3.3.5"
     val catsParse = "0.3.6"
     val circe = "0.15.0-M1"
-    val crypto = "0.2.0"
+    val crypto = "0.2.2"
     val cryptobits = "1.3"
     val disciplineCore = "1.4.0"
     val dropwizardMetrics = "4.2.8"
