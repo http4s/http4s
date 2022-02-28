361995cce4629f7284826583f60162a8d5768731
Scala Steward <me@scala-steward.org>
2022-02-28 16:23:35 +0100

    Revert commit(s) 7c4c89c240

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -212,7 +212,7 @@ object Http4sPlugin extends AutoPlugin {
     val scodecBits = "1.1.30"
     val servlet = "3.1.0"
     val slf4j = "1.7.35"
-    val tomcat = "9.0.59"
+    val tomcat = "9.0.58"
     val treehugger = "0.4.4"
     val twirl = "1.4.2"
     val vault = "3.1.0"
