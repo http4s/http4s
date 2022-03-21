382d521e6f3ff6b45e786ce646499e2aa2e0b40a
Ross A. Baker <ross@rossabaker.com>
2022-03-20 21:52:57 -0400

    Convert to property tests

--- tests/src/test/scala/org/http4s/internal/RandomSuite.scala
+++ tests/src/test/scala/org/http4s/internal/RandomSuite.scala
@@ -2,38 +2,50 @@ package org.http4s.internal
 
 import cats.effect.IO
 import org.http4s.Http4sSuite
+import org.scalacheck.Gen
+import org.scalacheck.effect.PropF._
 
 import java.security.SecureRandom
 import java.util.{Random => JRandom}
 import scala.util.Try
 
 class RandomSuite extends Http4sSuite {
-  test("javaUtilRandomBlocking.nextBigInt works") {
-    val rnd = new JRandom()
-    Random
-      .javaUtilRandomBlocking[IO](testBlocker)(rnd)
-      .nextBigInt(10)
-      .map(_ > 0)
-      .assert
+  // Nothing in the tests asserts that it's blocking, but let's try
+  private val blockingRandom: JRandom =
+    Try(SecureRandom.getInstance("NativePRNGBlocking")).getOrElse(new SecureRandom())
+
+  // Nothing in the tests asserts that it's non-blocking, but let's try
+  private val nonBlockingRandom: JRandom =
+    Try(SecureRandom.getInstance("NativePRNGNonBlocking")).getOrElse(new SecureRandom())
+
+  test("javaUtilRandomBlocking.nextBigInt") {
+    forAllF(Gen.chooseNum(0, 62)) { (n: Int) =>
+      Random
+        .javaUtilRandomBlocking[IO](testBlocker)(blockingRandom)
+        .nextBigInt(n)
+        .map(i => (i >= BigInt(0)) && (i < BigInt((1L << n))))
+        .assert
+    }
   }
 
-  test("javaUtilRandomNonBlocking.nextBigInt works") {
-    val rnd = Try(SecureRandom.getInstance("NativePRNGNonBlocking"))
-    // Actually, this test would run with anything, but let's try to
-    // give it a proper instance.
-    assume(rnd.isSuccess, "this test requires a NativePRNGNonBlocking SecureRandom")
-    Random
-      .javaUtilRandomNonBlocking[IO](rnd.get)
-      .nextBigInt(10)
-      .map(_ > 0)
-      .assert
+  test("javaUtilRandomNonBlocking.nextBigInt") {
+    forAllF(Gen.chooseNum(0, 62)) { (n: Int) =>
+      Random
+        .javaUtilRandomNonBlocking[IO](nonBlockingRandom)
+        .nextBigInt(n)
+        .map(i => (i >= BigInt(0)) && (i < BigInt((1L << n))))
+        .assert
+    }
   }
 
-  test("javaSecurityRandom.nextBigInt works") {
-    Random
-      .javaSecurityRandom[IO](testBlocker)
-      .flatMap(_.nextBigInt(10))
-      .map(_ > 0)
-      .assert
+  test("javaSecurityRandom.nextBigInt") {
+    Random.javaSecurityRandom[IO](testBlocker).map { random =>
+      forAllF(Gen.chooseNum(0, 62)) { (n: Int) =>
+        random
+          .nextBigInt(n)
+          .map(i => (i >= BigInt(0)) && (i < BigInt((1L << n))))
+          .assert
+      }
+    }
   }
 }
