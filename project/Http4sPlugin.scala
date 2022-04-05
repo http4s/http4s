aedeea94f6924212a689feabaacf7d80cd8ab3f0
Scala Steward <me@scala-steward.org>
2022-04-06 01:07:08 +0200

    Update cats-effect, cats-effect-laws, ... to 3.3.11 in series/0.23

--- project/Http4sPlugin.scala
+++ project/Http4sPlugin.scala
@@ -102,7 +102,7 @@ object Http4sPlugin extends AutoPlugin {
     val boopickle = "1.4.0"
     val caseInsensitive = "1.2.0"
     val cats = "2.7.0"
-    val catsEffect = "3.3.10"
+    val catsEffect = "3.3.11"
     val catsParse = "0.3.7"
     val circe = "0.14.1"
     val circe15 = "0.15.0-M1"
