c0e18b675fb04f096a67810c715d2d8fb51cbffd
Arman Bilge <armanbilge@gmail.com>
2022-05-18 18:22:16 +0000

    Revert "Some progress?"
    This reverts commit 18721147d24e20facc6ecd7d3ec6d4fec6b9e9a7.

--- core/js/src/main/scala/org/http4s/nodejs/ClientRequest.scala
+++ core/js/src/main/scala/org/http4s/nodejs/ClientRequest.scala
@@ -23,7 +23,6 @@ import cats.syntax.all._
 import fs2.io.Writable
 
 import scala.annotation.nowarn
-import scala.scalajs.concurrent.QueueExecutionContext
 import scala.scalajs.js
 
 /** Facade for [[https://nodejs.org/api/http.html#class-httpclientrequest]]
@@ -70,13 +69,7 @@ object ClientRequest {
         ()
       }
 
-      F.deferred[Either[Throwable, Unit]].flatMap { deferredError =>
-        error.attempt
-          .flatMap(deferredError.complete)
-          // register the error listener with high-priority on the promises queue
-          .backgroundOn(QueueExecutionContext.promises())
-          .surround(write.race(deferredError.get.rethrow).void)
-      }
+      write.race(error).void
     }
   }
 
