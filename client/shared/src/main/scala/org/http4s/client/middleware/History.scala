8bb191b72f2e3b973f875c694cf5736371d09394
Sally Perez <93544183+SallyPerez@users.noreply.github.com>
2022-07-25 17:23:37 -0700

    history w/o maxSize

--- client/shared/src/main/scala/org/http4s/client/middleware/History.scala
+++ client/shared/src/main/scala/org/http4s/client/middleware/History.scala
@@ -5,7 +5,7 @@ import cats.implicits.catsSyntaxApplicativeId
 import org.http4s._
 import org.http4s.client.Client
 import org.http4s.headers.Date
-import org.typelevel.ci.CIStringSyntax
+//import org.typelevel.ci.CIStringSyntax
 
 //def middleware[F[_]: MonadCancelThrow](client: Client[F], history: Ref[F, List[(HttpDate, Method, Uri)], maxSize: Int): Client[F] = Client[F]{(req: Request[F]) =>
 //  Resource.eval(req.headers.get[Date].fold(HttpDate.now[F])(d => d.date.pure[F]).flatMap{ date =>
@@ -18,16 +18,21 @@ import org.typelevel.ci.CIStringSyntax
 
 object History {
 
-  // maxSize = size of list? maybe drop all elements after max size
-  def apply[F[_]: MonadCancelThrow: Clock](client: Client[F], history: Ref[F, List[(HttpDate, Method, Uri)]], maxSize: Int): Client[F] = Client[F]{
+  // maxSize = size of list? maybe drop all elements after max size - probably this list has a maxSize - drop older stuff once max size is ht
+  // do I need to make and release the Resource in addition to using it? Or is that done somewher else?
+  // return a Resource of response
+  // TO DO - add in maxsize
+  def apply[F[_]: MonadCancelThrow: Clock](client: Client[F], history: Ref[F, List[(HttpDate, Method, Uri)]]): Client[F] = Client[F]{
     req: Request[F] =>
       Resource.eval(req.headers.get[Date].fold(HttpDate.current[F])(d => d.date.pure[F])).flatMap( date => {
         val method = req.method
         val uri = req.uri
 
-        // append these to the ref (hisotry)
-        // what the heck is B???
-        history.modify(l => (date, method, uri) :: l)
+        // history = Ref[F, List[(h, m, u)]]
+        // def eval[F[_], A](fa: F[A]): Resource[F, A]
+        Resource.eval(history.update(l => (date, method, uri) :: l))
+        // def run(req: Request[F]): Resource[F, Response[F]]
+        client.run(req)
 
       })
   }
