94479cfa4cd6e7b183c55fb1175c6b9524365553
Ross A. Baker <ross@rossabaker.com>
2022-10-22 14:05:32 -0400

    Functor[Service]

--- core/shared/src/main/scala/org/http4s/Service.scala
+++ core/shared/src/main/scala/org/http4s/Service.scala
@@ -1,14 +1,32 @@
 package org.http4s
 
+import cats.Functor
 import cats.effect.kernel.Resource
 
-sealed abstract class Service[F[_], -A, B] { self =>
+import Service._
+
+sealed abstract class Service[F[_], -A, B] {
   def run: A => Resource[F, Option[B]]
+
+  def map[C](f: B => C): Service[F, A, C] =
+    Run((a: A) => run(a).map(_.map(f)))
 }
 
-object Service {
+object Service extends ServiceKleisliFunctorInstance {
   private[Service] final case class Run[F[_], -A, B](run: A => Resource[F, Option[B]])
+      extends Service[F, A, B]
 
-  def apply[F[_], A, B](run: A => Resource[F, Option[B]]) =
+  def apply[F[_], A, B](run: A => Resource[F, Option[B]]): Run[F, A, B] =
     Run(run)
+}
+
+private[http4s] sealed abstract class ServiceFunctor[F[_], A] extends Functor[Service[F, A, *]] {
+
+  def map[B, C](fb: Service[F, A, B])(f: B => C): Service[F, A, C] =
+    fb.map(f)
+}
+
+private[http4s] trait ServiceKleisliFunctorInstance {
+  implicit def catsFunctorForHttp4sService[F[_], A, B]: Functor[Service[F, A, *]] =
+    new ServiceFunctor[F, A] {}
 }
