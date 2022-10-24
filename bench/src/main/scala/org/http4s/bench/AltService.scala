7b6c58935b226bfddcce597881b9016ff17e7698
Ross A. Baker <ross@rossabaker.com>
2022-10-23 23:53:07 -0400

    Fix header

--- bench/src/main/scala/org/http4s/bench/AltService.scala
+++ bench/src/main/scala/org/http4s/bench/AltService.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 http4s.org
+ * Copyright 2015 http4s.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
