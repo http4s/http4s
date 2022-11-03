a3b456f4089a2ab2e003bf98c2a3ab2765ee83b9
Tim Spence <timothywspence@gmail.com>
2022-11-03 16:35:27 +0000

    Add withAdditionalSocketOptions to EmberServerBuilder

--- ember-server/shared/src/main/scala/org/http4s/ember/server/EmberServerBuilder.scala
+++ ember-server/shared/src/main/scala/org/http4s/ember/server/EmberServerBuilder.scala
@@ -157,8 +157,8 @@ final class EmberServerBuilder[F[_]: Async: Network] private (
     copy(requestHeaderReceiveTimeout = requestHeaderReceiveTimeout)
   def withLogger(l: Logger[F]): EmberServerBuilder[F] = copy(logger = l)
 
-  def withHttp2 = copy(enableHttp2 = true)
-  def withoutHttp2 = copy(enableHttp2 = false)
+  def withHttp2: EmberServerBuilder[F] = copy(enableHttp2 = true)
+  def withoutHttp2: EmberServerBuilder[F] = copy(enableHttp2 = false)
 
   // If used will bind to UnixSocket
   def withUnixSocketConfig(
@@ -166,10 +166,15 @@ final class EmberServerBuilder[F[_]: Async: Network] private (
       unixSocketAddress: UnixSocketAddress,
       deleteIfExists: Boolean = true,
       deleteOnClose: Boolean = true,
-  ) =
+  ): EmberServerBuilder[F] =
     copy(unixSocketConfig = Some((unixSockets, unixSocketAddress, deleteIfExists, deleteOnClose)))
-  def withoutUnixSocketConfig =
+  def withoutUnixSocketConfig: EmberServerBuilder[F] =
     copy(unixSocketConfig = None)
+
+  def withAdditionalSocketOptions(
+      additionalSocketOptions: List[SocketOption]
+  ): EmberServerBuilder[F] =
+    copy(additionalSocketOptions = additionalSocketOptions)
 
   def build: Resource[F, Server] =
     for {
