eb5af4fc7a17fb5c06615505ab72c4595ee7aac6
jcz2 <jcz2@users.noreply.github.com>
2022-12-11 21:35:23 +0100

    Rfc5322 parsers

---
+++ core/shared/src/main/scala/org/http4s/internal/parsing/Rfc5322.scala
@@ -0,0 +1,18 @@
+package org.http4s.internal.parsing
+
+import cats.parse.Parser
+import cats.parse.Rfc5234.{crlf, vchar, wsp}
+import cats.parse.Parser.{char, charIn, string, void}
+
+object Rfc5322 {
+  val FWS: Parser[Unit] = (wsp.rep0 *> crlf).backtrack.?.with1 *> void(wsp.rep)
+  val ctext: Parser[Char] = charIn(((33 to 39) ++ (42 to 91) ++ (93 to 126)).map(_.toChar))
+  val `quoted-pair`: Parser[String] = char('\\') *> (vchar.string | string(wsp))
+  val ccontent: Parser[String] = Parser.defer(ctext.map(_.toString) | `quoted-pair` | comment)
+  val comment: Parser[String] = Parser.defer(char('(').string ~
+    (FWS.string.?.map(_.getOrElse("")).with1 ~ ccontent)
+      .backtrack
+      .map(p => p._1 + p._2)
+      .rep0.map(_.mkString) ~ FWS.string.?.map(_.getOrElse("")) ~ char(')').string)
+  .map { case (((s1, s2), s3), s4) => s1 + s2 + s3 + s4 }
+}
---
+++ tests/shared/src/test/scala/org/http4s/parser/Rfc5322ParserSpec.scala
@@ -0,0 +1,53 @@
+package org.http4s.parser
+
+import org.http4s.Http4sSuite
+import org.http4s.internal.parsing.Rfc5322
+
+class Rfc5322ParserSpec extends Http4sSuite {
+  test("FWS parser") {
+    val cases = List(
+      " ",
+      "  ",
+      "\r\n  ",
+      "   \r\n   "
+    )
+    cases.foreach(c => {
+      assert(Rfc5322.FWS.parse(c).isRight)
+    })
+    val cases2 = List(
+      "",
+      "  \r\n",
+      "\r\n"
+    )
+    cases2.foreach(c => {
+      assert(Rfc5322.FWS.parse(c).isLeft)
+    })
+  }
+
+  test("quoted-pair parser") {
+    val cases = List(
+      ("\\!", "!"),
+      ("\\ ", " ")
+    )
+    cases.foreach(c => {
+      assertEquals(Rfc5322.`quoted-pair`.parse(c._1).toOption.get._2, c._2)
+    })
+  }
+
+  test("comment parser") {
+    val cases = List(
+      ("( comment1 comment2 ( comment3 ))", "( comment1 comment2 ( comment3 ))"),
+      ("(    \r\n  comment)", "(    \r\n  comment)"),
+      ("( \\! )", "( ! )"),
+      ("(\\! )", "(! )"),
+      ("( \\!)", "( !)"),
+      ("(\\!)", "(!)"),
+      ("( \\!   \\!   )", "( !   !   )"),
+      ("( \\! comment )", "( ! comment )"),
+      ("( \\! comment (((comment2))) ((comment3 \r\n (comment4 \\$))) )", "( ! comment (((comment2))) ((comment3 \r\n (comment4 $))) )")
+    )
+    cases.foreach(c => {
+      assertEquals(Rfc5322.comment.parse(c._1).toOption.get._2, c._2)
+    })
+  }
+}
