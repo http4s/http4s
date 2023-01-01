d3af1aeb9c6700529a62e6f3da2ba371439dadd4
Arman Bilge <armanbilge@gmail.com>
2023-01-01 19:12:38 +0000

    Set `startYear := Some(2022)` instead

--- js-artifact-size-test/src/test/scala/Main.scala
+++ js-artifact-size-test/src/test/scala/Main.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 http4s.org
+ * Copyright 2022 http4s.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
