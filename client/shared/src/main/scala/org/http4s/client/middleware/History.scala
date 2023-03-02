60a3731ee745cf8b75e0ead2d5d7f1c969c4ca24
Sally Perez <93544183+SallyPerez@users.noreply.github.com>
2023-03-02 13:16:35 -0800

    add case class for HistoryEvent

--- client/shared/src/main/scala/org/http4s/client/middleware/History.scala
+++ client/shared/src/main/scala/org/http4s/client/middleware/History.scala
@@ -7,14 +7,17 @@ import org.http4s.client.Client
 import org.http4s.headers.Date
 
 object History {
+  case class HistoryEntry(httpDate: HttpDate, method: Method, uri: Uri)
 
-  def apply[F[_]: MonadCancelThrow: Clock](client: Client[F], history: Ref[F, Vector[(HttpDate, Method, Uri)]], maxSize: Int): Client[F] = Client[F]{
+  def apply[F[_]: MonadCancelThrow: Clock](client: Client[F],
+                                           history: Ref[F, Vector[HistoryEntry]],
+                                           maxSize: Int): Client[F] = Client[F]{
     req: Request[F] =>
       Resource.eval(req.headers.get[Date].fold(HttpDate.current[F])(d => d.date.pure[F])).flatMap( date => {
         val method = req.method
         val uri = req.uri
 
-        Resource.eval(history.update(l => ((date, method, uri) +: l).take(maxSize)))
+        Resource.eval(history.update(l => (HistoryEntry(date, method, uri) +: l).take(maxSize)))
         client.run(req)
       })
   }
