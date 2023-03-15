3aae2a51b4528ef0eee973b349b2fd2cd2b94494
kovstas <mail@kovstas.dev>
2023-03-16 02:13:38 +0400

    Revert "add disposed mark to producer side"
    This reverts commit d146e71374d5a4657d422387a025b24b109af22a.

--- client/shared/src/main/scala/org/http4s/client/Client.scala
+++ client/shared/src/main/scala/org/http4s/client/Client.scala
@@ -20,6 +20,7 @@ package client
 import cats.data._
 import cats.effect._
 import cats.effect.implicits.genSpawnOps
+import cats.effect.implicits.monadCancelOps_
 import cats.effect.kernel.CancelScope
 import cats.effect.kernel.Poll
 import cats.syntax.all._
@@ -310,7 +311,6 @@ object Client {
 
           _ <- resp.body.chunks
             .through(channel.sendAll)
-            .onFinalize(disposed.set(true))
             .compile
             .drain
             .background
@@ -327,6 +327,7 @@ object Client {
               .ifEmpty(ifDisposed)
               .onFinalize(
                 channel.stream.compile.drain
+                  .guarantee(disposed.set(true))
               )
           )
 
@@ -336,6 +337,7 @@ object Client {
                 F.unit,
                 r.body.compile.drain.void,
               )
+              .guarantee(disposed.set(true))
           }
 
         } yield r
