8a003a18abe089ea5af971fb46524c09a951ee0b
Arman Bilge <armanbilge@gmail.com>
2023-06-13 18:13:09 +0000

    Close `readBuffer` on `headers.endStream`

--- ember-core/shared/src/main/scala/org/http4s/ember/core/h2/H2Stream.scala
+++ ember-core/shared/src/main/scala/org/http4s/ember/core/h2/H2Stream.scala
@@ -231,7 +231,7 @@ private[h2] class H2Stream[F[_]: Concurrent](
                       case Some(resp) =>
                         response.complete(Either.right(attribute(resp))) >>
                           checkLengthOf(resp) >>
-                          (if (headers.endStream) s.trailWith(List.empty).void
+                          (if (headers.endStream) s.readBuffer.close *> s.trailWith(List.empty).void
                            else Applicative[F].unit) >>
                           (if (newstate == StreamState.Closed) onClosed
                            else Applicative[F].unit)
@@ -248,7 +248,7 @@ private[h2] class H2Stream[F[_]: Concurrent](
                       case Some(req) =>
                         request.complete(Either.right(attribute(req))) >>
                           checkLengthOf(req) >>
-                          (if (headers.endStream) s.trailWith(List.empty).void
+                          (if (headers.endStream) s.readBuffer.close *> s.trailWith(List.empty).void
                            else Applicative[F].unit) >>
                           (if (newstate == StreamState.Closed) onClosed
                            else Applicative[F].unit)
