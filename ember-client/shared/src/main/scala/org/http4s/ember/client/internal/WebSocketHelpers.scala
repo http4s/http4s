44c7addfa5a5a8f351dcd4fe337bd5f29dd97053
Dang Trung Hieu <hieudangtrung@protonmail.com>
2023-07-12 17:01:04 +0800

    Use shared function from ember-core WebSocketHelpers

--- ember-client/shared/src/main/scala/org/http4s/ember/client/internal/WebSocketHelpers.scala
+++ ember-client/shared/src/main/scala/org/http4s/ember/client/internal/WebSocketHelpers.scala
@@ -19,35 +19,27 @@ package org.http4s.ember.client.internal
 import cats.Applicative
 import cats.MonadThrow
 import cats.data.NonEmptyList
-import cats.effect.Async
 import cats.effect.Concurrent
 import cats.effect.MonadCancel
 import cats.effect.Resource
 import cats.effect.SyncIO
-import cats.effect.implicits._
-import cats.effect.std.Queue
 import cats.syntax.all._
 import fs2.io.net.Socket
 import org.http4s.Request
 import org.http4s.Status
 import org.http4s._
 import org.http4s.client.Client
-import org.http4s.client.websocket.WSClient
-import org.http4s.client.websocket.WSConnection
 import org.http4s.client.websocket.WSFrame
 import org.http4s.crypto.Hash
 import org.http4s.crypto.HashAlgorithm
-import org.http4s.ember.core.WebSocketHelpers.decodeFrames
-import org.http4s.ember.core.WebSocketHelpers.frameToBytes
 import org.http4s.headers._
 import org.http4s.websocket.Rfc6455
 import org.http4s.websocket.WebSocketFrame
 import org.typelevel.ci._
 import org.typelevel.vault._
 import scodec.bits.ByteVector
-import org.http4s.client.websocket.WSRequest
 
-object WebSocketHelpers {
+private[internal] object WebSocketHelpers {
 
   private[internal] val supportedWebSocketVersion = 13L
 
@@ -75,7 +67,7 @@ object WebSocketHelpers {
       }
   }
 
-  private def toWebSocketFrame[F[_]: Concurrent](wsFrame: WSFrame): F[WebSocketFrame] =
+  def toWebSocketFrame[F[_]: Concurrent](wsFrame: WSFrame): F[WebSocketFrame] =
     wsFrame match {
       case WSFrame.Close(code, reason) =>
         MonadThrow[F].fromEither(WebSocketFrame.Close(code, reason))
@@ -85,7 +77,7 @@ object WebSocketHelpers {
       case WSFrame.Binary(data, last) => Applicative[F].pure(WebSocketFrame.Binary(data, last))
     }
 
-  private def toWSFrame(wsf: WebSocketFrame): WSFrame =
+  def toWSFrame(wsf: WebSocketFrame): WSFrame =
     wsf match {
       case c: WebSocketFrame.Close => WSFrame.Close(c.closeCode, c.reason)
       case WebSocketFrame.Ping(data) => WSFrame.Ping(data)
@@ -93,46 +85,6 @@ object WebSocketHelpers {
       case WebSocketFrame.Text(data, last) => WSFrame.Text(data, last)
       case WebSocketFrame.Binary(data, last) => WSFrame.Binary(data, last)
     }
-
-  object EmberWSClient {
-    def apply[F[_]](
-        emberClient: Client[F]
-    )(implicit F: Async[F]): WSClient[F] =
-      WSClient[F](respondToPings = false) { wsRequest =>
-        val httpWSRequest = Request[F]()
-          .withUri(wsRequest.uri)
-          .withHeaders(wsRequest.headers)
-          .withMethod(Method.GET)
-
-        for {
-          socketOption <- getSocket(emberClient, httpWSRequest)
-          socket <- socketOption.liftTo[F](new RuntimeException("Not an Ember client")).toResource
-
-          clientReceiveQueue <- Queue.bounded[F, WebSocketFrame](100).toResource
-          clientSendQueue <- Queue.bounded[F, WebSocketFrame](100).toResource
-
-          _ <- socket.reads
-            .through(decodeFrames(true))
-            .evalTap(clientReceiveQueue.offer(_))
-            .compile
-            .drain
-            .background
-
-          _ <- clientSendQueue.take
-            .flatMap(f => frameToBytes(f, true).traverse_(c => socket.write(c)))
-            .foreverM
-            .void
-            .background
-        } yield new WSConnection[F] {
-          def receive: F[Option[WSFrame]] = clientReceiveQueue.take.map(toWSFrame(_).some)
-          def send(wsf: WSFrame): F[Unit] = toWebSocketFrame(wsf).map(clientSendQueue.offer(_)).void
-          def sendMany[G[_], A <: WSFrame](wsfs: G[A])(implicit
-              evidence$1: cats.Foldable[G]
-          ): F[Unit] = wsfs.traverse_(send(_))
-          def subprotocol: Option[String] = ???
-        }
-      }
-  }
 
   /** Validate the opening handshake response from the server
     * https://datatracker.ietf.org/doc/html/rfc6455#page-6
