74bc2debb9c2dfb9718e9f0b13c66ddebd2bd592
Dang Trung Hieu <hieudangtrung@protonmail.com>
2023-10-13 11:35:42 +0800

    Fix pattern matching

--- ember-server/shared/src/test/scala/org/http4s/ember/server/EmberServerWebSocketSuite.scala
+++ ember-server/shared/src/test/scala/org/http4s/ember/server/EmberServerWebSocketSuite.scala
@@ -97,7 +97,7 @@ class EmberServerWebSocketSuite extends Http4sSuite with DispatcherIOFixture {
     )
 
   def toWSFrame(wsf: WebSocketFrame): WSFrame =
-    wsf match {
+    (wsf: @unchecked) match {
       case c: WebSocketFrame.Close => WSFrame.Close(c.closeCode, c.reason)
       case WebSocketFrame.Ping(data) => WSFrame.Ping(data)
       case WebSocketFrame.Pong(data) => WSFrame.Pong(data)
