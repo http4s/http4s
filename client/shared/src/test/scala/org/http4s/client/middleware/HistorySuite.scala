6eab97767fe07726e093cd5515e38e4824691049
Sally Perez <93544183+SallyPerez@users.noreply.github.com>
2023-11-02 16:01:58 -0700

    refactor tests wip - test with Clock

--- client/shared/src/test/scala/org/http4s/client/middleware/HistorySuite.scala
+++ client/shared/src/test/scala/org/http4s/client/middleware/HistorySuite.scala
@@ -1,20 +1,18 @@
 package org.http4s.client.middleware
 
-import cats.effect.{IO, Ref}
-import cats.syntax.all._
+import cats.Applicative
+import cats.effect.{Clock, IO, Ref}
 import org.http4s.client.Client
 import org.http4s.client.middleware.History.HistoryEntry
-import org.http4s.{Http4sSuite, HttpDate, HttpRoutes, Request, Response, Status}
+import org.http4s.{Http4sSuite, HttpDate, HttpRoutes, Request}
 import org.http4s.dsl.io._
 import org.http4s.headers.Date
 import org.http4s.implicits.http4sLiteralsSyntax
 
-import java.time.Instant
+import java.time.{Duration, Instant}
+import scala.concurrent.duration.FiniteDuration
 
 
-// uses munit
-// create a history object
-// make some calls
 // see if history matches?
 // test 1 history blank if no sitesvisited
 // test 2 history shows last NUM sites visited
@@ -24,8 +22,6 @@ import java.time.Instant
 // or coudl use loop and loop 3 times and history should show3 visits
 class HistorySuite extends Http4sSuite {
 
-  // should i return a status? or is it enough that it was an OK?
-  // get rid of this complication
   private val app = HttpRoutes
     .of[IO] {
       case req @ _ -> Root / "site1" =>
@@ -38,12 +34,20 @@ class HistorySuite extends Http4sSuite {
           case "OK" => Ok()
           case "" => InternalServerError()
         }
-      case _ -> Root / status =>
+      case _ =>
         Ok()
     }
     .orNotFound
 
   private val defaultClient = Client.fromHttpApp(app)
+
+  private val date1 = Date(HttpDate.unsafeFromInstant(Instant.now()))
+  private val date2 = Date(HttpDate.unsafeFromInstant(Instant.now().plus(Duration.ofMinutes(1L))))
+  private val date3 = Date(HttpDate.unsafeFromInstant(Instant.now().plus(Duration.ofMinutes(2L))))
+
+  private val req1 = Request[IO](uri = uri"/request").putHeaders(date1)
+  private val req2 = Request[IO](uri = uri"/request").putHeaders(date2)
+  private val req3 = Request[IO](uri = uri"/request").putHeaders(date3)
 
   test("History middeware should return empty history if no sites have been visited") {
     val expected: Vector[HistoryEntry] = Vector.empty
@@ -54,41 +58,65 @@ class HistorySuite extends Http4sSuite {
     }
   }
 
-  test("History middeware should return 1 history item if 1 sites have been visited/req run") {
+  test("History middeware should return 1 history item if 1 site has been visited") {
     val date = Date(HttpDate.unsafeFromInstant(Instant.now()))
     val req = Request[IO](uri = uri"/request").putHeaders(date)
     val expected = Vector(HistoryEntry(req.headers.get[Date].get.date, req.method, req.uri))
 
     Ref.of[IO, Vector[HistoryEntry]](Vector.empty).flatMap { ref =>
-      History.apply(defaultClient, ref, 3).run(req).use(_ => ().pure[IO]).flatMap(_ => ref.get)
+      History.apply(defaultClient, ref, 3).run(req).use(_ => IO.unit) >> ref.get.assertEquals(expected)
     }
-      .assertEquals(expected)
-
   }
 
-  // retry w/o creating so many darn Refs...
-  // this isn't working :(
-//  test("History middeware should return max number of visits if visits exceeds maxSize"){
-//    val historyRef = Ref.of[IO, Vector[HistoryEntry]](Vector.empty).unsafeRunSync()
-//    val clientWithHistory = History.apply(defaultClient, historyRef, 2)
-//
-//    val req1 = Request[IO](uri = uri"/request").putHeaders(Date(HttpDate.current[IO].unsafeRunSync()))
-//    val req2 = Request[IO](uri = uri"/request").putHeaders(Date(HttpDate.current[IO].unsafeRunSync()))
-//    val req3 = Request[IO](uri = uri"/request").putHeaders(Date(HttpDate.current[IO].unsafeRunSync()))
-//
-//    clientWithHistory.run(req1)
-//    clientWithHistory.run(req2)
-//    clientWithHistory.run(req3)
-//
-//    val expectedHistoryEntry1 = HistoryEntry(req1.headers.get[Date].get.date, req1.method, req1.uri)
-//    val expectedHistoryEntry2 = HistoryEntry(req2.headers.get[Date].get.date, req2.method, req2.uri)
-//    val expectedHistoryEntry3 = HistoryEntry(req3.headers.get[Date].get.date, req3.method, req3.uri)
-//
-//    // don't use eq .get
-//    // use assert
-//    // use === where you can
-////    historyRef.get.unsafeRunSync().assert === Vector(expectedHistoryEntry3)
-//    assert(historyRef.get.unsafeRunSync())
-//  }
+    test("History middeware should return visits in order of most recent to oldest"){
+
+      val expected: Vector[HistoryEntry] = Vector(HistoryEntry(req1.headers.get[Date].get.date, req1.method, req1.uri)).prepended(HistoryEntry(req2.headers.get[Date].get.date, req2.method, req2.uri)).prepended(HistoryEntry(req3.headers.get[Date].get.date, req3.method, req3.uri))
+
+      Ref.of[IO, Vector[HistoryEntry]](Vector.empty).flatMap { ref =>
+        History(defaultClient, ref, 3).run(req1).use_ >>
+          History(defaultClient, ref, 3).run(req2).use_ >>
+          History(defaultClient, ref, 3).run(req3).use_ >>
+          ref.get.assertEquals(expected)
+      }
+  }
+
+  // the last history middleware run
+  test("History middeware should return max number of visits if visits exceeds maxSize"){
+
+    val expected: Vector[HistoryEntry] = Vector(HistoryEntry(req2.headers.get[Date].get.date, req2.method, req2.uri)).prepended(HistoryEntry(req3.headers.get[Date].get.date, req3.method, req3.uri))
+
+    Ref.of[IO, Vector[HistoryEntry]](Vector.empty).flatMap { ref =>
+      val historyClient = History(defaultClient, ref, 2)
+
+      historyClient.run(req1).use_ >>
+        historyClient.run(req2).use_ >>
+        historyClient.run(req3).use_ >>
+        ref.get.assertEquals(expected)
+    }
+  }
+
+  // test 5 what happens when sending requests w/o date - what goes into history?
+  // what am I expecting to come back? a date, any date?
+  test("History middeware should return max number of visits if visits exceeds maxSize"){
+
+    val sallysClock = new Clock[IO] {
+      override def applicative: Applicative[IO] = Applicative[IO] // IO.asyncForIO
+
+      override def monotonic: IO[FiniteDuration] = IO.pure(FiniteDuration(0L, scala.concurrent.duration.HOURS ))
+
+      override def realTime: IO[FiniteDuration] = IO.pure(FiniteDuration(0L, scala.concurrent.duration.HOURS ))
+    }
+
+    val expected: Vector[HistoryEntry] = Vector(HistoryEntry(req2.headers.get[Date].get.date, req2.method, req2.uri)).prepended(HistoryEntry(req3.headers.get[Date].get.date, req3.method, req3.uri))
+
+    Ref.of[IO, Vector[HistoryEntry]](Vector.empty).flatMap { ref =>
+      val historyClient = History(defaultClient, ref, 2)
+
+      historyClient.run(req1).use_ >>
+        historyClient.run(req2).use_ >>
+        historyClient.run(req3).use_ >>
+        ref.get.assertEquals(expected)
+    }
+  }
 
 }
