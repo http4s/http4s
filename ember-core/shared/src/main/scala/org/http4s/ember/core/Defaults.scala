fb593d35e11158b70d17ad50c894f85be4ca2ed0
rlavolee <remi.lavolee@gmail.com>
2023-11-23 21:48:13 +0100

    quicklint run

--- ember-core/shared/src/main/scala/org/http4s/ember/core/Defaults.scala
+++ ember-core/shared/src/main/scala/org/http4s/ember/core/Defaults.scala
@@ -1,6 +1,7 @@
 package org.http4s.ember.core
 
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.FiniteDuration
 
 private[ember] object Defaults {
   val idleTimeout: FiniteDuration = 60.seconds
