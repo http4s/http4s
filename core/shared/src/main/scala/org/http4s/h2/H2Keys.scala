d55049226a9712b5c8f7e2078d4938c0e4123d77
Erlend Hamnaberg <erlend@hamnaberg.net>
2024-03-09 16:32:20 +0100

    Move ember H2Keys PriorKnowlegde key to core
    This should rather live in http4s-client so more clients have a standard
    way to detect weather the request should be sent into http/2 prior
    knowledge land.
    
    Had to put this into core, since H2Client lives in ember-core and should
    not depend on client.

--- core/shared/src/main/scala/org/http4s/h2/H2Keys.scala
+++ core/shared/src/main/scala/org/http4s/h2/H2Keys.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 http4s.org
+ * Copyright 2013 http4s.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,27 +14,16 @@
  * limitations under the License.
  */
 
-package org.http4s.ember.core.h2
+package org.http4s.h2
 
 import cats.effect._
-import fs2.Pure
-import org.http4s.Request
 import org.typelevel.vault._
 
 object H2Keys {
-  val PushPromiseInitialStreamIdentifier: Key[Int] = Key.newKey[SyncIO, Int].unsafeRunSync()
-  val StreamIdentifier: Key[Int] = Key.newKey[SyncIO, Int].unsafeRunSync()
-
-  val PushPromises: Key[List[Request[Pure]]] =
-    Key.newKey[SyncIO, List[org.http4s.Request[fs2.Pure]]].unsafeRunSync()
-
   // Client Side Key To Try Http2-Prior-Knowledge
   // which means immediately using http2 without any upgrade mechanism
   // but is invalid if the receiving server does not support the
   // mechanism.
-  @deprecated(message = "Use org.http4s.h2.H2Keys.Http2PriorKnowledge instead", since = "0.23.27")
-  val Http2PriorKnowledge: Key[Unit] = org.http4s.h2.H2Keys.Http2PriorKnowledge
+  val Http2PriorKnowledge: Key[Unit] = Key.newKey[SyncIO, Unit].unsafeRunSync()
 
-  private[ember] val H2cUpgrade =
-    Key.newKey[SyncIO, (H2Frame.Settings.ConnectionSettings, Request[fs2.Pure])].unsafeRunSync()
 }
