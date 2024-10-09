7ab319378a604c9514c09d11139f11f81bba7e97
Sam Pillsworth <spillsworth@jackhenry.com>
2024-10-09 13:53:07 -0400

    nowarn on the unused implicit

--- tests/shared/src/test/scala/org/http4s/LiteralSyntaxMacrosSuite.scala
+++ tests/shared/src/test/scala/org/http4s/LiteralSyntaxMacrosSuite.scala
@@ -18,6 +18,8 @@ package org.http4s
 
 import org.http4s.implicits._
 
+import scala.annotation.nowarn
+
 class LiteralSyntaxMacrosSuite extends Http4sSuite {
   test("'uri' macro works for valid input") {
     assertEquals(uri"a.b.c", Uri.fromString("a.b.c").toOption.get)
@@ -26,9 +28,8 @@ class LiteralSyntaxMacrosSuite extends Http4sSuite {
     val org = "blerf"
     assertEquals(uri"org" / org, Uri.fromString("org/blerf").toOption.get)
   }
-
   test("'uri' macro works with implicit variable named 'org' in scope") {
-    implicit val org = "the-problem"
+    @nowarn implicit val org = "the-problem"
     assertEquals(uri"example", uri"example")
   }
   test("invalid uri won't compile") {
