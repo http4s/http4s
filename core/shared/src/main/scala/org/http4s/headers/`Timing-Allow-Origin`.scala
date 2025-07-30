5c6a3c526d717e56bf2e5312f9c69b93bfaa71b9
Francesco Serra <fn.serra@gmail.com>
2025-07-30 20:57:11 +0100

    make Timing-Allow-Origin an ADT

--- core/shared/src/main/scala/org/http4s/headers/`Timing-Allow-Origin`.scala
+++ core/shared/src/main/scala/org/http4s/headers/`Timing-Allow-Origin`.scala
@@ -18,32 +18,65 @@ package org.http4s.headers
 
 import cats.Semigroup
 import cats.data.NonEmptyList
+import cats.syntax.all.*
 import org.http4s.Header
 import org.http4s.ParseResult
 import org.http4s.internal.parsing.CommonRules
-import org.typelevel.ci._
+import org.typelevel.ci.*
+
+
+sealed trait `Timing-Allow-Origin`
+case object TimingAllowAllOrigins extends `Timing-Allow-Origin`
+final case class TimingAllowSelectedOrigins(head: CIString, tail: CIString*) extends `Timing-Allow-Origin`
+
+
 
 object `Timing-Allow-Origin` {
-  def apply(head: CIString, tail: CIString*): `Timing-Allow-Origin` =
-    apply(NonEmptyList(head, tail.toList))
+  def apply(headers: Option[NonEmptyList[CIString]]): `Timing-Allow-Origin` = headers match {
+    case Some(headers)  => TimingAllowSelectedOrigins(headers.head, headers.tail)
+    case _              => TimingAllowAllOrigins
+  }
+
+
+  private def fromString(s: String) = if (s == "*")
+                                Option.empty
+                              else Option(CIString(s))
+
+  private def fromNel(nel: NonEmptyList[Option[CIString]]) =
+    if (nel.toList.contains(Option.empty)) {
+      if (nel.size == 1)
+        ParseResult.success(TimingAllowAllOrigins)
+      else
+        ParseResult.fail(
+          "Invalid origin value",
+          s"Timing-Allow-Origin must contain either a '*' or a non-empty list of allowed origins"
+        )
+    } else ParseResult.success(
+      apply(nel.sequence)
+    )
 
   private[http4s] def parse(s: String): ParseResult[`Timing-Allow-Origin`] =
-    ParseResult.fromParser(parser, "Invalid Timing-Allow-Origin headers")(s)
+    ParseResult.fromParser(parser, "Invalid Timing-Allow-Origin headers")(s).flatten
 
   private[http4s] val parser =
     CommonRules
-      .headerRep1(CommonRules.token.map(CIString(_)))
-      .map(`Timing-Allow-Origin`(_))
+      .headerRep1(CommonRules.token.map(fromString))
+      .map(fromNel)
 
   implicit val headerInstance: Header[`Timing-Allow-Origin`, Header.Recurring] =
     Header.createRendered(
       ci"Timing-Allow-Origin",
-      _.values,
+      {
+        case TimingAllowAllOrigins => List.empty
+        case TimingAllowSelectedOrigins(origin, origins*) => origin :: origins.toList
+      },
       parse,
     )
 
   implicit val timingAllowOriginSemigroup: Semigroup[`Timing-Allow-Origin`] =
-    (a, b) => `Timing-Allow-Origin`(a.values.concatNel(b.values))
+    (a, b) => (a, b) match {
+      case (TimingAllowAllOrigins, _) | (_, TimingAllowAllOrigins) => TimingAllowAllOrigins
+      case (TimingAllowSelectedOrigins(origin1, origins1*), TimingAllowSelectedOrigins(origin2, origins2*)) =>
+        TimingAllowSelectedOrigins(origin1, (origin2 :: origins1.toList) ::: origins2.toList)
+    }
 }
-
-final case class `Timing-Allow-Origin`(values: NonEmptyList[CIString])
