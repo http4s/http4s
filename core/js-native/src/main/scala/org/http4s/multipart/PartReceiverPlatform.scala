7a9f5c1971927dde114063971ddcc599ec89e4be
danicheg <esik.danny@gmail.com>
2026-04-24 16:56:18 +0300

    Use fs2-io file API for js-native

--- core/js-native/src/main/scala/org/http4s/multipart/PartReceiverPlatform.scala
+++ core/js-native/src/main/scala/org/http4s/multipart/PartReceiverPlatform.scala
@@ -17,14 +17,22 @@
 package org.http4s.multipart
 
 import cats.effect.kernel.Async
+import cats.syntax.functor._
+import fs2.Chunk
 import fs2.io.file.Files
+import fs2.io.file.Flags
 import fs2.io.file.Path
 import org.http4s.Entity
 
 private[multipart] trait PartReceiverPlatform {
   def writeToFile[F[_]](path: Path, entity: Entity[F])(implicit F: Files[F], A: Async[F]): F[Unit] =
     entity match {
-      case Entity.Empty => A.unit
+      case Entity.Empty =>
+        A.unit
+      case Entity.Strict(bv) =>
+        Files[F]
+          .open(path, Flags.Write)
+          .use(_.write(Chunk.byteVector(bv), 0).void)
       case Entity.Strict(_) | Entity.Streamed(_, _) =>
         entity.body.through(F.writeAll(path)).compile.drain
     }
