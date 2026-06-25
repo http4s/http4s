6e825719b8d2f1538f2c49089c2dc81321a833b4
Ross A. Baker <ross@rossabaker.com>
2026-06-25 13:21:27 -0400

    FollowRedirect strip sensitive headers on scheme downgrade

--- client/shared/src/main/scala/org/http4s/client/middleware/FollowRedirect.scala
+++ client/shared/src/main/scala/org/http4s/client/middleware/FollowRedirect.scala
@@ -71,8 +71,14 @@ object FollowRedirect {
         fragment = uri.fragment.orElse(req.uri.fragment),
       )
 
+      def sameSecurityContext(uri: Uri): Boolean = {
+        val schemeDowngrade =
+          uri.scheme.contains(Uri.Scheme.https) && nextUri.scheme.contains(Uri.Scheme.http)
+        uri.authority == nextUri.authority && !schemeDowngrade
+      }
+
       def stripSensitiveHeaders(req: Request[F]): Request[F] =
-        if (req.uri.authority != nextUri.authority)
+        if (!sameSecurityContext(req.uri))
           req.transformHeaders(hs =>
             Headers(hs.headers.filterNot(h => sensitiveHeaderFilter(h.name)))
           )
@@ -80,7 +86,7 @@ object FollowRedirect {
           req
 
       def propagateCookies(req: Request[F]): Request[F] =
-        if (req.uri.authority == nextUri.authority)
+        if (sameSecurityContext(req.uri))
           cookies.foldLeft(req) { case (nextReq, cookie) =>
             nextReq.addCookie(cookie.name, cookie.content)
           }
